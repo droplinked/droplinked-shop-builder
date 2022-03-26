7be2d133f1c5a5dcbe6b9bd6e82a4b5665b655f3
Behdad Mansouri <bedi.mns@gmail.com>
2022-03-27 03:16:32 +0430

    cart validationi

--- src/components/Payments/Payments.jsx
+++ src/components/Payments/Payments.jsx
@@ -1,34 +1,58 @@
-import {useState} from 'react'
+import {useState , useRef} from 'react'
 import {BrowserRouter as Router, Switch,Route,Link, useParams} from "react-router-dom";
 import info from './icons/info.png'
 import add from './icons/add.png'
 import credit from './icons/credit-card.png'
+import { useForm } from 'react-hook-form'
 
 import './payments.scss'
 
 function Payments () {
-	console.log("123456789101112".replace(/(\d{4}(?!\s))/g, "$1 "));
-
 	
+	const[cards , setCard]=useState([]);
+
+		const setCardData = (data) => {
+			console.log(cards);
+			let lastArray = cards;
+			lastArray.push(data);
+			setCard(lastArray);
+		}
 		return (
 			<div className='payments d-flex flex-column mb-4 px-4'>
 				<h2 className='text-center'>payments</h2>
 				<p className='text text-center'>
 					keep cards on file to easily make one-click purchases
 				</p>
-				<div className='m-3'>
-					<div className='p5 text-center text-muted'>
-						<img src={info} alt='info' width='24px' height='24px' />
-						<p>there is no card</p>
+
+				{cards.length>0
+				?
+					
+						cards.map((card) =>{
+							return <div className='m-3'>
+								<p>cartnumber:{card.CardNumber}</p>
+								<p>cardExpiratioin:{card.cardExpiratioin}</p>
+								<p>cardCvc:{card.cardCvc}</p>
+							</div>
+						})
+
+				:
+					<div className='m-3'>
+						<div className='p5 text-center text-muted'>
+							<img src={info} alt='info' width='24px' height='24px' />
+							<p>there is no card</p>
+						</div>
 					</div>
-				</div>
+				}
+				
+
+
 				<div className='text-center p-3'>
 					<button className='btn'>
 						<img src={add} alt='add' width='18px' height='18px' />
 						<span className='ms-2'>add new card</span>
 					</button>
 				</div>
-				{<NewPayment />}
+				{<NewPayment add={setCardData}/>}
 				<Link to="/confirm">
 					<button>
 						proceed to confirm
@@ -39,11 +63,42 @@ function Payments () {
 	
 }
 
-function NewPayment () {
+function NewPayment (props) {
+	const { register, handleSubmit , formState : {errors}  } = useForm()
 	const[cardNumber , setCardNumber]=useState("")
+	const[cardExpire , setCardExpire]=useState("")
+	const[cardCvc , setCardCvc]=useState("")
+
 	function change(event){
 		setCardNumber(event.target.value.toString());
+		if(cardNumber.length>17)document.getElementById("expire").focus();
 	}
+
+	function changeExpire(event){
+		if(event.target.value.length==2){
+			setCardExpire(event.target.value.toString()+"/")
+		}else{
+			setCardExpire(event.target.value.toString())
+		}
+		if(event.target.value.length==5)document.getElementById("cvc").focus();
+	}
+
+	const changeCvc= (event) => {
+		setCardCvc(event.target.value);
+	}
+
+
+
+	const addNewCard =()=>{
+		let cardData = {
+			CardNumber : cardNumber ,
+			cardExpiratioin : cardExpire , 
+			cardCvc : cardCvc
+		};
+
+		return cardData;
+	}
+
 		return (
 			<div className='card payments-new p-3 rounded-2'>
 				<p className='text-center'>new card</p>
@@ -55,12 +110,27 @@ function NewPayment () {
 						value={cardNumber.replace(/(\d{4}(?!\s))/g, "$1 ")}
 						maxLength="19"
 						/>
-						<input type='text' placeholder='mm / yy' className='ms-3' />
-						<input type='text' placeholder='cvc' className='ms-3' />
+
+						<input type='text' placeholder='mm / yy' className='ms-3' id="expire"
+							onChange={changeExpire}
+							value={cardExpire}
+							maxLength="5"
+						/>
+
+						<input type='text' placeholder='cvc' className='ms-3' 
+							maxLength="3"
+							id="cvc"
+							onChange={changeCvc}
+							value={cardCvc}
+						/>
 					</div>
 				</div>
 				<div class='d-flex align-items-end justify-content-end'>
-					<button class='btn btn-dark btn-sm rounded-pill px-4' type='submit'>
+					<button class='btn btn-dark btn-sm rounded-pill px-4' type='submit'
+						onClick={()=>{
+							props.add(addNewCard());
+						}}
+					>
 						save
 					</button>
 					<button class='btn btn-sm ml-3' type='button'>
--- src/components/creator-component/ShopifyCard.jsx
+++ src/components/creator-component/ShopifyCard.jsx
@@ -14,34 +14,22 @@ export default function ShopifyCard(props) {
   }, []);
 
   return (
+    
     <div className="content-wrap d-flex flex-column">
+    
       <div className="img-wrap mr-3">
+        <Link to={`buy/${props.index}`}>
         <img src={props.product.images[0].src} alt="" />
+        </Link>
       </div>
       <div className="info">
         <p className="mt-1 mb-3 title">{props.product.title}</p>
         <p className="mb-5 price" >
           {props.product.variants[0].formatted_price}
         </p>
-        <div className="row">
-          <div className="col-6">
-            <a className="view-btn">
-              <span>View</span>
-            </a>
-          </div>
-          <div className="col-6">
-            {token ? (
-              <Link to={`buy/${props.index}`}>
-                <button className="buy-btn btn btn-primary">Buy</button>
-              </Link>
-            ) : (
-              <Link to={`buy/${props.index}`}>
-                <button className="buy-btn btn btn-primary">Buy</button>
-              </Link>
-            )}
-          </div>
-        </div>
+        
       </div>
     </div>
+    
   );
 }
