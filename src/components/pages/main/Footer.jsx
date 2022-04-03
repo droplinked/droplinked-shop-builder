078f20e3b8ff704bf6dcec6a04730945759c42e6
Behdad Mansouri <bedi.mns@gmail.com>
2022-04-03 21:28:12 +0430

    .

--- src/components/pages/main/Footer.jsx
+++ src/components/pages/main/Footer.jsx
@@ -9,9 +9,9 @@ export default function Footer(){
             style={{ width: "100%", height: "80px", borderTop: "2px solid black" }}
                  >       
                 <div className="d-flex row align-items-start justify-content-end"
-                 style={{width:"88%" , height:"100%" ,  margin:"auto auto" , border: "1px solid red" }}>
-                     <div className="col-12 col-md-4 footer-text d-flex justify-content-between" style={{border: "1px solid blue" }}>
-                     <p>droplinked by  <a href="https://flatlay.io/" style={{color:"inherit" , textDecoration:"none" , border: "1px solid green"}}><img src={whitelogo} className="footer-icon" /> </a></p>
+                 style={{width:"88%" , height:"100%" ,  margin:"auto auto"  }}>
+                     <div className="col-12 col-md-4 footer-text d-flex justify-content-between" >
+                     <p>droplinked by  <a href="https://flatlay.io/" style={{color:"inherit" , textDecoration:"none" }}><img src={whitelogo} className="footer-icon" /> </a></p>
                      </div>
     
                  </div>
