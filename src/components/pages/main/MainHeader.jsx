ead039ae27cbeafa1c78c2e986e36cb9644e012f
Behdad Mansouri <bedi.mns@gmail.com>
2022-04-03 21:03:59 +0430

    fix-header

--- src/components/pages/main/MainHeader.jsx
+++ src/components/pages/main/MainHeader.jsx
@@ -1,13 +1,6 @@
-import "./main.scss";
-import { useState, useEffect , useRef} from "react";
-import modalIcon from "../../../assest/modal/checked.png";
+import "./mainHeader.scss";
 import { UseWalletInfo } from "../../context/context";
-import mainImg from "../../../assest/creator-box.png";
-import { GiWallet } from "react-icons/gi";
-import whitelogo from "../../../assest/image/footer/FlatlayLogo.svg"
 import walletIcon from "../../../assest/header/Unknown.svg"
-import {Link } from "react-router-dom";
-
 
 
 
@@ -18,38 +11,29 @@ export default function MainHeader(){
 
     return(
     <div className="header container-fluid">
-          {/* <img src={logo} alt="" className="headerImg  col-5 " /> */}
-          <div className="headerText  col-3 col-lg-4 d-flex justify-content-start align-self-center">
-            <Link to="/" style={{textDecoration:"none"}}>
-            <h1 className="headerText col-10" style={{ paddingRight: "30px" }}>
-              droplinked
-            </h1>
-            </Link>
-          </div>
 
-          <div className="d-flex justify-content-between col-7 col-lg-4 ">
-            <div className="col-8 col-lg-6 d-flex justify-content-end align-self-center res-btn invisible" >
-                <button className="sign-up d-flex align-self-center">
-                  <p>Sign up free</p>
-                </button>
-            </div>
-            
-
-            <div className="col-3 col-lg-4 d-flex justify-content-center" >
-                
-                {userData?
+         <div className="header-wrapper d-flex justify-content-between">
+              <div className="brand-logo col-6 col-md-4">
+                 <h1>droplinked</h1>
+             </div>
+           <div className="wallet col-6 col-md-3">
+                  {userData?
                       <img src={walletIcon} alt=""  className="wallet-icon wallet-icon-fill"  
                       onClick={onSignOut}
                       />
-                :
+                  :
                        <img src={walletIcon} alt=""  className="wallet-icon wallet-icon-notfill" 
                        onClick={authenticate}  
                       />
 
-                }
+                  }
+            </div>
+         </div>
+      </div>
+        )
+}
 
-
-                {/* {userData
+{/* {userData
                 ?
                     (<GiWallet className="wallet-icon wallet-icon-fill"
                     onClick={onSignOut}
@@ -60,8 +44,3 @@ export default function MainHeader(){
                     />)
                 } */}
               
-            </div>
-          </div>
-        </div>
-        )
-}
