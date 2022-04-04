0e9f0385a96e7564a2cf7f304453d66e64d4b28f
Behdad Mansouri <bedi.mns@gmail.com>
2022-04-05 00:48:30 +0430

    crashpunks collection page

--- src/components/pages/nft/Collection123.jsx
+++ src/components/pages/nft/Collection123.jsx
@@ -1,51 +1,88 @@
 import "./collection123.scss"
 import Carousel from 'react-bootstrap/Carousel'
-import image1 from "../../../assest/image/nft/img1.png"
-import image2 from "../../../assest/image/nft/img2.png"
-import image3 from "../../../assest/image/nft/img3.png"
+import image1 from "../../../assest/image/nft/Rectangle 7.jpg"
+import image2 from "../../../assest/image/nft/Rectangle 8.jpg"
+import image3 from "../../../assest/image/nft/Rectangle 9.jpg"
+import image4 from "../../../assest/image/nft/Rectangle 10.jpg"
+import image5 from "../../../assest/image/nft/Rectangle 11.jpg"
+import productImg1 from "../../../assest/image/crashpunk/collection/collection1.jpg"
+import productImg2 from "../../../assest/image/crashpunk/collection/collection2.jpg"
+import productImg3 from "../../../assest/image/crashpunk/collection/collection3.jpg"
 
 
 export default function Collection123(){
 
     return(<>
-            <div className="container-fluid collection-container d-flex flex-column">
+          <div className="container-fluid collection-container d-flex flex-column">
                 {/* nft gallery */}
-                <div className="d-flex justify-content-center p-4">
-                    <div className="nft-gallery">
+                 <div className="d-flex justify-content-center p-4">
+                       <div className="boxes">
+                            <a href="https://stxnft.com/collections/crashpunks">
+                            <Carousel variant="dark">
+                                <Carousel.Item interval={5000}>
+                                  <img
+                                    className="carousel-img"
+                                    src={image1}
+                                    alt="First slide"
+                                  />
+                                </Carousel.Item>
 
-                        <Carousel variant="dark">
-                            <Carousel.Item interval={500}>
-                              <img
-                                className="carousel-img"
-                                src={image1}
-                                alt="First slide"
-                              />
-                            </Carousel.Item>
-
-                            <Carousel.Item interval={500}>
-                              <img
-                                className="carousel-img"
-                                src={image2}
-                                alt="Second slide"
-                              />
-                            </Carousel.Item>
-                            <Carousel.Item interval={500}>
-                              <img
-                                className="carousel-img"
-                                src={image3}
-                                alt="Third slide"
-                              />
-                            </Carousel.Item>
-                        </Carousel>
+                                <Carousel.Item interval={5000}>
+                                  <img
+                                    className="carousel-img"
+                                    src={image2}
+                                    alt="Second slide"
+                                  />
+                                </Carousel.Item>
+                                <Carousel.Item interval={5000}>
+                                  <img
+                                    className="carousel-img"
+                                    src={image3}
+                                    alt="Third slide"
+                                  />
+                                </Carousel.Item>
+                                <Carousel.Item interval={5000}>
+                                  <img
+                                    className="carousel-img"
+                                    src={image4}
+                                    alt="Third slide"
+                                  />
+                                </Carousel.Item>
+                                <Carousel.Item interval={5000}>
+                                  <img
+                                    className="carousel-img"
+                                    src={image5}
+                                    alt="Third slide"
+                                  />
+                                </Carousel.Item>
+                            </Carousel>
+                            </a>
 
                     </div>
-                </div>
+                 </div>
                 {/* nft gallery */}
 
                 {/* merch gallery */}
                 <div className="d-flex justify-content-center p-4">
-                    <div className="holder-merch">
+                    <div className="boxes">
+                        <div className="image-wrapper d-flex">
+                          <div className="image1 col-7">
+                            <img src={productImg1} className="" alt="" />
+                          </div>
+                          {/* img-thumbnail */}
 
+                          <div className="images col-5">
+                            <img src={productImg2} className="" alt="" />
+                            <img src={productImg3} className="" alt=""  />
+                           
+                          </div>
+
+                        </div>
+                        <div className="detail-wrapper d-flex justify-content-start align-content-center">
+                          
+                            <p>holder merch</p>
+
+                        </div>
                     </div>
                 </div>
 
@@ -54,13 +91,29 @@ export default function Collection123(){
 
                 {/* public gallery */}
                 <div className="d-flex justify-content-center p-4">
-                    <div className="public-merch">
+                    <div className="boxes">
+                        <div className="image-wrapper d-flex">
+                          <div className="image1 col-7">
+                            <img src={productImg1} className="" alt="" />
+                          </div>
+                          {/* img-thumbnail */}
 
+                          <div className="images col-5">
+                            <img src={productImg2} className="" alt="" />
+                            <img src={productImg3} className="" alt="" />
+                          </div>
+
+                        </div>
+                        <div className="detail-wrapper d-flex justify-content-start align-content-center">
+                          
+                          <p>public merch</p>
+
+                        </div>
                     </div>
                 </div>
 
                 {/* public gallery */}
 
-            </div>
+          </div>
     </>)
 }
--- src/layout/test/Test1.jsx
+++ src/layout/test/Test1.jsx
@@ -2,6 +2,7 @@ import MainWrapper from "../../components/Structure/page wrapper/MainWrapper"
 import Footer from "../../components/features/footer/Footer"
 import MainHeader from "../../components/features/header/MainHeader"
 import Collection123 from "../../components/pages/nft/Collection123"
+import ProfileTopSection from "../../components/features/top section/ProfileTopSection"
 import React from 'react';
 
 export default function Test1(){
@@ -10,6 +11,7 @@ export default function Test1(){
     return(
         <div style={{backgroundColor:"#222"}}>
         <MainHeader/>
+        <ProfileTopSection />
         <MainWrapper>
         <Collection123 />
         </MainWrapper>
