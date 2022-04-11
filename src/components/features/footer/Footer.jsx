457709f15bdec771d899e445d7f05ce6d0ad653b
alireza fazeli <alirezafazeli2684@gmail.com>
2022-04-11 18:39:45 +0430

    footer is recreated by chakra

--- src/components/features/footer/Footer.jsx
+++ src/components/features/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import whitelogo from '../../../assest/image/footer/FlatlayLogo.svg';
 import './footer.scss';
+import { Text, Flex, Box, Image, Icon } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
 	return (
 		<>
-			<div
+			{/* <div
 				className="d-flex justify-content-between"
 				style={{ width: '100%', height: '80px', borderTop: '1px solid white' }}
 			>
@@ -14,13 +16,7 @@ export default function Footer() {
 				>
 					<div className="col-12 col-md-4 footer-text d-flex justify-content-between">
 						<p>
-							<span
-								style={{
-									paddingLeft: '5px',
-								}}
-							>
-								droplinked by
-							</span>
+							<Text paddingLeft={'1PX'}>droplinked by</Text>
 							<a
 								href="https://flatlay.io/"
 								style={{ color: 'inherit', textDecoration: 'none' }}
@@ -30,7 +26,24 @@ export default function Footer() {
 						</p>
 					</div>
 				</div>
-			</div>
+			</div> */}
+
+			<Flex
+				justifyContent={'flex-end'}
+				paddingX="10"
+				borderTop={'1px'}
+				borderColor="white"
+				paddingY={'2'}
+			>
+				<Link to={'/'}>
+					<Box display={'flex'} flexDirection="column">
+						<Text fontWeight={'bold'} fontSize="xl" color={'white'}>
+							droplinked by
+						</Text>
+						<Image filter={'brightness(0) invert(1)'} src={whitelogo} />
+					</Box>
+				</Link>
+			</Flex>
 		</>
 	);
 }
