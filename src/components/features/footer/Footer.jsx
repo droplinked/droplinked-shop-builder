d240adcf7212368303d745c02d6324dd2bad09a9
alireza fazeli <alirezafazeli2684@gmail.com>
2022-04-11 16:50:50 +0430

    logo problem is fixed

--- src/components/features/footer/Footer.jsx
+++ src/components/features/footer/Footer.jsx
@@ -1,21 +1,36 @@
-import whitelogo from "../../../assest/image/footer/FlatlayLogo.svg"
-import "./footer.scss"
+import whitelogo from '../../../assest/image/footer/FlatlayLogo.svg';
+import './footer.scss';
 
-
-export default function Footer(){
-
-    return(<>
-    <div className="d-flex justify-content-between"
-            style={{ width: "100%", height: "80px", borderTop: "1px solid white" }}
-                 >       
-                <div className="d-flex row align-items-start justify-content-end"
-                 style={{width:"88%" , height:"100%" ,  margin:"auto auto"  }}>
-                     <div className="col-12 col-md-4 footer-text d-flex justify-content-between" >
-                     <p>droplinked by  <a href="https://flatlay.io/" style={{color:"inherit" , textDecoration:"none" }}><img src={whitelogo} className="footer-icon" /> </a></p>
-                     </div>
-    
-                 </div>
-            
-        </div>
-    </>)
+export default function Footer() {
+	return (
+		<>
+			<div
+				className="d-flex justify-content-between"
+				style={{ width: '100%', height: '80px', borderTop: '1px solid white' }}
+			>
+				<div
+					className="d-flex row align-items-start justify-content-end"
+					style={{ width: '88%', height: '100%', margin: 'auto auto' }}
+				>
+					<div className="col-12 col-md-4 footer-text d-flex justify-content-between">
+						<p>
+							<span
+								style={{
+									paddingLeft: '5px',
+								}}
+							>
+								droplinked by
+							</span>
+							<a
+								href="https://flatlay.io/"
+								style={{ color: 'inherit', textDecoration: 'none' }}
+							>
+								<img src={whitelogo} className="footer-icon" />
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
