a814e9339dfc06a8e30b38e65aa0b7db7e2891c6
Behdadmns <bedi.mns@gmail.com>
2022-04-12 19:06:08 +0430

    set footer in a line

--- src/components/features/footer/Footer.jsx
+++ src/components/features/footer/Footer.jsx
@@ -6,27 +6,19 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
 	return (
 		<>
-			{/* <div
-				className="d-flex justify-content-between"
-				style={{ width: '100%', height: '80px', borderTop: '1px solid white' }}
-			>
-				<div
-					className="d-flex row align-items-start justify-content-end"
-					style={{ width: '88%', height: '100%', margin: 'auto auto' }}
-				>
-					<div className="col-12 col-md-4 footer-text d-flex justify-content-between">
-						<p>
-							<Text paddingLeft={'1PX'}>droplinked by</Text>
-							<a
-								href="https://flatlay.io/"
-								style={{ color: 'inherit', textDecoration: 'none' }}
-							>
-								<img src={whitelogo} className="footer-icon" />
-							</a>
-						</p>
-					</div>
-				</div>
-			</div> */}
+		{/* <div className="d-flex justify-content-between"
+            style={{ width: "100%", height: "80px", borderTop: "1px solid white" }}
+                 >       
+                <div className="d-flex row align-items-start justify-content-end"
+                 style={{width:"88%" , height:"100%" ,  margin:"auto auto"  }}>
+                     <div className="col-12 col-md-4 footer-text d-flex justify-content-between" >
+                     <p>droplinked by  <a href="https://flatlay.io/" style={{color:"inherit" , textDecoration:"none" }}><img src={whitelogo} className="footer-icon" /> </a></p>
+                     </div>
+    
+                 </div>
+            
+        </div> */}
+
 
 			<Flex
 				justifyContent={'flex-end'}
@@ -36,11 +28,9 @@ export default function Footer() {
 				paddingY={'2'}
 			>
 				<Link to={'/'}>
-					<Box display={'flex'} flexDirection="column">
-						<Text fontWeight={'bold'} fontSize="xl" color={'white'}>
-							droplinked by
-						</Text>
-						<Image filter={'brightness(0) invert(1)'} src={whitelogo} />
+					<Box display={'flex'} flexDirection="row">
+						<Text fontWeight={'bold'} fontSize="l" color={'white'} mr={"3"}>droplinked by  </Text>  
+						<Image filter={'brightness(0) invert(1)'} src={whitelogo}  maxW="45vw"/>
 					</Box>
 				</Link>
 			</Flex>
