ab33024a3ce6f468db65b5a7b8342c8ad1794f58
Behdadmns <bedi.mns@gmail.com>
2022-04-17 20:09:07 +0430

    responsive change home page

--- src/components/features/header/MainHeader.jsx
+++ src/components/features/header/MainHeader.jsx
@@ -9,21 +9,35 @@ export default function MainHeader() {
   return (
     <div className="header-wrapp d-flex justify-content-center">
       <div className="header-body d-flex justify-content-between">
-
         <Link to="/">
           <div className="header-brand">
             <p>droplinked</p>
           </div>
         </Link>
-        
+
         <div className="header-nav d-flex ">
           <div className="login-wrapper col-6 d-flex align-items-center">
             <p>Login</p>
           </div>
-          <div className="wallet-wrapper col-6 d-flex justify-content-center align-items-center">
-            <img src={headerWalletIcon} alt="" />
-            <p>Wallet</p>
-          </div>
+          {userData ? (
+            <div
+              className="wallet-wrapper col-6 d-flex justify-content-center align-items-center"
+              onClick={onSignOut}
+            >
+              <img src={headerWalletIcon} className="sing-wallet" alt="" />
+
+              <p>Wallet</p>
+            </div>
+          ) : (
+            <div
+              className="wallet-wrapper col-6 d-flex justify-content-center align-items-center"
+              onClick={authenticate}
+            >
+              <img src={headerWalletIcon} alt="" />
+
+              <p>Wallet</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
