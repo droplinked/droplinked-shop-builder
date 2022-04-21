30fafa0f9cad424d52ee48e79ae552c15e0a4f7e
Behdadmns <bedi.mns@gmail.com>
2022-04-21 15:10:15 +0430

    new top section

--- src/components/features/top section/ProfileTopSection.jsx
+++ src/components/features/top section/ProfileTopSection.jsx
@@ -1,100 +1,80 @@
 import profilePic from '../../../assest/profile/crashpunks.gif'
-import background from '../../../assest/background.jpg'
-import userinfo from '../../creator-component/userinfo'
-import { UseWalletInfo } from '../../../sevices/context/context'
-import { getUserAddress } from '../../../wallet-auth/api'
-import therLogo from '../../../assest/ether.png'
-import crashpunkLogo from "../../../assest/image/crashpunk/crashpunkLogo.svg"
-/** socials */
+import "./ProfileTopSection.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons'
 import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProfileTopic() {
-	let isPublicProfile = false
-	let isCurrentUser = true
-	let isLoggedIn = false
-
-	let userinfo1 = userinfo
-	let topMainBackground = { backgroundImage: `url(${background})` }
-
-	const { onSignOut, checkTokens, userData, authenticate } = UseWalletInfo()
-	const userAddress = userData ? getUserAddress(userData) : null
-
 
 	return (
 		<>
-			<section className='profile-top-section'>
-				
-					<div className='top-main' >
-						{/* <div className='account-settings button' style={{ left: '24px', right: 'auto' }}>
-							Display public profile</div> */}
-							
-							<img src={crashpunkLogo} className="top-main-logo"  style={{ left: '24px', right: 'auto' }} />
-							
-						{/* {!isPublicProfile && (
-							<>
-								{isCurrentUser && isLoggedIn && <div className='account-settings button'>Account settings</div>}
-								
-								{!isCurrentUser && isLoggedIn && (
-									<div className={`account-settings button ${userinfo1.isFollowed ? 'followed' : ''}`}>
-										{!userinfo1.isFollowed && <span>+ Follow</span>}
-										{userinfo1.isFollowed && (
-											<>
-												<span className='following'>Following</span>
-												<span className='unFollow'> Unfollow </span>
-											</>
-										)}
-									</div>
-								)}
-							</>
-						)} */}
+
+
+			<div className="wrapper">
+				<div className="d-flex justify-content-center align-content-center">
+					<a href="https://stxnft.com/collections/crashpunks">
 						<div className='img-wrap'>
-							<a href="https://stxnft.com/collections/crashpunks">
-								<img src={profilePic} alt='' />
-							</a>
+							<img src={profilePic} alt='' />
 						</div>
-					</div>
-
-					<div className='user-data text-center'>
-						<h1 className='font-gt'>CrashPunks</h1>
-						<div className='d-flex-inline flex-row align-items-center'>
-							<a href='https://discord.gg/crashpunks' target='_blank' rel='noreferrer' className='social '>
-								<FontAwesomeIcon icon={faDiscord} />
-							</a>
-							<a href='http://crashpunks.com/' target='_blank' rel='noreferrer' className='social '>
-								<FontAwesomeIcon icon={faEarthAmericas} />
-							</a>
-							<a href='https://twitter.com/crashpunks' target='_blank' rel='noreferrer' className='social '>
-								<FontAwesomeIcon icon={faTwitter} />
-							</a>
-							<a href='https://www.instagram.com/crashpunks/' target='_blank' rel='noreferrer' className='social social__ig '>
-								<FontAwesomeIcon icon={faInstagram} style={{color:"00FFFB"}} />
-							</a>
-						</div>
-					</div>
-
-					{userinfo.myInterests.length > 0 && (
-						<div className='featured'>
-							<h1 className='font-gt heading text-center'>Communities</h1>
-							<div className='carousel-wrap'>{/* p-carousel */}</div>
-						</div>
-					)}
-				{/* <div style={{width:"100%" , height:"3px" , backgroundImage:"linear-gradient(84.41deg, #F00190 0%, #00FFFB 100%)" , marginTop:"10px"}}></div> */}
-			</section>
-
-			<section className='profile-top-section'>
-				<div className='post-collection-toggle'>
-					{userData ? (
-						<div className='profile-address'>
-							<img src={therLogo} alt='' />
-							<p>{userAddress ? userAddress.mainnet : ''}</p>
-						</div>
-					) : (
-						<div className=''></div>
-					)}
+					</a>
 				</div>
-			</section>
+				<div className="d-flex justify-content-center align-content-center">
+					<div className="name-text">CrashPunks</div>
+				</div>
+
+				<div className='user-data text-center'>
+					<div className='d-flex-inline flex-row align-items-center'>
+						<a href='https://discord.gg/crashpunks' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faDiscord} />
+						</a>
+						<a href='http://crashpunks.com/' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faEarthAmericas} />
+						</a>
+						<a href='https://twitter.com/crashpunks' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faTwitter} />
+						</a>
+						<a href='https://www.instagram.com/crashpunks/' target='_blank' rel='noreferrer' className='social social__ig '>
+							<FontAwesomeIcon icon={faInstagram} style={{ color: "fff" }} />
+						</a>
+					</div>
+				</div>
+
+			</div>
+			{/* <section className='profile-top-section'>
+
+				<div className='top-main'>
+					<div className='img-wrap'>
+						<a href="https://stxnft.com/collections/crashpunks">
+							<img src={profilePic} alt='' />
+						</a>
+					</div>
+				</div>
+
+				<div className="text-name">CrashPunks</div>
+
+				<div className='user-data text-center'>
+					<h1 className='font-gt'>CrashPunks</h1>
+					<div className='d-flex-inline flex-row align-items-center'>
+						<a href='https://discord.gg/crashpunks' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faDiscord} />
+						</a>
+						<a href='http://crashpunks.com/' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faEarthAmericas} />
+						</a>
+						<a href='https://twitter.com/crashpunks' target='_blank' rel='noreferrer' className='social '>
+							<FontAwesomeIcon icon={faTwitter} />
+						</a>
+						<a href='https://www.instagram.com/crashpunks/' target='_blank' rel='noreferrer' className='social social__ig '>
+							<FontAwesomeIcon icon={faInstagram} style={{ color: "fff" }} />
+						</a>
+					</div>
+				</div>
+
+
+
+			</section> */}
+
+
 		</>
 	)
 }
