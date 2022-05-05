084de864afaceee0dc63e96735b78ade3727f905
Behdadmns <bedi.mns@gmail.com>
2022-05-05 18:21:15 +0430

    collection modal

--- src/layout/Brand/management/CollectionManagement.jsx
+++ src/layout/Brand/management/CollectionManagement.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react"
 import axios from 'axios';
 import Product from "../../../components/features/product/Product";
 import { useNavigate } from 'react-router-dom'
+import AddCollectoin from "./add collection modal/AddCollection"
 
 export default function CollectionManagement() {
     const navigate = useNavigate()
-
+    const [modal, setModal] = useState(true)
+    const [show, setShow] = useState(false)
     const [product, setProduct] = useState(null)
     useEffect(() => {
         axios.post('https://r4qwnd5837.execute-api.us-west-2.amazonaws.com/v1/search',
@@ -21,36 +23,40 @@ export default function CollectionManagement() {
             });
     }, [])
 
-    const [show, setShow] = useState(false)
-
+    const closeModal = () =>{
+        setModal(false)
+    }
     return (
-        <div className="collection-management-page">
-            <div className="header-text">Collection management</div>
-            {(true) ?
-                <div className="collection-wraper">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="collection-name">Collection name</div>
-                        <div className="rule-name">collection's rule name</div>
+        <>
+            <div className="collection-management-page">
+                <div className="header-text">Collection management</div>
+                {(true) ?
+                    <div className="collection-wraper">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="collection-name">Collection name</div>
+                            <div className="rule-name">collection's rule name</div>
+                        </div>
+                        <div className="row mt-2 mb-2">
+                            {(product != null) &&
+                                <>
+                                    {product.map((item) => {
+                                        return <Product title={item.title} price={item.variants[0].formatted_price} imageUrl={item.images[0].src} id={item.product_id} />
+                                    })}
+                                </>
+                            }
+                        </div>
                     </div>
-                    <div className="row mt-2 mb-2">
-                        {(product != null) &&
-                            <>
-                                {product.map((item) => {
-                                    return <Product title={item.title} price={item.variants[0].formatted_price} imageUrl={item.images[0].src} id={item.product_id} />
-                                })}
-                            </>
-                        }
+                    :
+                    <div className="collection-wraper">
+                        <div className="text"> no collection</div>
                     </div>
-                </div>
-                :
-                <div className="collection-wraper">
-                    <div className="text"> no collection</div>
-                </div>
-            }
+                }
 
-            <button className="add-btn"
-            onClick={()=>{navigate('/addNewCollection')}}
-            >+ add collection</button>
-        </div>
+                <button className="add-btn"
+                onClick={()=>{setModal(true)}}
+                >+ add collection</button>
+            </div>
+            {(modal) && <AddCollectoin close={closeModal}/>}
+        </>
     )
 }
