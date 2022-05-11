8f368d40f8e4b1f050256953d3527fbf7ce4cf69
Behdadmns <bedi.mns@gmail.com>
2022-05-11 13:02:28 +0430

    edit modal for mobile

--- src/layout/homepage test/modal/EmailModal.jsx
+++ src/layout/homepage test/modal/EmailModal.jsx
@@ -1,60 +1,60 @@
 import "./EmailModal.scss"
 import closePng from "../../../assest/feature/home page images/Close.png"
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 
-function EmailModal({ close  , name , clear}) {
+function EmailModal({ close, name, clear }) {
 
     const [email, setEmail] = useState("");
 
 
     function submitform() {
         if (email != "" && name != "") {
-          fetch(
-            "https://uui8anv8g0.execute-api.eu-central-1.amazonaws.com/latest/register",
-            {
-              method: "POST",
-              body: JSON.stringify({ name: name, email: email }),
-              headers: { "Content-Type": "application/json" },
-            }
-          )
-            .then((res) => res.json())
-            .then((json) => {
-              console.log(json.user);
-            });
+            fetch(
+                "https://uui8anv8g0.execute-api.eu-central-1.amazonaws.com/latest/register",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ name: name, email: email }),
+                    headers: { "Content-Type": "application/json" },
+                }
+            )
+                .then((res) => res.json())
+                .then((json) => {
+                    console.log(json.user);
+                });
         }
         clear();
         setEmail("");
         close();
-      }
+    }
 
 
 
     return (<>
         <div className="modal-wrap">
             <div className="modal-body d-flex justify-content-center align-item-center">
-           
+
                 <div className="modal-contain d-flex flex-column">
-              
+
 
                     {/* header */}
                     <div className="modal-headerr d-flex justify-content-center align-item-center">
                         <p>Create a free account</p>
                         <div className="close-wrap">
-                    <img className="close rounded-circle ratio ratio-1x1" src={closePng} alt="closePng" onClick={close} />
-                </div>
+                            <img className="close rounded-circle ratio ratio-1x1" src={closePng} alt="closePng" onClick={close} />
+                        </div>
                     </div>
                     {/* header */}
 
                     {/* form */}
                     <div className="modal-form d-flex flex-column">
                         <div className="label">Email</div>
-                        <input type="email" className="modal-input" placeholder="example@email.com" 
-                        onChange={(e)=>{setEmail(e.target.value)}}
-                        value={email}
+                        <input type="email" className="modal-input" placeholder="example@email.com"
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            value={email}
                         />
                         <button className="modal-button"
-                        onClick={()=>{submitform()}}
-                        ><p>Sign up</p></button>
+                            onClick={() => { submitform() }}
+                        >Sign up</button>
                     </div>
                     {/* form */}
 
