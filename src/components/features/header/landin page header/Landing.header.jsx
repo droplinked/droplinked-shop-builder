b1a53a4b47a529f81d853481b74fb6422d9a6c18
Behdadmns <bedi.mns@gmail.com>
2022-05-14 18:27:56 +0430

    connect register modal to join today

--- src/components/features/header/landin page header/Landing.header.jsx
+++ src/components/features/header/landin page header/Landing.header.jsx
@@ -1,9 +1,13 @@
 import "./Landing.header.style.scss"
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react"
+import SignUpModal from "../../../Modal/authen/register/SignUpModal"
 
 export default function LandingHeader() {
+    const [modal, setModal] = useState(false)
 
-    return (
+    const toggle = () => { setModal(p => !p) }
+    return (<>
         <div className="header-body d-flex justify-content-between">
             <Link to="/">
                 <div className="header-brand">
@@ -11,9 +15,11 @@ export default function LandingHeader() {
                 </div>
             </Link>
             <div className="d-flex">
-            <div className="login-wrapper">Login</div>
-            <div className="login-wrapper" style={{backgroundColor:"#353536"}}>Join today</div>
+                <div className="login-wrapper">Login</div>
+                <div className="login-wrapper" style={{ backgroundColor: "#353536" }} onClick={toggle}>Join today</div>
             </div>
         </div>
+        {modal && <SignUpModal close={toggle} />}
+    </>
     )
 }
