205702292bf42498aef7f99a8efba7a98d33fbdf
Behdadmns <bedi.mns@gmail.com>
2022-05-23 12:05:15 +0430

    add image upload in personal info

--- src/layout/registering/personal info/PersonalInfo.jsx
+++ src/layout/registering/personal info/PersonalInfo.jsx
@@ -5,20 +5,24 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import img from "../../../assest/image/default profile/icons8-user-100.png"
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function PersonalInfo() {
+    const [profileImg, setProfileImg] = useState(undefined)
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState(undefined);
+    const [uploadingImage, setUploadingImage] = useState(false);
+
     let user = JSON.parse(localStorage.getItem('profile'));
 
     const { register, formState: { errors }, handleSubmit } = useForm({
         defaultValues: {
-            firstName : (user.user.firstname) && user.user.firstname,
-            lastName : (user.user.lastname) && user.user.lastname,
-            phoneNumber : (user.user.phone) && user.user.phone
+            firstName: (user.user.firstname) && user.user.firstname,
+            lastName: (user.user.lastname) && user.user.lastname,
+            phoneNumber: (user.user.phone) && user.user.phone
         }
     });
-    const [profileImg, setProfileImg] = useState(undefined)
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState(undefined);
 
     const inputFile = useRef(null);
     let navigate = useNavigate();
@@ -28,53 +32,41 @@ export default function PersonalInfo() {
     };
 
 
-    const enterImage = (e) => {
-        // let uploadimg = e.target.files[0];
-        // const formData = new FormData();
-        // formData.append("image", uploadimg);
-        // axios.post('http://18.215.217.156:2021/upload', formData)
-        //     .then(res => { console.log(res) })
-        //     .catch(e => { console.log(e) })
+    const changeImage = (e) => {
+        setUploadingImage(true);
+        const file = e.target.files[0];
 
-        // const reader = new FileReader();
-        // reader.onload = () => {
-        //     img = reader.result;
-        //     changeAvatar();
-        // }
-        // reader.readAsDataURL(uploadimg);
+        if (file.size > 200000) {
+            toast.error("File size exceeded (Max: 200 kb)");
+            setUploadingImage(false);
+            return;
+        }
+        if (
+            file.type !== "image/jpeg" &&
+            file.type !== "image/png" &&
+            file.type !== "image/gif" &&
+            file.type !== "image/jpg"
+        ) {
+            toast.error("File type not supported");
+            setUploadingImage(false);
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("image", file);
+        axios.post('http://18.215.217.156:2021/upload', formData)
+            .then(e => {
+                setUploadingImage(false);
+                toast.success(e.data.message);
+                setProfileImg(e.data.small)
+            })
+            .catch(e => {
+                toast.error(e.response.data.message);
+                setUploadingImage(false);
+                return;
+            })
+
     }
-
-    // async function changeAvatar() {
-    //     try {
-    //         if (img) {
-    //             const image = base64ToImage(img, "image");
-    //             const formData = new FormData();
-    //             formData.append("image", image, image.name);
-    //             axios.post('http://18.215.217.156:2021/upload', formData)
-    //                 .then(res => { console.log(res) })
-    //                 .catch(e => { console.log(e) })
-
-    //             //    localStorage.setItem("picUrl", res.ID);
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         this.toastr.error("Error in change your image", "Error");
-
-    //     }
-    // }
-
-    // function base64ToImage(dataurl, filename) {
-    //     const arr = dataurl.split(",");
-    //     const mime = arr[0].match(/:(.*?);/)[1];
-    //     const bstr = atob(arr[1]);
-    //     let n = bstr.length;
-    //     const u8arr = new Uint8Array(n);
-    //     while (n--) {
-    //         u8arr[n] = bstr.charCodeAt(n);
-    //     }
-    //     return new File([u8arr], filename, { type: mime });
-    // }
-
 
 
 
@@ -83,8 +75,8 @@ export default function PersonalInfo() {
         let profileInfo = {
             firstname: data.firstName,
             lastname: data.lastName,
-            avatar: "",
-            phone: data.phoneNumber
+            avatar: (profileImg == undefined)? "" : profileImg,
+            phone: data.phoneNumber,
         }
         axios.put('https://api.droplinked.com/dev/producer/profile', profileInfo,
             { headers: { Authorization: 'Bearer ' + token } }
@@ -109,11 +101,13 @@ export default function PersonalInfo() {
             <div className="register-personalinfo-wrapper">
                 <div className="error-message">{(message != undefined) && message}</div>
                 <div className="input-perosnal-image" onClick={chooseFile}
-                    style={{ backgroundImage: `url(${(profileImg == undefined) ? img : img})` }}
-                >
-                    <input className="d-none" type="file" ref={inputFile}
-                        onClick={enterImage}
-                    />
+                    style={{ backgroundImage: `url(${(profileImg == undefined) ? img : profileImg})` }}>
+                    {(uploadingImage) &&
+                        <div className="spinner-border" role="status">
+                            <span className="sr-only"></span>
+                        </div>
+                    }
+                    <input className="d-none" type="file" ref={inputFile} onChange={changeImage} />
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="d-flex justify-content-between w-100" style={{ maxWidth: "100%" }}>
@@ -146,6 +140,16 @@ export default function PersonalInfo() {
                         <input type="submit" className={`next-back-btn ${(loading) ? "loading-btn" : "non-loading-btn"}`} />
                     </div>
                 </form>
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover />
             </div>
         </RegisterStructure>
     )
--- src/layout/test/Test1.jsx
+++ src/layout/test/Test1.jsx
@@ -8,35 +8,20 @@ import axios from "axios"
 
 
 export default function Test1() {
-  let image;
-
-
-//  axios.get('https://gamma.io/api/v1/collections/SP3QSAJQ4EA8WXEDSRRKMZZ29NH91VZ6C5X88FGZQ.crashpunks-v2/floor')
-//       .then(res => { console.log(res) })
-//       .catch(e => { console.log(e) })
-
-  function base64ToImage(dataurl, filename) {
-    const arr = dataurl.split(",");
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new File([u8arr], filename, { type: mime });
-  }
-
+ 
 
   const changeimg = (e) => {
 
     const file = e.target.files[0];
     const formData = new FormData();
-    formData.append("image", file );
+    formData.append("image", file);
 
-    axios.post('http://18.215.217.156:2021/upload', formData , {
-      header:{'Access-Control-Allow-Origin' : '*'}
-    })
+    axios.post('http://18.215.217.156:2021/upload', formData)
+      .then(e =>{
+       
+      })
+      .catch(e => console.log(e))
+
 
     // const reader = new FileReader();
     // reader.onload = () => {
@@ -50,29 +35,9 @@ export default function Test1() {
     //   .catch(e => { console.log(e) })
   }
 
-  function changeAvatar() {
-    if (image) {
-      const img = base64ToImage(image, "image");
-      const formData = new FormData();
-      formData.append("image", img, img.name);
-
-      axios.post('http://18.215.217.156:2021/upload', formData , {
-        header:{'Access-Control-Allow-Origin' : '*'}
-      })
-        .then(res => { console.log(res) })
-        .catch(e => { console.log(e) })
-
-    }
-  }
-
-
   return (
     <>
-
-
-      <input type="file"  onChange={changeimg}/>
-
-
+      <input type="file" onChange={changeimg} />
     </>
 
   )
