a6be90598b742415ff64fc8b854b525e2d0a67c7
Behdadmns <bedi.mns@gmail.com>
2022-05-26 12:56:01 +0430

    ADD TOP SECTION COMPONENT

--- src/layout/Producer pages/Main page/shop page/Shop-page-component.jsx
+++ src/layout/Producer pages/Main page/shop page/Shop-page-component.jsx
@@ -1,10 +1,21 @@
 import "./Shop-page-style.scss"
 import axios from "axios"
 import { useParams } from "react-router-dom";
+import { useProfile } from "../../../../sevices/hooks/useProfile"
+import ProducerTopSection from "../../../../components/producer component/producer top section/Producer-top-section"
 
-export default function ShopPage(){
+export default function ShopPage() {
+    const { profile } = useProfile();
     let { shopname } = useParams();
-    
 
-    return(<><h1>{ shopname } </h1></>)
+    console.log(profile);
+    return (<>
+        <ProducerTopSection
+            pic={profile.avatar}
+            shopname={profile.shopName}
+            insta={profile.instagram}
+            twitter={profile.twitter}
+            discord={profile.discord}
+            web={profile.web} />
+    </>)
 }
