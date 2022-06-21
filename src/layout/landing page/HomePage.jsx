17b8d1ffc3b3fdd64e536c7993de91a7e8204940
Behdadmns <bedi.mns@gmail.com>
2022-06-21 20:09:59 +0430

    edit main text

--- src/layout/landing page/HomePage.jsx
+++ src/layout/landing page/HomePage.jsx
@@ -87,7 +87,7 @@ export default function HomePage() {
             <div className="child d-flex rg">
                 <div className="input-form w-100">
                     <div className="discove">Discover, create <br />&amp; connect.</div>
-                    <div className="earn">Earn money for sharing collections.</div>
+                    <div className="earn">Earn cash or crypto for sharing collections.</div>
                     <div className="signup-wrapper d-flex justify-content-between">
                         <div className="d-flex justify-content-start" style={{ width: "75%", maxWidth: "75%" }}>
                             <span className="item-span">droplinked.com/</span>
