b57e8da48bf7ac055b40121f4f6eb45999b93461
alirezafazeli <alirezafazeli34@gmail.com>
2022-06-25 20:51:50 +0430

    checkout container is added to checkout component

--- src/layout/checkout/Checkout.jsx
+++ src/layout/checkout/Checkout.jsx
@@ -1,24 +1,36 @@
 import React from "react";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import ContentWrapper from "../../components/Structure/content-wrapper/Content-wrapper-component";
 
 // components
 import CheckoutTitle from "./CheckoutTitle";
 import CheckoutItem from "./checkout_item/CheckoutItem";
+import CheckoutItemsContainer from "./checkout_item/CheckoutItemsContainer";
 
 function checkout() {
 	return (
 		<ContentWrapper>
-			<Flex flexDirection={"column"} alignItems="self-start">
-				<CheckoutTitle />
+			<Flex
+				flexDirection={"column"}
+				alignItems="self-start"
+				margin={"auto"}
+				width={["100%", "50%", "100%", "100%", "100%"]}
+				// width=""
+				color="white"
+			>
+				<Box paddingBottom={"5"}>
+					<CheckoutTitle title={"Your rders"} />
+				</Box>
 
 				{/* items */}
-				<Flex flexDirection={"column"}>
-					<CheckoutItem />
-					<CheckoutItem />
-					<CheckoutItem />
-					<CheckoutItem />
-				</Flex>
+				<CheckoutItemsContainer />
+
+				<Box alignSelf={"end"} alignItems="end" justifyContent={"end"}>
+					{/* total price */}
+					<Text>Total Price : 30$</Text>
+					{/* checkout button */}
+					<Button>Checkout</Button>
+				</Box>
 			</Flex>
 		</ContentWrapper>
 	);
