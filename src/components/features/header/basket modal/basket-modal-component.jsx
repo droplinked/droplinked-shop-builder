e49bb01c7b64249f1fa7047c04abe2a421516303
Behdadmns <bedi.mns@gmail.com>
2022-07-02 20:03:45 +0430

    fix empty modal bug

--- src/components/features/header/basket modal/basket-modal-component.jsx
+++ src/components/features/header/basket modal/basket-modal-component.jsx
@@ -12,7 +12,7 @@ import BasketItemComponent from "./basket item/basket-item-component"
 export default function BasketModal({ close }) {
 
     const { cart } = useCart();
-
+    console.log(cart)
     let navigate = useNavigate();
 
     const ClickCheckuot = () => {
@@ -22,31 +22,43 @@ export default function BasketModal({ close }) {
     return (
 
         <div className="basket-modal-wrapper">
-             {(cart)
-                ?
+            {(cart) &&
                 <>
-                    {cart.map(item => {
-                       return <BasketItemComponent item={item} />
-                    })}
-                    <Button
-                        mt="20px"
-                        color="white"
-                        w="100%"
-                        bgColor="#8053ff"
-                        fontSize="20px"
-                        fontWeight="600"
-                        _hover={{ color: "#222" }}
-                        onClick={ClickCheckuot}
-                    >
-                        Check out
-                    </Button>
+                    {(cart.length > 0)
+                        ?
+                        <>
+                            {cart.map(item => {
+                                return <BasketItemComponent item={item} />
+                            })}
+                            <Button
+                                mt="20px"
+                                color="white"
+                                w="100%"
+                                bgColor="#8053ff"
+                                fontSize="20px"
+                                fontWeight="600"
+                                _hover={{ color: "#222" }}
+                                onClick={ClickCheckuot}
+                            >
+                                Check out
+                            </Button>
 
+                        </>
+                        :
+                        <Text
+                            color="white"
+                            w='100%'
+                            textAlign='center'
+                            fontSize={{ base: "18px", md: "24px" }}
+                            fontWeight='600'
+                            my={{ base: "0px", md: "20px" }}
+                            h="100%"
+                        >
+                            Empty
+                        </Text>
+                    }
                 </>
-                :
-                <>
-                </>
-            } 
-
+            }
         </div>
 
     )
