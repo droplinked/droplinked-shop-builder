2aa31d81661d86152189f0773c2e1df95f97fc99
Behdadmns <bedi.mns@gmail.com>
2022-07-03 13:54:06 +0430

    single image input

--- src/layout/basic pages/settings-page/personal-info-component/Personal-info-component.jsx
+++ src/layout/basic pages/settings-page/personal-info-component/Personal-info-component.jsx
@@ -1,13 +1,19 @@
 import { Flex, Button, Text } from '@chakra-ui/react'
+import { useState } from 'react';
+import InputImageComponent from '../../../../components/single-input-image-component/single-input-image-component'
 
 export default function PersonalInfoComponent() {
+    const profile = JSON.parse(localStorage.getItem("profile"));
+   
+    const [profileImage , setProfileImage] = useState(profile.avatar)
+    console.log(profile)
 
     return (
-    <Text
-    color='white'
-    fontSize='22px' 
-    >
-        personal info
-    </Text>
+        <Text
+            color='white'
+            fontSize='22px'
+        >
+            <InputImageComponent image={profileImage} setImage={setProfileImage} />
+        </Text>
     )
 }
