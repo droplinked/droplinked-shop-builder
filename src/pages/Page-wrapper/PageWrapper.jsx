a9ef2e8125e0f8d76a8247339d29d4fcbdfb224e
Behdadmns <bedi.mns@gmail.com>
2022-07-04 18:19:43 +0430

    refactor wrapper

--- src/pages/Page-wrapper/PageWrapper.jsx
+++ src/pages/Page-wrapper/PageWrapper.jsx
@@ -1,7 +1,7 @@
 import MainHeader from "../../components/layouts/Header/MainHeader"
 import Footer from "../../components/layouts/Footer/Footer"
 
-import { Box , Flex } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom";
 // import { useCart } from "../../../sevices/hooks/useCart"
 import { useEffect } from "react"
@@ -20,25 +20,37 @@ export default function PageWrapper({ children }) {
     // },[])
 
     return (
-        <Box
-            w="100vw"
-            h="100vh"
-            bgColor='#222'
-            overflowX='hidden'
-        >
-            <MainHeader />
+     
+       
+        <Flex
+         flexDirection='column'
+         overflowX='hidden'
+         >  
+                <MainHeader />
             <Box
-            w='100vw'
-            minH='95%'
-            h="auto"
-            bgColor='#222'
-           
-         //   pt='50px'
-       //     pb='20px'
+                w="100vw"
+                h="auto"
+                minH="100vh"
+                bgColor='#222'
+                pt='50px'
+                pb='100px'
+                m='0px'
             >
                 <Outlet />
-            </Box>
-            <Footer />
-        </Box>
+            </Box>      
+                <Footer /> 
+                
+         </Flex>
     )
 }
+
+// margin: 0px;
+//   outline: 0px;
+//   overflow: hidden;
+//   width: 100vw;
+//   height: auto;
+//   min-height: 100vh;
+//   background-color: #222222;
+//   padding:50px 0px 100px 0px;
+//   overflow-x: hidden;
+//   margin-top: 10px;
