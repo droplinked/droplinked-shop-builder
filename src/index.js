741e185fb76161be65a223bd7d0838ea5cfe3965
Behdadmns <bedi.mns@gmail.com>
2022-07-09 15:33:27 +0430

    fix :  index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-ReactDOM.createRoot(
-    document.querySelector('#root')
-).render( < App / > )
+ReactDOM.render(
+		<App />	,
+	document.getElementById('root')
+);
