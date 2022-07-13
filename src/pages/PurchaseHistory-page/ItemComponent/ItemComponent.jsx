2e623a856b891056bb505e509a0051e33ebcae78
Behdadmns <bedi.mns@gmail.com>
2022-07-13 16:12:30 +0430

    update purchase history

---
+++ src/pages/PurchaseHistory-page/ItemComponent/ItemComponent.jsx
@@ -0,0 +1,15 @@
+import { Text, Flex, Img, AspectRatio } from "@chakra-ui/react"
+
+
+export default function Item({ item }) {
+
+    return (
+        <Flex flexDirection='column' w={{ base: "50%", md: "25%" }} mb='15px'>
+            <AspectRatio ratio={1}>
+                <Img src={item.product.media[0].url} w='100%' h='100%' borderRadius='16px' p='6px' />
+            </AspectRatio>
+            <Text w='100%' textAlign='center' color='white' fontSize='16px' fontWeight='600' >
+                {item.product.title}
+            </Text>
+        </Flex>)
+}
