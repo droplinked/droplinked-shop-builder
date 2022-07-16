9036f0c70ff009b433992494f5009c4484fda2c6
Behdadmns <bedi.mns@gmail.com>
2022-07-16 12:17:47 +0430

    refactor : checkshop name api & landing page

--- src/pages/Landing-page/Landing-page.jsx
+++ src/pages/Landing-page/Landing-page.jsx
@@ -3,15 +3,15 @@
 import { useState } from "react"
 import { useSearchParams } from "react-router-dom";
 import { Flex, Box, Image, Text, Input, Button, Spinner, AspectRatio } from '@chakra-ui/react'
+import { checkShopname } from "../../api/Public-apis/CheckShopname-api"
 
-import { BasicURL } from "../../sevices/functoinal-service/CallApiService"
 
 import figmaImage1 from "../../assest/feature/home page images/figmaImage1.png"
 import alertIcon from "../../assest/feature/home page images/alert.png"
 import SignUpModal from "../../components/Modal/Register-modal/SignUpModal"
 import LoginModal from "../../components/Modal/Login-modal/LoginModal"
 import ResetPassModal from "../../components/Modal/ResetPass-modal/ResetPassModal-component"
-import axios from "axios"
+
 
 
 
@@ -51,7 +51,6 @@ export default function LandingPage() {
     }
 
     const switchResetAndLogin = () => {
-        console.log("x");
         toggleReset();
         toggleLogin();
     }
@@ -64,23 +63,17 @@ export default function LandingPage() {
     }
 
 
-    const landingSignin = () => {
+    // check shopname
+    const landingSignin = async () => {
         if (!(/^[A-Za-z0-9_]*$/.test(userName))) {
             setUserNameVaidation(true);
             return;
         }
         setCheckshopname(true);
-
-        axios.get(`${BasicURL}/shop-name/${userName}`)
-            .then(e => {
-                setCheckshopname(false);
-                toggleSignUp();
-            })
-            .catch(e => {
-                console.log(e.response.data.reason);
-                setCheckshopname(false);
-                setShopnameError(e.response.data.reason)
-            })
+        let result = await checkShopname(userName);
+        if (result == true) toggleSignUp();
+        else setShopnameError(result)
+        setCheckshopname(false);
     }
 
 
@@ -88,7 +81,7 @@ export default function LandingPage() {
         <Box display="flex" h='auto' flexDirection={{ base: "column", md: "row" }} pl={{ base: "20px", md: "80px" }} w='100%'>
             {/* inputs */}
             <Box display="flex" w={{ base: '100%', md: '50%' }} pr={{ base: "20px", md: "0px" }} mb={{ base: "70px", md: "0px" }}>
-                <Flex w='100%' flexDir='column'  mt='4.5vw'>
+                <Flex w='100%' flexDir='column' mt='4.5vw'>
                     <Text
                         fontWeight='600'
                         color="#fff"
@@ -97,7 +90,7 @@ export default function LandingPage() {
                     >Community <br />driven commerce</Text>
 
                     <Text
-                        mt={{base:'25px' , md:'1.8vw'}}
+                        mt={{ base: '25px', md: '1.8vw' }}
                         fontWeight='400'
                         fontSize={{ base: "17px", md: '1.95vw' }}
                         lineHeight={{ base: "22px", md: '33px' }}
@@ -114,8 +107,8 @@ export default function LandingPage() {
                         alignItems='center'
                         border='2px'
                         borderColor='#8053ff'
-                        mt={{base:"36px" ,md:'3vw'}}
-                       // maxH={{ base: "56px", md: "68px" }}
+                        mt={{ base: "36px", md: '3vw' }}
+                    // maxH={{ base: "56px", md: "68px" }}
                     >
                         <Flex justifyContent='start' w='75%' maxW='75%'>
                             <Text
@@ -167,7 +160,7 @@ export default function LandingPage() {
                         </Flex>
                     </Flex>
                     {former &&
-                        <Flex h='30px' w='100%' mt={{base:'12px',md:'0.8vw'}}>
+                        <Flex h='30px' w='100%' mt={{ base: '12px', md: '0.8vw' }}>
                             <Image w='20px' h='20px' m='auto 0px' src={alertIcon} alt="" />
                             <Text m='auto 0px' pl='5px' fontWeight='500' fontSize={{ base: "12px", md: '14px' }} color='#b3b3b3' lineHeight='28px'>
                                 Please enter a valid username.
@@ -175,7 +168,7 @@ export default function LandingPage() {
                         </Flex>
                     }
                     {userNameValidation &&
-                        <Flex h='30px' w='100%' mt={{base:'12px',md:'0.8vw'}}>
+                        <Flex h='30px' w='100%' mt={{ base: '12px', md: '0.8vw' }}>
                             <Image w='20px' h='20px' m='auto 0px' src={alertIcon} alt="" />
                             <Text m='auto 0px' pl='5px' fontWeight='500' fontSize={{ base: "12px", md: '14px' }} color='#b3b3b3' lineHeight='28px'>
                                 Username can contain letters (a-z), numbers (0-9) and underscores.
@@ -183,7 +176,7 @@ export default function LandingPage() {
                         </Flex>
                     }
                     {(shopnameError) &&
-                        <Flex h='30px' w='100%' mt={{base:'12px',md:'0.8vw'}}>
+                        <Flex h='30px' w='100%' mt={{ base: '12px', md: '0.8vw' }}>
                             <Image w='20px' h='20px' m='auto 0px' src={alertIcon} alt="" />
                             <Text m='auto 0px' pl='5px' fontWeight='500' fontSize={{ base: "12px", md: '14px' }} color='#b3b3b3' lineHeight='28px'>
                                 {shopnameError}
@@ -198,14 +191,14 @@ export default function LandingPage() {
             {/* image */}
             <Box w={{ base: "100%", md: "50%" }}>
                 <AspectRatio ratio={1 / 1}>
-                    <Box  w='100%' h='100%' pos='relative'>
-                        <Image 
-                        pos='absolute'
-                        top='0px'
-                        maxW='100%'
-                         src={figmaImage1} alt="" /> 
-                        </Box>
-                </AspectRatio>    
+                    <Box w='100%' h='100%' pos='relative'>
+                        <Image
+                            pos='absolute'
+                            top='0px'
+                            maxW='100%'
+                            src={figmaImage1} alt="" />
+                    </Box>
+                </AspectRatio>
             </Box>
             {/* image */}
         </Box>
