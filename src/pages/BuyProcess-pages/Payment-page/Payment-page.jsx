69bdbadae383f35854537b2c2f44580dd210371e
Behdadmns <bedi.mns@gmail.com>
2022-07-19 12:16:50 +0430

    add rootpaymentsOrderID to payment page

--- src/pages/BuyProcess-pages/Payment-page/Payment-page.jsx
+++ src/pages/BuyProcess-pages/Payment-page/Payment-page.jsx
@@ -23,6 +23,11 @@ export default function PaymentPage() {
     const [disableBtns, setDisables] = useState(false)
     const { cart } = useCart();
 
+    
+    const [rootpaymentsOrderID, setRootpaymentsOrderID] = useState(null);
+
+    let token = JSON.parse(localStorage.getItem("token"));
+
 
     const appearance = {
         theme: 'night',
