3cd48814170b0561426cf9124ad1c9cc8de69666
Behdadmns <bedi.mns@gmail.com>
2022-07-20 13:33:45 +0430

    refactor : location condition in regsiter page

--- src/pages/Registering-pages/Register-page/Register-page.jsx
+++ src/pages/Registering-pages/Register-page/Register-page.jsx
@@ -5,14 +5,14 @@ import { Outlet ,useLocation } from "react-router-dom";
 export default function RegisterPage() {
 
     let location = useLocation().pathname.split("/")[2];
-
+console.log(location);
     return (
         <div className="Register-structure-wrapper">
             <div className="Register-structure-body">
                 <div className="Register-structure-sidebar">
-                    <div className={`item ${(location == "personalinfo") ? "selecteditem" : "unselecteditem"}`}>Personal info</div>
-                    <div className={`item ${(location == "shopinfo") ? "selecteditem" : "unselecteditem"}`}>Shop info</div>
-                    <div className={`item ${(location == "imstype") ? "selecteditem" : "unselecteditem"}`}>Inventory</div>
+                    <div className={`item ${(location == "personalInfo") ? "selecteditem" : "unselecteditem"}`}>Personal info</div>
+                    <div className={`item ${(location == "shopInfo") ? "selecteditem" : "unselecteditem"}`}>Shop info</div>
+                    <div className={`item ${(location == "IMSSelect") ? "selecteditem" : "unselecteditem"}`}>Inventory</div>
                 </div>
                  <div className="Register-structure-content"><Outlet /></div>
             </div>
