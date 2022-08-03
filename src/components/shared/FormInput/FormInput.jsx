9f32309dfa190cbb025bc4ae98f97305c9caa11e
Behdadmns <bedi.mns@gmail.com>
2022-08-03 18:56:11 +0430

    add textarea in forminput

--- src/components/shared/FormInput/FormInput.jsx
+++ src/components/shared/FormInput/FormInput.jsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
+  Textarea
 } from '@chakra-ui/react'
 
 export default function FormInput({ value, changeValue, label, type, placeholder, isError, ...otherProps }) {
@@ -14,28 +15,49 @@ export default function FormInput({ value, changeValue, label, type, placeholder
       <FormLabel
         htmlFor='input-com'
         fontWeight='600'
-        fontSize={{base:'14px' , md:'20px'}}
+        fontSize={{ base: '14px', md: '20px' }}
         color='#fff'
       >{label}</FormLabel>
-      <Input
-        id='input-com'
-        type={(type) ? type : "text"}
-        value={value}
-        onChange={changeValue}
-        fontWeight='600'
-        fontSize={{base:'14px' , md:'20px'}}
-        color='#fff'
-        border='2px'
-        borderColor='#b3b3b3'
-        borderRadius='8px'
-        px="16px"
-        py={{base:"8px" , md:"12px"}}
-        outline='none'
-        _focus={{ borderColor: "#8053ff" }}
-        h='auto'
-        placeholder={placeholder}
-      />
-
+      {(type == "textarea")
+        ?
+        <Textarea
+          id='input-com'
+          type={(type) ? type : "text"}
+          value={value}
+          onChange={changeValue}
+          fontWeight='600'
+          fontSize={{ base: '14px', md: '20px' }}
+          color='#fff'
+          border='2px'
+          borderColor='#b3b3b3'
+          borderRadius='8px'
+          px="16px"
+          py={{ base: "8px", md: "12px" }}
+          outline='none'
+          _focus={{ borderColor: "#8053ff" }}
+          h='auto'
+          placeholder={placeholder}
+        />
+        :
+        <Input
+          id='input-com'
+          type={(type) ? type : "text"}
+          value={value}
+          onChange={changeValue}
+          fontWeight='600'
+          fontSize={{ base: '14px', md: '20px' }}
+          color='#fff'
+          border='2px'
+          borderColor='#b3b3b3'
+          borderRadius='8px'
+          px="16px"
+          py={{ base: "8px", md: "12px" }}
+          outline='none'
+          _focus={{ borderColor: "#8053ff" }}
+          h='auto'
+          placeholder={placeholder}
+        />
+      }
       {isError && (
         <FormHelperText
           color='red'
