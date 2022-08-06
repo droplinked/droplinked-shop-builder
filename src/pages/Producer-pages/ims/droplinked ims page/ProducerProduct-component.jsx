f39d29af616730d569d87b19c82dd13f8288dd60
Behdadmns <bedi.mns@gmail.com>
2022-08-06 15:22:42 +0430

    add animation  to product component

--- src/pages/Producer-pages/ims/droplinked ims page/ProducerProduct-component.jsx
+++ src/pages/Producer-pages/ims/droplinked ims page/ProducerProduct-component.jsx
@@ -7,12 +7,12 @@ export default function ProducerProduct({  title, imageUrl, id , shopname}){
             <Link to={`/producer/Merch/${id}`}>
                 <div className="product-image-st">
                     <div className="ratio ratio-1x1">
-                        <img className={` main-image `} src={imageUrl} />
+                        <img className='main-image' src={imageUrl} />
                     </div>
                     {/* {viewBtn && <div className="product-view-button"><p>View</p></div>} */}
                 </div>
-            </Link>
-            <div className="brand-name">{title}</div>
+                <div className="brand-name">{title}</div>
+            </Link>     
         </div>
     )
 
