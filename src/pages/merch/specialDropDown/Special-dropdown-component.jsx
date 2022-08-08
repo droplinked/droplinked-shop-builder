c0d1aebcaedfb34338f4f77f10869ad021ff86c6
Behdadmns <bedi.mns@gmail.com>
2022-08-08 12:37:35 +0430

    dropdown fixed

--- src/pages/merch/specialDropDown/Special-dropdown-component.jsx
+++ src/pages/merch/specialDropDown/Special-dropdown-component.jsx
@@ -5,9 +5,10 @@ export default function SpcialDropDownComp({ option, change, index }) {
 
 
     return (
-        <div className="basic-dropdown-component-wrapper">
+        <div className="basic-dropdown-component-wrapper" >
             <label className="select-lbl">{option.name}</label>
             <select className="w-100"
+                style={{ border: '1px solid #353536' }}
                 value={option.selected}
                 onChange={(e) => { change(e, option.name, index) }} >
                 {option.values.map((value, i) => {
