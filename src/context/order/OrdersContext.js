f3b77afa3800736c111548704dd37b6a53f1f9de
Behdadmns <bedi.mns@gmail.com>
2022-08-10 01:08:53 +0430

    add condition in order

--- src/context/order/OrdersContext.js
+++ src/context/order/OrdersContext.js
@@ -1,44 +1,41 @@
-import { createContext, useState, useEffect ,useContext} from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { useProfile } from "../profile/ProfileContext";
-import { getOrdersList ,SeenOrder } from "../../api/producer/Orders-api"
-
+import { getOrdersList, SeenOrder } from "../../api/producer/Orders-api";
 
 export const OrderContext = createContext();
 
 export default function OrderContextProvider({ children }) {
-   const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
 
-  const { profile } = useProfile();
+  const { profile, isRegisteredProducer } = useProfile();
   let token = JSON.parse(localStorage.getItem("token"));
+
 
   useEffect(() => {
     if (profile == null || token == null) return;
-    if (profile.type != "PRODUCER") return;
 
-    updateOrder()
-    setInterval(updateOrder, 60000);
+    if (isRegisteredProducer) {
+      updateOrder();
+      setInterval(updateOrder, 60000);
+    }
+
   }, [profile]);
 
-
-  const updateOrder = async() => {
-    let result = await getOrdersList()
-    if(result != null) setOrders(result)
+  const updateOrder = async () => {
+    let result = await getOrdersList();
+    if (result != null) setOrders(result);
   };
 
-
-  const seenOrder = async(orderId) => {
-
-      let result = await SeenOrder(orderId)
-      if(result == true)updateOrder()
-      else console.log(result)
-
-  }
-
+  const seenOrder = async (orderId) => {
+    let result = await SeenOrder(orderId);
+    if (result == true) updateOrder();
+    else console.log(result);
+  };
 
   const ContextValue = {
     seenOrder,
     updateOrder,
-    orders
+    orders,
   };
 
   return (
@@ -49,10 +46,9 @@ export default function OrderContextProvider({ children }) {
 }
 
 export const useOrder = () => {
-   
-  const ctx = useContext(OrderContext)
+  const ctx = useContext(OrderContext);
 
   return {
-      ...ctx
-  }
-}
+    ...ctx,
+  };
+};
