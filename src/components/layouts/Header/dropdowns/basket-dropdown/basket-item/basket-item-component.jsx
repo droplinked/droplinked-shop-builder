3786bee283341fb02ed7508d4819f8fa8b4d0026
Behdadmns <bedi.mns@gmail.com>
2022-08-13 20:08:19 +0430

    add delete item in basket

--- src/components/layouts/Header/dropdowns/basket-dropdown/basket-item/basket-item-component.jsx
+++ src/components/layouts/Header/dropdowns/basket-dropdown/basket-item/basket-item-component.jsx
@@ -1,11 +1,19 @@
 
-import { Box, Text, Image } from '@chakra-ui/react'
+import { Box, Text, Image, Spinner } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react"
+import { deleteSkuFromCart } from "../../../../../../api/base-user/Cart-api"
+import { useCart } from "../../../../../../context/cart/CartContext"
+import { ReactComponent as CloseIcon } from '../../../../../../assest/icon/xmark.svg';
+import { useToasty } from "../../../../../../context/toastify/ToastContext"
 
+export default function BasketItemComponent({ item, close }) {
 
-export default function BasketItemComponent({ item , close}) {
+    const [loading, setLoading] = useState(false)
 
     const navigate = useNavigate()
+    const { updateCart } = useCart()
+    const { successToast, errorToast } = useToasty()
 
     //navigate to merch page after click on product
     const navigateToProductPage = () => {
@@ -13,6 +21,18 @@ export default function BasketItemComponent({ item , close}) {
         close()
     }
 
+    const deleteItem = async () => {
+        setLoading(true)
+        let result = await deleteSkuFromCart(item.skuID)
+        if (result == true) {
+            successToast("Item removed")
+            updateCart();
+        } else {
+            errorToast(result)
+        }
+        setLoading(false)
+    }
+    console.log(item.skuID);
     return (
         <Box
             w="100%"
@@ -21,6 +41,7 @@ export default function BasketItemComponent({ item , close}) {
             borderBottom='1px'
             borderColor='white'
             display="flex"
+            pos='relative'
         >
             <Image
                 src={item.product.media[0].url}
@@ -45,6 +66,22 @@ export default function BasketItemComponent({ item , close}) {
                 <Box w="100%" display="flex" justifyContent="space-between">
                     <Text textAlign="center" fontSize="12px" color="#ccc" fontWeight="500" >Quantity: {item.quantity}</Text>
                 </Box>
+            </Box>
+            <Box
+                pos='absolute'
+                w='20px'
+                h='20px'
+                right='0px'
+                bottom='10px'
+            >
+                {loading
+                    ?
+                    <Spinner color='#e74c3c' w='100%' h='100%' />
+                    :
+                    <CloseIcon
+                        onClick={deleteItem}
+                        style={{ fill: "#e74c3c", cursor: "pointer", width: "100%", height: "100%" }} />
+                }
             </Box>
 
         </Box>
