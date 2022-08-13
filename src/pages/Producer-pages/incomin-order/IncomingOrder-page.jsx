b34d42a3656da18dc042b91352464d687b30f04c
Behdadmns <bedi.mns@gmail.com>
2022-08-13 13:49:58 +0430

    add filter in incoming order modal

--- src/pages/Producer-pages/incomin-order/IncomingOrder-page.jsx
+++ src/pages/Producer-pages/incomin-order/IncomingOrder-page.jsx
@@ -1,14 +1,26 @@
 import { Text, Box } from "@chakra-ui/react"
 import { useOrder } from "../../../context/order/OrdersContext"
 import { ORDER_TYPES } from "../../../constant/order.types"
+import { useMemo, useState } from "react"
 
 import OrderComponent from "./orderComponent/OrderComponent"
 import Order from "../../../components/shared/Order/Order-component"
-
+import Dropdown from "../../../components/shared/Dropdown/Dropdown-component"
 
 export default function IncomingOrderPage() {
 
+    const [filter, setFilter] = useState("All")
     const { orders } = useOrder()
+
+    const setTypesArray = () => {
+        let arr = []
+        for (const type in ORDER_TYPES)
+            arr.push({ id: ORDER_TYPES[type], value: ORDER_TYPES[type] })
+        arr.push({ id: "All", value: "All" })
+        return arr
+    }
+
+    let typesArray = useMemo(() => setTypesArray(), []);
 
 
     return (
@@ -30,23 +42,45 @@ export default function IncomingOrderPage() {
                 >
                     Incoming orders
                 </Text>
+                <Box w='40%' mb='40px'>
+                    <Dropdown
+                        value={filter}
+                        pairArray={typesArray}
+                        placeholder={'Filter'}
+                        change={(e) => { setFilter(e.target.value) }}
+                    />
+                </Box>
 
-                {orders.map((order, i) => {
-                    if(order.status == ORDER_TYPES.WAITING_FOR_CONFIRMATION)
-                    return (
-                        <Box key={i} mb='30px'>
-                            <Order order={order} />
-                        </Box>
-                    )
-                })}
-                {orders.map((order, i) => {
-                    if((order.status != (ORDER_TYPES.WAITING_FOR_CONFIRMATION) && order.status != (ORDER_TYPES.WAITING_FOR_PAYMENT)))
-                    return (
-                        <Box key={i} mb='30px'>
-                            <Order order={order} />
-                        </Box>
-                    )
-                })}
+                {(filter == "All")
+                    ?
+                    <>
+                        {orders.map((order, i) => {
+                            if (order.status == ORDER_TYPES.WAITING_FOR_CONFIRMATION)
+                                return (
+                                    <Box key={i} mb='30px'>
+                                        <Order order={order} />
+                                    </Box>
+                                )
+                        })}
+                        {orders.map((order, i) => {
+                            if ((order.status != (ORDER_TYPES.WAITING_FOR_CONFIRMATION) && order.status != (ORDER_TYPES.WAITING_FOR_PAYMENT)))
+                                return (
+                                    <Box key={i} mb='30px'>
+                                        <Order order={order} />
+                                    </Box>
+                                )
+                        })}
+                    </>
+                    :
+                    <>
+                        {orders.map((order, i) => {
+                            if (order.status == filter)
+                                return <Order key={i} order={order} />
+                        })}
+                    </>
+                }
+
+
 
             </Box>
         </Box>
