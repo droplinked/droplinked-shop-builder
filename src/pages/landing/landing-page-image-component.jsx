d1e5754e2ab05859a0efaa9f9e3251f46c6abd2a
Behdadmns <bedi.mns@gmail.com>
2022-08-15 19:12:18 +0430

    new image

--- src/pages/landing/landing-page-image-component.jsx
+++ src/pages/landing/landing-page-image-component.jsx
@@ -1,5 +1,5 @@
 //import mainImage from "../../assest/image/landingPageImage.png"
-import mainImage from "../../assest/image/lsp2.png"
+import mainImage from "../../assest/image/imgtest.png"
 import { Box, Image, AspectRatio, keyframes, usePrefersReducedMotion } from '@chakra-ui/react'
 
 
