cb0fac8c0b746b11270e487cd1864516179f4c02
behdadmns <bedi.mns@gmail.com>
2022-08-23 17:24:51 +0430

    add button for import json file

--- src/pages/Producer-pages/ims/droplinked ims page/droplink-ims-page.jsx
+++ src/pages/Producer-pages/ims/droplinked ims page/droplink-ims-page.jsx
@@ -1,50 +1,95 @@
-import "./droplink-ims-page.scss"
+import "./droplink-ims-page.scss";
 
-import BasicButton from "../../../../components/shared/BasicButton/BasicButton"
-import SeachBox from "../search box/Search-box-component"
-import ProducerProduct from "./ProducerProduct-component"
-
-import { useState } from "react"
+import BasicButton from "../../../../components/shared/BasicButton/BasicButton";
+import SeachBox from "../search box/Search-box-component";
+import ProducerProduct from "./ProducerProduct-component";
+import { useToasty } from "../../../../context/toastify/ToastContext";
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
-
 export default function DroplinkedImsPage({ products }) {
+  const [searchText, setSearchText] = useState("");
+  const fileRef = useRef(null);
 
-    const [searchText, setSearchText] = useState("")
+  const { errorToast } = useToasty()
 
-    const onChangeSearchBox = (e) => {
-        setSearchText(e.target.value.toLowerCase())
+  const onChangeSearchBox = (e) => {
+    setSearchText(e.target.value.toLowerCase());
+  };
+
+  function onReaderLoad(event) {
+    var obj = JSON.parse(event.target.result);
+    alert(obj);
+  }
+  function onReaderLoad(event) {
+    console.log(event.target.result);
+  }
+
+  const uploadFile = (e) => {
+    const file = e.target.files[0];
+    console.log(file.type);
+    if (file.type != "application/json") {
+        errorToast("dadash eshtebe");
+      return;
+    } else {
+      var reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = onReaderLoad;
     }
+  };
 
-    return (
-        <>
-            <div className="w-100 d-flex justify-content-center align-items-center mt-5">
-                <Link to="/producer/add-product" style={{ width: "200px", display: "flex" }}>
-                    <BasicButton>Add item</BasicButton>
-                </Link>
-            </div>
-            <div style={{ margin: "15px 0xp" }}>
-                <SeachBox onch={onChangeSearchBox} />
-            </div>
-            <div className="product-small-wrapper">
-                {(products.length <= 0)
-                    ?
-                    <div className="w-100 d-flex justify-content-center align-items-center">
-                        <p className="no-product"></p>
-                    </div>
-                    :
-                    <>
-                        {(products).filter(pr => pr.title.toLowerCase().includes(searchText)).map((item , i) => {
-                            return (
-                                <div className="col-6 col-md-4 col-lg-3 p-1" key={i}>
-                                    <ProducerProduct title={item.title} imageUrl={item.media[0].url} id={item._id} />
-                                </div>
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        </>
-
-    )
+  return (
+    <>
+      <div className="w-100 d-flex justify-content-center align-items-center mt-5">
+        <Link
+          to="/producer/add-product"
+          style={{ width: "200px", display: "flex" }}
+        >
+          <BasicButton>Add item</BasicButton>
+        </Link>
+      </div>
+      <input
+        type="file"
+        className="d-none"
+        ref={fileRef}
+        onChange={uploadFile}
+      />
+      <div style={{ margin: "20px 0px" }}>
+        <div
+          className="Import-item-button"
+          onClick={() => {
+            fileRef.current.click();
+          }}
+        >
+          Import item
+        </div>
+      </div>
+      <div style={{ margin: "25px 0xp" }}>
+        <SeachBox onch={onChangeSearchBox} />
+      </div>
+      <div className="product-small-wrapper">
+        {products.length <= 0 ? (
+          <div className="w-100 d-flex justify-content-center align-items-center">
+            <p className="no-product"></p>
+          </div>
+        ) : (
+          <>
+            {products
+              .filter((pr) => pr.title.toLowerCase().includes(searchText))
+              .map((item, i) => {
+                return (
+                  <div className="col-6 col-md-4 col-lg-3 p-1" key={i}>
+                    <ProducerProduct
+                      title={item.title}
+                      imageUrl={item.media[0].url}
+                      id={item._id}
+                    />
+                  </div>
+                );
+              })}
+          </>
+        )}
+      </div>
+    </>
+  );
 }
