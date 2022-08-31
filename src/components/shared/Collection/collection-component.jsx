aaa15e6c1467cd444a067449cc7e4e5771ee8358
behdadmns <bedi.mns@gmail.com>
2022-08-31 16:44:00 +0430

    change collection style

--- src/components/shared/Collection/collection-component.jsx
+++ src/components/shared/Collection/collection-component.jsx
@@ -1,67 +1,72 @@
-
 import { useNavigate, useParams } from "react-router-dom";
-import { Flex, Text, Box } from "@chakra-ui/react"
+import {
+  Flex,
+  Text,
+  Box,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import { BASE_URL } from "../../../api/BaseUrl"
+import { BASE_URL } from "../../../api/BaseUrl";
+import { BsThreeDots } from "react-icons/bs";
 
-import Product from "../Product/Product"
-import IframeSnipped from "../../Modal/Iframe-snipped-modal/Iframe-snipped-modal"
+import Product from "../Product/Product";
+import IframeSnipped from "../../Modal/Iframe-snipped-modal/Iframe-snipped-modal";
 
 // collection format : {
-//     ._id: id 
+//     ._id: id
 //     products:[]
-//     title : string 
+//     title : string
 // }
 
 export default function Collection({ collection, shopname }) {
+  // state for open and close snipped modal
+  const [snippedModal, setSnippedModal] = useState(false);
+  const param = useParams();
+  const navigate = useNavigate();
 
-    // state for open and close snipped modal
-    const [snippedModal, setSnippedModal] = useState(false)
-    const param = useParams()
-    const navigate = useNavigate()
-
-    // collection iframe code
-    let iframeCode = `<iframe  
+  // collection iframe code
+  let iframeCode = `<iframe  
     style={{width:'100%' , height:"100%"  , overflow:'hidden' }}
             scrolling="no"
                 title='product'
                 src='${window.location.origin}/collection-iframe/${param.shopname}/${collection._id}'
                 allowFullScreeng
-            />`
+            />`;
 
-    const link = ` ${BASE_URL}/public/collection/${collection._id}`
+  const link = ` ${BASE_URL}/public/collection/${collection._id}`;
 
-    // opent collection page
-    const seeMore = () => {
-        navigate(`/${shopname}/collection/${collection._id}`)
-    }
+  // opent collection page
+  const seeMore = () => {
+    navigate(`/${shopname}/collection/${collection._id}`);
+  };
 
-    return (
-        <>
-            <Flex
-                justifyContent='center'
-                alignItems='start'
-                flexDirection='column'
-                w='100%'
-                maxW='750px'
-                mb={{ base: '20px', md: '30px' }}
-                p='10px 10px 0px 10px'
-            >
-
-                {/* head */}
-                <Flex
-                    w='100%'
-                    justifyContent='space-between'
-                    h='auto'
-                    mb='10px'
-                >
-                    <Text
-                        color='#fff'
-                        fontSize={{ base: '14px', sm: '16px', md: '22px' }}
-                        fontWeight='600'
-                    >{collection.title}</Text>
-                    <Flex >
-                        <Flex
+  return (
+    <>
+      <Flex
+        justifyContent="center"
+        alignItems="start"
+        flexDirection="column"
+        w="100%"
+        maxW="750px"
+        mb={{ base: "20px", md: "30px" }}
+        p="10px 10px 0px 10px"
+      >
+        {/* head */}
+        <Flex w="100%" justifyContent="space-between" h="auto" mb="10px">
+          <Text
+            color="#fff"
+            fontSize={{ base: "14px", sm: "16px", md: "22px" }}
+            fontWeight="600"
+            onClick={seeMore}
+            cursor='pointer'
+          >
+            {collection.title}
+          </Text>
+          <Flex>
+            {/* <Flex
                             p={{ base: "4px 10px 1px 10px", md: '4px 20px' }}
                             color='#fff'
                             bgColor="#353536"
@@ -95,52 +100,110 @@ export default function Collection({ collection, shopname }) {
                             onClick={seeMore}
                         >
                             See more
-                        </Flex>
-                    </Flex>
-
-                </Flex>
-                {/* head */}
-                {(collection.products.length == 0) &&
-                    <Text
-                        color='#fff'
-                        fontSize={{ base: "18px", md: '24px' }}
-                        fontWeight='600'
-                        w='100%'
-                        textAlign='center'
-                        mt='30px'
-                    >
-                        Empty
-                    </Text>
-                }
-
+                        </Flex> */}
+            {/* <Flex
+                            p={{ base: "4px 10px 1px 10px", md: '0px 20px' }}
+                            color='#fff'
+                            bgColor="#353536"
+                          //  fontSize={{ base: "10px", sm: '12px', md: '14px' }}
+                          fontSize='20px'
+                            fontWeight="500"
+                            borderRadius='8px'
+                            justifyContent='center'
+                            alignItems='center'
+                            textAlign='center'
+                            ml='10px'
+                            cursor='pointer'
+                            _hover={{
+                                bgColor: "#555558"
+                            }}
+                            onClick={seeMore}
+                        >
+                            <BsThreeDots color='white' />
+                        </Flex> */}
+            <Menu>
+              {/* <MenuButton
+   // as={IconButton}
+    aria-label='Options'
+    icon={<BsThreeDots />}
+    variant='outline'
+  /> */}
+              <MenuButton  >
                 <Flex
-                    w='100%'
-                    wrap='wrap'
+                  p={{ base: "4px 10px 1px 10px", md: "4px 20px" }}
+                  color="#fff"
+                  bgColor="#353536"
+                  borderRadius="8px"
+                  justifyContent="center"
+                  alignItems="center"
+                  textAlign="center"
+                  ml="10px"
+                  cursor="pointer"
+                  _hover={{
+                    bgColor: "#555558",
+                  }}
+                  onClick={seeMore}
                 >
-                    {collection.products.map((product, i) => {
-                        if (i < 4) {
-                            return (
-                                <Box
-                                    key={i}
-                                    w={{ base: '50%', md: '25%' }}
-                                    p='3px'
-                                >
-                                    <Product
-                                        title={product.title}
-                                        imageUrl={product.media[0].url}
-                                        id={product._id}
-                                        shopname={shopname}
-                                    />
-                                </Box>
-                            )
-                        }
-                    })}
+                  <BsThreeDots color="white" />
                 </Flex>
+              </MenuButton>
+              <MenuList bgColor="#353536" w={{base:"auto" , md:'auto'}} minW='auto' border='none'>
+                <MenuItem
+                  bgColor="#353536"
+                 
+                  color="#fff"
+                  fontSize={{ base: "14px", md: '18px' }}
+                  _hover={{ bgColor: "#555" }}
+                  _focus={{bgColor:"#353536"}}
+                  onClick={() => { setSnippedModal(true) }}
+               //   style={{backgroundColor:'#353536'}}
+                >
+                  Embed collection
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+        {/* head */}
+        {collection.products.length == 0 && (
+          <Text
+            color="#fff"
+            fontSize={{ base: "18px", md: "24px" }}
+            fontWeight="600"
+            w="100%"
+            textAlign="center"
+            mt="30px"
+          >
+            Empty
+          </Text>
+        )}
 
-            </Flex>
-            {snippedModal && <IframeSnipped
-                link={link}
-                code={iframeCode} close={() => { setSnippedModal(false) }} />}
-        </>
-    )
+        <Flex w="100%" wrap="wrap">
+          {collection.products.map((product, i) => {
+            if (i < 4) {
+              return (
+                <Box key={i} w={{ base: "50%", md: "25%" }} p="3px">
+                  <Product
+                    title={product.title}
+                    imageUrl={product.media[0].url}
+                    id={product._id}
+                    shopname={shopname}
+                  />
+                </Box>
+              );
+            }
+          })}
+        </Flex>
+      </Flex>
+      {snippedModal && (
+        <IframeSnipped
+          link={link}
+          code={iframeCode}
+          close={() => {
+            setSnippedModal(false);
+          }}
+        />
+      )}
+    </>
+  );
 }
