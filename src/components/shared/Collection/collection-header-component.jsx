69c6039cfc81678468bdd2d5dd2002010056a5b5
behdadmns <bedi.mns@gmail.com>
2022-09-06 23:38:50 +0430

    collection new style

--- src/components/shared/Collection/collection-header-component.jsx
+++ src/components/shared/Collection/collection-header-component.jsx
@@ -1,43 +1,38 @@
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
 
 const CollectionHeader = ({ title, openSnipedModal, seeMore }) => {
+
   return (
-    <>
-      <Flex w="100%" justifyContent="space-between" h="auto" mb="10px">
-        <Text
-          color="#fff"
-          fontSize={{ base: "10px", sm: "16px", md: "22px" }}
-          fontWeight="600"
-        >
-          {title}
-        </Text>
-        <Flex>
+    <Flex w="100%" justifyContent="space-between" h="auto" mb="10px">
+      
+      <Text
+        color="#fff"
+        fontSize={{ base: "14px", sm: "16px", md: "22px" }}
+        fontWeight="600"
+        onClick={seeMore}
+        cursor="pointer"
+      >
+        {title}
+      </Text>
+
+      <Menu>
+        <MenuButton>
           <Flex
             p={{ base: "4px 10px 1px 10px", md: "4px 20px" }}
             color="#fff"
             bgColor="#353536"
-            fontSize={{ base: "6px", sm: "8px", md: "14px" }}
-            fontWeight="500"
             borderRadius="8px"
             justifyContent="center"
             alignItems="center"
-            cursor="pointer"
-            _hover={{
-              bgColor: "#555558",
-            }}
-            onClick={openSnipedModal}
-          >
-            Embed collection
-          </Flex>
-          <Flex
-            p={{ base: "4px 10px 1px 10px", md: "4px 20px" }}
-            color="#fff"
-            bgColor="#353536"
-            fontSize={{ base: "6px", sm: "8px", md: "14px" }}
-            fontWeight="500"
-            borderRadius="8px"
-            justifyContent="center"
-            alignItems="center"
+            textAlign="center"
             ml="10px"
             cursor="pointer"
             _hover={{
@@ -45,11 +40,29 @@ const CollectionHeader = ({ title, openSnipedModal, seeMore }) => {
             }}
             onClick={seeMore}
           >
-            See more
+            <BsThreeDots color="white" />
           </Flex>
-        </Flex>
-      </Flex>
-    </>
+        </MenuButton>
+        <MenuList
+          bgColor="#353536"
+          w={{ base: "auto", md: "auto" }}
+          minW="auto"
+          border="none"
+        >
+          <MenuItem
+            bgColor="#353536"
+            color="#fff"
+            fontSize={{ base: "14px", md: "18px" }}
+            _hover={{ bgColor: "#555" }}
+            _focus={{ bgColor: "#353536" }}
+            onClick={openSnipedModal}
+            //   style={{backgroundColor:'#353536'}}
+          >
+            Embed collection
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
   );
 };
 
