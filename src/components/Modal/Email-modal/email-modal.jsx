abb9221bc7dcbc5d3a258463c913f7d7755e9199
behdadmns <bedi.mns@gmail.com>
2022-09-06 15:31:30 +0430

    add check email modal

--- src/components/Modal/Email-modal/email-modal.jsx
+++ src/components/Modal/Email-modal/email-modal.jsx
@@ -1,39 +1,56 @@
-import { Box, Flex , Text } from "@chakra-ui/react"
-import { useState } from "react"
-import { useAddress } from "../../../context/address/AddressContext"
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
-import FormInput from "../../shared/FormInput/FormInput"
-import BasicButton from "../../shared/BasicButton/BasicButton"
-import ModalContainer from "../modal-container/modal-container"
+import FormInput from "../../shared/FormInput/FormInput";
+import BasicButton from "../../shared/BasicButton/BasicButton";
+import ModalContainer from "../modal-container/modal-container";
 
+export default function EmailModal({ close, loading }) {
+  return (
+    <ModalContainer close={close}>
+      <Box>
+        <Text
+          color="#fff"
+          w="100%"
+          textAlign="center"
+          mb="20px"
+          fontWeight="600"
+          fontSize={{ base: "14px", md: "20px" }}
+        >
+          Please enter your email address
+        </Text>
 
-export default function EmailModal({close , loading}) {
+        <FormInput
+          mb="30px"
+          placeholder={"Email"}
+          //  isError={(error == "line1") && "Address line1 is required"}
+        />
 
-
-
-    return (
-        <ModalContainer close={close}>
-             
-            <Box>
-
-            <Text color='#fff' w='100%' textAlign='center' mb='20px' fontWeight='600'  fontSize={{ base: '14px', md: '20px' }}>Please enter your email address</Text>
-
-                <FormInput
-                    mb='30px'
-                    placeholder={"Email"}
-                  //  isError={(error == "line1") && "Address line1 is required"}
-                />
-
-                <Flex
-                    justifyContent='space-between'
-                    alignItems='center'
-                    p='0px'
-                    w='100%'
-                >
-                    <BasicButton w='45%' p='12px 16px' click={close} loading={loading} disabled={loading} >Cancel</BasicButton>
-                    <BasicButton w='45%' p='12px 16px'  loading={loading} disabled={loading}>Submit</BasicButton>
-                </Flex>
-            </Box>
-        </ModalContainer>
-    )
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          p="0px"
+          w="100%"
+        >
+          <BasicButton
+            w="45%"
+            p="12px 16px"
+            click={close}
+            loading={loading}
+            disabled={loading}
+          >
+            Cancel
+          </BasicButton>
+          <BasicButton
+            w="45%"
+            p="12px 16px"
+            loading={loading}
+            disabled={loading}
+          >
+            Submit
+          </BasicButton>
+        </Flex>
+      </Box>
+    </ModalContainer>
+  );
 }
--- src/pages/Page-wrapper/PageWrapper.jsx
+++ src/pages/Page-wrapper/PageWrapper.jsx
@@ -1,113 +1,94 @@
 //import MainHeader from "../../components/layouts/Header/MainHeader"
 //import Footer from "../../components/layouts/Footer/Footer"
 
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
-import { useCart } from "../../context/cart/CartContext"
-import { useEffect , useState } from "react"
-import { useAddress } from "../../context/address/AddressContext"
-import { useNotifications } from "../../context/notifications/NotificationsContext"
-import { useProfile } from "../../context/profile/ProfileContext"
-import { isJwtValid ,getProfileData} from "../../api/base-user/Profile-api"
-import { useShop } from "../../context/shop/ShopContext"
-import EmailModal from "../../components/Modal/Email-modal/email-modal"
+import { useCart } from "../../context/cart/CartContext";
+import { useEffect, useState } from "react";
+import { useAddress } from "../../context/address/AddressContext";
+import { useNotifications } from "../../context/notifications/NotificationsContext";
+import { useProfile } from "../../context/profile/ProfileContext";
+import { isJwtValid, getProfileData } from "../../api/base-user/Profile-api";
+import { useShop } from "../../context/shop/ShopContext";
+import EmailModal from "../../components/Modal/Email-modal/email-modal";
 import MainHeader from "../../components/layouts/Header/MainHeader";
 import Footer from "../../components/layouts/Footer/Footer";
 
 export default function PageWrapper() {
+  const [showEmailModal, setEmailModal] = useState(false);
+  const { updateCart } = useCart();
+  const { updateAddressList } = useAddress();
+  const { profile, isCustomer } = useProfile();
+  const { updateNotifications } = useNotifications();
+  const { updateShop } = useShop();
 
-    const [ showEmailModal , setEmailModal] = useState(false)
-    const { updateCart } = useCart();
-    const { updateAddressList } = useAddress();
-    const { profile, isCustomer } = useProfile()
-    const { updateNotifications } = useNotifications()
-    const { updateShop } = useShop()
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("token"));
+    if (token != null || token != undefined) firtsCheck();
+  }, []);
 
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("token"));
+    if (token != null || token != undefined) {
+      // if (isCustomer()) updateCart();
+      if (!isCustomer()) updateShop();
+      updateAddressList();
+      updateNotifications();
+      setInterval(updateNotifications, 60000);
+    }
+  }, [profile]);
 
-    useEffect(() => {
-
-        let token = JSON.parse(localStorage.getItem("token"));
-        if (token != null || token != undefined)  firtsCheck()
-    
-    }, [])
-
-
-    useEffect(() => {
-
-        let token = JSON.parse(localStorage.getItem("token"));
-        if (token != null || token != undefined) {
-           // if (isCustomer()) updateCart();
-            if (!isCustomer()) updateShop();
-            updateAddressList()
-            updateNotifications()
-            setInterval(updateNotifications, 60000);
-        }
-    }, [profile])
-
-
-
-    const firtsCheck = async() => {
-       await checkJWT()
-       let res = await getProfileData()
-       if(res.status ==  "success"){
-        if(res.data.user.email == null)setEmailModal(true)
-       // console.log(res.data.user);
-       }else{
-       // console.log(res.response.data.reason);
-       }
-       
-       lastSeen()
+  const firtsCheck = async () => {
+    lastSeen();
+    checkJWT();
+    let res = await getProfileData();
+    if (res.status == "success") {
+      if (res.data.user.email == null) setEmailModal(true);
     }
 
+  };
 
-    const checkJWT = async () => {
-        let result = await isJwtValid()
-        if (!result) {
-            cleanStorage()
-            return
-        }
+  const checkJWT = async () => {
+    let result = await isJwtValid();
+    if (!result) {
+      cleanStorage();
+      return;
     }
+  };
 
-    const lastSeen = () => {
-        // delete localstorage after 8 hour 
-        const loginTime = JSON.parse(localStorage.getItem("login-time"));
-        let currentTime = new Date().getTime()
-        let hour = (((currentTime - loginTime) / 1000) / 60 / 60)
-        if (hour > 8) {
-            cleanStorage()
-            return
-        }
+  const lastSeen = () => {
+    // delete localstorage after 8 hour
+    const loginTime = JSON.parse(localStorage.getItem("login-time"));
+    let currentTime = new Date().getTime();
+    let hour = (currentTime - loginTime) / 1000 / 60 / 60;
+    if (hour > 8) {
+      cleanStorage();
+      return;
     }
+  };
 
-    const cleanStorage = () => {
-        localStorage.clear()
-        window.location.replace('/');
-    }
+  const cleanStorage = () => {
+    localStorage.clear();
+    window.location.replace("/");
+  };
 
-
-
-    return (
-
-        <Flex
-            flexDirection='column'
-            overflowX='hidden'
-            w='100%'
-        >
-            <MainHeader />
-            <Box
-                w="100%"
-                h="auto"
-                minH="100vh"
-                bgColor='#222'
-                pt='50px'
-                pb='100px'
-                m='0px'
-                overflowX='hidden'
-            >
-                <Outlet />
-            </Box>
-            {false && <EmailModal close={false} loading={false}/>}
-            <Footer />
-        </Flex>
-    )
+  return (
+    <Flex flexDirection="column" overflowX="hidden" w="100%">
+      <MainHeader />
+      <Box
+        w="100%"
+        h="auto"
+        minH="100vh"
+        bgColor="#222"
+        pt="50px"
+        pb="100px"
+        m="0px"
+        overflowX="hidden"
+      >
+        <Outlet />
+      </Box>
+      {showEmailModal && <EmailModal close={()=>{setEmailModal(false)}} loading={false} />}
+      <Footer />
+    </Flex>
+  );
 }
