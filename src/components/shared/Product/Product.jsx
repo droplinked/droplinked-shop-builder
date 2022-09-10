351ffb97fabfb9639eb5714d53090fc9d1d86f37
behdadmns <bedi.mns@gmail.com>
2022-09-10 15:37:55 +0430

    cleaning collection page

--- src/components/shared/Product/Product.jsx
+++ src/components/shared/Product/Product.jsx
@@ -1,21 +1,21 @@
-import "./Product-style.scss"
+import "./Product-style.scss";
 
-import { Link } from "react-router-dom"
+import { Box, AspectRatio } from "@chakra-ui/react";
+import { ProductContent, ProductImage, ProductTitle } from "./Product-style";
+import { Link } from "react-router-dom";
 
-export default function Product({  title, imageUrl, id , shopname}){
-
-    return (
-        <div className="Lproduct-component-wrapper">
-            <Link to={`/${shopname}/merch/${id}`}>
-                <div className="product-image-st">
-                    <div className="ratio ratio-1x1">
-                        <img className={` main-image `} src={imageUrl} />
-                    </div>
-                    {/* {viewBtn && <div className="product-view-button"><p>View</p></div>} */}
-                </div>
-                <div className="brand-name">{title}</div>
-            </Link>        
-        </div>
-    )
-
+export default function Product({ title, imageUrl, id, shopname }) {
+  return (
+    <Box w="100%" m="0px 0px 10px 0px" p="5px" >
+      <Link to={`/${shopname}/merch/${id}`}>
+        <ProductContent>
+          <AspectRatio ratio={1}>
+            <ProductImage src={imageUrl ? imageUrl : ""} />
+          </AspectRatio>
+        </ProductContent>
+        <ProductTitle>{title}</ProductTitle>
+      </Link>
+    </Box>
+  );
 }
+
