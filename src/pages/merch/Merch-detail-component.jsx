cbe6a67a23d2126595a4523950ceb3be3945340f
behdadmns <bedi.mns@gmail.com>
2022-09-11 02:42:47 +0430

    fix merch page

--- src/pages/merch/Merch-detail-component.jsx
+++ src/pages/merch/Merch-detail-component.jsx
@@ -1,32 +1,23 @@
 import {
-  MerchPageWrapper,
   DetailWrapper,
   ProductTitle,
   ProductShopname,
   QuantityButton,
-  DescriptionWrapper,
-  DescriptionText,
-  ReadmoreButton,
   VariantSelect,
 } from "./Shopify-merch-style";
 import { Flex, Box, Text, Image } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import Dropdown from "../../components/shared/Dropdown/Dropdown-component";
 import BasicButton from "../../components/shared/BasicButton/BasicButton";
 import plus from "../../assest/icon/plusIcon.png";
 import minus from "../../assest/icon/minusIcon.png";
-import { FALSE } from "sass";
 
 const DetailComponent = ({
   title,
   shopName,
-  price,
   variants,
   optionsList,
-  selectedOption,
-  setSelectedOption,
   quantity,
   setQuantity,
   submit,
@@ -35,43 +26,47 @@ const DetailComponent = ({
   const [selectedVariant, setSelectedVariant] = useState(null);
 
   const navigate = useNavigate();
-  console.log(selectedVariant);
-  console.log(optionsList);
-  // console.log(optionsList);
+
   useEffect(() => {
     initialVariant();
   }, []);
 
+  // function
   const initialVariant = () => {
+    let firstOption = optionsList.map((opt) => {
+      return { name: opt.name, option_id: opt.id, value: opt.values[0] };
+    });
 
-    // let firstOption = optionsList.map((opt) => {
-    //   return { name: opt.name, option_id: opt.id, value: opt.values[0] };
-    // });
+    variants.forEach((variant) => {
+      let find = true;
 
-    // variants.forEach((variant) => {
-    //   let find = true;
+      variant.option_values.forEach((option) => {
+        let findOption = firstOption.find(
+          (opt) => opt.option_id == option.option_id
+        );
+        if (
+          option.option_id == findOption.option_id &&
+          option.value != findOption.value
+        )
+          find = false;
+      });
 
-    //   variant.option_values.forEach((option) => {
-    //     if (
-    //       option.option_id == firstOption.option_id &&
-    //       option.value != firstOption.value
-    //     )
-    //       find = false;
-    //   });
-
-    //   if (find == true) {
-    //     setSelectedVariant(variant)
-    //   };
-    // });
+      if (find == true) {
+        setSelectedVariant(variant);
+        return;
+      }
+    });
   };
 
+  // function
   const navigateToShoppage = () => navigate(`/${shopName}`);
+  // function
   const decreaseQuantity = () => {
     if (quantity > 1) setQuantity((p) => p - 1);
   };
-
+  // function
   const increaseQuantity = () => setQuantity((p) => p + 1);
-
+  // function
   const changeVariant = (e) => {
     let currentVariantOption = selectedVariant.option_values;
 
@@ -87,87 +82,97 @@ const DetailComponent = ({
       }
     });
 
-    // console.log(newVariantOption);
-    let find = true;
     variants.forEach((variant) => {
+      let find = true;
       variant.option_values.forEach((option) => {
         let findOption = newVariantOption.find(
           (op) => op.option_id == option.option_id
         );
-        let find = false;
-        //console.log(option);
-        //console.log(findOption);
+
         if (
-          option.value == findOption.value &&
-          findOption.option_id == e.target.id
+          findOption.option_id == option.option_id &&
+          option.value != findOption.value
         ) {
-          find = true;
-          setSelectedVariant(variant);
-          return;
+          find = false;
         }
       });
-      if (find == true) return;
+      if (find == true) {
+        setSelectedVariant(variant);
+        return;
+      }
     });
   };
 
   return (
     <DetailWrapper>
-      <ProductTitle>{title}</ProductTitle>
-      <ProductShopname onClick={navigateToShoppage}>{shopName}</ProductShopname>
-      <Text fontWeight="600" fontSize="24px" color="#fff">
-        ${selectedVariant && selectedVariant.price}
-      </Text>
+      {selectedVariant && (
+        <>
+          <ProductTitle>{title}</ProductTitle>
+          <ProductShopname onClick={navigateToShoppage}>
+            {shopName}
+          </ProductShopname>
+          <Text fontWeight="600" fontSize="24px" color="#fff">
+            ${selectedVariant && selectedVariant.price}
+          </Text>
 
-      <Flex justifyContent="space-between" w="100%" flexWrap="wrap">
-        {optionsList.map((option, i) => {
-          return (
-            <Box w="49%" key={i}>
-              <VariantSelect
-                id={option.id}
-                name={option.name}
-                onChange={changeVariant}
-              >
-                {option.values.map((value, i) => {
-                  return (
-                    <option key={i} value={value}>
-                      {value}
-                    </option>
-                  );
-                })}
-              </VariantSelect>
-            </Box>
-          );
-        })}
-      </Flex>
+          <Flex justifyContent="space-between" w="100%" flexWrap="wrap">
+            {optionsList.map((option, i) => {
+              return (
+                <Box w="49%" key={i}>
+                  <VariantSelect
+                    id={option.id}
+                    name={option.name}
+                    onChange={changeVariant}
+                  >
+                    {option.values.map((value, i) => {
+                      return (
+                        <option key={i} value={value}>
+                          {value}
+                        </option>
+                      );
+                    })}
+                  </VariantSelect>
+                </Box>
+              );
+            })}
+          </Flex>
 
-      <Flex>
-        <QuantityButton
-          bgColor="#353536"
-          color="#b3b3b3"
-          cursor="pointer"
-          onClick={decreaseQuantity}
-        >
-          <Image src={minus} alt="minus" />
-        </QuantityButton>
+          <Flex>
+            <QuantityButton
+              bgColor="#353536"
+              color="#b3b3b3"
+              cursor="pointer"
+              onClick={decreaseQuantity}
+            >
+              <Image src={minus} alt="minus" />
+            </QuantityButton>
 
-        <QuantityButton color="white" fontSize="20px">
-          {quantity}
-        </QuantityButton>
+            <QuantityButton color="white" fontSize="20px">
+              {quantity}
+            </QuantityButton>
 
-        <QuantityButton
-          bgColor="#353536"
-          color="#b3b3b3"
-          cursor="pointer"
-          onClick={increaseQuantity}
-        >
-          <Image src={plus} alt="minus" />
-        </QuantityButton>
-      </Flex>
-      <Box h="auto">
-        <BasicButton click={submit} loading={loading}>
-          Add to basket
-        </BasicButton>
-      </Box>
+            <QuantityButton
+              bgColor="#353536"
+              color="#b3b3b3"
+              cursor="pointer"
+              onClick={increaseQuantity}
+            >
+              <Image src={plus} alt="minus" />
+            </QuantityButton>
+          </Flex>
+          <Box h="auto">
+            <BasicButton
+              click={submit}
+              loading={loading}
+              disabled={selectedVariant.inventory_quantity <= 0}
+            >
+              {selectedVariant.inventory_quantity > 0
+                ? "Add to basket"
+                : "Sold out"}
+            </BasicButton>
+          </Box>
+        </>
+      )}
     </DetailWrapper>
   );
 };
