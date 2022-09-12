162542964b77b1f620923cb186e1882e3428fb1a
behdadmns <bedi.mns@gmail.com>
2022-09-12 15:40:47 +0430

    cleaning cart icon

---
+++ src/components/layouts/Header/icons/cart/cart-icon-style.jsx
@@ -0,0 +1,19 @@
+import { chakra } from "@chakra-ui/react";
+
+export const CartIconWrapper = chakra("div", {
+  baseStyle: {
+    pos: "relative",
+    w: { base: "25px", md: "40px" },
+    h: { base: "25px", md: "40px" },
+    cursor: "pointer",
+    mr: { base: "8px", md: "12px" },
+  },
+});
+
+export const IconImage = chakra("img", {
+  baseStyle: {
+    h: "100%",
+    w: "100%",
+    pos: "absolute",
+  },
+});
