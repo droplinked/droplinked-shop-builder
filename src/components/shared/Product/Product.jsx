e93d6517e97fd4c10780f88918daac4b409e7f61
behdadmns <bedi.mns@gmail.com>
2022-09-13 13:09:35 +0430

    add type to Product component

--- src/components/shared/Product/Product.jsx
+++ src/components/shared/Product/Product.jsx
@@ -1,13 +1,16 @@
-import "./Product-style.scss";
+//import "./Product-style.scss";
 
 import { Box, AspectRatio } from "@chakra-ui/react";
 import { ProductContent, ProductImage, ProductTitle } from "./Product-style";
 import { Link } from "react-router-dom";
+import { USER_TYPE } from "../../../constant/user-types"
 
-export default function Product({ title, imageUrl, id, shopname }) {
+export default function Product({ title, imageUrl, id, shopname , type }) {
+
+  let linkAddress  = (type == USER_TYPE.PRODUCER)? `/producer/merch/${id}` :`/${shopname}/merch/${id}`
   return (
     <Box w="100%" m="0px 0px 10px 0px" p="5px" >
-      <Link to={`/${shopname}/merch/${id}`}>
+      <Link to={linkAddress}>
         <ProductContent>
           <AspectRatio ratio={1}>
             <ProductImage src={imageUrl ? imageUrl : ""} />
