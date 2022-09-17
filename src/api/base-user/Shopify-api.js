c381348a7339b0f4107ff4684c5897b23dffe029
behdadmns <bedi.mns@gmail.com>
2022-09-17 20:28:31 +0430

    update api

--- src/api/base-user/Shopify-api.js
+++ src/api/base-user/Shopify-api.js
@@ -29,7 +29,7 @@ export const updateCheckout = async (shopname, checkoutId, handle) => {
     const res = await axios.post(
       `${BASE_URL}/producer/shopify/checkout/update`,
       {
-        shopName: shopname,
+        shopDomain: shopname,
         checkoutId: checkoutId,
         checkoutItem: {
           checkout: {
