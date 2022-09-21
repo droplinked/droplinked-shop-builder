9510c7544514b4c3e3ca82c947c764bf14a03d55
behdadmns <bedi.mns@gmail.com>
2022-09-21 13:11:33 +0430

    fix padding style

--- src/pages/shop/owner/Owner-shop-page.jsx
+++ src/pages/shop/owner/Owner-shop-page.jsx
@@ -9,7 +9,7 @@ const OwnerShopPage = ({ shopData, shopName, update, collections }) => {
   return (
     <>
       <EditableTopSection shopData={shopData} shopName={shopName} />
-      <Flex mt="40px" flexDir="column" alignItems="center" w="100%">
+      <Flex mt="40px" flexDir="column" alignItems="center" w="100%" px={{base:'20px' , md:'80px'}}>
         {collections &&
           collections.collections.map((collection) => {
             return (
