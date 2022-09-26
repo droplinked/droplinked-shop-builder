288d7eb0ca1045b1d04a7da2738f28cfe5acbd6f
behdadmns <bedi.mns@gmail.com>
2022-09-26 17:42:32 +0330

    add rule set modal

--- src/components/shared/ProducerCollection/rule-collection-modal/Rule-modal.jsx
+++ src/components/shared/ProducerCollection/rule-collection-modal/Rule-modal.jsx
@@ -4,74 +4,108 @@ import {
   ModalHeader,
   RulesetText,
   RuleSelect,
-  AddRuleButton
+  AddRuleButton,
 } from "./Rule-modal-style";
 import { Flex, Box } from "@chakra-ui/react";
 import { TiDelete } from "react-icons/ti";
 import { useState } from "react";
 
-import RuleAddressComponent from "./Rule-address-component"
+import RuleAddressComponent from "./Rule-address-component";
 
 const RuleModal = ({ open, close }) => {
+  const [ruleList, setRuleList] = useState([
+    { address: "", type: "NFT", index: "1" },
+  ]);
 
-  const [ruleList, setRuleList] = useState([{ address: "", type: "NFT", index:'1' }]);
+  const handleChildClick = (event) => {
+    event.stopPropagation();
+  };
 
-  const changeRuleType = (value , index) => {
-    let arrNew =  Array.from(ruleList)
-    arrNew = arrNew.map((rule , i ) =>{
-        if(i == index) return {...rule , type:value }
-        else return {...rule}
-    })
-    setRuleList(arrNew)
-  }
+  const changeRuleType = (value, index) => {
+    let arrNew = Array.from(ruleList);
+    arrNew = arrNew.map((rule, i) => {
+      if (i == index) return { ...rule, type: value };
+      else return { ...rule };
+    });
+    setRuleList(arrNew);
+  };
 
-  const changeRuleAddress = (value , index) => {
-    let arrNew = Array.from(ruleList)
-    arrNew = arrNew.map((rule , i ) =>{
-        if(i == index) return {...rule , address:value }
-        else return {...rule}
-    })
-    setRuleList(arrNew)
-  }
+  const changeRuleAddress = (value, index) => {
+    let arrNew = Array.from(ruleList);
+    arrNew = arrNew.map((rule, i) => {
+      if (i == index) return { ...rule, address: value };
+      else return { ...rule };
+    });
+    setRuleList(arrNew);
+  };
 
   const addNewRule = () => {
-    let arrNew = Array.from(ruleList)
-    arrNew.push({address: "", type: "NFT", index:arrNew.length})
-    setRuleList(arrNew)
-  }
+    let arrNew = Array.from(ruleList);
+    arrNew.push({ address: "", type: "NFT", index: arrNew.length });
+    setRuleList(arrNew);
+  };
 
-console.log(ruleList);
+  const deleteRule = (index) => {
+    let arrNew = Array.from(ruleList);
+    arrNew = arrNew.filter((rule, i) => {
+      if (i != index) return rule;
+    });
+    arrNew = arrNew.map((rule, i) => {
+      return { ...rule, index: i };
+    });
+    setRuleList(arrNew);
+  };
+
+  console.log(ruleList);
 
   return (
-    <RuleModalWrapper>
-      <RuleModalCotent>
-        <Flex w="100%" flexDir="column" alignItems="center">
+    <>
+      {open && (
+        <RuleModalWrapper onMouseDown={close}>
+          <RuleModalCotent onMouseDown={handleChildClick}>
+            <Flex w="100%" flexDir="column" alignItems="center">
+              <ModalHeader>Ruleset</ModalHeader>
+              <RulesetText>
+                The customer must meet at least one of the rules listed below
+                (OR)
+              </RulesetText>
 
-          <ModalHeader>Ruleset</ModalHeader>
-          <RulesetText>
-            The customer must meet at least one of the rules listed below (OR)
-          </RulesetText>
+              {ruleList.map((rule, index) => {
+                return (
+                  <Box w="100%" mb="40px" key={index}>
+                    <Flex w="100%" mb="10px" alignItems="center">
+                      <TiDelete
+                        color="red"
+                        size="30px"
+                        cursor="pointer"
+                        onClick={() => {
+                          deleteRule(index);
+                        }}
+                      />
+                      <RuleSelect
+                        value={rule.type}
+                        onChange={(e) => changeRuleType(e.target.value, index)}
+                      >
+                        <option value="NFT">NFT</option>
+                        <option value="CONTRACT">CONTRACT</option>
+                      </RuleSelect>
+                    </Flex>
+                    <RuleAddressComponent
+                      type={rule.type}
+                      index={index}
+                      address={rule.address}
+                      change={changeRuleAddress}
+                    />
+                  </Box>
+                );
+              })}
 
-          {ruleList.map((rule, index) => {
-            return (
-              <Box w="100%" mb="40px" key={index}>
-                <Flex w="100%" mb="10px" alignItems="center">
-                  <TiDelete color="red" size="30px" cursor="pointer" />
-                  <RuleSelect onChange={(e)=>changeRuleType(e.target.value , index)}>
-                    <option value="NFT">NFT</option>
-                    <option value="CONTRACT">CONTRACT</option>
-                  </RuleSelect>
-                </Flex>
-                <RuleAddressComponent type={rule.type} index={index} address={rule.address} change={changeRuleAddress} />
-              </Box>
-            );
-          })}
-
-          <AddRuleButton onClick={addNewRule}>Add rule</AddRuleButton>
-
-        </Flex>
-      </RuleModalCotent>
-    </RuleModalWrapper>
+              <AddRuleButton onClick={addNewRule}>Add rule</AddRuleButton>
+            </Flex>
+          </RuleModalCotent>
+        </RuleModalWrapper>
+      )}
+    </>
   );
 };
 
