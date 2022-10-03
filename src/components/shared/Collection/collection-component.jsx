4c311bd4e93f4c4f8a02d21eaa980a59ac787670
behdadmns <bedi.mns@gmail.com>
2022-10-04 02:00:48 +0330

    change collection page

--- src/components/shared/Collection/collection-component.jsx
+++ src/components/shared/Collection/collection-component.jsx
@@ -44,11 +44,15 @@ export default function Collection({ collection, shopname, type }) {
         alignItems="start"
         flexDirection="column"
         w="100%"
-        maxW="1000px"
+        // maxW="1000px"
         mb={{ base: "20px", md: "30px" }}
-        p={{base:'10px 10px 0px 10px', md:"20px 30px 20px 30px",lg:'60px 80px 60px 80px'}}
-        bgColor='#242424'
-        borderRadius='8px'
+        p={{
+          base: "10px 10px 0px 10px",
+          md: "20px 30px 20px 30px",
+          lg: "60px 80px 60px 80px",
+        }}
+        bgColor="#242424"
+        borderRadius="8px"
       >
         {/* head */}
         <CollectionHeader
