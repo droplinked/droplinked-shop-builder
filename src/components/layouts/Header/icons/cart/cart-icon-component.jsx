abe070010f23b7e2a9c271c4a117737e7c8f5841
behdadmns <bedi.mns@gmail.com>
2022-10-09 19:11:42 +0330

    add item counter in cart icon

--- src/components/layouts/Header/icons/cart/cart-icon-component.jsx
+++ src/components/layouts/Header/icons/cart/cart-icon-component.jsx
@@ -1,16 +1,33 @@
 import { useCart } from "../../../../../context/cart/CartContext";
-import { CartIconWrapper, IconImage } from "./cart-icon-style";
-
+import { CartIconWrapper, IconImage, ItemCounter } from "./cart-icon-style";
+import { SHOP_TYPES } from "../../../../../constant/shop-types";
 import cartIcon from "../../../../../assest/icon/shopCart.svg";
 import activeCartIcon from "../../../../../assest/icon/activeShopCart.svg";
 
 export default function Cart({ clickBasket }) {
+
   const { cart } = useCart();
+
+
+  const getQuantity = () => {
+
+    let total = 0;
+
+    cart.items.forEach((item) => {
+      if (cart.type == SHOP_TYPES.SHOPIFY) total += item.amount;
+      else total += item.quantity;
+    });
+    return total
+
+  };
 
   return (
     <CartIconWrapper onClick={clickBasket}>
       {cart != null && cart.items.length > 0 ? (
-        <IconImage src={activeCartIcon} />
+        <>
+          <IconImage src={activeCartIcon} />
+          <ItemCounter>{getQuantity()}</ItemCounter>
+        </>
       ) : (
         <IconImage src={cartIcon} fill="red" />
       )}
