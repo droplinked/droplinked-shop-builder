a494856f037a761de4077861c8a2e92a01c75680
behdadmns <bedi.mns@gmail.com>
2022-10-18 12:45:21 +0330

    change address page title

--- src/pages/buy-process-pages/address/Address-Page.jsx
+++ src/pages/buy-process-pages/address/Address-Page.jsx
@@ -120,7 +120,7 @@ function AddressPage() {
         color="#fff"
         m="0px auto 48px auto"
       >
-        Select address
+        Address
       </Text>
 
       <Box w="100%" maxW="1000px" m="auto">
