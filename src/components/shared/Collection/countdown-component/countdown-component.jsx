791929328499dce7665bb220f888a58880aa215b
behdadmns <bedi.mns@gmail.com>
2022-10-22 22:38:41 +0330

    build useCountdown hook

---
+++ src/components/shared/Collection/countdown-component/countdown-component.jsx
@@ -0,0 +1,13 @@
+import { useCountdown } from "../../../../hooks/countdown/useCountdown"
+
+const Countdown = () => {
+
+    const [days, hours, minutes, seconds] = useCountdown("2022-10-31 24:00:00 GMT-4");
+ 
+
+
+
+    return <></>
+}
+
+export default Countdown
