573c6bb35bf3e15b110e3fa578cc306e27f1e326
behdadmns <bedi.mns@gmail.com>
2022-10-24 15:35:40 +0330

    fix email validation error

--- src/utils/validations/emailValidation.js
+++ src/utils/validations/emailValidation.js
@@ -1,7 +1,7 @@
 // validation emails 
 
 export const isValidEmail = (email) => {
-    let regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    let regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/
     if (regx.test(email)) {
         return true
     } else {
