4a1dc34d9ae8e92e627e7294904c9eb6022b9f63
behdadmns <bedi.mns@gmail.com>
2022-10-26 15:36:52 +0330

    delete extra code

--- src/pages/publics/shop/owner/Owner-shop-page.jsx
+++ src/pages/publics/shop/owner/Owner-shop-page.jsx
@@ -1,14 +1,11 @@
-  import EditableTopSection from "./editable-profile-section/Editable-profile-section";
 import ProducerCollection from "../../../../components/shared/ProducerCollection/Producre-collection";
 
-import { COLLECTION_TYPE } from "../../../../constant/collection-types";
 import { Flex, Box } from "@chakra-ui/react";
 
 const OwnerShopPage = ({ shopData, shopName, update, collections }) => {
 
   return (
     <>
-      {/* <EditableTopSection shopData={shopData} shopName={shopName} /> */}
       <Flex mt="40px" flexDir="column" alignItems="center" w="100%" px={{base:'20px' , md:'80px'}}>
         {collections &&
           collections.collections.map((collection) => {
