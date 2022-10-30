af693538731c3d26e718852d19d4e9269b4abb98
bedi.mns@gmail.com <bedi.mns@gmail.com>
2022-10-30 13:21:54 +0330

    fix ims type condition

--- src/pages/Producer/producer-wrapper/Producer-wrapper-page.jsx
+++ src/pages/Producer/producer-wrapper/Producer-wrapper-page.jsx
@@ -1,24 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { useProfile } from "../../../context/profile/ProfileContext"
-
+import { useProfile } from "../../../context/profile/ProfileContext";
+import { PROFILE_STATUS } from "../../../constant/profile-status-types";
 
 export default function Producer() {
+  const { profile } = useProfile();
+  const navigate = useNavigate();
 
-    const { profile } = useProfile()
-    const navigate = useNavigate();
+  useEffect(() => {
+    if (
+      profile.type == "PRODUCER" &&
+      profile.status !=
+        (PROFILE_STATUS.ACTIVE || PROFILE_STATUS.IMS_TYPE_COMPLETED)
+    ) {
+      navigate("/register/shop-info");
+    }
+    if (profile.type == "CUSTOMER") {
+      navigate("/");
+    }
+  }, []);
 
-    useEffect(() => {
-        if (profile.type == "PRODUCER" && profile.status != "IMS_TYPE_COMPLETED") {
-                navigate('/register/shop-info')
-        }
-        if (profile.type == "CUSTOMER") {
-            navigate('/')
-        }
-    }, [])
-
-    return (<>
-        <Outlet />
-    </>)
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
