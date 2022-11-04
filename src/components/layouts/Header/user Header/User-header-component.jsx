c3b48cdb309efe600756bed43ec528de05cd0639
bedi.mns@gmail.com <bedi.mns@gmail.com>
2022-11-04 21:56:32 +0330

    refactor header for producer

--- src/components/layouts/Header/user Header/User-header-component.jsx
+++ src/components/layouts/Header/user Header/User-header-component.jsx
@@ -5,16 +5,20 @@ import newWalletIcon from "../../../../assest/icon/new-wallet-icon.svg";
 
 import { DROPDOWN_TYPE } from "../dropdowns/dropdown.type";
 import { useState } from "react";
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 import { useProfile } from "../../../../context/profile/ProfileContext";
 import { UseWalletInfo } from "../../../../context/wallet/WalletContext";
+import { useShop } from "../../../../context/shop/ShopContext";
 import { UserHeaderWrapper, WalletAddressText } from "./User-header-style";
 
 export default function UserHeader() {
   // state for manage dropdowns
   const [dropdown, setDropdown] = useState(null);
   const { userData } = UseWalletInfo();
-  const { isCustomer, isRegisteredProducer } = useProfile();
+  const { isCustomer } = useProfile();
+  const { shop } = useShop();
+
+  console.log(shop.name);
 
   const openProfileDropdown = () => {
     setDropdown(DROPDOWN_TYPE.PROFILE);
@@ -43,31 +47,42 @@ export default function UserHeader() {
   };
 
   return (
-    <>
-      {/* {isCustomer() && <WalletButton />} */}
+    <Flex alignItems="center" ml={{ base: "10px", md: "15px" }}>
+      {isCustomer() ? (
+        <>
+          <Cart clickBasket={openBasket} />
 
-      <Flex alignItems="center" ml={{ base: "10px", md: "15px" }}>
-        {/* cart icon (show if type == producer) */}
-        {isCustomer() && <Cart clickBasket={openBasket} />}
+          <Notification click={openNotification} />
 
-        {/* notification icon (show if type == producer)*/}
-        <Notification click={openNotification} />
-
-        {/* profile icon */}
-        {/* <ProfileIcon click={openProfileDropdown} /> */}
-
-        <UserHeaderWrapper onClick={openProfileDropdown}>
-          <Image
-            w={{ base: "25px", md: "36px" }}
-            h={{ base: "25px", md: "36px" }}
-            src={newWalletIcon}
-          />
-          <WalletAddressText>{walletAddress()}</WalletAddressText>
-        </UserHeaderWrapper>
-
-        {/* get dropdown state and show dropdown match with state value */}
-        {dropdown && <DropdownContainer close={close} dropdown={dropdown} />}
-      </Flex>
-    </>
+          <UserHeaderWrapper onClick={openProfileDropdown}>
+            <Image
+              w={{ base: "25px", md: "36px" }}
+              h={{ base: "25px", md: "36px" }}
+              src={newWalletIcon}
+            />
+            <WalletAddressText>{walletAddress()}</WalletAddressText>
+          </UserHeaderWrapper>
+        </>
+      ) : (
+        <>
+          <Text color="white" fontSize={{base:'16px',md:"24px"}} fontWeight="500" mr={{base:'8px',md:"16px"}}>
+            {shop.name}
+          </Text>
+          <Flex
+            w={{base:'36px',md:"46px"}}
+            h={{base:'36px',md:"46px"}}
+            borderRadius="50%"
+            bg="subLayer"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text color="white" fontSize={{base:'20px',md:"28px"}} fontWeight="600">
+              {shop.name.charAt(0).toUpperCase()}
+            </Text>
+          </Flex>
+        </>
+      )}
+      {dropdown && <DropdownContainer close={close} dropdown={dropdown} />}
+    </Flex>
   );
 }
