500337e8d887a8524d75a38c65a726715a99726b
bedi.mns@gmail.com <bedi.mns@gmail.com>
2022-11-17 19:57:45 +0330

    cleaning tasts context codes

--- src/context/toastify/ToastContext.jsx
+++ src/context/toastify/ToastContext.jsx
@@ -1,44 +1,36 @@
-import { ToastContainer, toast } from 'react-toastify';
-import { createContext ,useContext } from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import { createContext, useContext } from "react";
 
 import "react-toastify/dist/ReactToastify.css";
 
-export const toastValue = createContext(null)
+export const toastValue = createContext(null);
 
-export  default function ToastifyProvider({ children }) {
+export default function ToastifyProvider({ children }) {
 
-    const successToast = (text) =>{
-        toast.success(text)
-    }
+  const successToast = (text) => toast.success(text);
+  const errorToast = (text) => toast.error(text);
 
-    const errorToast = (text) =>{
-        toast.error(text)
-    }
-
-    return (
-        <toastValue.Provider value={{successToast , errorToast}}>
-            {children}
-            <ToastContainer
-                position="bottom-right"
-                autoClose={2500}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                theme="dark"
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover />
-        </toastValue.Provider>
-    )
+  return (
+    <toastValue.Provider value={{ successToast, errorToast }}>
+      {children}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="dark"
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </toastValue.Provider>
+  );
 }
 
-export function useToasty(){
+export function useToasty() {
+  const toast = useContext(toastValue);
 
-    const toast = useContext(toastValue);
-
-    return { ...toast }
-
+  return { ...toast };
 }
-
-
