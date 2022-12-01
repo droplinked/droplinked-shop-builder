8abcd83af9fe6ef336f2f528bfc8a7b09a38e86f
bedi.mns@gmail.com <bedi.mns@gmail.com>
2022-12-02 01:22:45 +0330

    add property to form input share

--- src/components/shared/FormInput/FormInput.jsx
+++ src/components/shared/FormInput/FormInput.jsx
@@ -4,70 +4,71 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
-  Textarea
-} from '@chakra-ui/react'
+  Textarea,
+} from "@chakra-ui/react";
 
-export default function FormInput({ value, changeValue, label, type, placeholder, isError, ...otherProps }) {
-
-
+export default function FormInput({
+  value,
+  changeValue,
+  label,
+  type,
+  placeholder,
+  isError,
+  gray,
+  ...otherProps
+}) {
   return (
-    <FormControl {...otherProps} >
-      <FormLabel
-        htmlFor='input-com'
-        fontWeight='600'
-        fontSize={{ base: '14px', md: '20px' }}
-        color='white'
-      >{label}</FormLabel>
-      {(type == "textarea")
-        ?
-        <Textarea
-          id='input-com'
-          type={(type) ? type : "text"}
-          value={value}
-          onChange={changeValue}
-          fontWeight='600'
-          fontSize={{ base: '14px', md: '20px' }}
-          color='white'
-          border='none'
-          bgColor='subLayer'
-        //  borderColor='#b3b3b3'
-          borderRadius='8px'
-          px="16px"
-          py={{ base: "8px", md: "12px" }}
-          outline='none'
-          _focus={{ outline:'none'}}
-          h='auto'
-          placeholder={placeholder}
-        />
-        :
-        <Input
-          id='input-com'
-          type={(type) ? type : "text"}
-          value={value}
-          onChange={changeValue}
-          fontWeight='600'
-          fontSize={{ base: '14px', md: '20px' }}
-          color='#DBDBDB'
-          bgColor='subLayer'
-          border='none'
-        //  border='2px'
-         // borderColor='#b3b3b3'
-          borderRadius='8px'
-          px="16px"
-          py={{ base: "8px", md: "12px" }}
-          outline='none'
-          _focus={{ outline:'none' }}
-          h='auto'
-          placeholder={placeholder}
-        />
-      }
-      {isError && (
-        <FormHelperText
-          color='red'
+    <FormControl {...otherProps}>
+      {label && (
+        <FormLabel
+          htmlFor="input-com"
+          fontWeight="600"
+          fontSize={{ base: "14px", md: "20px" }}
+          color="white"
         >
-          {isError}
-        </FormHelperText>
+          {label}
+        </FormLabel>
       )}
+      {type == "textarea" ? (
+        <Textarea
+          id="input-com"
+          type={type ? type : "text"}
+          value={value}
+          onChange={changeValue}
+          fontWeight="600"
+          fontSize={{ base: "14px", md: "20px" }}
+          color="white"
+          border="none"
+          bgColor={(gray)?"mainLayer":"subLayer"}
+          borderRadius="8px"
+          px="16px"
+          py={{ base: "8px", md: "12px" }}
+          outline="none"
+          _focus={{ outline: "none" }}
+          h="auto"
+          placeholder={placeholder}
+        />
+      ) : (
+        <Input
+          id="input-com"
+          type={type ? type : "text"}
+          value={value}
+          onChange={changeValue}
+          fontWeight="600"
+          fontSize={{ base: "14px", md: "20px" }}
+          color="#DBDBDB"
+          bgColor={(gray)?"mainLayer":"subLayer"}
+          border="none"
+          borderRadius="8px"
+          px="16px"
+          py={{ base: "8px", md: "12px" }}
+          outline="none"
+          _focus={{ outline: "none" }}
+          h="auto"
+          placeholder={placeholder}
+        />
+      )}
+      {isError && <FormHelperText color="red">{isError}</FormHelperText>}
     </FormControl>
-  )
+  );
 }
