4556b8fc72696f2cfaba1a06c697929e9c9043c7
bedi.mns@gmail.com <bedi.mns@gmail.com>
2022-12-27 16:49:44 +0330

    fix show variant in order modal

--- src/components/Modal/Order/Order-merch-component.jsx
+++ src/components/Modal/Order/Order-merch-component.jsx
@@ -12,12 +12,18 @@ export default function OrderMerch({ item ,type}) {
     const imageUrl = (type != SHOP_TYPES.SHOPIFY)? item.product.media[0].url : item.image_url
 
     const merchPrice = (type != SHOP_TYPES.SHOPIFY)? item.sku.price : item.price
-    // get variant name and value in set in string
-    // let variantText = ' '
-    // item.sku.options.forEach(option => {
-    //     let variantName = (option.variantID == "62a989ab1f2c2bbc5b1e7153" ? "Size" : "Color")
-    //     variantText += `${variantName}: ${option.value}\xa0\xa0\xa0\xa0\xa0\xa0`
-    // })
+
+
+    const getVariantText = () => {
+        if(item.sku.options.length == 0) return ''
+        let variantText = ' '
+
+        item.sku.options.forEach(option => {
+                 let variantName = (option.variantID == "62a989ab1f2c2bbc5b1e7153" ? "Size" : "Color")
+                 variantText += `${variantName}: ${option.value}\xa0\xa0\xa0\xa0\xa0\xa0`
+             })
+             return variantText
+    }
 
   //  const Totalprice = parseFloat(item.quantity * item.sku.price)
 
@@ -58,7 +64,7 @@ export default function OrderMerch({ item ,type}) {
                     justifyContent='space-between'
                 >
 
-                    {/* <Text style={textStyel} w='55%' fontSize={{base:'8px' , sm: "10px", md: "14px" }} >{variantText}</Text> */}
+                     <Text style={textStyel} w='55%' fontSize={{base:'8px' , sm: "10px", md: "14px" }} >{getVariantText()}</Text> 
                     <Text style={textStyel} w='30%' fontSize={{base:'8px' , sm: "10px", md: "14px" }} > Quantity: {item.quantity}</Text>
                     <Text style={textStyel} textAlign='end' w='15%' fontSize={{base:'8px' , sm: "10px", md: "14px" }} >${merchPrice}</Text>
                 </Flex>
