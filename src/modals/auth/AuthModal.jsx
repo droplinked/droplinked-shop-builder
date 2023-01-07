5c975b0db4bebfb28872a59d298ab65b4052985d
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-01-08 01:04:37 +0330

    add reset modal to auth modal

--- src/modals/auth/AuthModal.jsx
+++ src/modals/auth/AuthModal.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 
 import LoginModal from "../login/LoginModal";
 import SignupModal from "../signup/SignupModal";
+import ResetPassModal from "../resetpass/ResetPassModal";
 
 const MODAL_TYPE = {
   SIGNIN: "SIGNIN",
   SIGNUP: "SIGNUP",
+  RESET: "RESET",
 };
 
-const AuthModal = ({show , close}) => {
+const AuthModal = ({ show, close }) => {
   const [modalType, setModalType] = useState(MODAL_TYPE.SIGNIN);
 
   const switchModal = () =>
@@ -16,15 +18,29 @@ const AuthModal = ({show , close}) => {
       modalType == MODAL_TYPE.SIGNIN ? MODAL_TYPE.SIGNUP : MODAL_TYPE.SIGNIN
     );
 
-  return (
-    <>
-      {modalType == MODAL_TYPE.SIGNIN ? (
-        <LoginModal show={show} close={close} switchModal={switchModal} />
-      ) : (
-        <SignupModal show={show} close={close} switchModal={switchModal} />
-      )}
-    </>
-  );
+    const switchResetModal = () =>
+    setModalType(
+      modalType == MODAL_TYPE.SIGNIN ? MODAL_TYPE.RESET : MODAL_TYPE.SIGNIN
+    );
+
+  const correctComponent = () => {
+    switch (modalType) {
+      case MODAL_TYPE.SIGNIN:
+        return (
+          <LoginModal show={show} close={close} switchModal={switchModal} switchReset={switchResetModal}/>
+        );
+      case MODAL_TYPE.SIGNUP:
+        return (
+          <SignupModal show={show} close={close} switchModal={switchModal}  />
+        );
+      case MODAL_TYPE.RESET:
+        return (
+          <ResetPassModal show={show} close={close} switchReset={switchResetModal} />
+        );
+    }
+  };
+
+  return <>{correctComponent()}</>;
 };
 
 export default AuthModal;
