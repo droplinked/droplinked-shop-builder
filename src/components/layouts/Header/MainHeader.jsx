2380eb38e18a7dd5594a2d6e89e49d41c00cfc41
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-01-10 21:08:32 +0330

    comment last header component

--- src/components/layouts/Header/MainHeader.jsx
+++ src/components/layouts/Header/MainHeader.jsx
@@ -1,58 +1,58 @@
-import { useNavigate } from "react-router-dom";
-import { useProfile } from "../../../context/profile/ProfileContext";
-import { Flex } from "@chakra-ui/react";
-import { useState } from "react";
-import { HeaderWrapper, HeaderTitle, BurgerIcon } from "./MainHeader-style";
-import { useSideBar } from "../../../context/sidebar/sidebar-context";
+// import { useNavigate } from "react-router-dom";
+// import { useProfile } from "../../../context/profile/ProfileContext";
+// import { Flex } from "@chakra-ui/react";
+// import { useState } from "react";
+// import { HeaderWrapper, HeaderTitle, BurgerIcon } from "./MainHeader-style";
+// import { useSideBar } from "../../../context/sidebar/sidebar-context";
 
-import DefaulHeader from "./components/default-header/default-header";
-import CustomerHeader from "./components/customer-header/customer-header";
-import EmailModal from "../../../modals/email/EmailModal";
-import ProducerHeader from "./components/producer-header/producer-header";
-import burger from "../../../assest/icon/test-burger-icon.svg";
+// import DefaulHeader from "./components/default-header/default-header";
+// import CustomerHeader from "./components/customer-header/customer-header";
+// import EmailModal from "../../../modals/email/EmailModal";
+// import ProducerHeader from "./components/producer-header/producer-header";
+// import burger from "../../../assest/icon/test-burger-icon.svg";
 
-function MainHeader() {
+// function MainHeader() {
   
-  const [showEmailModal, setEmailModal] = useState(false);
+//   const [showEmailModal, setEmailModal] = useState(false);
 
-  const { profile, isCustomer, isRegisteredProducer } = useProfile();
-  const { showSideBar, toggleSideBar } = useSideBar();
-  const navigate = useNavigate();
+//   const { profile, isCustomer, isRegisteredProducer } = useProfile();
+//   const { showSideBar, toggleSideBar } = useSideBar();
+//   const navigate = useNavigate();
 
 
-  const toggleEmailModal = () =>  setEmailModal(p => !p);
+//   const toggleEmailModal = () =>  setEmailModal(p => !p);
 
-  const navigateToLandingPage = () => navigate("/");
-  // show droplinked logo in leftside and condition for right side
-  // if have any profile show(UserHeader component)
-  // if havent any profile show default component
+//   const navigateToLandingPage = () => navigate("/");
+//   // show droplinked logo in leftside and condition for right side
+//   // if have any profile show(UserHeader component)
+//   // if havent any profile show default component
 
-  return (
-    <HeaderWrapper>
-      <Flex w="auto" alignItems="center">
-        {isRegisteredProducer() && (
-          <BurgerIcon
-            src={burger}
-            onClick={toggleSideBar}
-            transform={showSideBar ? "rotate(90deg)" : "rotate(0deg)"}
-            transition="1s"
-            // transform: rotate(45deg);
-          />
-        )}
-        <HeaderTitle onClick={navigateToLandingPage}>droplinked</HeaderTitle>
-      </Flex>
-      <Flex h="100%">
-        {profile ? (
-          <Flex h="100%" alignItems="center">
-            {isCustomer() ? <CustomerHeader /> : <ProducerHeader />}
-          </Flex>
-        ) : (
-          <DefaulHeader />
-        )}
-        <EmailModal show={showEmailModal} close={toggleEmailModal} />
-      </Flex>
-    </HeaderWrapper>
-  );
-}
+//   return (
+//     <HeaderWrapper>
+//       <Flex w="auto" alignItems="center">
+//         {isRegisteredProducer() && (
+//           <BurgerIcon
+//             src={burger}
+//             onClick={toggleSideBar}
+//             transform={showSideBar ? "rotate(90deg)" : "rotate(0deg)"}
+//             transition="1s"
+//             // transform: rotate(45deg);
+//           />
+//         )}
+//         <HeaderTitle onClick={navigateToLandingPage}>droplinked</HeaderTitle>
+//       </Flex>
+//       <Flex h="100%">
+//         {profile ? (
+//           <Flex h="100%" alignItems="center">
+//             {isCustomer() ? <CustomerHeader /> : <ProducerHeader />}
+//           </Flex>
+//         ) : (
+//           <DefaulHeader />
+//         )}
+//         <EmailModal show={showEmailModal} close={toggleEmailModal} />
+//       </Flex>
+//     </HeaderWrapper>
+//   );
+// }
 
-export default MainHeader;
+// export default MainHeader;
--- src/pages/publics/loading/Loading-page.jsx
+++ src/pages/publics/loading/Loading-page.jsx
@@ -1,13 +1,13 @@
 import { Flex, Box } from "@chakra-ui/react"
 
 import Loading from "../../../components/shared/loading/Loading"
-import MainHeader from "../../../components/layouts/Header/MainHeader";
+import Header from "../../../layouts/header/Header"
 import Footer from "../../../components/layouts/Footer/Footer";
 
 const LoadingPage = () => {
     return (
         <Box w='100%'  bgColor="subLayer" >
-            <MainHeader />
+            <Header />
             <Flex bgColor="subLayer" w='100%' h='100vh' justifyContent='center' alignItems='center'
                 overflow='hidden'
             >
