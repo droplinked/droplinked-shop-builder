118c6c531a01692b7903e58687e1f23e8196e9cb
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-01-14 23:55:01 +0330

    delete extra file

--- src/index.js
+++ src/index.js
@@ -8,36 +8,21 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import { Provider } from "react-redux";
 import { store } from './store/store';
-
 import { ChakraProvider } from "@chakra-ui/react";
-//import { WalletProvider } from "./context/wallet/WalletContext";
 import { theme } from "./theme";
+
 import ToastifyProvider from "./context/toastify/ToastContext";
 import CartProvider from "./context/cart/CartContext";
-//import ProfileProvider from "./context/profile/ProfileContext";
-//import AddressProvider from "./context/address/AddressContext";
-//import NotificationProvider from "./context/notifications/NotificationsContext";
-//import ShopProvider from "./context/shop/ShopContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ToastifyProvider>
       <Provider store={store}>
-        {/* <ProfileProvider> */}
-          {/* <WalletProvider> */}
             <CartProvider>
               <ChakraProvider theme={theme}>
-                {/* <AddressProvider> */}
-                  {/* <NotificationProvider> */}
-                    {/* <ShopProvider> */}
                       <App />
-                    {/* </ShopProvider> */}
-                  {/* </NotificationProvider> */}
-                {/* </AddressProvider> */}
               </ChakraProvider>
             </CartProvider>
-          {/* </WalletProvider> */}
-        {/* </ProfileProvider> */}
       </Provider>
     </ToastifyProvider>
   </React.StrictMode>,
