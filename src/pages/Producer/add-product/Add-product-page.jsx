1bc7e6c608dedc2ad0458c66254ae024240e85f5
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-02-11 19:40:31 +0330

    delete extra file

--- src/pages/Producer/add-product/Add-product-page.jsx
+++ src/pages/Producer/add-product/Add-product-page.jsx
@@ -1,25 +1,4 @@
-// import BasicButton from "../../../components/shared/BasicButton/BasicButton";
-// import ProductInformation from "../components/product-information-component";
-// import OptionCheckboxes from "./option-checkbox-component/option-checkbox";
-// import SkusComponent from "./skus-component/Skus-component";
-// import AddSkuModal from "../../../modals/add-sku/AddSkuModal";
-
-// import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { getVariants, postProduct } from "../../../api/producer/Product-api";
-// import { useToasty } from "../../../context/toastify/ToastContext";
-// import {
-//   ModalContainerWrapper,
-//   TitleText,
-//   TypeSelect,
-//   LableInput,
-//   InputComponent,
-// } from "./Add-product-style";
-// import { Flex, Box } from "@chakra-ui/react";
-// import { SHIPING_TYPES } from "./shippings-type";
-// import { API_STATUS } from "../../../constant/api-status";
 import { useReducer, useState } from "react";
-import { Box } from "@chakra-ui/react";
 
 import { SHIPING_TYPES } from "../../../constant/shipping-types";
 import { productIntroReducer } from "./reducer/product-intro-reducer";
