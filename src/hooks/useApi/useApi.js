4112b2f707371489720d256cb56fe892fb8b4d20
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-07 16:05:28 +0330

    change baseurl

--- src/hooks/useApi/useApi.js
+++ src/hooks/useApi/useApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //import { BASE_URL } from "./baseUrl";
 import { useToasty } from "../../context/toastify/ToastContext";
 
-const BASE_URL = `${process.env.REACT_APP_BASE_API_URL}`;
+const BASE_URL = "https://apiv2dev.droplinked.com";
 
 export function useApi() {
   const { errorToast } = useToasty();
