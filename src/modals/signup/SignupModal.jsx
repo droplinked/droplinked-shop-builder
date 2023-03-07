1524aa95467d7c0b3d11ac94248342b47275317b
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-07 19:48:50 +0330

    delete extra code

--- src/modals/signup/SignupModal.jsx
+++ src/modals/signup/SignupModal.jsx
@@ -1,19 +1,30 @@
 import ModalWrapper from "../modal-wrapper/ModalWrapper";
 import SignupProducer from "./signup-producer/SignupProducer";
-import SignupCustomer from "./signup-customer/SignupCustomer";
 
 import { Box } from "@chakra-ui/react";
 import { Title } from "./SignupModal-style";
-import { useParams } from "react-router-dom";
+
 
 const SignupModal = ({ show, close, switchModal, shopName }) => {
-  const params = useParams();
 
   return (
     <ModalWrapper close={close} show={show}>
       <Box w="100%" h="100%">
         <Title>Create a free account</Title>
-        {params.shopName != undefined ? (
+        <SignupProducer
+          close={close}
+          shopname={shopName}
+          switchToggle={switchModal}
+        />
+      </Box>
+    </ModalWrapper>
+  );
+};
+
+export default SignupModal;
+
+{
+  /* {params.shopName != undefined ? (
           <SignupCustomer close={close} switchToggle={switchModal} />
         ) : (
           <SignupProducer
@@ -21,10 +32,5 @@ const SignupModal = ({ show, close, switchModal, shopName }) => {
             shopname={shopName}
             switchToggle={switchModal}
           />
-        )}
-      </Box>
-    </ModalWrapper>
-  );
-};
-
-export default SignupModal;
+        )} */
+}
