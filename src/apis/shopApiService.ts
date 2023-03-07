20077304ff32c2250a3818592a4a376929a5ea20
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-07 19:36:06 +0330

    implement shopApiService

---
+++ src/apis/shopApiService.ts
@@ -0,0 +1,34 @@
+export const putUpdateShop = (body: any) => {
+  const token = JSON.parse(localStorage.getItem("token") || "");
+  let apiObj = {
+    url: `shop`,
+    body: body,
+    token: token,
+  };
+  return { ...apiObj };
+};
+
+export const getIsShopExist = (shopName: string) => {
+  const token = JSON.parse(localStorage.getItem("token") || "");
+  let apiObj = {
+    url: `shop/${shopName}`,
+    token: token,
+  };
+  return { ...apiObj };
+};
+
+export const getShopPublic = (shopName: string) => {
+  let apiObj = {
+    url: `shop/public/${shopName}`,
+  };
+  return { ...apiObj };
+};
+
+export const getShopInfo = (shopName: string) => {
+  const token = JSON.parse(localStorage.getItem("token") || "");
+  let apiObj = {
+    url: `shop/public/${shopName}`,
+    token: token,
+  };
+  return { ...apiObj };
+};
