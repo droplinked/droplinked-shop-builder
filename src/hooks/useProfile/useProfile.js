5e7835fb0378dd6458b38fb4cb8f5d0f3598429b
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-09 14:44:57 +0330

    add in useProfile hook

--- src/hooks/useProfile/useProfile.js
+++ src/hooks/useProfile/useProfile.js
@@ -3,13 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { selectCurrentProfile } from "../../store/profile/profile.selector";
 import { selectCurrentShop } from "../../store/shop/shop.selector";
+import { setCurrentShop } from "../../store/shop/shop.action";
+
 //this hook have been used for handle shop and user data
 export function useProfile() {
+  const dispatch = useDispatch();
+
   const profile = useSelector(selectCurrentProfile);
   const shop = useSelector(selectCurrentShop);
+
+  const setShopData = (data) => {
+    dispatch(setCurrentShop(data));
+  };
 
   return {
     profile,
     shop,
+    setShopData
   };
 }
