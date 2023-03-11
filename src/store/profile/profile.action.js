2017e6fe3afdd7fb19324c14d857e4be5237002b
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-11 17:56:04 +0330

    fix profile action in redux

--- src/store/profile/profile.action.js
+++ src/store/profile/profile.action.js
@@ -7,9 +7,9 @@ export const setCurrentUser = (data) => {
   let time = new Date().getTime();
   localStorage.setItem("login-time", JSON.stringify(time));
   localStorage.setItem("token", JSON.stringify(data.access_token));
-  localStorage.setItem("profile", JSON.stringify(data.user));
+  localStorage.setItem("profile", JSON.stringify(data.others));
 
-  return createAction(Profile_ACTION_TYPES.SET_CURRENT_PROFILE, data.user);
+  return createAction(Profile_ACTION_TYPES.SET_CURRENT_PROFILE, data.others);
 };
 
 export const logoutUser = () => {
