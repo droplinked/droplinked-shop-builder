b12c4c33ac0725b4e363b86179633159e55b14df
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-11 18:14:52 +0330

    add border for sizing components

--- src/pages/admin-wrapper/AdminWrapper.jsx
+++ src/pages/admin-wrapper/AdminWrapper.jsx
@@ -4,9 +4,12 @@ import Sidebar from "../../layouts/sidebar/Sidebar";
 
 const AdminWrapper = () => {
   return (
-    <Flex w="100%" border="3px solid red" minH="100%" h="100vh">
-      <Box w="72px" border="2px solid blue" h="100%">
+    <Flex w="100%" border="3px solid red" minH="100%" h="100%">
+      <Box w="72px" border="2px solid blue" h="100%" pt='24px'>
         <Sidebar />
+      </Box>
+      <Box w="100%" border="4px solid pink" h="100%" pt='24px'>
+
       </Box>
     </Flex>
   );
