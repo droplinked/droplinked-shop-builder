3a4f60e3c407b25ff620cbba6aff15da0dec53e2
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-13 23:23:29 +0330

    increase h of side

--- src/pages/admin-wrapper/AdminWrapper.jsx
+++ src/pages/admin-wrapper/AdminWrapper.jsx
@@ -6,7 +6,7 @@ import Sidebar from "../../layouts/sidebar/Sidebar";
 const AdminWrapper = () => {
   return (
     <Flex w="100%"  height='100%' minHeight='100%' >
-      <Box w="72px" borderRight='1px solid' borderColor='line' height='100%' minHeight='100%' pt='24px'>
+      <Box w="72px" borderRight='1px solid' borderColor='line' maxHeight='auto'  h='100%' minH="100vh" pt='24px'>
         <Sidebar />
       </Box>
       <Box w="100%" h="100%" p='24px'>
