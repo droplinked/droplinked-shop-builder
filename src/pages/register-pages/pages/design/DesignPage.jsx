0402d4b6cde55a9b07006c9d41aca9fdc5245c13
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-14 20:05:46 +0330

    adsfasddf

--- src/pages/register-pages/pages/design/DesignPage.jsx
+++ src/pages/register-pages/pages/design/DesignPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex , Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import { useState } from "react";
 
 import {
@@ -6,19 +6,19 @@ import {
   PageInformationComponent,
   PageContentWrapper,
   SaveButton,
-  Text18px
+  Text18px,
 } from "../../RegisterPages-style";
 import { MainThemeImage } from "./DesignPage-style";
 
 import InputImage from "./components/input-image/InputImage";
+import InputComponent from "../../component/input-component/InputComponent";
 
-import theme1Image from "./theme-1.jpg"
-import theme2Image from "./theme-2.jpg"
-import theme3Image from "./theme-3.jpg"
+import theme1Image from "./theme-1.jpg";
+import theme2Image from "./theme-2.jpg";
+import theme3Image from "./theme-3.jpg";
 
 const DesignPage = () => {
-
-    const images = [theme1Image , theme2Image ,theme3Image ]
+  const images = [theme1Image, theme2Image, theme3Image];
   return (
     <PageContent>
       <PageInformationComponent>
@@ -30,16 +30,54 @@ const DesignPage = () => {
         <Box mb="48px" />
         <MainThemeImage src={theme3Image} />
         <Box mb="48px" />
-        <Flex w='100%' maxW='100%' overflow='hidden' gap='24px' justifyContent='space-between' alignItems='center' >
-            {images.map((currentImage)=>{
-                return <Image src={currentImage} w='30%' h='154px' borderRadius='8px' cursor='pointer' />
-            })}
+        <Flex
+          w="100%"
+          maxW="100%"
+          overflow="hidden"
+          gap="24px"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          {images.map((currentImage) => {
+            return (
+              <Image
+                src={currentImage}
+                w="30%"
+                h="154px"
+                borderRadius="8px"
+                cursor="pointer"
+              />
+            );
+          })}
         </Flex>
         <Box mb="104px" />
-
         <Text18px>Details</Text18px>
         <Box mb="48px" />
-        <InputImage label='Logo' placeHolder='This image will display on the left side of the store page.' setImage={(e)=>{console.log('e ' ,e)}} />
+        <InputImage
+          label="Logo"
+          placeHolder="This image will display on the left side of the store page."
+          setImage={(e) => {
+            console.log("e ", e);
+          }}
+        />
+        <Box mb="48px" />
+        <InputImage
+          label="Header logo"
+          placeHolder="This image will display at the upper left corner of the store page."
+          setImage={(e) => {
+            console.log("e ", e);
+          }}
+        />
+        <Box mb="48px" />
+        <InputComponent label='Header title' placeHolder='Write a catchy title for the header' isRequired={true} />
+        <Box mb="48px" />
+        <InputImage
+          label="Header banner"
+          placeHolder="This image will display at the top of the store page."
+          setImage={(e) => {
+            console.log("e ", e);
+          }}
+        />
       </PageContentWrapper>
     </PageContent>
   );
