7401c74ef24d91383e1bb58e514d79b697156fe7
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-14 19:04:49 +0330

    build page structure

---
+++ src/pages/register-pages/pages/design/DesignPage-style.jsx
@@ -0,0 +1,10 @@
+import { chakra } from "@chakra-ui/react";
+
+export const MainThemeImage = chakra("img", {
+  baseStyle: {
+    w:'100%' ,
+    h:'auto',
+    borderRadius:'12px',
+    overflow:'hidden'
+  },
+});
--- src/pages/register-pages/pages/design/DesignPage.jsx
+++ src/pages/register-pages/pages/design/DesignPage.jsx
@@ -1,7 +1,46 @@
+import { Box, Flex , Image } from "@chakra-ui/react";
+import { useState } from "react";
 
+import {
+  PageContent,
+  PageInformationComponent,
+  PageContentWrapper,
+  SaveButton,
+  Text18px
+} from "../../RegisterPages-style";
+import { MainThemeImage } from "./DesignPage-style";
+
+import theme1Image from "./theme-1.jpg"
+import theme2Image from "./theme-2.jpg"
+import theme3Image from "./theme-3.jpg"
 
 const DesignPage = () => {
-    return <><h1 style={{color:'white'}} >design</h1></>
-}
 
-export default DesignPage
+    const images = [theme1Image , theme2Image ,theme3Image ]
+  return (
+    <PageContent>
+      <PageInformationComponent>
+        Pick an editable template and customize it with your brand logo, title,
+        header banner, etc.
+      </PageInformationComponent>
+      <PageContentWrapper>
+        <Text18px>Templates</Text18px>
+        <Box mb="48px" />
+        <MainThemeImage src={theme3Image} />
+        <Box mb="48px" />
+        <Flex w='100%' maxW='100%' overflow='hidden' gap='24px' justifyContent='space-between' alignItems='center' >
+            {images.map((currentImage)=>{
+                return <Image src={currentImage} w='30%' h='154px' borderRadius='8px' cursor='pointer' />
+            })}
+        </Flex>
+        <Box mb="104px" />
+
+        <Text18px>Details</Text18px>
+        <Box mb="48px" />
+        
+      </PageContentWrapper>
+    </PageContent>
+  );
+};
+
+export default DesignPage;
