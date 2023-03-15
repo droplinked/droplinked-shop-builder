bfac3bf25e94571e6b755e442d64884bb8fad7a7
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-15 18:40:01 +0330

    AddProductComponent

--- src/pages/products/ProductsPage.jsx
+++ src/pages/products/ProductsPage.jsx
@@ -20,6 +20,7 @@ import { useProfile } from "../../hooks/useProfile/useProfile";
 import { PageWrapper } from "./ProductsPage-style";
 
 import PageHeader from "./components/page-header/PageHeader";
+import AddProductComponent from "./components/add-product-component/AddProductComponent";
 
 import plusIcon from "../../assest/icon/plus-icon.svg";
 import addProductIcon from "../../assest/icon/add-item-green-icon.svg";
@@ -57,102 +58,7 @@ const ProductsPage = () => {
             </Thead>
           </Table>
         </TableContainer>
-        <Flex
-          w="100%"
-          p="36px 60px"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Flex
-            w="169px"
-            h="148px"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            bg="subLayer"
-            borderRadius="8px"
-            cursor="pointer"
-            onClick={navigateToAddProductPage}
-          >
-            <Image src={plusIcon} h="48px" w="48px" />
-            <Box mb="24px" />
-            <Text color="#fff" fontWeight="500" fontSize="18px">
-              Add Product
-            </Text>
-          </Flex>
-          <Box mb="50px" />
-          <Flex w="auto" flexDir="column" alignItems="start">
-            <Flex
-              w="auto"
-              alignItems="center"
-              justifyContent="center"
-              gap="24px"
-              mb="12px"
-            >
-              <Image src={addProductIcon} w="18px" h="18px" />
-              <Text
-                fontFamily="Avenir Next"
-                fontWeight="400"
-                fontSize="14px"
-                color="#C2C2C2"
-              >
-                Interoduce your product
-              </Text>
-            </Flex>
-            <Flex
-              w="auto"
-              alignItems="center"
-              justifyContent="center"
-              gap="24px"
-              mb="12px"
-            >
-              <Image src={variantsIcon} w="18px" h="18px" />
-              <Text
-                fontFamily="Avenir Next"
-                fontWeight="400"
-                fontSize="14px"
-                color="#C2C2C2"
-              >
-                Set your invetory variants and quantities
-              </Text>
-            </Flex>
-            <Flex
-              w="auto"
-              alignItems="center"
-              justifyContent="center"
-              gap="24px"
-              mb="12px"
-            >
-              <Image src={collectionIcon} w="18px" h="18px" />
-              <Text
-                fontFamily="Avenir Next"
-                fontWeight="400"
-                fontSize="14px"
-                color="#C2C2C2"
-              >
-                Set collection
-              </Text>
-            </Flex>
-            <Flex
-              w="auto"
-              alignItems="center"
-              justifyContent="center"
-              gap="24px"
-              mb="12px"
-            >
-              <Image src={ruleIcon} w="18px" h="18px" />
-              <Text
-                fontFamily="Avenir Next"
-                fontWeight="400"
-                fontSize="14px"
-                color="#C2C2C2"
-              >
-                Set Crypto based rules for your purchases
-              </Text>
-            </Flex>
-          </Flex>
-        </Flex>
+       <AddProductComponent />
       </PageWrapper>
     </Box>
   );
