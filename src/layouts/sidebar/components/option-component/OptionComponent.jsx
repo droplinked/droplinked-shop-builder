e0e209e4ed41f7cede48b8d112fd2bb6d4af4343
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-16 16:40:48 +0330

    add condition for onClick

--- src/layouts/sidebar/components/option-component/OptionComponent.jsx
+++ src/layouts/sidebar/components/option-component/OptionComponent.jsx
@@ -1,21 +1,29 @@
-import { Tooltip } from '@chakra-ui/react'
+import { Tooltip } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-import { useProfile } from '../../../../hooks/useProfile/useProfile';
+import { useProfile } from "../../../../hooks/useProfile/useProfile";
 import { IconComponent, IconWrapper } from "../../Sidebar-style";
 
-const OptionComponent = ({ icon , label , path}) => {
-
+const OptionComponent = ({ icon, label, path }) => {
   const navigate = useNavigate();
   const { shop } = useProfile();
 
-  const clickOnIcon = () => navigate(`/${shop.name}/${path}`);
+  const clickOnIcon = () => {
+    if (path) {
+      navigate(`/${shop.name}/${path}`);
+    }
+  };
 
   return (
-    <Tooltip label={label}  placement='right-start' borderRadius='8px' bg='mainLayer' >
-    <IconWrapper onClick={clickOnIcon}>
-      <IconComponent src={icon} />
-    </IconWrapper>
+    <Tooltip
+      label={label}
+      placement="right-start"
+      borderRadius="8px"
+      bg="mainLayer"
+    >
+      <IconWrapper onClick={clickOnIcon}>
+        <IconComponent src={icon} />
+      </IconWrapper>
     </Tooltip>
   );
 };
