bd59738c7cfc48b572e242310a3bdcd694394354
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-17 22:19:22 +0330

    admin wrapper

--- src/pages/admin-wrapper/AdminWrapper.jsx
+++ src/pages/admin-wrapper/AdminWrapper.jsx
@@ -1,16 +1,34 @@
 import { Flex, Box } from "@chakra-ui/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+import { useProfile } from "../../hooks/useProfile/useProfile";
 
 import Sidebar from "../../layouts/sidebar/Sidebar";
 
 const AdminWrapper = () => {
+  const { profile } = useProfile();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!profile) navigate("/");
+  }, []);
+
   return (
-    <Flex w="100%"  height='100%' minHeight='100%' >
-      <Box w="72px" borderRight='1px solid' borderColor='line' maxHeight='auto'  h='100%' minH="100vh" pt='24px'>
+    <Flex w="100%" height="100%" minHeight="100%">
+      <Box
+        w="72px"
+        borderRight="1px solid"
+        borderColor="line"
+        maxHeight="auto"
+        h="100%"
+        minH="100vh"
+        pt="24px"
+      >
         <Sidebar />
       </Box>
-      <Box w="100%" h="100%" p='24px'>
-      <Outlet />
+      <Box w="100%" h="100%" p="24px">
+        <Outlet />
       </Box>
     </Flex>
   );
