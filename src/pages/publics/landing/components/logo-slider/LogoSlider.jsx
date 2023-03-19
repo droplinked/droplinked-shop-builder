01df9339fe6cfe324445489f5efe12214be4186f
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-20 01:59:42 +0330

    add nead logo

--- src/pages/publics/landing/components/logo-slider/LogoSlider.jsx
+++ src/pages/publics/landing/components/logo-slider/LogoSlider.jsx
@@ -5,6 +5,7 @@ import logo1 from "./casper-logo.svg"
 import logo2 from "./stacks-logo.svg"
 import logo3 from "./polygon-logo.svg"
 import logo4 from "./skace.logo.svg"
+import logo5 from "./near-logo.svg"
 //Frame 20774
 const LogoSlider = () => {
 
@@ -13,22 +14,22 @@ const LogoSlider = () => {
         logo2,
         logo3,
         logo4 ,
+        logo5,
         logo1,
         logo2,
         logo3,
         logo4 ,
+        logo5,
         logo1,
         logo2,
         logo3,
         logo4 ,
+        logo5,
         logo1,
         logo2,
         logo3,
         logo4 ,
-        logo1,
-        logo2,
-        logo3,
-        logo4 ,
+        logo5,
       ];
     
       return (
