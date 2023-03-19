920746eb23cd829c1cd61cbabe5e96ee3695ac3e
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-20 03:17:50 +0330

    add sidebar to shop again

--- src/pages/user-wrapper/user-wrapper.jsx
+++ src/pages/user-wrapper/user-wrapper.jsx
@@ -1,17 +1,31 @@
-import { Outlet } from "react-router-dom";
-import {
-  UserWrapperPage,
-  PageContainer,
-} from "./user-wrapper-style";
+import { Outlet, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import Sidebar from "../../layouts/sidebar/Sidebar"
+import { UserWrapperPage, PageContainer } from "./user-wrapper-style";
+import { useApi } from "../../hooks/useApi/useApi";
+import { getShopPublic } from "../../apis/shopApiService";
+
+//import Sidebar from "../../layouts/sidebar/Sidebar"
+import ShopInformationSidebar from "../../layouts/sidebar/components/shop-information-side/ShopInformationSidebar";
 
 const UserWrapper = () => {
+  const [shopData, setShop] = useState(null);
 
+  const { getApi } = useApi();
+  let { shopname } = useParams();
+
+  const getShopData = async () => {
+    const result = await getApi(getShopPublic(shopname));
+    if (result) setShop(result);
+  };
+
+  useEffect(() => {
+    getShopData();
+  }, []);
 
   return (
     <UserWrapperPage>
-        {/* <Sidebar /> */}
+      {shopData && <ShopInformationSidebar ShopData={shopData} />}
       <PageContainer>
         <Outlet />
       </PageContainer>
