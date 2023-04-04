aa322b9667955927d725da33a2a5fba61f32d511
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-04-04 22:41:30 +0330

    add InputFieldComponent

---
+++ src/components/shared/input-field-component/InputFieldComponent.jsx
@@ -0,0 +1,37 @@
+import { Input, FormControl, FormLabel, Text } from "@chakra-ui/react";
+
+export default function InputFieldComponent({
+  value,
+  change,
+  label,
+  placeholder,
+  description,
+  ...otherProps
+}) {
+  return (
+    <FormControl {...otherProps}>
+      {label && (
+        <FormLabel htmlFor="input-com" color="white">
+          {label}
+        </FormLabel>
+      )}
+      <Input
+        id="input-com"
+        value={value}
+        onChange={change}
+        color="#DBDBDB"
+        bgColor="subLayer"
+        border="none"
+        borderRadius="8px"
+        _focus={{ outline: "none" }}
+        _placeholder={{ color: "#808080" }}
+        fontSize="14px"
+        size="lg"
+        placeholder={placeholder}
+      />
+      <Text fontSize="14px" color="#808080">
+        {description}
+      </Text>
+    </FormControl>
+  );
+}
