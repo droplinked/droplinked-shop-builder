41d1031550103cac744a789c81528cbbb8535bab
Mehrpouya <pou-ya@outlook.com>
2023-04-15 14:29:05 +0330

    store refactor

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { compose, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reduce';
+import { configureStore } from '@reduxjs/toolkit';
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
@@ -9,4 +10,8 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  enhancers: composedEnhancers
+});
