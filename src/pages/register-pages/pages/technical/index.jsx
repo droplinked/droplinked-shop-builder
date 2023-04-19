b18caf5b7521d78346f3019f39587a7509f545be
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-04-19 18:13:11 +0430

    set development condition for droplinked

--- src/pages/register-pages/pages/technical/index.jsx
+++ src/pages/register-pages/pages/technical/index.jsx
@@ -14,6 +14,7 @@ import { useProfile } from "hooks/useProfile/useProfile";
 import { useApi } from "hooks/useApi/useApi";
 import { putUpdateShop } from "apis/shopApiService";
 import technicalContext, { technicalContextState } from "./context";
+import { appDeveloment } from "utils/app/variable";
 import technicalModel from "./model";
 import Wallet from "./parts/wallet";
 import BasicButton from "components/shared/BasicButton/BasicButton";
@@ -22,7 +23,7 @@ function Technical() {
   const [Technical, setTechnical] = useState(technicalContextState);
 
   const { putApi } = useApi();
-  const { updateShopData , shop  } = useProfile();
+  const { updateShopData, shop } = useProfile();
   const { errorToast, successToast } = useToasty();
   const currentPath = useLocation().pathname;
   const { shopNavigate } = useCustomNavigate();
@@ -31,7 +32,7 @@ function Technical() {
   const updateState = (key, value) => {
     setTechnical((prev) => ({ ...prev, [key]: value }));
   };
-console.log('shop ' , shop);
+  console.log("currentPath ", currentPath);
   // update Technical as state managment
   useEffect(() => {
     setTechnical({
@@ -40,7 +41,6 @@ console.log('shop ' , shop);
   }, [selector]);
 
   const clickSubmit = async () => {
-    
     try {
       if (!validate().data.imsType) {
         throw Error("Required IMS Type");
@@ -59,7 +59,7 @@ console.log('shop ' , shop);
     } catch (error) {
       errorToast(error.message);
     }
- };
+  };
 
   const validate = useCallback(
     () => technicalModel.validate(Technical),
@@ -77,8 +77,12 @@ console.log('shop ' , shop);
         <PageContentWrapper>
           <VStack spacing={10} align="stretch">
             <Ims />
-            <Payments />
-            <Wallet />
+            {appDeveloment && (
+              <>
+                <Payments />
+                <Wallet />
+              </>
+            )}
             <Flex justifyContent={"right"} width={"100%"}>
               <Box>
                 <BasicButton
