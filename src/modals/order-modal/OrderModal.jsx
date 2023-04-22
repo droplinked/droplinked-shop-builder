eb84321f6499997d16a7edd4e690975e9a2f1b0a
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-04-22 22:03:03 +0430

    ruleset details

--- src/modals/order-modal/OrderModal.jsx
+++ src/modals/order-modal/OrderModal.jsx
@@ -21,6 +21,7 @@ import OrderAddress from "./components/OrderAddress";
 import SmallModal from "../../modals/small-modal/SmallModal";
 
 import CustomerInformationComponent from "./components/customer-information-component/customerInformationComponent";
+import OrderDetailComponent from "./components/order-detail-component/OrderDetailComponent";
 
 export default function OrderModal({ updateOrder, order, show, close }) {
   // // this state use for loading stauts
@@ -91,6 +92,8 @@ export default function OrderModal({ updateOrder, order, show, close }) {
       <ModalContent mt="120px" maxW="700px" w="100%" mx="20px" bgColor="#222">
         <ModalBody p="60px 80px" borderRadius="8px">
           <CustomerInformationComponent order={order} />
+          <Box mb='64px' />
+          <OrderDetailComponent order={order} />
         </ModalBody>
         {/* <ModalHeader
           color="white"
