7c33cb899591829e1b1a00c4f31768a0e547a173
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-04-26 14:24:03 +0430

    fix shop name redirect

--- src/pages/public-pages/shop-page/ShopPage.jsx
+++ src/pages/public-pages/shop-page/ShopPage.jsx
@@ -1,20 +1,19 @@
-import {  useParams } from "react-router-dom";
-import { useEffect  } from "react";
-
-
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+import LoadingComponent from "components/shared/loading-component/LoadingComponent"; 
 
 const ShopPage = () => {
-
   let { shopname } = useParams();
 
-
   useEffect(() => {
-    window.location = `https://droplinked.io/${shopname}`
+    window.location = `https://droplinked.io/${shopname}`;
   }, [shopname]);
 
   return (
-    <>
-    </>
+    <Box minH='70vh'>
+      <LoadingComponent />
+    </Box>
   );
 };
 
