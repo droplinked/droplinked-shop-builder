9ff16fde57d18f7e9fff6d5a8fb27f6aea1387e4
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-05-11 16:30:05 +0330

    add data to RequestsList

--- src/pages/affiliate/requests/parts/list/RequestsList.tsx
+++ src/pages/affiliate/requests/parts/list/RequestsList.tsx
@@ -1,48 +1,55 @@
-import { Box, Flex, VStack } from '@chakra-ui/react'
-import { faker } from '@faker-js/faker'
-import AppBadge from 'components/shared/badge/AppBadge'
-import { publisherRequestService } from 'lib/apis/affiliate/shopServices'
-import AffiliateDetailCard from 'pages/affiliate/parts/detail/affiliateDetailCard'
-import React, { useEffect } from 'react'
-import { useMutation } from 'react-query'
+import { Box, Flex, VStack } from "@chakra-ui/react";
+import { faker } from "@faker-js/faker";
+import AppBadge from "components/shared/badge/AppBadge";
+import { publisherRequestService } from "lib/apis/affiliate/shopServices";
+import AffiliateDetailCard from "pages/affiliate/parts/detail/affiliateDetailCard";
+import React, { useEffect } from "react";
+import { useMutation } from "react-query";
 
 function RequestsList() {
-    const { mutate, isLoading, data } = useMutation(() => publisherRequestService())
+  const { mutate, isLoading, data } = useMutation(() =>
+    publisherRequestService()
+  );
 
-    useEffect(() => mutate(), [])
+  useEffect(() => mutate(), []);
 
-    return (
-        <VStack align={"stretch"}>
-            {data?.data?.data ? data?.data?.data.map((el, key) => (
-                <Flex key={key} justifyContent={"space-between"} borderTop="1px solid #262626" padding={"20px 0"}>
-                    <Box>
-                        <AffiliateDetailCard
-                            image={faker.image.image()}
-                            title={faker.commerce.productName()}
-                            decript={faker.company.name()}
-                            options={[
-                                {
-                                    caption: "size",
-                                    value: "xl"
-                                },
-                                {
-                                    caption: "Quantity",
-                                    value: "40/120"
-                                },
-                                {
-                                    caption: "Commision",
-                                    value: "%20"
-                                },
-                            ]}
-                            price="12 ETH"
-                            earning='12 ETH'
-                        />
-                    </Box>
-                    <Box><AppBadge text={"accepted"} /></Box>
-                </Flex>
-            )) : ""}
-        </VStack>
-    )
+  return (
+    <VStack align={"stretch"}>
+      {data?.data?.data
+        ? data?.data?.data.map((el, key) => (
+            <Flex
+              key={key}
+              justifyContent={"space-between"}
+              borderTop="1px solid #262626"
+              padding={"20px 0"}
+            >
+              <Box>
+                <AffiliateDetailCard
+                  image={el?.publisherShop[0].logo}
+                  title={el?.product[0]?.title}
+                  decript={el?.product[0]?.description}
+                  options={[
+                    {
+                      caption: "Quantity",
+                      value: el?.sku[0]?.quantity,
+                    },
+                    {
+                      caption: "Commision",
+                      value: "  -  ",
+                    },
+                  ]}
+                  price={`$${el?.sku[0]?.price}   `}
+                  earning=" - "
+                />
+              </Box>
+              <Box>
+                <AppBadge text={el?.status} />
+              </Box>
+            </Flex>
+          ))
+        : ""}
+    </VStack>
+  );
 }
 
-export default RequestsList
+export default RequestsList;
