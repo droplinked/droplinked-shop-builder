82d66fa5e9bec9879a3bd54ce8cd113121928e62
Mehrpouya <pou-ya@outlook.com>
2023-05-16 18:41:38 +0330

    update

--- src/pages/product/single/parts/modules/variants/parts/table/parts/recordModal/parts/success/recordSuccess.tsx
+++ src/pages/product/single/parts/modules/variants/parts/table/parts/recordModal/parts/success/recordSuccess.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Link, Text, VStack } from '@chakra-ui/react'
 import BasicButton from 'components/shared/BasicButton/BasicButton'
 import React, { useContext } from 'react'
 import recordContext from '../../context'
@@ -9,7 +9,11 @@ function RecordSuccess({ close }) {
     return (
         <VStack align={"stretch"} spacing={3}>
             <Box textAlign={"center"}><Text fontSize={"larger"} margin="12px 0" fontFamily="aven" color={"green.400"}>Sku record successful</Text></Box>
-            <Box><Text color={"#FFF"} whiteSpace={"pre-wrap"} fontSize={"md"} fontFamily="aven" textAlign={"center"}>{hashkey}</Text></Box>
+            <Box>
+                <Link href={`https://testnet.cspr.live/deploy/${hashkey}`} _hover={{ textDecoration: "none" }} target="_blank">
+                    <Text color={"#FFF"} whiteSpace={"pre-wrap"} fontSize={"md"} _hover={{ color: "#2EC99E", textDecoration: "none" }} fontFamily="aven" textAlign={"center"}>{hashkey}</Text>
+                </Link>
+            </Box>
             <Flex paddingTop={6} justifyContent="center"><BasicButton onClick={close}>Close</BasicButton></Flex>
         </VStack>
     )
