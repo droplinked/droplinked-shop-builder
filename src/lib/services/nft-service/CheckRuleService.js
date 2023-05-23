da88a940e1c19773b2f425a193afbc62490c52e8
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-05-23 18:28:45 +0330

    delete extra files

--- src/lib/services/nft-service/CheckRuleService.js
+++
@@ -1,9 +0,0 @@
-
-import { hasToken } from "../wallet-auth/api"
-
-export const CheckRules = (rules, mainet) => {
-    // let result = false ;
-    rules.forEach((rule) => {
-        let x = hasToken(mainet, rule.address)
-    });
-};
