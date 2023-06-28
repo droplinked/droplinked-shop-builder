dda435d5bc178f5e1aa65637626c49cd85f39422
Mehrpouya <pou-ya@outlook.com>
2023-06-28 13:33:50 +0330

    add pagination

--- src/lib/apis/product/interfaces.ts
+++ src/lib/apis/product/interfaces.ts
@@ -1,3 +1,8 @@
+export interface IproductList {
+    page: number
+    limit: number
+}
+
 export type product_type = "NORMAL" | "PRINT_ON_DEMAND" | "DIGITAL"
 export interface IproductState {
     title: string
