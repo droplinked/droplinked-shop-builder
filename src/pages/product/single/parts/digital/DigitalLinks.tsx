156b505d63aca8289acf5d596e42216ec271a613
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-07-22 16:31:09 +0330

    add Product Link in product page

---
+++ src/pages/product/single/parts/digital/DigitalLinks.tsx
@@ -0,0 +1,25 @@
+import { VStack } from '@chakra-ui/react'
+import React, { useContext } from 'react'
+import { productContext } from '../../context'
+import ProductCollapse from '../modules/collapse/ProductCollapse'
+import MessageDigital from '../modules/messageDigital/MessageDigital'
+import OrginalFile from '../modules/orginalFile/OrginalFile'
+
+function DigitalLinks() {
+    const { state: { product_type } } = useContext(productContext)
+
+    return (
+        <>
+            {product_type === "DIGITAL" && (
+                <ProductCollapse title='Product Link' description='Write a message to your customers and attach the original file link.'>
+                    <VStack spacing={10} align={"stretch"}>
+                        <MessageDigital />
+                        <OrginalFile />
+                    </VStack>
+                </ProductCollapse>
+            )}
+        </>
+    )
+}
+
+export default DigitalLinks
