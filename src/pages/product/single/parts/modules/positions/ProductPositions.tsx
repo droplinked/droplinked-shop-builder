bf62487ff018509a8e91c748439e153ef93444ba
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-07-28 13:38:13 +0330

    add new positions

--- src/pages/product/single/parts/modules/positions/ProductPositions.tsx
+++ src/pages/product/single/parts/modules/positions/ProductPositions.tsx
@@ -1,41 +1,50 @@
-import { Box, Flex, Image } from '@chakra-ui/react'
-import React from 'react'
-import { useQuery } from 'react-query'
-import shirtBack from 'assest/image/positions/back.svg'
-import shirtCenter from 'assest/image/positions/center.svg'
-import shirtLeft from 'assest/image/positions/left.svg'
-import shirtRight from 'assest/image/positions/right.svg'
+import { Box, Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import { useQuery } from "react-query";
+import shirtBack from "assest/image/positions/back.svg";
+import shirtCenter from "assest/image/positions/center.svg";
+import shirtLeft from "assest/image/positions/left.svg";
+import shirtRight from "assest/image/positions/right.svg";
+import pantLeft from "assest/image/positions/pent-left.svg";
+import pantRight from "assest/image/positions/pent-right.svg";
 
 interface IProps {
-    update(element: string): void
-    state: Array<string>
-    positions: Array<string>
+  update(element: string): void;
+  state: Array<string>;
+  positions: Array<string>;
 }
 
 function ProductPositions({ update, state, positions }: IProps) {
-    const icons = {
-        FRONT_CENTER: shirtCenter,
-        FRONT_RIGHT_CHEST: shirtLeft,
-        FRONT_LEFT_CHEST: shirtRight,
-        BACK_CENTER: shirtBack,
-    }
+  const icons = {
+    FRONT_CENTER: shirtCenter,
+    FRONT_RIGHT_CHEST: shirtLeft,
+    FRONT_LEFT_CHEST: shirtRight,
+    BACK_CENTER: shirtBack,
+    LEFT_LEG_FRONT: pantLeft,
+    RIGHT_LEG_FRONT: pantRight,
+  };
 
-    return (
-        <Flex gap={3}>
-            {positions.map((el: any, key: number) => (
-                <Box key={key}>
-                    <Image
-                        style={{ border: `3px solid ${state.includes(el) ? "#2EC99E" : "transparent"}`, borderRadius: "8px" }}
-                        onClick={() => update(el)}
-                        src={icons[el]}
-                        cursor="pointer"
-                        width="58px"
-                        height="58px"
-                    />
-                </Box>
-            ))}
-        </Flex>
-    )
+  return (
+    <Flex gap={3}>
+      {positions.map((el: any, key: number) => (
+        <Box key={key}>
+          <Image
+            style={{
+              border: `3px solid ${
+                state.includes(el) ? "#2EC99E" : "transparent"
+              }`,
+              borderRadius: "8px",
+            }}
+            onClick={() => update(el)}
+            src={icons[el]}
+            cursor="pointer"
+            width="58px"
+            height="58px"
+          />
+        </Box>
+      ))}
+    </Flex>
+  );
 }
 
-export default ProductPositions
+export default ProductPositions;
