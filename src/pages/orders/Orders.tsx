d34977b99e963b126220a37cb485e874956684f9
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-07-31 14:13:32 +0330

    remove console

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -27,8 +27,6 @@ function Orders() {
 
     // Handle search and without search
     const rows = useMemo(() => {
-        console.log("data", data);
-
         return data ? OrdersModel.refactorData({
             data: data.data.data?.data,
             search: States.search
