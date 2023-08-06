0e60b9e7eb4e0abdb29d5fb49e082481dcfa5a1a
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-08-06 17:25:13 +0330

    link transaction

---
+++ src/components/common/hashKey/model.ts
@@ -0,0 +1,23 @@
+import { appDeveloment } from "lib/utils/app/variable"
+
+interface IgetLink {
+    blockchain: string
+    hashkey: string
+}
+
+const hashkeyModel = {
+    getLink: ({ blockchain, hashkey }: IgetLink) => {
+        switch (blockchain) {
+            case "CASPER":
+                return `https://${appDeveloment ? "testnet." : ""}cspr.live/deploy/${hashkey}`
+            case "STACKS":
+                return `https://explorer.hiro.so/txid/${hashkey}?chain=${appDeveloment ? "testnet" : "mainnet"}`
+            case "POLYGON":
+                return `https://${appDeveloment ? "mumbai." : ""}polygonscan.com/tx/${hashkey}`
+            default:
+                return ""
+        }
+    }
+}
+
+export default hashkeyModel
