8fe64ed3a7771574b8e4955ac0ce17fb99be1f7f
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-08-08 09:47:00 +0330

    update search

--- src/lib/apis/coupons/addressServices.ts
+++ src/lib/apis/coupons/addressServices.ts
@@ -5,6 +5,6 @@ export const giftcardCreateService = (params: IgiftcardCreateService) => {
     return axiosInstance.post(`giftcard/create`, params)
 }
 
-export const giftcardsService = ({ limit, page }: IgiftcardsService) => {
-    return axiosInstance.get(`giftcard?page=${page || 1}&limit=${limit || 20}`)
+export const giftcardsService = ({ limit, page, search }: IgiftcardsService) => {
+    return axiosInstance.get(`giftcard?page=${page || 1}&limit=${limit || 20}${search ? `&search=${search}` : ''}`)
 }
--- src/pages/register-pages/pages/coupons/parts/list/parts/head/CouponsListHead.tsx
+++ src/pages/register-pages/pages/coupons/parts/list/parts/head/CouponsListHead.tsx
@@ -1,16 +1,35 @@
 import { Box, Flex, useDisclosure } from '@chakra-ui/react'
 import BasicButton from 'components/common/BasicButton/BasicButton'
 import SearchDatagrid from 'components/common/datagrid/parts/search/SearchDatagrid'
-import React from 'react'
+import CouponsSettingContext from 'pages/register-pages/pages/coupons/context'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import CouponsCreate from '../../../parts/create/CouponsCreate'
 
 function CouponsListHead() {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const { updateFilters } = useContext(CouponsSettingContext)
+    const [searchParams] = useSearchParams()
+    const [Keyword, setKeyword] = useState(null)
+    const search = searchParams.get('search')
+
+    useEffect(() => search && setKeyword(search), [searchParams, search])
+
+    const submit = useCallback((form: any) => {
+        form.preventDefault()
+        if (search === Keyword) return false
+
+        updateFilters('search', Keyword)
+    }, [Keyword])
 
     return (
         <>
             <Flex justifyContent="space-between" alignItems="center">
-                <Box><SearchDatagrid onChange={() => { }} /></Box>
+                <Box>
+                    <form method="post" onSubmit={submit}>
+                        <SearchDatagrid value={Keyword} onChange={e => setKeyword(e.target.value)} />
+                    </form>
+                </Box>
                 <Box><BasicButton sizes="medium" onClick={onOpen}>Create Coupon</BasicButton></Box>
             </Flex>
             {isOpen && <CouponsCreate close={onClose} open={isOpen} />}
