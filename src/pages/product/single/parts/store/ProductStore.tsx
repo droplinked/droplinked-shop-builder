4d05d8b963bb627e87932339df30528db3b1dc4d
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-08-15 14:46:10 +0330

    fix bugs

--- src/pages/product/single/parts/store/ProductStore.tsx
+++ src/pages/product/single/parts/store/ProductStore.tsx
@@ -1,5 +1,5 @@
-import { IpodAvailableVariantsService, IpodPrintPositionsService, IpodProductService } from 'lib/apis/pod/interfaces'
-import { podAvailableVariantsService, podPrintPositionsService, podProductService } from 'lib/apis/pod/services'
+import { IpodAvailableVariantsService, IpodPrintPositionsService, IpodProductService, IproviderIDService } from 'lib/apis/pod/interfaces'
+import { podAvailableVariantsService, podPrintPositionsService, podProductService, providerIDService } from 'lib/apis/pod/services'
 import React, { useContext, useEffect } from 'react'
 import { useMutation } from 'react-query'
 import { productContext } from '../../context'
@@ -9,10 +9,11 @@ interface IProps {
 }
 
 function ProductStore({ children }: IProps) {
-    const { state: { pod_blank_product_id }, store: { methods: { update } } } = useContext(productContext)
+    const { state: { pod_blank_product_id, prodviderID }, productID, methods: { updateState }, store: { methods: { update } } } = useContext(productContext)
     const providerService = useMutation((params: IpodProductService) => podProductService(params))
     const availableVariants = useMutation((params: IpodAvailableVariantsService) => podAvailableVariantsService(params))
     const printPositions = useMutation((params: IpodPrintPositionsService) => podPrintPositionsService(params))
+    const provider = useMutation((params: IproviderIDService) => providerIDService(params))
 
     // Get providers
     useEffect(() => {
@@ -29,6 +30,17 @@ function ProductStore({ children }: IProps) {
             }
         })
     }, [pod_blank_product_id])
+
+    // Get product types
+    useEffect(() => {
+        if (prodviderID) provider.mutate({ prodviderID }, {
+            onSuccess: res => {
+                const data = res.data?.data
+                update("product_types", data)
+                if (!productID) updateState("pod_blank_product_id", data[0]._id)
+            }
+        })
+    }, [prodviderID, productID])
 
     return children
 }
