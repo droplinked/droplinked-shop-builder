f08e9c7b16793ef4fcb50bd7c53652c747dc5a2f
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-08-23 11:45:46 +0330

    update items variant

--- src/components/modals/order-modal/components/order-detail-component/components/table/parts/product/OrderModalProduct.jsx
+++ src/components/modals/order-modal/components/order-detail-component/components/table/parts/product/OrderModalProduct.jsx
@@ -2,12 +2,15 @@ import { Box, HStack, Image, Text, VStack } from '@chakra-ui/react'
 import AppTypography from 'components/common/typography/AppTypography'
 import React from 'react'
 
-function OrderModalProduct({ product }) {
+function OrderModalProduct({ data }) {
+  const product = data.product
+  console.log(data.color);
   return (
     <HStack>
       <Box><Image width={"50px"} borderRadius={"7px"} src={product.media.length ? product.media[0].url : ""} /></Box>
       <VStack align="stretch">
-        <Box><AppTypography size='12px'>{product.title}</AppTypography></Box>
+        <Box><AppTypography size='12px'>{product.title}: {data.size}</AppTypography></Box>
+        <Box width="18px" height="18px" borderRadius="100%" backgroundColor={data.color} border="1px solid #333"></Box>
         <Box><AppTypography size='12px' color="#808080">{product?.shippingType}</AppTypography></Box>
       </VStack>
     </HStack>
