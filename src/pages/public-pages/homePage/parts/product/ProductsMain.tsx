043435768062bf7fa679454397378ca5cac138a3
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-09-20 14:10:58 +0330

    up

--- src/pages/public-pages/homePage/parts/product/ProductsMain.tsx
+++ src/pages/public-pages/homePage/parts/product/ProductsMain.tsx
@@ -11,22 +11,22 @@ function ProductsMain({ loaded }) {
         {
             title: 'Physical Items',
             url: '',
-            icon: '/assets/images/homepage/physicalProducts.svg'
+            icon: '/assets/images/homepage/physicalProducts.png'
         },
         {
-            title: 'Print On Demand',
+            title: 'Print on Demand',
             url: '',
-            icon: '/assets/images/homepage/pod.svg'
+            icon: '/assets/images/homepage/pod.png'
         },
         {
             title: 'Digital Goods',
             url: '',
-            icon: '/assets/images/homepage/digital.svg'
+            icon: '/assets/images/homepage/digital.png'
         },
         {
             title: 'Events',
             url: '',
-            icon: '/assets/images/homepage/event.svg'
+            icon: '/assets/images/homepage/event.png'
         }
     ]
 
@@ -38,11 +38,11 @@ function ProductsMain({ loaded }) {
                 <VStack width="95%" maxWidth={checkLoad ? "1400px" : "100%"} opacity={checkLoad ? 1 : 0} transition={".7s"} justifyContent="center" color="#FFF">
                     <Box><LandingTitle title='Decentralized Registration of Products' /></Box>
                     <Box padding="10px 0 30px 0"><LandingDescription text='Sell types of products on chain in your customized online store integrated with web3 tools' /></Box>
-                    <Flex width={{ base: "80%", md: "100%" }} flexWrap="wrap" justifyContent="space-between">
+                    <Flex width="100%" flexWrap={{ base: "wrap", md: "nowrap" }} transition=".7s" justifyContent="center" gap={{ base: "4%", md: checkLoad ? "40px" : "70px", xl: checkLoad ? "64px" : "130px" }}>
                         {data.map((el, key) => (
                             <MainCard key={key}>
-                                <VStack justifyContent="center" spacing="20px">
-                                    <AppTypography size={{ base: '14px', sm: '20px' }} color="#f5f5f5">{el.title}</AppTypography>
+                                <VStack justifyContent="center" spacing="40px">
+                                    <AppTypography size={{ base: '14px', sm: '16px', xl: '20px' }} color="#f5f5f5">{el.title}</AppTypography>
                                     <Box width="100%" height={{ base: "60px", sm: "130px" }} className={classes.images} position="relative">
                                         <Image width={{ base: "63px", sm: "130px" }} src={el.icon} alt={el.title} />
                                         <Image width={{ base: "63px", sm: "130px" }} src={el.icon} alt={el.title} />
