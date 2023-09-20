d4ab1d787a2d19acf2e7ecc5488024d9d3a22504
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-09-20 11:22:09 +0330

    banner signup

--- src/pages/public-pages/homePage/parts/banner/Banner.tsx
+++ src/pages/public-pages/homePage/parts/banner/Banner.tsx
@@ -1,46 +1,56 @@
-import { Box, Flex, Image, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Image, Text, useDisclosure, VStack } from '@chakra-ui/react'
 import BasicButton from 'components/common/BasicButton/BasicButton'
 import AppTypography from 'components/common/typography/AppTypography'
 import React from 'react'
 import Droplinked from './parts/droplinked/Droplinked'
 import Typewriter from 'typewriter-effect';
+import useHookStore from 'functions/hooks/store/useHookStore'
+import AuthModal from 'components/modals/auth-modal/AuthModal'
 
 function Banner() {
+    const { app: { shop } } = useHookStore();
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
-        <Box position="relative">
-            <Image src="assets/images/homepage/droplinked1.svg" position="absolute" zIndex="1" top="-200px" width="100%" />
-            <Image src="assets/images/homepage/shape.svg" position="absolute" zIndex="1" right={{ base: "8%", sm: "26%" }} bottom={{ base: "-100px", sm: "0" }} width={{ base: "24px", sm: "34px" }} />
-            <Flex height="100%" justifyContent="center" alignItems="center" position="relative" zIndex="2">
-                <VStack justifyContent="center" color="#FFF">
-                    <Droplinked />
-                    <Box textAlign="center" padding="0 20px"><AppTypography size={{ base: "20px", sm: "25px", lg: "30px", xl: "50px" }} weight='bolder'>Powering the Next Generation of Commerce</AppTypography></Box>
-                    <Box padding="10px 0 30px 0">
-                        <Text fontSize={{ base: "14px", sm: "24px", xl: "34px" }} display="flex">
-                            <Typewriter
-                                options={{
-                                    strings: [
-                                        'Build a Customizable Store',
-                                        'Sell Diverse Products',
-                                        'Token Gating Collections',
-                                        'Mint to Merch',
-                                        'Decentralize Inventory',
-                                        'Sales Tracking',
-                                        'Transparent Co-selling',
-                                        'Increase Earnings'
-                                    ],
-                                    cursor: '',
-                                    autoStart: true,
-                                    loop: true
-                                }}
-                            /> | On-Chain
-                        </Text>
-                    </Box>
-                    <Box><BasicButton minWidth={{ base: "120px", sm: "160px" }} height={{ base: "32px", sm: "40px" }}>
-                        <AppTypography size={{ base: "12px", sm: "16px" }}>Start Selling</AppTypography>
-                    </BasicButton></Box>
-                </VStack>
-            </Flex>
-        </Box>
+        <>
+            <Box position="relative">
+                <Image src="assets/images/homepage/droplinked1.svg" position="absolute" zIndex="1" top="-200px" width="100%" />
+                <Image src="assets/images/homepage/shape.svg" position="absolute" zIndex="1" right={{ base: "8%", sm: "26%" }} bottom={{ base: "-100px", sm: "0" }} width={{ base: "24px", sm: "34px" }} />
+                <Flex height="100%" justifyContent="center" alignItems="center" position="relative" zIndex="2">
+                    <VStack justifyContent="center" color="#FFF">
+                        <Droplinked />
+                        <Box textAlign="center" padding="0 20px"><AppTypography size={{ base: "20px", sm: "25px", lg: "30px", xl: "50px" }} weight='bolder'>Powering the Next Generation of Commerce</AppTypography></Box>
+                        <Box padding="10px 0 30px 0">
+                            <Text fontSize={{ base: "14px", sm: "24px", xl: "34px" }} display="flex">
+                                <Typewriter
+                                    options={{
+                                        strings: [
+                                            'Build a Customizable Store',
+                                            'Sell Diverse Products',
+                                            'Token Gating Collections',
+                                            'Mint to Merch',
+                                            'Decentralize Inventory',
+                                            'Sales Tracking',
+                                            'Transparent Co-selling',
+                                            'Increase Earnings'
+                                        ],
+                                        cursor: '',
+                                        autoStart: true,
+                                        loop: true
+                                    }}
+                                /> | On-Chain
+                            </Text>
+                        </Box>
+                        <Box>
+                            <BasicButton onClick={onOpen} minWidth={{ base: "120px", sm: "160px" }} height={{ base: "32px", sm: "40px" }}>
+                                <AppTypography size={{ base: "12px", sm: "16px" }}>Start Selling</AppTypography>
+                            </BasicButton>
+                        </Box>
+                    </VStack>
+                </Flex>
+            </Box>
+            {isOpen && <AuthModal show={isOpen} type="SIGNUP" shopName={shop?.name} close={onClose} />}
+        </>
     )
 }
 
