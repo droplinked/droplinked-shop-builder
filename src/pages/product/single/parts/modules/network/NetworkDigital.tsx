3de5281a1cbca261f1f8563bb4362b5ccf8ab466
Mehrpouya <mehrpouya.ramzi@gmail.com>
2023-09-23 18:05:17 +0330

    up

--- src/pages/product/single/parts/modules/network/NetworkDigital.tsx
+++ src/pages/product/single/parts/modules/network/NetworkDigital.tsx
@@ -1,22 +1,29 @@
 import { HStack, VStack } from '@chakra-ui/react'
 import AppIcons from 'assest/icon/Appicons'
 import AppTypography from 'components/common/typography/AppTypography'
-import React from 'react'
+import { productContext } from 'pages/product/single/context'
+import React, { useContext } from 'react'
 import BlockchainNetwork from '../variants/parts/table/parts/recordModal/parts/form/parts/blockchainNetwork/BlockchainNetwork'
 
 function NetworkDigital() {
+    const { state: { product_type } } = useContext(productContext)
+
     return (
-        <VStack align="stretch">
-            <BlockchainNetwork
-                error={null}
-                onChange={(e) => console.log("blockchain", e)}
-                value={[]}
-            />
-            <HStack alignItems="center">
-                <AppIcons.Info />
-                <AppTypography color="#757575" size='14px'>??? NFT Creating Blockchain.. <a style={{ color: "#25BB92" }} target={"_blank"}>Learn more</a></AppTypography>
-            </HStack>
-        </VStack>
+        <>
+            {product_type === "DIGITAL" && (
+                <VStack align="stretch">
+                    <BlockchainNetwork
+                        error={null}
+                        onChange={(e) => console.log("blockchain", e)}
+                        value={[]}
+                    />
+                    <HStack alignItems="center">
+                        <AppIcons.Info />
+                        <AppTypography color="#757575" size='14px'>??? NFT Creating Blockchain.. <a style={{ color: "#25BB92" }} target={"_blank"}>Learn more</a></AppTypography>
+                    </HStack>
+                </VStack>
+            )}
+        </>
     )
 }
 
