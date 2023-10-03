3042a6c2ff5929f6764b5f93bf977278cb8288d3
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-10-03 14:12:13 +0330

    landing page update

--- src/pages/public-pages/homePage/parts/parts/card/MainCard.tsx
+++ src/pages/public-pages/homePage/parts/parts/card/MainCard.tsx
@@ -6,7 +6,7 @@ interface IProps extends FlexProps { }
 
 function MainCard(props: IProps) {
     return (
-        <Flex className={classes.box} flexDirection="column" backdropFilter="blur(20px)" gap="10px" borderRadius={{ base: "12px", md: "24px" }} background="rgba(62, 62, 62, 0.3)" padding={{ base: "25px 10px", lg: "40px 10px" }} marginBottom="20px" width={{ base: "46%", md: "25%", xl: "267px" }} {...props}>
+        <Flex className={classes.box} flexDirection="column" backdropFilter="blur(20px)" gap="10px" borderRadius={{ base: "12px", md: "24px" }} background="rgba(62, 62, 62, 0.3)" padding={{ base: "25px 10px", lg: "40px 10px" }} marginBottom="20px" width={{ base: "46%", md: "25%", xl: "285px" }} {...props}>
             {props.children}
         </Flex>
     )
--- src/pages/public-pages/homePage/parts/product/ProductsMain.tsx
+++ src/pages/public-pages/homePage/parts/product/ProductsMain.tsx
@@ -36,14 +36,14 @@ function ProductsMain({ loaded }) {
         <Flex alignItems="center" justifyContent="center" height="100%">
             <Box position="relative">
                 <Flex justifyContent="center" alignItems="center">
-                    <VStack width="95%" maxWidth={checkLoad ? "1400px" : "100%"} opacity={checkLoad ? 1 : 0} transition={".7s"} justifyContent="center" color="#FFF">
+                    <VStack maxWidth={checkLoad ? "1400px" : "100%"} opacity={checkLoad ? 1 : 0} transition={".7s"} justifyContent="center" color="#FFF">
                         <Box><LandingTitle title='Decentralized Registration of Products' /></Box>
                         <Box padding="10px 0 30px 0"><LandingDescription text='Start selling today with a customizable storefront paired with web3 tools for on-chain transparency' /></Box>
-                        <Flex width="100%" maxWidth={{ base: "500px", md: "100%" }} flexWrap={{ base: "wrap", md: "nowrap" }} transition=".7s" justifyContent="center" gap={{ base: "4%", md: checkLoad ? "20px" : "70px", xl: checkLoad ? "64px" : "130px" }}>
+                        <Flex width="100%" maxWidth={{ base: "500px", md: "100%" }} flexWrap={{ base: "wrap", md: "nowrap" }} transition=".7s" justifyContent="center" gap={{ base: "4%", md: checkLoad ? "20px" : "70px", xl: checkLoad ? "56px" : "130px" }}>
                             {data.map((el, key) => (
                                 <MainCard key={key}>
                                     <VStack justifyContent="center" spacing={{ base: "10px", lg: "40px" }}>
-                                        <AppTypography size={{ base: '14px', sm: '16px', xl: '20px' }} color="#f5f5f5">{el.title}</AppTypography>
+                                        <AppTypography textAlign="center" height={{ base: "42px", sm: "30px", md: "40px", lg: "auto" }} size={{ base: '14px', sm: '16px', xl: '20px' }} color="#f5f5f5">{el.title}</AppTypography>
                                         <Box width="100%" height={{ base: "60px", sm: "130px" }} className={classes.images} position="relative">
                                             <Image width={{ base: "63px", sm: "90px", lg: "130px" }} src={el.icon} alt={el.title} />
                                             <Image width={{ base: "63px", sm: "90px", lg: "130px" }} src={el.icon} alt={el.title} />
