ba3b6e97f6a86a10d312f498b8cbafc9966b2596
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-10-15 16:43:56 +0330

    up

--- src/components/layouts/app/main/parts/footer/FooterLayout.jsx
+++ src/components/layouts/app/main/parts/footer/FooterLayout.jsx
@@ -1,35 +1,26 @@
 import { Box, Flex, HStack, Text, VStack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-//
-
-import { footerStyles } from "./FooterLayout-style";
-//
-
 import Description from "./parts/description";
 import Links from "./parts/links";
 import BottomComponent from "./parts/bottom/BottomComponent";
 
 export default function FooterLayout() {
-  const { FooterWrapper, FooterBody, FooterText, FooterLogo, TermText } =
-    footerStyles;
+
   return (
     <VStack
       w="100%"
       padding="60px 0"
       backgroundColor="black"
-      justifyContent="center"
       position="relative"
     >
-      <VStack align="stretch" width="80%" maxWidth="1400px">
+      <VStack align="stretch" width="90%" maxWidth="1400px">
         <Flex
           flexDir={{ base: 'column', md: 'row' }}
-          display="flex"
-          justifyContent="center"
+          justifyContent="space-between"
           alignItems={{ base: "start", md: "center" }}
           mb={{ base: '86px', md: "54px" }}
+          gap="30px"
         >
           <Description />
-          <Box mb={{ base: '86px', md: '0px' }} />
           <Links />
         </Flex>
         <BottomComponent />
