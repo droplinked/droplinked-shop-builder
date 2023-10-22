26293f9f4eed612887a830b0e6f5f27ecfc1cc7f
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-10-22 15:54:34 +0330

    up

--- src/functions/hooks/web3/useWeb3.tsx
+++ src/functions/hooks/web3/useWeb3.tsx
@@ -1,8 +1,11 @@
+import useAppStore, { IUserProps } from 'lib/stores/app/appStore'
 import { appDeveloment } from 'lib/utils/app/variable'
 import { getNetworkProvider } from 'lib/utils/chains/chainProvider'
 import { Chain, Network } from 'lib/utils/chains/Chains'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useStore } from 'zustand'
 import useStack from '../stack/useStack'
+import useHookStore from '../store/useHookStore'
 import web3Model, { IAcceptData, IRecordPrams, IRequestData } from './models'
 import RecordCasperModule from './models/module/record/modules/casperModel'
 
@@ -23,35 +26,71 @@ export type IWeb3 = {
 
 function useAppWeb3() {
     const { record, request, accept } = web3Model
+    const { app: { user, updateState } } = useHookStore()
     const stack = useStack()
 
-    const _login = async (chain: string) => {
-        if (chain == "STACKS") {
-            await stack.login()
-        } else {
-            return await (await getNetworkProvider(Chain[chain], Network[appDeveloment ? "TESTNET" : "MAINNET"], null).walletLogin(null)).address
-        }
-    }
+    const updateWallet = useCallback((values: IUserProps) => {
+        updateState({ key: "user", params: { ...user, wallets: [...user?.wallets || [], values] } })
+    }, [user])
 
-    const web3 = ({ method, params, chain }: IWeb3) => {
+    // console.log('user', user);
+
+    const _login = (chain: string) => {
         return new Promise<any>(async (resolve, reject) => {
             try {
-                const accountAddress = await _login(chain)
-                if (method === "record") {
-                    const records = await record({ params, accountAddress })
-                    resolve(records)
-                } else if (method === "request") {
-                    const requests = await request({ params, accountAddress })
-                    resolve(requests)
-                } else if (method === "accept") {
-                    const requests = await accept({ params, accountAddress })
-                    resolve(requests)
+                const findAccountAddress = user?.wallets ? user.wallets.find(el => el.type === chain && el?.address) : null
+                console.log("findAccountAddress", findAccountAddress);
+
+                if (findAccountAddress) {
+                    resolve(findAccountAddress.address)
+                } else if (chain == "STACKS") {
+                    const address: any = await stack.login()
+                    resolve(address)
+                    updateWallet({ type: "STACKS", address })
+                } else {
+                    if (chain === "CASPER") {
+                        await (await getNetworkProvider(Chain[chain], Network[appDeveloment ? "TESTNET" : "MAINNET"], null).walletLogin((res) => {
+                            updateWallet({ type: "CASPER", address: res.account_hash, public_key: res.publicKey })
+                            resolve(res.account_hash)
+                        }))
+                    } else {
+                        const address = await (await getNetworkProvider(Chain[chain], Network[appDeveloment ? "TESTNET" : "MAINNET"], null).walletLogin(null)).address
+                        resolve(address)
+                        updateWallet({ type: chain, address })
+                    }
+                    console.log("adasda 1");
                 }
             } catch (error) {
-                reject(error);
+                console.log("ads", error);
+                reject(error)
             }
         })
     }
+
+    const web3 = useCallback(({ method, params, chain }: IWeb3) => {
+        {
+            return new Promise<any>(async (resolve, reject) => {
+                try {
+                    const accountAddress = await _login(chain)
+                    console.log("accountAddress", accountAddress);
+
+                    if (method === "record") {
+                        const records = await record({ params, accountAddress })
+                        resolve(records)
+                    } else if (method === "request") {
+                        const requests = await request({ params, accountAddress })
+                        resolve(requests)
+                    } else if (method === "accept") {
+                        const requests = await accept({ params, accountAddress })
+                        resolve(requests)
+                    }
+                } catch (error) {
+
+                    reject(error);
+                }
+            })
+        }
+    }, [user])
 
     return { web3 }
 }
--- src/pages/register-pages/pages/design/parts/preview/DesignPagePreview.tsx
+++ src/pages/register-pages/pages/design/parts/preview/DesignPagePreview.tsx
@@ -1,19 +1,26 @@
 import { Box, Flex } from '@chakra-ui/react'
 import AppScrollBar from 'components/common/scrollbar'
 import { useProfile } from 'functions/hooks/useProfile/useProfile';
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { designContext } from '../../design-context'
 
 function DesignPagePreview() {
-    const { shop } = useProfile();
-    const { state: { device } } = useContext(designContext)
+    const dataShop = useProfile();
+    const { state: { device, shop } } = useContext(designContext)
+    const url = "https://dev.droplinked.io/" + dataShop.shop.name
+    const iframeElement = useRef<any>(null)
+
+    useEffect(() => {
+        iframeElement.current.contentWindow.postMessage(shop, url);
+    }, [shop])
 
     return (
         <Flex justifyContent="center">
             <AppScrollBar transition=".3s" width={device === "mobile" ? "300px" : "100%"}>
                 <iframe
                     style={{ width: "100%", height: "600px" }}
-                    src={"https://dev.droplinked.io/" + shop.name}
+                    src={url}
+                    ref={iframeElement}
                     title="Module"
                 ></iframe>
             </AppScrollBar>
