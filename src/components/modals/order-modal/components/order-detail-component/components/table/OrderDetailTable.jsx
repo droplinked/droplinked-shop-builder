0cf06a4fdb98b66763de3124965e5846e4ead49a
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-10-29 15:23:49 +0330

    up

--- src/components/modals/order-modal/components/order-detail-component/components/table/OrderDetailTable.jsx
+++ src/components/modals/order-modal/components/order-detail-component/components/table/OrderDetailTable.jsx
@@ -1,4 +1,5 @@
-import { Box, HStack, VStack } from '@chakra-ui/react';
+import { Box, HStack, VStack, Flex } from '@chakra-ui/react';
+import AppIcons from 'assest/icon/Appicons';
 import AppTypography from 'components/common/typography/AppTypography';
 import orderModalContext from 'components/modals/order-modal/context'
 import React, { useContext } from 'react'
@@ -18,7 +19,13 @@ function OrderDetailTable() {
                 <HStack key={key} justifyContent="space-between">
                     <Box width="50%"><OrderModalProduct data={el} /></Box>
                     <Box><AppTypography size='12px'>x{el?.quantity}</AppTypography></Box>
-                    <Box><AppTypography size='12px'>${pricePerItem(el)}</AppTypography></Box>
+                    <VStack justifyContent="right" align="stretch">
+                        <Box textAlign="right"><AppTypography size='12px'>${pricePerItem(el)}</AppTypography></Box>
+                        <Flex alignItems="center" justifyContent="right" gap="4px">
+                            {el?.passedRule ? <AppIcons.DiscountIcon width="10px" height="10px" /> : <AppIcons.GatedIcon width="10px" height="10px" />}
+                            <AppTypography size='12px' color="#808080">{!el?.passedRule ? "Gated Product" : `$ ${el?.passedRule?.rules[0]?.discountPercentage} Discount`}</AppTypography>
+                        </Flex>
+                    </VStack>
                 </HStack>
             )) : null}
         </VStack>
