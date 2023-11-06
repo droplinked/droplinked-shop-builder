5fd2cdb06ddff2f0f58b00533a7c80464fd7e02c
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-11-06 12:26:27 +0330

    up

--- src/components/modals/order-modal/components/order-detail-component/components/table/OrderDetailTable.jsx
+++ src/components/modals/order-modal/components/order-detail-component/components/table/OrderDetailTable.jsx
@@ -13,19 +13,24 @@ function OrderDetailTable() {
             <Box marginBottom={2}>
                 <AppTypography size="16px" weight="bolder" color="#FFF">Items</AppTypography>
             </Box>
-            {order?.products ? order.products.map((el, key) => (
-                <HStack alignItems="self-start" key={key} justifyContent="space-between">
-                    <Box width="50%"><OrderModalProduct product={el} /></Box>
-                    <Box width="15%"><AppTypography size='12px'>x{el?.quantity}</AppTypography></Box>
-                    <VStack justifyContent="right" align="stretch">
-                        <Box textAlign="right"><AppTypography size='12px'>${parseFloat(el?.amount).toFixed(2)}</AppTypography></Box>
-                        <Flex alignItems="center" justifyContent="right" gap="4px">
-                            {el?.discount && el?.hasRule ? <AppIcons.DiscountIcon width="10px" height="10px" /> : <AppIcons.GatedIcon width="10px" height="10px" />}
-                            <AppTypography size='12px' color="#808080">{!el?.discount ? "Gated Product" : `$ ${el?.discount} Discount`}</AppTypography>
-                        </Flex>
-                    </VStack>
-                </HStack>
-            )) : null}
+            {order?.products ? order.products.map((el, key) => {
+                const hasDiscountRule = el?.hasRule && el?.discount;
+                const hasGatedRule = el?.hasRule && !el?.discount;
+
+                return (
+                    <HStack alignItems="self-start" key={key} justifyContent="space-between">
+                        <Box width="50%"><OrderModalProduct product={el} /></Box>
+                        <Box width="10%"><AppTypography size='12px'>x{el?.quantity}</AppTypography></Box>
+                        <VStack width="25%" justifyContent="right" align="stretch">
+                            <Box textAlign="right"><AppTypography size='12px'>${parseFloat(el?.amount).toFixed(2)}</AppTypography></Box>
+                            <Flex alignItems="center" justifyContent="right" gap="4px">
+                                {hasDiscountRule ? <AppIcons.DiscountIcon width="10px" height="10px" /> : hasGatedRule ? <AppIcons.GatedIcon width="10px" height="10px" /> : null}
+                                <AppTypography size='12px' color="#808080">{hasGatedRule ? "Gated Product" : hasDiscountRule ? `$ ${el?.discount} Discount` : null}</AppTypography>
+                            </Flex>
+                        </VStack>
+                    </HStack>
+                )
+            }) : null}
         </VStack>
     )
 }
--- src/components/modals/order-modal/components/order-detail-component/components/table/parts/cost/OrderModalProduct.jsx
+++ src/components/modals/order-modal/components/order-detail-component/components/table/parts/cost/OrderModalProduct.jsx
@@ -14,11 +14,6 @@ function OrderDetailCost() {
       value: order?.details?.shippingCost ? parseFloat(order?.details?.shippingCost).toFixed(2) : ''
     },
     {
-      caption: 'Final Earning',
-      placeholder: null,
-      value: order?.details?.finalEarning ? parseFloat(order?.details?.finalEarning).toFixed(2) : ''
-    },
-    {
       caption: 'Total Product Cost',
       placeholder: null,
       value: order?.details?.totalProductCost ? parseFloat(order?.details?.totalProductCost).toFixed(2) : ''
@@ -26,13 +21,20 @@ function OrderDetailCost() {
     {
       caption: 'Cost Of Good Sold',
       placeholder: null,
+      description: '(Tax + Shipping + Product Cost)',
       value: order?.details?.totalOfGoodSold ? parseFloat(order?.details?.totalOfGoodSold).toFixed(2) : ''
     },
     {
       caption: 'Affiliate Share',
       placeholder: null,
       value: order?.details?.affiliateShare ? parseFloat(order?.details?.affiliateShare).toFixed(2) : ''
-    }
+    },
+    {
+      caption: 'Final Earning',
+      placeholder: null,
+      value: order?.details?.finalEarning ? parseFloat(order?.details?.finalEarning).toFixed(2) : '',
+      color: "#2BCFA1"
+    },
   ]
 
   return (
@@ -40,8 +42,11 @@ function OrderDetailCost() {
       <AppTypography size="16px" weight="bolder" color="#FFF">Total Cost</AppTypography>
       <VStack align={"stretch"} spacing={3} color="#C2C2C2">
         {data.map((el, key) => el.value !== null ? (
-          <HStack key={key} justifyContent={"space-between"} alignItems="center">
-            <AppTypography size="12px">{el.caption}</AppTypography>
+          <HStack key={key} color={el?.color || "#C2C2C2"} justifyContent={"space-between"} alignItems="center">
+            <VStack align="stretch" spacing="0">
+              <AppTypography size="12px">{el.caption}</AppTypography>
+              {el.description && <AppTypography size="12px" color="#808080">{el.description}</AppTypography>}
+            </VStack>
             {el.placeholder && <AppTypography size="12px" color="#808080">{el.placeholder}</AppTypography>}
             <AppTypography size="12px">{el.value ? '$' + el.value : "-"}</AppTypography>
           </HStack>
