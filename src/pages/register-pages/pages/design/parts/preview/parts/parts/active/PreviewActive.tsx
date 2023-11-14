04d61f632de3ef7b464cfc23160859bd09b1df33
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-11-14 14:18:20 +0330

    up

--- src/pages/register-pages/pages/design/parts/preview/parts/parts/active/PreviewActive.tsx
+++ src/pages/register-pages/pages/design/parts/preview/parts/parts/active/PreviewActive.tsx
@@ -1,8 +1,7 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 import { designContext } from 'pages/register-pages/pages/design/design-context'
 import { IPreviewSections } from 'pages/register-pages/pages/design/reducer'
-import React, { useContext, useEffect, useRef } from 'react'
-import designPreviewContext from '../../../context'
+import React, { useContext } from 'react'
 
 interface IProps {
     section: IPreviewSections
@@ -13,7 +12,7 @@ interface IProps {
 function PreviewActive({ children, section, props }: IProps) {
     const { state: { optionSelected } } = useContext(designContext)
 
-    return <Box {...props} border={`1px solid ${optionSelected === section ? '#E63F43' : 'transparent'}`} borderRadius="8px">{children}</Box>
+    return <Box borderRadius="8px" {...props} border={`1px solid ${optionSelected === section ? '#E63F43' : 'transparent'}`}>{children}</Box>
 }
 
 export default PreviewActive
