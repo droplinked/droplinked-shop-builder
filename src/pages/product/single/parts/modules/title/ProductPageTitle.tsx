18ab50d37e309c1276b3c8ff65057cd0570c817a
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-11-20 11:22:15 +0330

    up

--- src/pages/product/single/parts/modules/title/ProductPageTitle.tsx
+++ src/pages/product/single/parts/modules/title/ProductPageTitle.tsx
@@ -1,6 +1,4 @@
 import { Box, HStack, VStack } from '@chakra-ui/react'
-import { CardTitle } from 'components/common/card/component-style'
-import FieldLabel from 'components/common/form/fieldLabel/FieldLabel'
 import FieldLabelReuired from 'components/common/form/fieldLabel/parts/required/FieldLabelReuired'
 import AppTypography from 'components/common/typography/AppTypography'
 import React from 'react'
@@ -17,7 +15,7 @@ function ProductPageTitle({ description, title, head, isReuired }: IProps) {
         <VStack align={"stretch"}>
             <Box>
                 <HStack width={"fit-content"}>
-                    <AppTypography size='18px' weight={head ? 'bolder' : "bold"}>{title}</AppTypography>
+                    <AppTypography size='18px' fontWeight={head ? 'bold' : "normal"}>{title}</AppTypography>
                     {isReuired && <FieldLabelReuired fontSize={"larger"} fontFamily="aven" fontWeight={"bold"} />}
                 </HStack>
             </Box>
