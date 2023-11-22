23edd5749d9db90fd319302290c7b862fdb0b1bf
mehrpouya <mehrpouya.ramzi@gmail.com>
2023-11-22 11:03:44 +0330

    up

--- src/lib/utils/app/variable.js
+++ src/lib/utils/app/variable.js
@@ -1,4 +1,7 @@
+import { version } from "../../../../package.json"
+
 export const appDeveloment = typeof window !== "undefined" && window.location.origin !== "https://droplinked.com"
 
 export const BASE_URL = process.env.REACT_APP_BASE_API_URL
 export const SHOP_URL = appDeveloment ? "https://dev.droplinked.io" : "https://droplinked.io"
+export const appVersion = version
