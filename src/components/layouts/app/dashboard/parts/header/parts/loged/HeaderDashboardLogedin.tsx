930b9cf7fe88993df87739a4bed8260887c4b230
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-01-17 19:12:07 +0330

    Refactor: Improve some code snippets

--- src/components/layouts/app/dashboard/parts/header/parts/loged/HeaderDashboardLogedin.tsx
+++ src/components/layouts/app/dashboard/parts/header/parts/loged/HeaderDashboardLogedin.tsx
@@ -4,10 +4,9 @@ import AppImage from 'components/common/image/AppImage';
 import AppTypography from 'components/common/typography/AppTypography';
 import useHookStore from 'functions/hooks/store/useHookStore';
 import { useProfile } from 'functions/hooks/useProfile/useProfile';
-import { appDeveloment } from 'lib/utils/app/variable';
 import React, { useCallback } from 'react';
-import { Link } from 'react-router-dom';
 import { ShopnameText } from '../../HeaderLayout-style';
+import ProfileDropdownLinks from './parts/ProfileDropdownLinks';
 
 function HeaderDashboardLogedin() {
     const { onOpen, onClose, isOpen } = useDisclosure();
@@ -17,12 +16,6 @@ function HeaderDashboardLogedin() {
         logoutUser()
         onClose()
     }, []);
-    const links = [
-        { label: "Store", href: `https://${appDeveloment ? 'dev.' : ''}droplinked.io/${shop?.name}`, icon: <AppIcons.WhiteShopIcon width={"24px"} height={"24px"} color={"#FFFFFF"} /> },
-        { label: "Credit", href: "/dashboard", icon: <AppIcons.WhiteOpenWallet width={"24px"} height={"24px"} color={"#FFFFFF"} /> },
-        { label: "Settings", href: "/dashboard/settings/shop-info", icon: <AppIcons.SettingIcon width={"24px"} height={"24px"} color={"#FFFFFF"} /> },
-        { label: "Help", href: "https://droplinked.gitbook.io/droplinked-store-front-help-center/about-us/what-is-droplinked", icon: <AppIcons.HelpCenter width={"24px"} height={"24px"} color={"#FFFFFF"} /> },
-    ]
 
     return (
         <Popover
@@ -51,32 +44,11 @@ function HeaderDashboardLogedin() {
                 <PopoverBody>
                     <Flex direction={"column"} gap="24px">
                         <Flex alignItems={"center"} gap={"16px"}>
-                            <AppImage src={shop?.logo} objectFit={"cover"} width="48px" height="48px" borderRadius="4px" />
-                            <Flex direction={"column"} justifyContent={"space-between"}>
-                                <AppTypography color={"#FFFFFF"} fontSize={"16px"}>{shop?.name}</AppTypography>
-                                <AppTypography color={"#808080"} fontSize={"14px"}>Casper</AppTypography>
-                            </Flex>
+                            <AppImage src={shop?.logo} objectFit={"contain"} backgroundPosition={"center"} width="48px" height="48px" borderRadius="4px" />
+                            <AppTypography color={"#FFFFFF"} fontSize={"16px"}>{shop?.name}</AppTypography>
                         </Flex>
                         <Divider />
-                        <Flex direction={"column"} gap="24px">
-                            {links.map(link => {
-                                return link.label !== "Credit" ?
-                                    <Link to={link.href} target={"_blank"}>
-                                        <Flex alignItems={"center"} gap={"12px"}>
-                                            {link.icon}
-                                            <AppTypography color={"#FFFFFF"} fontSize={"16px"}>{link.label}</AppTypography>
-                                        </Flex>
-                                    </Link>
-                                    :
-                                    <Flex justifyContent={"space-between"} alignItems={"center"} gap={"12px"}>
-                                        <Flex alignItems={"center"} gap={"12px"}>
-                                            {link.icon}
-                                            <AppTypography color={"#FFFFFF"} fontSize={"16px"}>{link.label}</AppTypography>
-                                        </Flex>
-                                        <AppTypography color={"#2BCFA1"} fontSize={"16px"}>${shop?.credit.toFixed(2)}</AppTypography>
-                                    </Flex>
-                            })}
-                        </Flex>
+                        <ProfileDropdownLinks shop={shop} />
                         <Divider />
                         <Flex alignItems={"center"} gap={"12px"} cursor={"pointer"} onClick={logout}>
                             <AppIcons.Logout width={"24px"} height={"24px"} color={"#FFFFFF"} />
