5e62afedab9fa02fd32850f848641b6218e06552
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-01-21 11:39:49 +0330

    Redesign order modal

--- src/components/modals/order-modal/OrderModal.tsx
+++ src/components/modals/order-modal/OrderModal.tsx
@@ -1,13 +1,14 @@
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import LoadingComponent from "components/common/loading-component/LoadingComponent";
 import AppModal from 'components/common/modal/AppModal';
 import { IgetOrderService } from "lib/apis/order/interfaces";
 import { getOrderService } from "lib/apis/order/services";
 import React, { useEffect } from "react";
 import { useMutation } from "react-query";
-import BlockChainOrderModal from "./components/blockchain/BlockChainOrderModal";
-import CustomerInformationComponent from "./components/customer-information-component/customerInformationComponent";
-import OrderDetailComponent from "./components/order-detail-component/OrderDetailComponent";
-import LoadingComponent from "components/common/loading-component/LoadingComponent";
+import CustomerInformation from "./components/customer-information/CustomerInformation";
+import OrderDetails from "./components/order-details/OrderDetails";
+import OrderInformation from "./components/order-information/OrderInformation";
+import OrderItems from "./components/order-items/OrderItems";
 import orderModalContext from "./context";
 
 export default function OrderModal({ orderID, show, close }) {
@@ -17,17 +18,23 @@ export default function OrderModal({ orderID, show, close }) {
 
   return (
     <orderModalContext.Provider value={{ order: data?.data?.data }}>
-      <AppModal open={show} isCentered={false} close={close} size="3xl" contentProps={{ padding: 9 }}>
+      <AppModal open={show} isCentered={false} close={close} size="3xl" contentProps={{ padding: 9 }} title={"Order Details"}>
         <Flex justifyContent="center">
           {isLoading ? <LoadingComponent /> : (
-            <VStack align="stretch" spacing="24px" width="100%">
-              <CustomerInformationComponent />
-              <OrderDetailComponent />
-              <BlockChainOrderModal />
-            </VStack>
+            <Flex direction={"column"} gap={"36px"} width={"100%"}>
+              <OrderInformation />
+              <CustomerInformation />
+              <OrderDetails title="Shipping" />
+              <OrderDetails title="Tax" />
+              <OrderDetails title="Gift Card" />
+              <OrderDetails title="Affiliate" />
+              <OrderDetails title="Commission" />
+              <OrderDetails title="Payment Detail" />
+              <OrderItems />
+            </Flex>
           )}
         </Flex>
       </AppModal>
-    </orderModalContext.Provider>
+    </orderModalContext.Provider >
   )
 }
---
+++ src/components/modals/order-modal/components/customer-information/CustomerInformation.tsx
@@ -0,0 +1,28 @@
+import { Flex } from "@chakra-ui/react";
+import AppTypography from 'components/common/typography/AppTypography';
+import orderModalContext from "components/modals/order-modal/context";
+import React, { useContext } from "react";
+
+const CustomerInformation = () => {
+  const { order } = useContext(orderModalContext)
+  const information = [
+    { label: "Name", value: "Behdad Mansouri" },
+    { label: "Email", value: "Bedimns@gmail.com" },
+    { label: "Address", value: "229 Black Stallion Road, Fort Wayne, IN, Indiana, United State - 46805 ggrg ntkjn tkjrn tjrknrtjknrtk" },
+  ]
+
+  return (
+    <Flex direction={"column"} gap={"16px"}>
+      <AppTypography fontSize={"16px"} fontWeight={500} color={"#FFFFFF"}>Customer Information</AppTypography>
+      <Flex direction={"column"} gap={"12px"} as="dl">
+        {information.map((row, index) =>
+          <Flex key={index} alignItems={"start"}>
+            <AppTypography flexShrink={0} width={"200px"} fontSize={"14px"} color={"#C2C2C2"} as="dt">{row.label}</AppTypography>
+            <AppTypography fontSize={"14px"} fontWeight={500} color={"#FFFFFF"} as="dd">{row.value}</AppTypography>
+          </Flex>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+export default CustomerInformation;
