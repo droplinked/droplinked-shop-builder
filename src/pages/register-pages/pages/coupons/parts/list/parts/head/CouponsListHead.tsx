d522112b113f736315d8ac07d91144436d6534b9
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-01-30 10:10:33 +0330

    Fix account credit balance display issue

--- src/pages/register-pages/pages/coupons/parts/list/parts/head/CouponsListHead.tsx
+++ src/pages/register-pages/pages/coupons/parts/list/parts/head/CouponsListHead.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { Flex, HStack, useDisclosure, VStack } from '@chakra-ui/react'
 import BasicButton from 'components/common/BasicButton/BasicButton'
 import AppTypography from 'components/common/typography/AppTypography'
+import useAppStore from 'lib/stores/app/appStore'
+import React from 'react'
 import CouponsCreate from './parts/modal/create/CouponsCreate'
 import RechargeModel from './parts/modal/recharge/RechargeModel'
-import useAppStore from 'lib/stores/app/appStore'
 
 function CouponsListHead() {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -17,7 +17,7 @@ function CouponsListHead() {
                 <VStack align="stretch" spacing="12px">
                     <AppTypography color="#C2C2C2" fontSize="12px">Your Credit</AppTypography>
                     <HStack alignItems="center">
-                        <AppTypography color="#FFF" fontWeight="bold" fontSize="18px">$ {shop?.credit}</AppTypography>
+                        <AppTypography color="#FFF" fontWeight="bold" fontSize="18px">$ {shop?.credit?.toFixed(2)}</AppTypography>
                         <AppTypography color="#C2C2C2" fontSize="12px">USD</AppTypography>
                     </HStack>
                 </VStack>
