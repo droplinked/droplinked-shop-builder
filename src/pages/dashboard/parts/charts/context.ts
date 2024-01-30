ac572e113e8c1d92a8eeba64c6808f0cb91b734b
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-01-30 11:33:30 +0330

    feat: revenue chart interfaces

--- src/pages/dashboard/parts/charts/context.ts
+++ src/pages/dashboard/parts/charts/context.ts
@@ -1,29 +1,65 @@
 import { createContext } from "react";
 
+export interface IrevenueChartItem {
+    title: string;
+    value: number;
+    details: {
+        order: number;
+        revenue: number;
+        profit: number;
+        direct: number;
+        affiliate: number;
+    };
+}
+
+export interface IrevenueReports {
+    profit: {
+        affiliate: number;
+        direct: number;
+        value: number;
+    };
+    orders: {
+        affiliate: number;
+        direct: number;
+        value: number;
+    };
+    customerChart: {
+        affiliate: number;
+        direct: number;
+        value: number;
+    };
+}
+
+export interface IrevenueData {
+    chart: IrevenueChartItem[];
+    total: number;
+    report: IrevenueReports;
+}
+
 export interface IdashboardChartsStates {
-  revenue: any
-  date: 'WEEKLY' | 'MONTHLY' | 'YEARLY'
+    revenue: IrevenueData;
+    date: "WEEKLY" | "MONTHLY" | "YEARLY";
 }
 
 interface IProps {
-  states: IdashboardChartsStates
-  isLoading: boolean
-  method: {
-    updateStates: Function
-  }
+    states: IdashboardChartsStates;
+    isLoading: boolean;
+    method: {
+        updateStates: Function;
+    };
 }
 
 export const dashboardChartsStates: IdashboardChartsStates = {
-  revenue: null,
-  date: 'YEARLY'
-}
+    revenue: null,
+    date: "YEARLY",
+};
 
 const dashboardChartsContext = createContext<IProps>({
-  states: dashboardChartsStates,
-  method: {
-    updateStates: () => { }
-  },
-  isLoading: false
-})
+    states: dashboardChartsStates,
+    method: {
+        updateStates: () => {},
+    },
+    isLoading: false,
+});
 
-export default dashboardChartsContext
+export default dashboardChartsContext;
