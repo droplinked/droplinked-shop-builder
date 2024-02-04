96557086d6d0ca3ceb587090fe3e00e8f3d9ece8
mehrpouya <mehrpouya.ramzi@gmail.com>
2024-02-04 13:41:24 +0330

    up

--- src/lib/utils/app/variable.js
+++ src/lib/utils/app/variable.js
@@ -3,6 +3,6 @@ import { version } from "../../../../package.json"
 const origin = window.location.origin
 export const appDeveloment = typeof window !== "undefined" && window.location.origin !== "https://droplinked.com"
 
-export const BASE_URL = appDeveloment ? origin === "https://stage.droplinked.io" ? process.env.REACT_APP_BASE_API_URL_STAGE : process.env.REACT_APP_BASE_API_URL_DEV : process.env.REACT_APP_BASE_API_URL
+export const BASE_URL = appDeveloment ? origin === "https://stage.droplinked.com" ? process.env.REACT_APP_BASE_API_URL_STAGE : process.env.REACT_APP_BASE_API_URL_DEV : process.env.REACT_APP_BASE_API_URL
 export const SHOP_URL = appDeveloment ? "https://dev.droplinked.io" : "https://droplinked.io"
 export const appVersion = version
