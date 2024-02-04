4617a201ffdcca114ea22ef79fa2d5d3e8d3387e
mehrpouya <mehrpouya.ramzi@gmail.com>
2024-02-04 13:40:01 +0330

    up

--- src/lib/utils/app/variable.js
+++ src/lib/utils/app/variable.js
@@ -6,4 +6,3 @@ export const appDeveloment = typeof window !== "undefined" && window.location.or
 export const BASE_URL = appDeveloment ? origin === "https://stage.droplinked.io" ? process.env.REACT_APP_BASE_API_URL_STAGE : process.env.REACT_APP_BASE_API_URL_DEV : process.env.REACT_APP_BASE_API_URL
 export const SHOP_URL = appDeveloment ? "https://dev.droplinked.io" : "https://droplinked.io"
 export const appVersion = version
-console.log('BASE_URL', BASE_URL);
