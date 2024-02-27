24a28aca5837479f5140fec4fdcff2d7ec2c936c
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-02-27 09:48:43 +0330

    Improve order modal functionality

--- src/components/modals/order-modal/parts/order-details/OrderDetails.tsx
+++ src/components/modals/order-modal/parts/order-details/OrderDetails.tsx
@@ -10,7 +10,7 @@ export default function OrderDetails() {
 
     return (
         <>
-            {orderDetails && orderDetails.length ? orderDetails.filter(section => section.renderCondition !== false).map((section, index) => <Flex direction={"column"} gap={"16px"} key={index}>
+            {orderDetails.map((section, index) => <Flex direction={"column"} gap={"16px"} key={index}>
                 <AppTypography fontSize={"16px"} fontWeight={500} color={"#FFFFFF"}>{section.title}</AppTypography>
                 <Flex direction={"column"} gap={"12px"}>
                     {section.rows.map((row, index) =>
@@ -20,7 +20,7 @@ export default function OrderDetails() {
                         </Flex>
                     )}
                 </Flex>
-            </Flex>) : null}
+            </Flex>)}
         </>
     )
 }
