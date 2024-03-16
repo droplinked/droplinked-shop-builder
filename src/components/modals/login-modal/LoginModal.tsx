aeacbbf3d5450cf9aedb3f9130f72eda3f50f80a
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-03-16 10:52:24 +0330

    feat: auth modals referral feature

--- src/components/modals/login-modal/LoginModal.tsx
+++ src/components/modals/login-modal/LoginModal.tsx
@@ -2,141 +2,137 @@ import { Box } from "@chakra-ui/react";
 import BasicButton from "components/common/BasicButton/BasicButton";
 import AppInput from "components/common/form/textbox/AppInput";
 import AppModal from "components/common/modal/AppModal";
-import { Form, Formik } from 'formik';
+import { Form, Formik } from "formik";
 import useHookStore from "functions/hooks/store/useHookStore";
 import useAppToast from "functions/hooks/toast/useToast";
 import { useCustomNavigate } from "functions/hooks/useCustomeNavigate/useCustomNavigate";
 import { appDeveloment } from "lib/utils/app/variable";
 import AppErrors from "lib/utils/statics/errors/errors";
 import React from "react";
-import * as Yup from 'yup';
-import { BottomText } from "./LoginModal-style";
+import * as Yup from "yup";
+import AppTypography from "components/common/typography/AppTypography";
 
 interface Iform {
-  email: string
-  password: string
+    email: string;
+    password: string;
 }
 
 const LoginModal = ({ show, close, switchModal, switchReset }) => {
-  const { app: { login, loading } } = useHookStore()
-  const { showToast } = useAppToast()
-  const { shopNavigate } = useCustomNavigate()
+    const {
+        app: { login, loading },
+    } = useHookStore();
+    const { showToast } = useAppToast();
+    const { shopNavigate } = useCustomNavigate();
 
-  // submit form function
-  const onSubmit = async (data: Iform) => {
-    try {
-      let result = await login(data)
-      if (result) loginFunction(result);
-    } catch (error) {
-      showToast({ message: error?.message, type: "error" });
-    }
-  };
+    // submit form function
+    const onSubmit = async (data: Iform) => {
+        try {
+            let result = await login(data);
+            if (result) loginFunction(result);
+        } catch (error) {
+            showToast({ message: error?.message, type: "error" });
+        }
+    };
 
-  // action on user data based on type and status
-  const loginFunction = (data: any) => {
-    // check customer
-    if (data.user.type !== "PRODUCER") return showToast({ message: "This account can not login", type: "error" });
+    // action on user data based on type and status
+    const loginFunction = (data: any) => {
+        // check customer
+        if (data.user.type !== "PRODUCER") return showToast({ message: "This account can not login", type: "error" });
 
-    //first close modal
-    close();
-    const status = appDeveloment && data.user.status === "NEW" ? "VERIFIED" : data.user.status
+        //first close modal
+        close();
+        const status = appDeveloment && data.user.status === "NEW" ? "VERIFIED" : data.user.status;
 
-    if (status === "DELETED") {
-      showToast({ message: "This account has been deleted", type: "error" });
-      return;
-    } else {
-      navigateUser(status, data);
-      return;
-    }
-  };
+        if (status === "DELETED") {
+            showToast({ message: "This account has been deleted", type: "error" });
+            return;
+        } else {
+            navigateUser(status, data);
+            return;
+        }
+    };
 
-  // navigate user based on status
-  const navigateUser = (status: string, data: any) => {
-    // eslint-disable-next-line default-case
-    switch (status) {
-      case "NEW":
-        localStorage.setItem("registerEmail", JSON.stringify(data.user.email));
-        shopNavigate("/email-confirmation");
-        return;
-      case "VERIFIED":
-        shopNavigate(`register/shop-info`);
-        return;
-      case "PROFILE_COMPLETED":
-        shopNavigate(`register/shop-info`);
-        return;
-      case "SHOP_INFO_COMPLETED":
-        shopNavigate(``);
-        return;
-      case "IMS_TYPE_COMPLETED":
-        shopNavigate(``);
-        return;
-      case "ACTIVE":
-        shopNavigate(``);
-        return;
-    }
-  };
+    // navigate user based on status
+    const navigateUser = (status: string, data: any) => {
+        // eslint-disable-next-line default-case
+        switch (status) {
+            case "NEW":
+                localStorage.setItem("registerEmail", JSON.stringify(data.user.email));
+                shopNavigate("/email-confirmation");
+                return;
+            case "VERIFIED":
+                shopNavigate(`register/shop-info`);
+                return;
+            case "PROFILE_COMPLETED":
+                shopNavigate(`register/shop-info`);
+                return;
+            case "SHOP_INFO_COMPLETED":
+                shopNavigate(``);
+                return;
+            case "IMS_TYPE_COMPLETED":
+                shopNavigate(``);
+                return;
+            case "ACTIVE":
+                shopNavigate(``);
+                return;
+        }
+    };
 
-  const formSchema = Yup.object().shape({
-    email: Yup.string().email(AppErrors.signin.invalid_email_address).required('Required'),
-    password: Yup.string().required('Required'),
-  });
+    const formSchema = Yup.object().shape({
+        email: Yup.string().email(AppErrors.signin.invalid_email_address).required("Required"),
+        password: Yup.string().required("Required"),
+    });
 
-  return (
-    <AppModal open={show} title="Sign in" close={close}>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validateOnChange={false}
-        validationSchema={formSchema}
-        onSubmit={onSubmit}
-      >
-
-        {({ errors, values, setFieldValue }) => (
-          <Form>
-            <Box w="100%">
-              <Box w="100%">
-                <AppInput
-                  error={errors.email ? errors.email.toString() : ""}
-                  name="email"
-                  onChange={(e) => setFieldValue("email", e.target.value)}
-                  value={values.email}
-                />
-                <Box mb="16px"></Box>
-                <AppInput
-                  type="password"
-                  name="password"
-                  error={errors.password ? errors.password.toString() : ""}
-                  onChange={(e) => setFieldValue("password", e.target.value)}
-                  value={values.password}
-                />
-                <Box mb="16px"></Box>
-                <BasicButton type="submit" minWidth={"100%"} isDisabled={loading} isLoading={loading}>
-                  Login
-                </BasicButton>
-              </Box>
-              <Box mb="8px"></Box>
-              <BottomText onClick={switchReset}>
-                Forgot
-                <Box as="span" ml={1} color="green.500">
-                  password?
-                </Box>
-              </BottomText>
-              <Box mb="4px"></Box>
-              <BottomText onClick={switchModal}>
-                Don’t have an account?{" "}
-                <Box as="span" mx={1} color="green.500">
-                  Sign up
-                </Box>
-                now!
-              </BottomText>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </AppModal>
-  );
+    return (
+        <AppModal open={show} title="Sign in" close={close}>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                validateOnChange={false}
+                validationSchema={formSchema}
+                onSubmit={onSubmit}
+            >
+                {({ errors, values, setFieldValue }) => (
+                    <Form>
+                        <Box w="100%">
+                            <Box w="100%">
+                                <AppInput error={errors.email ? errors.email.toString() : ""} name="email" onChange={(e) => setFieldValue("email", e.target.value)} value={values.email} />
+                                <Box mb="16px"></Box>
+                                <AppInput
+                                    type="password"
+                                    name="password"
+                                    error={errors.password ? errors.password.toString() : ""}
+                                    onChange={(e) => setFieldValue("password", e.target.value)}
+                                    value={values.password}
+                                />
+                                <Box mb="16px"></Box>
+                                <BasicButton type="submit" minWidth={"100%"} isDisabled={loading} isLoading={loading}>
+                                    Login
+                                </BasicButton>
+                            </Box>
+                            <Box mb="8px"></Box>
+                            <AppTypography fontWeight={"400"} fontSize={{ base: "12px", md: "14px" }} color={"white"} cursor={"pointer"} _hover={{ color: "#b3b3b3" }} onClick={switchReset}>
+                                Forgot
+                                <Box as="span" ml={1} color="green.500">
+                                    password?
+                                </Box>
+                            </AppTypography>
+                            <Box mb="4px"></Box>
+                            <AppTypography fontWeight={"400"} fontSize={{ base: "12px", md: "14px" }} color={"white"} cursor={"pointer"} _hover={{ color: "#b3b3b3" }} onClick={switchModal}>
+                                Don’t have an account?{" "}
+                                <Box as="span" mx={1} color="green.500">
+                                    Sign up
+                                </Box>
+                                now!
+                            </AppTypography>
+                        </Box>
+                    </Form>
+                )}
+            </Formik>
+        </AppModal>
+    );
 };
 
 export default LoginModal;
