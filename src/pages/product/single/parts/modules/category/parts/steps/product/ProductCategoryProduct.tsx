e2549b038e77b37e12fd56d834dd8769d35965b6
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-03-17 19:51:14 +0330

    Improve POD product category design

--- src/pages/product/single/parts/modules/category/parts/steps/product/ProductCategoryProduct.tsx
+++ src/pages/product/single/parts/modules/category/parts/steps/product/ProductCategoryProduct.tsx
@@ -23,9 +23,10 @@ function ProductCategoryProduct() {
     <>
       {isLoading ? <Flex justifyContent="center"><LoadingComponent /></Flex > : data?.data?.data?.data.length ?
         (
-          <SimpleGrid columns={5} spacing="13px">
-            {data && data?.data?.data?.data.map((el, key) => (
-              <CategoryBox key={key} padding="10px" onClick={async () => {
+          <SimpleGrid columns={5} spacing="12px">
+            {data && data?.data?.data?.data.map((el, key) => {
+              const titleLimit = !!el.rating ? 30 : 45
+              return <CategoryBox key={key} padding="10px" onClick={async () => {
                 ProductTypeModel.updateProductType({ value: el.id.toString(), updateState: product.methods.updateState })
                 dispatch({
                   type: "updateProduct", params: {
@@ -36,26 +37,26 @@ function ProductCategoryProduct() {
                 product.methods.updateState('title', el?.title)
                 product.methods.updateState('description', `<p>${el?.description}</p>`)
               }}>
-                <Flex direction={"column"} gap={3} height={"fit-content"}>
-                  <Flex justifyContent="center"><Image src={el?.image} alt={el?.title} borderRadius="5px" width="100%" /></Flex>
-                  <Flex direction={"column"} gap={2}>
-                    <AppTypography fontSize='14px' fontWeight={500} height={"40px"}>
+                <Flex height={"full"} direction={"column"} gap={3}>
+                  <Image src={el?.image} alt={el?.title} borderRadius="5px" width="100%" />
+                  <Flex flex={1} direction={"column"} justifyContent={"space-between"} gap={3}>
+                    <AppTypography fontSize='14px' fontWeight={500}>
                       {
-                        el.title.length <= 30 ? el.title :
-                          <AppTooltip label={el.title}>{`${el.title.slice(0, 30)}...`}</AppTooltip>
+                        el.title.length <= titleLimit ? el.title :
+                          <AppTooltip label={el.title}>{`${el.title.slice(0, titleLimit)}...`}</AppTooltip>
                       }
                     </AppTypography>
                     {el.priceRange && <AppTypography color="#C2C2C2">{el.priceRange}</AppTypography>}
+                    {el.rating &&
+                      <Flex justifyContent="space-between" alignItems={"center"}>
+                        <AppTypography color="#C2C2C2">Quality</AppTypography>
+                        <StarRating rate={el.rating} />
+                      </Flex>
+                    }
                   </Flex>
-                  {el.rating &&
-                    <Flex justifyContent="space-between" alignItems={"center"}>
-                      <AppTypography color="#C2C2C2">Quality</AppTypography>
-                      <StarRating rate={el.rating} />
-                    </Flex>
-                  }
                 </Flex>
               </CategoryBox>
-            ))}
+            })}
           </SimpleGrid>
         )
         :
