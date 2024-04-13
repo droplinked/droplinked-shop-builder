a9f3f63d3a3ff745ec71f79f526ff11bed521c6c
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-04-13 11:39:22 +0330

    Hide non-functional buttons in analytics page

--- src/pages/dashboard/parts/parts/headcard/HeadCardDashboard.tsx
+++ src/pages/dashboard/parts/parts/headcard/HeadCardDashboard.tsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 interface IProps {
     title: string
-    link: string
+    link?: string
 }
 function HeadCardDashboard({ link, title }: IProps) {
     return (
         <Flex justifyContent="space-between" alignItems="center">
             <AppTypography color="#FFF" fontSize="16px">{title}</AppTypography>
-            <Link to={link}><AppTypography color="#33A9EC" fontSize="14px">See all</AppTypography></Link>
+            {link && <Link to={link}><AppTypography color="#33A9EC" fontSize="14px">See all</AppTypography></Link>}
         </Flex>
     )
 }
