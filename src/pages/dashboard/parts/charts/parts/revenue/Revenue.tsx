bb8b535d24ad49392cfc86d998e2e908804e5b0a
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-04-15 21:41:22 +0330

    Correct a typo in analytics page: "Total earning" to "Total earnings"

--- src/pages/dashboard/parts/charts/parts/revenue/Revenue.tsx
+++ src/pages/dashboard/parts/charts/parts/revenue/Revenue.tsx
@@ -1,9 +1,9 @@
-import { Flex, VStack } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 import AppSkeleton from 'components/common/skeleton/AppSkeleton';
 import AppTypography from 'components/common/typography/AppTypography'
 import moment from 'moment';
 import dashboardPageContext from 'pages/dashboard/context';
-import React, { useContext, useMemo } from 'react'
+import React, { useContext } from 'react'
 import dashboardChartsContext from '../../context';
 
 function Revenue() {
@@ -11,12 +11,10 @@ function Revenue() {
     const { states: { revenue }, isLoading } = useContext(dashboardChartsContext)
 
     return (
-        <VStack align="stretch" spacing="3px">
-            <AppTypography fontSize="16px">Total earning</AppTypography>
+        <VStack align="stretch" spacing="4px">
+            <AppTypography fontSize="16px">Total earnings</AppTypography>
             <AppSkeleton isLoaded={isLoading}>
-                <Flex alignItems="center" gap="12px">
-                    <AppTypography fontSize="32px" fontWeight="600">${revenue?.total.toFixed(2)} USD</AppTypography>
-                </Flex>
+                <AppTypography fontSize="32px" fontWeight="600">${revenue?.total.toFixed(2)} USD</AppTypography>
             </AppSkeleton>
             <AppTypography fontSize="12px" color="#C4C4C4">Your revenue from <strong>{moment(from).format('YYYY/MM/DD')}</strong> to <strong>{moment(to).format('YYYY/MM/DD')}</strong></AppTypography>
         </VStack>
