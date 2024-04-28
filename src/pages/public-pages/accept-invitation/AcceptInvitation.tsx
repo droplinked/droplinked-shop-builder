85e5e66b3a19f6d82fe258c474bcb4a9483b1928
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-04-28 19:20:43 +0330

    Add visual effects in invitation page

--- src/pages/public-pages/accept-invitation/AcceptInvitation.tsx
+++ src/pages/public-pages/accept-invitation/AcceptInvitation.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Spinner } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image, Spinner } from '@chakra-ui/react'
 import AppTypography from 'components/common/typography/AppTypography'
 import useAppToast from 'functions/hooks/toast/useToast'
 import { useCustomNavigate } from 'functions/hooks/useCustomeNavigate/useCustomNavigate'
@@ -23,27 +23,32 @@ function AcceptInvitation() {
     })
 
     return (
-        <Flex justifyContent={"center"} paddingBlock={120} paddingInline={{ base: "16px", sm: "28px" }}>
-            {
-                isFetching ?
-                    <Flex minHeight={"200px"} placeItems={"center"}><Spinner color='#fff' /></Flex> :
-                    <Flex
-                        width={{ base: "100%", lg: "50%" }}
-                        maxWidth="1400px"
-                        direction={"column"}
-                        gap={12}
-                        borderRadius={24}
-                        backgroundColor={"#1c1c1c"}
-                        padding={{ base: 6, md: 12 }}
-                    >
-                        <Flex direction={"column"} gap={2}>
-                            <Heading margin={0} fontSize={24} fontWeight={700} color={"#fff"}>Welcome to droplinked!</Heading>
-                            <AppTypography fontSize={14} color={"#fff"}>Sign up with your credentials to start managing your shop <Box as='span' fontWeight={600}>{data?.data.data.shop}</Box></AppTypography>
+        <Box position="relative" >
+            <Image src='/assets/images/homepage/ef1.png' position="fixed" top={{ base: "-100px", md: "-300px" }} right={{ base: "-200px", lg: "0" }} zIndex="0" />
+            <Image src='/assets/images/homepage/ef2.png' width="800px" position="absolute" bottom="-300px" left="0" zIndex="0" />
+            <Flex justifyContent={"center"} paddingBlock={120} paddingInline={{ base: "16px", sm: "28px" }}>
+                {
+                    isFetching ?
+                        <Flex minHeight={"200px"} placeItems={"center"}><Spinner color='#fff' /></Flex> :
+                        <Flex
+                            width={{ base: "100%", lg: "50%" }}
+                            maxWidth="1400px"
+                            direction={"column"}
+                            gap={12}
+                            borderRadius={24}
+                            backgroundColor={"#1c1c1c"}
+                            padding={{ base: 6, md: 12 }}
+                            zIndex={1}
+                        >
+                            <Flex direction={"column"} gap={2}>
+                                <Heading margin={0} fontSize={24} fontWeight={700} color={"#fff"}>Welcome to droplinked!</Heading>
+                                <AppTypography fontSize={14} color={"#fff"}>Sign up with your credentials to start managing your shop <Box as='span' fontWeight={600}>{data?.data.data.shop}</Box></AppTypography>
+                            </Flex>
+                            <InvitationForm invitationId={invitationId} email={data?.data.data.email || ""} />
                         </Flex>
-                        <InvitationForm invitationId={invitationId} email={data?.data.data.email || ""} />
-                    </Flex>
-            }
-        </Flex>
+                }
+            </Flex>
+        </Box>
     )
 }
 
