70aea5d0d867ed4d6b83ae57a084a7ce510a9505
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-05-01 01:37:43 +0330

    code cleanup

--- src/lib/utils/chains/providers/casper/casperProvider.ts
+++ src/lib/utils/chains/providers/casper/casperProvider.ts
@@ -1,10 +1,10 @@
 import { Chain, Network } from "../../dto/chains";
 import { ChainProvider } from "../../chainProvider";
 import { casperApproveRequest, casperDisapproveRequest } from "./casperAffiliate";
-import { casperPublishRequest } from "./casperPublish";
 import { casperRecordMerch } from "./casperRecord";
 import { casper_login } from "./casperWalletAuth";
-import { Beneficiary, ProductType } from "../../dto/chainStructs";
+import { DeployedShop, ProductType, Beneficiary, Uint256 } from "../../dto/chainStructs";
+import { ModalInterface } from "../../dto/modalInterface";
 
 export class CasperProvider implements ChainProvider{
     network: Network = Network.TESTNET;
@@ -14,22 +14,26 @@ export class CasperProvider implements ChainProvider{
         this.chain = _chain;
         this.network = _network;
     }
-    deployShop(shopName: string, shopAddress: string, shopOwner: string, shopLogo: string, shopDescription: string): Promise<string> {
+    deployShop(shopName: string, shopAddress: string, shopOwner: string, shopLogo: string, shopDescription: string): Promise<DeployedShop> {
         throw new Error("Method not implemented.");
     }
+    recordProduct(sku_properties: any, product_title: string, description: string, image_url: string, price: number, amount: number, commission: number, type: ProductType, beneficiaries: Beneficiary[], acceptsManageWallet: boolean, royalty: number, nftContract: string, shopAddress: string, currencyAddress: string, apiKey: string): Promise<{ transactionHash: string; productId: Uint256; amountRecorded: Uint256; }> {
+        throw new Error("Method not implemented.");
+    }
+    publishRequest(producerAccountAddress: string, tokenId: Uint256): Promise<{ transactionHash: string; requestId: Uint256; publisher: string; }> {
+        throw new Error("Method not implemented.");
+    }
+    setModal(modal: ModalInterface): ChainProvider {
+        throw new Error("Method not implemented.");
+    }
+    
     async walletLogin(): Promise<any> {
         return casper_login();
     }
     async casperRecordProduct(skuProperties: any, productTitle: string, description: string, imageUrl: string, price: number, amount: number, commission: number, apiKey: string): Promise<string> {
         return await casperRecordMerch(this.network, skuProperties, this.address, productTitle, description, imageUrl, price, amount, commission, apiKey);
     }
-    async recordProduct(sku_properties: any, product_title: string, discription: string, image_url: string, price: number, amount: number, commission: number, type: ProductType, paymentWallet: string, beneficiaries: Beneficiary[], acceptsManageWallet: boolean, royalty: number, apiKey: string): Promise<string> {
-        // nothing
-        return "";
-    }
-    async publishRequest(producerAccountAddress: string, tokenId: string | number): Promise<string> {
-        return await casperPublishRequest(this.network, this.address, producerAccountAddress, tokenId)
-    }
+  
     async approveRequest(requestId: number): Promise<string> {
         return await casperApproveRequest(this.network, this.address, requestId);
     }
