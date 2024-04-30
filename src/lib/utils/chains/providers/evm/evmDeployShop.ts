484803f4b25004dc26e2fd945178b8277ae1f873
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-04-30 20:28:01 +0330

    modify deploy shop logic

--- src/lib/utils/chains/providers/evm/evmDeployShop.ts
+++ src/lib/utils/chains/providers/evm/evmDeployShop.ts
@@ -1,15 +1,14 @@
 import { ethers } from 'ethers';
 import { Chain, Network } from "../../dto/chains";
-import { getDeployerAddress, getShopByteCode } from "../../dto/chainConstants";
+import { getDeployerAddress, getGasPrice, getShopByteCode } from "../../dto/chainConstants";
 import { deployerABI } from "../../dto/chainABI";
 import { chainLink } from '../../dto/chainLinkAddresses';
-import { DeployedShop } from '../../dto/chainStructs';
 
 function sixify(num: number): string {
     return num.toString().padStart(6, "0");
 }
 
-export async function EVMDeployShop(chain: Chain, network: Network, address: string, shopName: string, shopAddress: string, shopOwner: string, shopLogo: string, shopDescription: string): Promise<DeployedShop> {
+export async function EVMDeployShop(chain: Chain, network: Network, address: string, shopName: string, shopAddress: string, shopOwner: string, shopLogo: string, shopDescription: string) {
     const provider = new ethers.providers.Web3Provider((window as any).ethereum);
     const signer = provider.getSigner();
     if ((await signer.getAddress()).toLocaleLowerCase() !== address.toLocaleLowerCase()) {
@@ -21,14 +20,25 @@ export async function EVMDeployShop(chain: Chain, network: Network, address: str
     const byteCode = await getShopByteCode();
     const salt = "0x" + address.split("0x")[1] + "000000000000000000" + sixify((await provider.getTransactionCount(address)) + 1);
     const constructorArgs = [shopName, shopAddress, shopOwner, shopLogo, shopDescription, deployerAddress, chainLink[chain][network]];
-    console.log(constructorArgs)
     const bytecodeWithArgs = ethers.utils.defaultAbiCoder.encode(["string", "string", "address", "string", "string", "address", "address"], constructorArgs);
-    console.log(byteCode + bytecodeWithArgs.split("0x")[1])
-    const tx = await contract.deployShop(byteCode + bytecodeWithArgs.split("0x")[1], salt,);
-    const receipt = await tx.wait();
-    const logs = receipt.logs.map((log: any) => contract.interface.parseLog(log)) as any;
-    const shopDeployLog = logs.find((log: any) => log.name === "ShopDeployed");
-    const deployedShopAddress = shopDeployLog.args.shop;
-    const deployedNFTAddress = shopDeployLog.args.nftContract;
-    return { transactionHash: tx.hash, shop: deployedShopAddress, nft: deployedNFTAddress };
+    try {
+        await contract.callStatic.deployShop(byteCode + bytecodeWithArgs.split("0x")[1], salt,);
+        const gasEstimation = (await contract.estimateGas.deployShop(byteCode + bytecodeWithArgs.split("0x")[1], salt,)).toBigInt().valueOf();
+        const tx = await contract.deployShop(byteCode + bytecodeWithArgs.split("0x")[1], salt, {
+            gasLimit: gasEstimation * BigInt(105) / BigInt(100),
+            gasPrice: getGasPrice(provider)
+        })
+        const receipt = await tx.wait();
+        const logs = receipt.logs.map((log: any) => contract.interface.parseLog(log)) as any;
+        const shopDeployLog = logs.find((log: any) => log.name === "ShopDeployed");
+        const deployedShopAddress = shopDeployLog.args.shop;
+        const deployedNFTAddress = shopDeployLog.args.nftContract;
+        return { transaction_id: tx.hash, deployedShopAddress, deployedNFTAddress };
+    } catch (e: any) {
+        if (e.code.toString() === "ACTION_REJECTED") {
+            throw new Error("Transaction Rejected");
+        }
+        const err = contract.interface.parseError(e.data);
+        throw err;
+    }
 }
