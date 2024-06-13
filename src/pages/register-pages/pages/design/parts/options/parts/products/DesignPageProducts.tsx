a04d3d942cb84df0c201424b1339158651db839e
Aref Orumiehei <areforumiehei@gmail.com>
2024-06-13 14:08:37 +0330

    ✨ add: switch for show/hide collections

--- src/pages/register-pages/pages/design/parts/options/parts/products/DesignPageProducts.tsx
+++ src/pages/register-pages/pages/design/parts/options/parts/products/DesignPageProducts.tsx
@@ -2,12 +2,14 @@ import { VStack } from '@chakra-ui/react'
 import React from 'react'
 import DesignPageCard from '../card/DesignPageCard'
 import ProductSwitch from './parts/switch/ProductSwitch'
+import CollectionsSwitch from './parts/switch/CollectionsSwitch'
 
 function DesignPageProducts() {
     return (
         <DesignPageCard title='Displayed text' section='products'>
             <VStack align="stretch" spacing="24px">
                 <ProductSwitch />
+                <CollectionsSwitch />
             </VStack>
         </DesignPageCard>
     )
