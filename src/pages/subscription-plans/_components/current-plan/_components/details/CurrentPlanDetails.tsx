c03c063d02c9ac38c1950b791dafceb61d45a816
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-06-21 20:53:28 +0330

    Add expiration date in current plan's details and use local images in subscription payment status modal

--- src/pages/subscription-plans/_components/current-plan/_components/details/CurrentPlanDetails.tsx
+++ src/pages/subscription-plans/_components/current-plan/_components/details/CurrentPlanDetails.tsx
@@ -7,7 +7,7 @@ import Container from './Container'
 import LegalUsageItem from './LegalUsageItem'
 
 function CurrentPlanDetails({ shopSubscriptionData }: { shopSubscriptionData: ShopSubscriptionData }) {
-    const { subscriptionId: { type, description }, legalUsage } = shopSubscriptionData
+    const { subscriptionId: { type, description }, legalUsage, expiresAt } = shopSubscriptionData
 
     return (
         <SimpleGrid columns={{ base: 1, md: 2, xl: 4 }} gap={4}>
@@ -18,7 +18,16 @@ function CurrentPlanDetails({ shopSubscriptionData }: { shopSubscriptionData: Sh
                     <PlanHeading planTitle={type} />
                     {/* {type !== "STARTER" && <BasicButton sizes='medium'>renewal Plan</BasicButton>} */}
                 </Flex>
+
                 <AppTypography color={"white"}>{description}</AppTypography>
+            </Container>
+
+            {/* current plan's expiration date */}
+            <Container>
+                <AppTypography fontSize={14} fontWeight={600} color={"white"}>Expiration Date</AppTypography>
+                <AppTypography fontSize={14} color={"white"}>
+                    Your plan will expire on {(new Date(expiresAt)).toLocaleDateString("en-US", { minute: "numeric", hour: "numeric", day: "numeric", month: "long", year: "numeric" })}
+                </AppTypography>
             </Container>
 
             {/* product types */}
