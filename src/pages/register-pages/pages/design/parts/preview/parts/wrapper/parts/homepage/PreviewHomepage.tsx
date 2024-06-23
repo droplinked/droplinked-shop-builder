ef78b7395ea8d1f49dffdbd094bd881bde253f6a
Aref Orumiehei <areforumiehei@gmail.com>
2024-06-23 14:23:00 +0330

    ⚡️ fix: change scroll target sections in home page preview

--- src/pages/register-pages/pages/design/parts/preview/parts/wrapper/parts/homepage/PreviewHomepage.tsx
+++ src/pages/register-pages/pages/design/parts/preview/parts/wrapper/parts/homepage/PreviewHomepage.tsx
@@ -11,13 +11,14 @@ function PreviewHomepage() {
     const { scrollRef } = useContext(designPreviewContext)
 
     const isDesktop = useMemo(() => device === "desktop", [device])
+    const scrollTargetSections = isCollectionShown ? ["profile"] : [ "products", "profile"]
     
     return (
-        <Flex ref={["products", "profile"].includes(optionSelected) ? scrollRef : null} justifyContent="center" padding={isDesktop ? "80px 0" : "10px 0"} {...template_options?.['--dlk-wrp']?.['--dlk-wrp-styles']}>
+        <Flex ref={scrollTargetSections.includes(optionSelected) ? scrollRef : null} justifyContent="center" padding={isDesktop ? "80px 0" : "10px 0"} {...template_options?.['--dlk-wrp']?.['--dlk-wrp-styles']}>
             <Flex width={isDesktop ? "85%" : "100%"} gap={isDesktop ? "40px" : "20px"} alignItems="flex-start" flexDirection={isDesktop ? "row" : "column"}>
                 <Box width={isDesktop ? "25%" : "100%"}><PreviewProfile /></Box>
                 <Box width={isDesktop ? "75%" : "100%"}>
-                    <Flex flexDirection={"column"} gap={"24px"} alignItems={"flex-start"} alignSelf={"stretch"}>
+                    <Flex flexDirection={"column"} gap={"16px"} alignItems={"flex-start"} alignSelf={"stretch"}>
                         {isCollectionShown && <PreviewCollections />}
                         <PreviewProducts />
                     </Flex>
