018edf711131b750ef8ce87b17899d79ab6cc353
Aref Orumiehei <areforumiehei@gmail.com>
2024-06-23 14:20:32 +0330

    ⚡️ fix: add red border for editable components

--- src/pages/register-pages/pages/design/parts/preview/parts/wrapper/parts/homepage/parts/collections/PreviewCollections.tsx
+++ src/pages/register-pages/pages/design/parts/preview/parts/wrapper/parts/homepage/parts/collections/PreviewCollections.tsx
@@ -7,6 +7,8 @@ import CollectionCard from "./collection-card/CollectionCard";
 
 //Contexts
 import { designContext } from "pages/register-pages/pages/design/design-context";
+import PreviewActive from "../../../../../parts/active/PreviewActive";
+import designPreviewContext from "pages/register-pages/pages/design/parts/preview/context";
 
 // Sample Collection Data
 const collections = [
@@ -25,25 +27,37 @@ const collections = [
 ];
 
 const PreviewCollections = () => {
-  const { state: { device } } = useContext(designContext)
+  const { state: { device, optionSelected } } = useContext(designContext)
+  const { scrollRef } = useContext(designPreviewContext)
+
   const isDesktop = useMemo(() => device === "desktop", [device])
 
   return (
-    <Flex alignItems={"center"} justifyContent={"center"} gap={"14px"} flexDirection={"column"} alignSelf={"stretch"}>
-      <Flex alignItems={"center"} justifyContent={"space-between"} alignSelf={"stretch"} paddingX={isDesktop ? "" : "10px"}>
-        <PreviewTypo fontSize={"16px"} fontWeight={700}>Collections</PreviewTypo>
-        {/* <PreviewTypo fontSize={"12px"} fontWeight={500} color={"#2BCFA1"} textShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}>See all</PreviewTypo> */}
+    <PreviewActive 
+      section="products"
+      props={{
+        ...optionSelected === "products" && { ref: scrollRef },
+        display: "flex",
+        padding: "0 6px",
+        borderRadius: "8px",
+    }}
+    >
+      <Flex alignItems={"center"} justifyContent={"center"} gap={"4px"} flexDirection={"column"} alignSelf={"stretch"}>
+        <Flex alignItems={"center"} justifyContent={"space-between"} alignSelf={"stretch"} paddingX={isDesktop ? "" : "10px"}>
+          <PreviewTypo fontSize={"16px"} fontWeight={700}>Collections</PreviewTypo>
+          {/* <PreviewTypo fontSize={"12px"} fontWeight={500} color={"#2BCFA1"} textShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}>See all</PreviewTypo> */}
+        </Flex>
+        <SimpleGrid columns={isDesktop ? { base: 2, xl: 3 } : 2} spacing={isDesktop ? "25px" : "10px"}>
+          {collections.map((collection, index) => (
+            <CollectionCard 
+              key={index}
+              imageSrc={collection.imageSrc} 
+              title={collection.title} 
+            />
+          ))}
+        </SimpleGrid>
       </Flex>
-      <SimpleGrid columns={isDesktop ? { base: 2, xl: 3 } : 2} spacing={isDesktop ? "25px" : "10px"}>
-        {collections.map((collection, index) => (
-          <CollectionCard 
-            key={index}
-            imageSrc={collection.imageSrc} 
-            title={collection.title} 
-          />
-        ))}
-      </SimpleGrid>
-    </Flex>
+    </PreviewActive>
   )
 }
 
