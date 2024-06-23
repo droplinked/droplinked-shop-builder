f65a046e5f96d2fad8cf3d90753dfe174463f791
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-06-23 10:54:03 +0330

    Filter plan features based on prev plan

--- src/pages/subscription-plans/_components/plans/Plans.tsx
+++ src/pages/subscription-plans/_components/plans/Plans.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import AppTypography from "components/common/typography/AppTypography";
+import { SubOptionId, SubscriptionPlan } from "lib/apis/subscription/interfaces";
 import { getSubscriptionPlansService } from "lib/apis/subscription/subscriptionServices";
 import AppErrors from "lib/utils/statics/errors/errors";
 import React from "react";
@@ -22,9 +23,41 @@ const Plans = ({ showBuyButton }: { showBuyButton: boolean }) => {
 
     if (isError) return <AppTypography fontSize={16} color={"red.400"}>{AppErrors.permission.shop_subscription_data_unavailable}</AppTypography>
 
+    const getFilteredFeatures = (currentPlan: SubscriptionPlan, previousPlan?: SubscriptionPlan): SubOptionId[] => {
+        if (!previousPlan) return currentPlan.subOptionIds // Return all features for the starter plan
+
+        return currentPlan.subOptionIds.map(subOption => {
+            // Find the corresponding subOption in the previous plan
+            const previousSubOption = previousPlan?.subOptionIds.find(p => p.key === subOption.key)
+
+            // Filter the features based on the condition
+            const filteredFeatures = subOption.value.filter(feature => {
+                // If the feature exists in the current plan but not in the previous plan, or its value is different, include it
+                const previousFeature = previousSubOption?.value.find(pf => pf.key === feature.key)
+                return !previousFeature || feature.value !== previousFeature.value
+            })
+
+            return {
+                ...subOption,
+                value: filteredFeatures
+            }
+        })
+    }
+
+    const plans = data.data
+    const starterPlan = plans.find(plan => plan.type === 'STARTER')
+    const basicPlan = plans.find(plan => plan.type === 'BASIC')
+    const businessPlan = plans.find(plan => plan.type === 'BUSINESS')
+    const businessProPlan = plans.find(plan => plan.type === 'BUSINESS_PRO')
+    const enterprisePlan = plans.find(plan => plan.type === 'ENTERPRISE')
+
     return (
         <SimpleGrid columns={{ base: 1, md: 2, xl: 4 }} gap={{ lg: 8, md: 6 }}>
-            {data.data.map(plan => <PlanCard key={plan._id} plan={plan} showBuyButton={showBuyButton} />)}
+            <PlanCard plan={starterPlan} showBuyButton={showBuyButton} features={getFilteredFeatures(starterPlan)} />
+            <PlanCard plan={basicPlan} showBuyButton={showBuyButton} features={getFilteredFeatures(basicPlan, starterPlan)} />
+            <PlanCard plan={businessPlan} showBuyButton={showBuyButton} features={(getFilteredFeatures(businessPlan, basicPlan))} />
+            <PlanCard plan={businessProPlan} showBuyButton={showBuyButton} features={getFilteredFeatures(businessProPlan, businessPlan)} />
+            <PlanCard plan={enterprisePlan} showBuyButton={showBuyButton} features={getFilteredFeatures(enterprisePlan, businessProPlan)} />
         </SimpleGrid>
     )
 }
--- src/pages/subscription-plans/_components/plans/_components/plan-card/PlanCard.tsx
+++ src/pages/subscription-plans/_components/plans/_components/plan-card/PlanCard.tsx
@@ -1,18 +1,49 @@
-import { Flex, useDisclosure } from "@chakra-ui/react";
+import { Box, Flex, useDisclosure } from "@chakra-ui/react";
 import AppIcons from "assest/icon/Appicons";
 import BasicButton from "components/common/BasicButton/BasicButton";
 import AppTypography from "components/common/typography/AppTypography";
-import { SubscriptionPlan } from "lib/apis/subscription/interfaces";
+import { SubOptionId, SubscriptionPlan } from "lib/apis/subscription/interfaces";
 import { capitalizeFirstLetter } from "lib/utils/heper/helpers";
 import PlanHeading from "pages/subscription-plans/_components/PlanHeading";
 import React from "react";
 import SubscriptionPlanCheckoutModal from "../checkout/SubscriptionPlanCheckoutModal";
 
-const PlanCard = ({ plan, showBuyButton }: { plan: SubscriptionPlan, showBuyButton: boolean }) => {
+interface Props {
+    plan: SubscriptionPlan;
+    showBuyButton: boolean;
+    features: SubOptionId[];
+}
+
+const PlanCard = ({ plan, showBuyButton, features }: Props) => {
     const { onOpen, isOpen, onClose } = useDisclosure()
-    const { price, type, subOptionIds } = plan
+    const { price, type } = plan
     const isFree = type === "STARTER"
     const isEnterprise = type === "ENTERPRISE"
+
+    const getPlanDescription = () => {
+        switch (plan.type) {
+            case 'STARTER':
+                return 'Included in all plans:'
+            case 'BASIC':
+                return <>
+                    Everything in <Box as="span" color={"primary"} fontWeight={600}>Starter</Box>, plus:
+                </>
+            case 'BUSINESS':
+                return <>
+                    Everything in <Box as="span" color={"primary"} fontWeight={600}>Basic</Box>, plus:
+                </>
+            case 'BUSINESS_PRO':
+                return <>
+                    Everything in <Box as="span" color={"primary"} fontWeight={600}>Business</Box>, plus:
+                </>
+            case 'ENTERPRISE':
+                return <>
+                    Everything in <Box as="span" color={"primary"} fontWeight={600}>Business Pro</Box>, plus:
+                </>
+            default:
+                return ''
+        }
+    }
 
     const handlePlanPurchase = () => {
         if (isEnterprise) {
@@ -46,22 +77,25 @@ const PlanCard = ({ plan, showBuyButton }: { plan: SubscriptionPlan, showBuyButt
 
                 {showBuyButton && !isFree && <BasicButton onClick={handlePlanPurchase}>{isEnterprise ? "Contact Us" : "Buy"}</BasicButton>}
 
+                <AppTypography fontSize={12} fontWeight={600} color={"white"}>{getPlanDescription()}</AppTypography>
+
                 {
-                    subOptionIds.map(featureGroup =>
-                        <Flex key={featureGroup.key} direction={"column"} gap={2}>
+                    features.map(featureGroup => {
+                        if (featureGroup.value.every(feature => !feature.value)) return null
+                        return <Flex key={featureGroup.key} direction={"column"} gap={2}>
                             <AppTypography fontWeight={500} color={"white"}>{featureGroup.title || featureGroup.key}</AppTypography>
                             {
                                 featureGroup.value.filter(feature => feature.value).map(feature =>
                                     <Flex key={feature.key} alignItems={"center"} gap={2}>
                                         <AppIcons.Tick />
                                         <AppTypography color={"white"}>
-                                            {`${feature.title} ${(isNaN(Number(feature.value)) || typeof feature.value === "boolean") ? "" : `: ${feature.value}`}`}
+                                            {`${feature.title} ${typeof feature.value === "boolean" ? "" : `: ${feature.value}`}`}
                                         </AppTypography>
                                     </Flex>
                                 )
                             }
                         </Flex>
-                    )
+                    })
                 }
             </Flex>
             {isOpen && <SubscriptionPlanCheckoutModal selectedPlan={plan} open={isOpen} close={onClose} />}
