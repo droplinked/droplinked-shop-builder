960da09a57c90f1f1b5a285a957de19e07ed1850
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-06-25 12:36:16 +0330

    feat: variants

--- src/pages/product/single/model/modules/property.ts
+++ src/pages/product/single/model/modules/property.ts
@@ -1,43 +1,40 @@
 import { Iproperties } from "lib/apis/product/interfaces";
 import AppendModule from "../../parts/modules/properties/model/module/append";
 
-const propertyFactor = ({
-    refactor : (items: Array<any>): Array<Iproperties> => {
-
-        let properties = {}
-        items.forEach(item => {
-            const data: Array<any> = item
-            data.forEach(element => {
-                if (!properties[element?.variantID]) {
-                    properties[element?.variantID || "1"] = {
+const propertyFactor = {
+    refactor: (items: Array<any>): Array<Iproperties> => {
+        let properties = {};
+        items.forEach((item) => {
+            const data: Array<any> = item;
+            data.forEach((element) => {
+                const variantName = element?.variantName || AppendModule?.getCaption(element?.variantID);
+                if (!properties[variantName]) {
+                    properties[variantName] = {
                         items: [],
-                        title: AppendModule.getCaption(element.variantID || "1"),
-                        value: element.variantID || "1"
-                    }
+                        title: variantName,
+                        value: element.variantID,
+                    };
                 }
-                properties[element?.variantID || "1"]["items"][element.value] = { 
+                properties[variantName]["items"][element.value] = {
                     value: element.value,
-                    caption: element.caption
-                 }
+                    caption: element.caption,
+                };
             });
-
         });
-        
-        const refactorToArray = Object.keys(properties).map(el => {
-            const items = properties[el].items
+        const refactorToArray = Object.keys(properties).map((el) => {
+            const items = properties[el].items;
             return {
                 ...properties[el],
-                items: Object.keys(items).map(el => {
+                items: Object.keys(items).map((el) => {
                     return {
                         value: items[el].value,
                         caption: items[el].caption,
-                    }
-                })
-            }
-        })
+                    };
+                }),
+            };
+        });
+        return refactorToArray;
+    },
+};
 
-        return refactorToArray
-    }
-})
-
-export default propertyFactor
+export default propertyFactor;
