a34c22a4e1ae67bf0e0daed90ee1fb390d7d07b7
Aref Orumiehei <areforumiehei@gmail.com>
2024-06-28 15:00:22 +0330

    🔨 add: contact us api func to post data

--- src/pages/public-pages/landings/dpp-page/_components/dpp-contact-us/DppContactUs.tsx
+++ src/pages/public-pages/landings/dpp-page/_components/dpp-contact-us/DppContactUs.tsx
@@ -1,42 +1,97 @@
 import React from "react";
+import { Box, Flex, Input, Textarea } from "@chakra-ui/react";
+import { useMutation } from "react-query";
 import { Form, Formik } from "formik";
-import { Button, Flex, Input, Textarea } from "@chakra-ui/react";
+import * as Yup from "yup";
+
+
+//API
+import { contactUsService } from 'lib/apis/constact-us/services';
 
 //Components
+import useAppToast from 'functions/hooks/toast/useToast';
 import AppTypography from "components/common/typography/AppTypography";
+import BasicButton from "components/common/BasicButton/BasicButton";
+
+interface IFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  message: string;
+}
 
 const DppContactUs = () => {
+  const { mutateAsync, isLoading } = useMutation((data: IFormData) => contactUsService(data));
+  const { showToast } = useAppToast();
+
+  const onSubmit = async (data: IFormData, actions) => {
+    try {
+      await mutateAsync(data);
+      actions.resetForm();
+      showToast({ type: "success", message: "Message sent successfully!" });
+    } catch (error) {
+      showToast({ type: "error", message: "Oops. Something went wrong!" });
+    }
+  };
+
+  const formSchema = Yup.object().shape({
+    firstName: Yup.string().required("This field is required."),
+    lastName: Yup.string().required("This field is required."),
+    email: Yup.string().email("Please enter a valid email address.").required("This field is required."),
+    message: Yup.string().required("This field is required."),
+  });
+
   return (
     <Flex flexDirection={"column"} alignItems={"center"} gap={"32px"}>
-      <AppTypography fontSize={"48px"} fontWeight={700} color={"#FFF"} textAlign={"center"}>Contact us to learn more</AppTypography>
+      <AppTypography fontSize={"48px"} fontWeight={700} color={"#FFF"} textAlign={"center"}>
+        Contact us to learn more
+      </AppTypography>
       <Formik
-        initialValues={{ name: "", email: "", message: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
+        initialValues={{ firstName: "", lastName: "", email: "", message: "" }}
+        validateOnChange={false}
+        validationSchema={formSchema}
+        onSubmit={onSubmit}
       >
-        {({ values, handleChange, handleSubmit }) => (
+        {({ values, handleChange, handleSubmit, errors, touched }) => (
           <Form onSubmit={handleSubmit}>
             <Flex flexDirection={"column"} alignItems={"center"} justifyContent={"center"} gap={"24px"} width={"444px"}>
-              <Flex
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                gap="16px"
-                alignSelf="stretch"
-                width={"100%"}
-              >
-                <Input
-                  name="name"
-                  placeholder="Name"
-                  backgroundColor={"transparent"}
-                  border="1px solid #E0E0E0"
-                  padding={"12px 20px"}
-                  borderRadius={"8px"}
-                  height={"50px"}
-                  value={values.name}
-                  onChange={handleChange}
-                />
+              <Flex direction={{ base: "column", md: "row" }} gap="16px" width="100%">
+                <Box width="100%">
+                  <Input
+                    name="firstName"
+                    placeholder="First Name"
+                    backgroundColor={"transparent"}
+                    border="1px solid #E0E0E0"
+                    padding={"12px 20px"}
+                    borderRadius={"8px"}
+                    height={"50px"}
+                    color={"#FFF"}
+                    value={values.firstName}
+                    onChange={handleChange}
+                    isInvalid={touched.firstName && !!errors.firstName}
+                    errorBorderColor="red.300"
+                  />
+                  {touched.firstName && errors.firstName && <Box color="red.500" mt="2">{errors.firstName}</Box>}
+                </Box>
+                <Box width="100%">
+                  <Input
+                    name="lastName"
+                    placeholder="Last Name"
+                    backgroundColor={"transparent"}
+                    border="1px solid #E0E0E0"
+                    padding={"12px 20px"}
+                    borderRadius={"8px"}
+                    height={"50px"}
+                    color={"#FFF"}
+                    value={values.lastName}
+                    onChange={handleChange}
+                    isInvalid={touched.lastName && !!errors.lastName}
+                    errorBorderColor="red.300"
+                  />
+                  {touched.lastName && errors.lastName && <Box color="red.500" mt="2">{errors.lastName}</Box>}
+                </Box>
+              </Flex>
+              <Box width="100%">
                 <Input
                   name="email"
                   placeholder="Email"
@@ -45,9 +100,15 @@ const DppContactUs = () => {
                   padding={"12px 20px"}
                   borderRadius={"8px"}
                   height={"50px"}
+                  color={"#FFF"}
                   value={values.email}
                   onChange={handleChange}
+                  isInvalid={touched.email && !!errors.email}
+                  errorBorderColor="red.300"
                 />
+                {touched.email && errors.email && <Box color="red.500" mt="2">{errors.email}</Box>}
+              </Box>
+              <Box width="100%">
                 <Textarea
                   name="message"
                   placeholder="Your message"
@@ -56,26 +117,32 @@ const DppContactUs = () => {
                   padding={"12px 20px"}
                   borderRadius={"8px"}
                   height={"150px"}
+                  color={"#FFF"}
                   value={values.message}
                   onChange={handleChange}
+                  isInvalid={touched.message && !!errors.message}
+                  errorBorderColor="red.300"
                 />
-                <Button
-                  mt={4}
-                  colorScheme="teal"
-                  type="submit"
-                  width="100%"
-                  backgroundColor="#2BCFA1"
-                  color="black"
-                >
-                  Send
-                </Button>
-              </Flex>
+                {touched.message && errors.message && <Box color="red.500" mt="2">{errors.message}</Box>}
+              </Box>
+              <BasicButton
+                mt={4}
+                colorScheme="teal"
+                type="submit"
+                width="100%"
+                backgroundColor="#2BCFA1"
+                color="black"
+                isDisabled={!values.firstName || !values.lastName || !values.email || !values.message || isLoading}
+                isLoading={isLoading}
+              >
+                Send
+              </BasicButton>
             </Flex>
           </Form>
         )}
       </Formik>
-  </Flex>
-  )
-}
+    </Flex>
+  );
+};
 
 export default DppContactUs;
