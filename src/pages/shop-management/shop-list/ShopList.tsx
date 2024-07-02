aa2bf4b5c4b150d998277a9e6ea79bd812619fb1
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-07-02 12:39:59 +0330

    Cleanup ShopList component

--- src/pages/shop-management/shop-list/ShopList.tsx
+++ src/pages/shop-management/shop-list/ShopList.tsx
@@ -1,5 +1,6 @@
 import { Flex, useDisclosure } from '@chakra-ui/react'
 import BasicButton from 'components/common/BasicButton/BasicButton'
+import AppTypography from 'components/common/typography/AppTypography'
 import SimpleRegistrationModal from 'components/modals/simple-registration-modal/SimpleRegistrationModal'
 import { getUserShopsService } from 'lib/apis/shop/shopServices'
 import React from 'react'
@@ -9,22 +10,26 @@ import Loading from './_components/loading/Loading'
 import ShopRow from './_components/shop-row/ShopRow'
 
 function ShopList() {
-    const { isFetching, data, refetch } = useQuery({
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isFetching, error, data, refetch } = useQuery({
         queryFn: () => getUserShopsService(),
         queryKey: ["current-user-shops"],
         refetchOnWindowFocus: false
     })
-    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const renderContent = () => {
+        if (isFetching) return <Loading />
+        if (error) return <AppTypography fontSize={16} color={"red.400"}>Oops! It looks like we can not access shops at the moment. Give it another try soon?</AppTypography>
+        const shops = data.data
+        if (!shops.length) return <EmptyBox />
+        return shops.map(shop => <ShopRow key={shop._id} shop={shop} />)
+    }
 
     return (
         <>
             <Flex direction={"column"} gap={5}>
                 <BasicButton marginBlock={3} alignSelf={"flex-end"} onClick={onOpen}>+ Create Store</BasicButton>
-                {
-                    isFetching ? <Loading /> :
-                        !data.data.length ? <EmptyBox /> :
-                            data.data.map(shop => <ShopRow key={shop._id} shop={shop} />)
-                }
+                {renderContent()}
             </Flex>
             {isOpen && <SimpleRegistrationModal isOpen={isOpen} mode='CREATE_EXTRA_SHOP' close={onClose} refetchUserShops={refetch} />}
         </>
