1b06c366df65e7e2126da512091386007ca29d10
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-07-08 13:27:35 +0330

    Fix the issue that renders a toast message in login process

--- src/components/modals/login-modal/LoginModal.tsx
+++ src/components/modals/login-modal/LoginModal.tsx
@@ -39,14 +39,12 @@ const LoginModal = ({ show, close, switchModal, switchReset }) => {
     const processLogin = async (data: any) => {
         try {
             const { user } = data
-            if (!["PRODUCER", "ADMIN"].includes(user.type))
-                return showToast({ message: "This account is unable to log in. Please check your credentials.", type: "error" })
-
             const status = appDevelopment && user.status === "NEW" ? "VERIFIED" : user.status
-            if (status === "DELETED") {
-                showToast({ message: "This account has been deleted", type: "error" })
-                return
-            }
+            if (status === "DELETED")
+                return showToast({ message: "This account has been deleted", type: "error" })
+
+            if (user.type !== "SHOPBUILDER")
+                return showToast({ message: "This account is unable to log in. Please check your credentials.", type: "error" })
 
             const { href, dashboard } = navigating_user_based_on_status(status, data)
             dashboard ? shopNavigate(href) : navigate(href)
