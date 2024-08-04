1b8a2639932ec15920ad4c570f002cd757922d3d
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-08-04 14:48:54 +0330

    fix: refresh token refactored

--- src/pages/shop-management/hooks/useShopSwitch.ts
+++ src/pages/shop-management/hooks/useShopSwitch.ts
@@ -1,5 +1,6 @@
 import { switchShopService } from "lib/apis/shop/shopServices"
 import useAppStore from "lib/stores/app/appStore"
+import AppStorage from "lib/utils/app/sessions"
 import { useMutation } from "react-query"
 import { useNavigate } from "react-router-dom"
 
@@ -11,8 +12,9 @@ const useShopSwitcher = () => {
         mutationFn: (shopId: string) => switchShopService(shopId),
         onSuccess: async (response) => {
             const { data: { access_token, refresh_token, shop, user } } = response
-            updateState({ key: "access_token", params: access_token })
-            updateState({ key: "refresh_token", params: refresh_token })
+            // updateState({ key: "access_token", params: access_token })
+            // updateState({ key: "refresh_token", params: refresh_token })
+            AppStorage.set_tokens(access_token, refresh_token)
             updateState({ key: "shop", params: shop })
             updateState({ key: "user", params: user })
             navigate("/dashboard")
