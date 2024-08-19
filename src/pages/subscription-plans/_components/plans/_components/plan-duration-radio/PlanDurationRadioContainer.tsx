56deb376cdd0a449c72334184782619f400da465
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-08-19 20:08:56 +0330

    Add responsive styles for plan duration radio buttons in subscription plans page

--- src/pages/subscription-plans/_components/plans/_components/plan-duration-radio/PlanDurationRadioContainer.tsx
+++ src/pages/subscription-plans/_components/plans/_components/plan-duration-radio/PlanDurationRadioContainer.tsx
@@ -14,7 +14,37 @@ function PlanDurationRadioContainer() {
     })
 
     return (
-        <Flex width={"fit-content"} height={12} alignItems={"center"} gap={1} bg={"#222222"} p={1} borderRadius={8} {...getRootProps()}>
+        <Flex
+            width={"fit-content"}
+            maxWidth={"100%"}
+            overflowY={"auto"}
+            height={12}
+            alignItems={"center"}
+            gap={1}
+            bg={"#222222"}
+            p={1}
+            borderRadius={8}
+            sx={{
+                /* Firefox */
+                scrollbarWidth: "thin", /* Makes the scrollbar thinner */
+                scrollbarColor: "#2BCFA1 transparent", /* Thumb is #2BCFA1, track is transparent */
+
+                /* Webkit-based browsers (e.g., Chrome, Safari) */
+                "&::-webkit-scrollbar": {
+                    width: "4px", /* Adjusts the width of the scrollbar for vertical scroll */
+                    height: "4px", /* Adjusts the height of the scrollbar for horizontal scroll */
+                },
+                "&::-webkit-scrollbar-track": {
+                    background: "transparent", /* Makes the scrollbar track transparent */
+                },
+                "&::-webkit-scrollbar-thumb": {
+                    backgroundColor: "#2BCFA1", /* Sets the scrollbar thumb color to #2BCFA1 */
+                    borderRadius: "12px", /* Increases the border-radius for a more rounded thumb */
+                    border: "2px solid transparent", /* Optional: adds spacing around the thumb */
+                },
+            }}
+            {...getRootProps()}
+        >
             {planDurations.map((duration) => <PlanDurationRadio key={duration.month} duration={duration} {...getRadioProps({ value: duration.label })} />)}
         </Flex>
     )
