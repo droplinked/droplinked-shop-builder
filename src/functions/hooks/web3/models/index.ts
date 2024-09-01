14ff75d2f818c5130230579eb7e2c1bd96c825b7
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-09-01 17:49:57 +0330

    update: skale role based deployment

--- src/functions/hooks/web3/models/index.ts
+++ src/functions/hooks/web3/models/index.ts
@@ -1,81 +1,83 @@
-import { Isku } from 'lib/apis/product/interfaces'
-import { deployShopContractService } from 'lib/apis/shop/shopServices'
-import { SHOP_URL, appDevelopment } from 'lib/utils/app/variable'
+import { Isku } from 'lib/apis/product/interfaces';
+import {
+	deployShopContractService,
+	getDeployPermission,
+} from 'lib/apis/shop/shopServices';
+import { SHOP_URL, appDevelopment } from 'lib/utils/app/variable';
 // import { stacksRecord } from 'lib/utils/blockchain/stacks/record'
 // import stacksRequest from 'lib/utils/blockchain/stacks/request'
-import { getNetworkProvider } from 'lib/utils/chains/chainProvider'
-import { Chain, Network } from 'lib/utils/chains/dto/chains'
-import acceptModel from './module/accept/acceptModel'
-import recordModel, { IStacks, Ideploy, IdeployBatch } from './module/record/recordModel'
-import { SolanaProvider } from '../../../../lib/utils/chains/providers/solana/solana.provider'
-import { defaultModal } from '../../../../lib/utils/chains/dto/modalInterface'
-import { RecordProduct } from 'lib/utils/chains/dto/recordDTO'
-import { Beneficiary, ProductType } from 'lib/utils/chains/dto/chainStructs'
-import { droplink_wallet } from 'lib/utils/statics/adresses'
+import { getNetworkProvider } from 'lib/utils/chains/chainProvider';
+import { Chain, Network } from 'lib/utils/chains/dto/chains';
+import acceptModel from './module/accept/acceptModel';
+import recordModel, { IStacks, Ideploy, IdeployBatch } from './module/record/recordModel';
+import { SolanaProvider } from '../../../../lib/utils/chains/providers/solana/solana.provider';
+import { defaultModal } from '../../../../lib/utils/chains/dto/modalInterface';
+import { RecordProduct } from 'lib/utils/chains/dto/recordDTO';
+import { Beneficiary, ProductType } from 'lib/utils/chains/dto/chainStructs';
+import { droplink_wallet } from 'lib/utils/statics/adresses';
 
 export interface IRecordParamsData {
-	commission: any
-	quantity: any
-	blockchain: any
-	royalty: any
+	commission: any;
+	quantity: any;
+	blockchain: any;
+	royalty: any;
 }
 
 export interface IRecordBatchParamsData {
-	quantity: any
-	sku: any
-	imageUrl?: string
+	quantity: any;
+	sku: any;
+	imageUrl?: string;
 }
 
 export interface IRecordPrams {
-	data: IRecordParamsData
-	product: any
-	sku: any
-	imageUrl?: string
-	shop: any
+	data: IRecordParamsData;
+	product: any;
+	sku: any;
+	imageUrl?: string;
+	shop: any;
 }
 
-
 export interface Irecord {
-	params: IRecordPrams
-	accountAddress: any
-	stack: IStacks
+	params: IRecordPrams;
+	accountAddress: any;
+	stack: IStacks;
 }
 
 export interface IrecordBatch {
-	params: IRecordBatchParamsData[]
-	royalty: any
-	commission: any
-	accountAddress: any
-	shop: any
-	product: any
-	blockchain: any
-	stack: IStacks
+	params: IRecordBatchParamsData[];
+	royalty: any;
+	commission: any;
+	accountAddress: any;
+	shop: any;
+	product: any;
+	blockchain: any;
+	stack: IStacks;
 }
 
 export interface IRequestData {
-	sku: Isku
-	shop: any
-	deployedContracts: any
+	sku: Isku;
+	shop: any;
+	deployedContracts: any;
 }
 
 interface IRequest {
-	params: IRequestData
-	accountAddress: any
-	stack: IStacks
+	params: IRequestData;
+	accountAddress: any;
+	stack: IStacks;
 }
 
 export interface IAcceptData {
-	accept: boolean
-	shop: any
-	deployedContracts: any
-	sku: any
-	recordData?: any
+	accept: boolean;
+	shop: any;
+	deployedContracts: any;
+	sku: any;
+	recordData?: any;
 }
 
 interface IAccept {
-	params: IAcceptData
-	accountAddress: any
-	stack: IStacks
+	params: IAcceptData;
+	accountAddress: any;
+	stack: IStacks;
 }
 
 const web3Model = {
@@ -87,29 +89,68 @@ const web3Model = {
 		return new Promise<void>(async (resolve: any, reject) => {
 			try {
 				const chain =
-					product.product_type === 'DIGITAL' ? product.digitalDetail.chain : data.blockchain
-				let deployedContract
-				let targetChainContract
+					product.product_type === 'DIGITAL'
+						? product.digitalDetail.chain
+						: data.blockchain;
+				let deployedContract;
+				let targetChainContract;
 				if (shop.deployedContracts) {
-					targetChainContract = shop.deployedContracts.find((contract) => (contract.type === chain))
+					targetChainContract = shop.deployedContracts.find(
+						(contract) => contract.type === chain
+					);
 					if (!targetChainContract) {
-						deployedContract = await getNetworkProvider(
-							Chain[chain as string],
-							Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
-							accountAddress
-						).deployShop(
-							shop.name,
-							`${SHOP_URL}/${shop.name}`,
-							accountAddress,
-							shop.logo,
-							shop.description
-						)
-						await deployShopContractService({ type: chain, ...deployedContract })
+						if (chain === 'SKALE') {
+							const req =
+								await getDeployPermission();
+							if (req.status !== 201) {
+								console.log(
+									`Getting permission failed, reason: ${req.data}`
+								);
+								throw new Error(
+									'Permission denied'
+								);
+							}
+						}
+						deployedContract =
+							await getNetworkProvider(
+								Chain[chain as string],
+								Network[
+									appDevelopment
+										? 'TESTNET'
+										: 'MAINNET'
+								],
+								accountAddress
+							).deployShop(
+								shop.name,
+								`${SHOP_URL}/${shop.name}`,
+								accountAddress,
+								shop.logo,
+								shop.description
+							);
+						await deployShopContractService({
+							type: chain,
+							...deployedContract,
+						});
 					}
 				} else {
+					if (chain === 'SKALE') {
+						const req = await getDeployPermission();
+						if (req.status !== 201) {
+							console.log(
+								`Getting permission failed, reason: ${req.data}`
+							);
+							throw new Error(
+								'Permission denied'
+							);
+						}
+					}
 					deployedContract = await getNetworkProvider(
 						Chain[chain as string],
-						Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
+						Network[
+							appDevelopment
+								? 'TESTNET'
+								: 'MAINNET'
+						],
 						accountAddress
 					).deployShop(
 						shop.name,
@@ -117,18 +158,21 @@ const web3Model = {
 						accountAddress,
 						shop.logo,
 						shop.description
-					)
-					await deployShopContractService({ type: chain, ...deployedContract })
+					);
+					await deployShopContractService({
+						type: chain,
+						...deployedContract,
+					});
 				}
-				const commission = data.commission
-				const quantity: any = data.quantity
-				if (!data.royalty) data.royalty = 0
+				const commission = data.commission;
+				const quantity: any = data.quantity;
+				if (!data.royalty) data.royalty = 0;
 				const dataDeploy: Ideploy = {
 					data,
 					deployHash: '',
 					product,
 					sku,
-				}
+				};
 				if (data.blockchain === 'STACKS') {
 					// const query = await stacksRecord({
 					// 	isRequestPending,
@@ -145,23 +189,26 @@ const web3Model = {
 					// if (query) dataDeploy.deployHash = query.txId
 				} else {
 					const nftContract =
-						targetChainContract?.deployedNFTAddress || deployedContract.deployedNFTAddress
+						targetChainContract?.deployedNFTAddress ||
+						deployedContract.deployedNFTAddress;
 					const shopAddress =
-						targetChainContract?.deployedShopAddress || deployedContract.deployedShopAddress
-					const currencyAddress = '0x0000000000000000000000000000000000000000'
+						targetChainContract?.deployedShopAddress ||
+						deployedContract.deployedShopAddress;
+					const currencyAddress =
+						'0x0000000000000000000000000000000000000000';
 
-					const products: RecordProduct[] = []
+					const products: RecordProduct[] = [];
 
-					const beneficiaries: Beneficiary[] = []
+					const beneficiaries: Beneficiary[] = [];
 					if (product.product_type === 'PRINT_ON_DEMAND') {
 						beneficiaries.push({
 							isPercentage: false,
 							value: sku.rawPrice * 100,
-							wallet: droplink_wallet
-						})
+							wallet: droplink_wallet,
+						});
 					}
 
-					const productType = ProductType.DIGITAL // TODO: update this
+					const productType = ProductType.DIGITAL; // TODO: update this
 
 					products.push({
 						acceptsManageWallet: true,
@@ -177,7 +224,7 @@ const web3Model = {
 						sku_id: sku._id,
 						skuProperties: sku,
 						type: productType,
-					})
+					});
 
 					const res = await recordModel.record({
 						product,
@@ -185,19 +232,20 @@ const web3Model = {
 						accountAddress,
 						nftContract,
 						shopAddress,
-						products
-					})
-					if (res) dataDeploy.deployHash = res.transactionHash
+						products,
+					});
+					if (res)
+						dataDeploy.deployHash =
+							res.transactionHash;
 				}
 
-				await recordModel.deploy(dataDeploy)
-				resolve(dataDeploy.deployHash)
+				await recordModel.deploy(dataDeploy);
+				resolve(dataDeploy.deployHash);
 			} catch (error) {
-				reject(error)
+				reject(error);
 			}
-		})
+		});
 	},
-
 
 	recordBatch: async ({
 		params,
@@ -212,29 +260,45 @@ const web3Model = {
 		return new Promise<void>(async (resolve: any, reject) => {
 			try {
 				const chain =
-					product.product_type === 'DIGITAL' ? product.digitalDetail.chain : blockchain
-				let deployedContract
-				let targetChainContract
+					product.product_type === 'DIGITAL'
+						? product.digitalDetail.chain
+						: blockchain;
+				let deployedContract;
+				let targetChainContract;
 				if (shop.deployedContracts) {
-					targetChainContract = shop.deployedContracts.find((contract) => (contract.type === chain))
+					targetChainContract = shop.deployedContracts.find(
+						(contract) => contract.type === chain
+					);
 					if (!targetChainContract) {
-						deployedContract = await getNetworkProvider(
-							Chain[chain as string],
-							Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
-							accountAddress
-						).deployShop(
-							shop.name,
-							`${SHOP_URL}/${shop.name}`,
-							accountAddress,
-							shop.logo,
-							shop.description
-						)
-						await deployShopContractService({ type: chain, ...deployedContract })
+						deployedContract =
+							await getNetworkProvider(
+								Chain[chain as string],
+								Network[
+									appDevelopment
+										? 'TESTNET'
+										: 'MAINNET'
+								],
+								accountAddress
+							).deployShop(
+								shop.name,
+								`${SHOP_URL}/${shop.name}`,
+								accountAddress,
+								shop.logo,
+								shop.description
+							);
+						await deployShopContractService({
+							type: chain,
+							...deployedContract,
+						});
 					}
 				} else {
 					deployedContract = await getNetworkProvider(
 						Chain[chain as string],
-						Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
+						Network[
+							appDevelopment
+								? 'TESTNET'
+								: 'MAINNET'
+						],
 						accountAddress
 					).deployShop(
 						shop.name,
@@ -242,31 +306,38 @@ const web3Model = {
 						accountAddress,
 						shop.logo,
 						shop.description
-					)
-					await deployShopContractService({ type: chain, ...deployedContract })
+					);
+					await deployShopContractService({
+						type: chain,
+						...deployedContract,
+					});
 				}
-				const products: RecordProduct[] = []
+				const products: RecordProduct[] = [];
 
 				const nftContract =
-					targetChainContract?.deployedNFTAddress || deployedContract.deployedNFTAddress
+					targetChainContract?.deployedNFTAddress ||
+					deployedContract.deployedNFTAddress;
 				const shopAddress =
-					targetChainContract?.deployedShopAddress || deployedContract.deployedShopAddress
-				const currencyAddress = '0x0000000000000000000000000000000000000000'
+					targetChainContract?.deployedShopAddress ||
+					deployedContract.deployedShopAddress;
+				const currencyAddress =
+					'0x0000000000000000000000000000000000000000';
 
 				for (const data of params) {
 					const prod = data;
-					const quantity: any = prod.quantity
-					if (!royalty) royalty = 0
-					const beneficiaries: Beneficiary[] = []
+					const quantity: any = prod.quantity;
+					if (!royalty) royalty = 0;
+					const beneficiaries: Beneficiary[] = [];
 					if (product.product_type === 'PRINT_ON_DEMAND') {
 						beneficiaries.push({
 							isPercentage: false,
 							value: prod.sku.rawPrice * 100,
-							wallet: droplink_wallet
-						})
+							wallet: droplink_wallet,
+						});
 					}
-					let productType = ProductType.DIGITAL // TODO: update this
-					if (product.product_type === 'PRINT_ON_DEMAND') productType = ProductType.POD
+					let productType = ProductType.DIGITAL; // TODO: update this
+					if (product.product_type === 'PRINT_ON_DEMAND')
+						productType = ProductType.POD;
 					products.push({
 						acceptsManageWallet: true,
 						amount: quantity,
@@ -281,7 +352,7 @@ const web3Model = {
 						sku_id: prod.sku._id,
 						skuProperties: prod.sku,
 						type: productType,
-					})
+					});
 				}
 
 				const dataDeploy: IdeployBatch = {
@@ -289,8 +360,8 @@ const web3Model = {
 					deployHash: '',
 					product,
 					royalty,
-					commission
-				}
+					commission,
+				};
 
 				const res = await recordModel.record({
 					product,
@@ -298,18 +369,18 @@ const web3Model = {
 					accountAddress,
 					nftContract,
 					shopAddress,
-					products
-				})
+					products,
+				});
 
-				if (res) dataDeploy.deployHash = res.transactionHash
+				if (res) dataDeploy.deployHash = res.transactionHash;
 
-				await recordModel.deployBatch(dataDeploy)
+				await recordModel.deployBatch(dataDeploy);
 
-				resolve(dataDeploy.deployHash)
+				resolve(dataDeploy.deployHash);
 			} catch (error) {
-				reject(error)
+				reject(error);
 			}
-		})
+		});
 	},
 
 	request: ({
@@ -319,15 +390,16 @@ const web3Model = {
 	}: IRequest) => {
 		return new Promise<any>(async (resolve: any, reject) => {
 			try {
-				const deployedContractAddress = sku.deployedShopAddress
-				const productId = sku?.recordData?.data?.details?.productId
-				const blockchain: string = sku?.recordData?.recordNetwork
-				const quantity = sku.recorded_quantity
+				const deployedContractAddress = sku.deployedShopAddress;
+				const productId =
+					sku?.recordData?.data?.details?.productId;
+				const blockchain: string = sku?.recordData?.recordNetwork;
+				const quantity = sku.recorded_quantity;
 				if (!deployedContractAddress) {
-					reject('Contract not deployed')
+					reject('Contract not deployed');
 				}
 
-				const shopAddress = deployedContractAddress
+				const shopAddress = deployedContractAddress;
 				if (blockchain === 'STACKS') {
 					// const request = await stacksRequest({
 					// 	isRequestPending,
@@ -343,16 +415,20 @@ const web3Model = {
 				} else {
 					const request = await getNetworkProvider(
 						Chain[blockchain],
-						Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
+						Network[
+							appDevelopment
+								? 'TESTNET'
+								: 'MAINNET'
+						],
 						accountAddress
-					).publishRequest(productId, shopAddress)
+					).publishRequest(productId, shopAddress);
 
-					resolve(request.transactionHash)
+					resolve(request.transactionHash);
 				}
 			} catch (error) {
-				reject(error)
+				reject(error);
 			}
-		})
+		});
 	},
 
 	accept: ({
@@ -362,44 +438,63 @@ const web3Model = {
 	}: IAccept) => {
 		return new Promise<any>(async (resolve, reject) => {
 			try {
-				const requestID = shop?.recordData?.details?.requestId
-				const blockchain: string = sku?.recordData?.recordNetwork
-				const deployShopContract = sku.deployedShopAddress
+				const requestID = shop?.recordData?.details?.requestId;
+				const blockchain: string = sku?.recordData?.recordNetwork;
+				const deployShopContract = sku.deployedShopAddress;
 				if (!deployShopContract) {
-					reject('Contract not deployed')
+					reject('Contract not deployed');
 				}
-				let deployHash = null
+				let deployHash = null;
 				if (blockchain === 'STACKS') {
-					const request = await acceptModel.approveRequestStack({
-						isRequestPending,
-						openContractCall,
-						params: { id: requestID, publisher: shop?.recordData?.details?.publisher },
-					})
-					deployHash = request.txId
-					resolve(deployHash)
+					const request =
+						await acceptModel.approveRequestStack({
+							isRequestPending,
+							openContractCall,
+							params: {
+								id: requestID,
+								publisher: shop
+									?.recordData
+									?.details
+									?.publisher,
+							},
+						});
+					deployHash = request.txId;
+					resolve(deployHash);
 				} else if (blockchain === 'SOLANA') {
-					const accept = await new SolanaProvider(Chain.SOLANA, Network.TESTNET)
+					const accept = await new SolanaProvider(
+						Chain.SOLANA,
+						Network.TESTNET
+					)
 						.setAddress(accountAddress)
 						.setModal(new defaultModal())
-						.approveRequest(shop, sku, recordData)
-					deployHash = accept
-					resolve(deployHash)
+						.approveRequest(shop, sku, recordData);
+					deployHash = accept;
+					resolve(deployHash);
 				} else {
 					//    approveRequest(requestId: Uint256, shopAddress: EthAddress): Promise<string>;
 					const accept = await getNetworkProvider(
 						Chain[blockchain],
-						Network[appDevelopment ? 'TESTNET' : 'MAINNET'],
+						Network[
+							appDevelopment
+								? 'TESTNET'
+								: 'MAINNET'
+						],
 						accountAddress
-					).approveRequest(requestID, deployShopContract)
-					deployHash = accept
-					resolve(deployHash)
+					).approveRequest(requestID, deployShopContract);
+					deployHash = accept;
+					resolve(deployHash);
 				}
-				await acceptModel.deploy({ deployHash, accept, chain: blockchain, shop })
+				await acceptModel.deploy({
+					deployHash,
+					accept,
+					chain: blockchain,
+					shop,
+				});
 			} catch (error) {
-				reject(error)
+				reject(error);
 			}
-		})
+		});
 	},
-}
+};
 
-export default web3Model
+export default web3Model;
