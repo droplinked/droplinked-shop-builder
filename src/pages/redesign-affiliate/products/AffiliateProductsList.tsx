b59490c68bc197d546a99b231beaf25acfdaed4e
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2024-09-22 11:32:57 +0330

    feat: affiliate product list loading

--- src/pages/redesign-affiliate/products/AffiliateProductsList.tsx
+++ src/pages/redesign-affiliate/products/AffiliateProductsList.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, Skeleton, Text } from "@chakra-ui/react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useInfiniteQuery } from "react-query";
 import { getProductsCommunityService } from "lib/apis/product/productServices";
-import AffiliateItem from "../_components/AffiliateItems";
+import AffiliateItem, { LAffiliateItem } from "../_components/AffiliateItems";
 import { ProductContext } from "./affiliate.products.context";
 
 const AffiliateProductsList = () => {
@@ -22,12 +22,34 @@ const AffiliateProductsList = () => {
         },
     });
 
-    if (isLoading) return <Text>Loading...</Text>;
+    if (isLoading)
+        return (
+            <Flex flexWrap={"wrap"} flexDir={"row"} justifyContent={"space-between"} marginTop={"24px"} alignItems={"flex-start"} gap={"16px"}>
+                {Array(4)
+                    .fill(0)
+                    .map((_, index) => (
+                        <LAffiliateItem />
+                    ))}
+            </Flex>
+        );
     if (isError) return <Text>Error loading products</Text>;
 
     return (
         <Box color={"white"}>
-            <InfiniteScroll dataLength={data?.pages.flatMap((page) => page?.data)?.length || 0} next={fetchNextPage} hasMore={!!hasNextPage} loader={<Text>Loading more...</Text>}>
+            <InfiniteScroll
+                dataLength={data?.pages.flatMap((page) => page?.data)?.length || 0}
+                next={fetchNextPage}
+                hasMore={!!hasNextPage}
+                loader={
+                    <Flex flexWrap={"wrap"} flexDir={"row"} justifyContent={"space-between"} marginTop={"24px"} alignItems={"flex-start"} gap={"16px"}>
+                        {Array(4)
+                            .fill(0)
+                            .map((_, index) => (
+                                <LAffiliateItem />
+                            ))}
+                    </Flex>
+                }
+            >
                 <Flex flexWrap={"wrap"} flexDir={"row"} justifyContent={"space-between"} alignItems={"flex-start"} gap={"16px"}>
                     {data?.pages
                         ?.flatMap((page) => page?.data)
