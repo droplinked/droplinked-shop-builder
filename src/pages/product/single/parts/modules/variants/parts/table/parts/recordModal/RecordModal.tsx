583247f02db5fc86fa4ae09d04e35c303da4a558
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-10-07 20:56:00 +0330

    Clean up RecordModal component

--- src/pages/product/single/parts/modules/variants/parts/table/parts/recordModal/RecordModal.tsx
+++ src/pages/product/single/parts/modules/variants/parts/table/parts/recordModal/RecordModal.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useContext, useState } from 'react'
+import HashKey from 'components/common/hashKey/HashKey'
 import AppModal from 'components/common/modal/AppModal'
 import { Isku } from 'lib/apis/product/interfaces'
-import RecordForm from './parts/form/RecordForm'
+import React, { useState } from 'react'
 import recordContext, { recordStates } from './context'
-import { productContext } from 'pages/product/single/context'
-import HashKey from 'components/common/hashKey/HashKey'
+import RecordForm from './parts/form/RecordForm'
 
 export interface IRecordModalProduct {
     title: string
@@ -14,40 +13,53 @@ export interface IRecordModalProduct {
     sku: Isku
 }
 
-interface Iprops {
+interface Props {
     open: boolean
-    close: Function
+    close: () => void
     product: any
     sku: Isku
 }
 
-function RecordModal({ close, open, product, sku }: Iprops) {
+function RecordModal({ close, open, product, sku }: Props) {
     const [State, setState] = useState(recordStates)
 
     // Close Modal
-    const closeModal = useCallback(async () => {
+    const closeModal = () => {
         close()
         setState(recordStates)
-    }, [])
+    }
 
     const isRecordAllSKUs = Array.isArray(sku) && sku.length > 1;
 
     return (
-        <recordContext.Provider value={{
-            state: State,
-            product,
-            updateState: (key: string, value: string) => setState(prev => ({ ...prev, [key]: value })),
-        }}>
+        <recordContext.Provider
+            value={{
+                state: State,
+                product,
+                updateState: (key: string, value: string) => setState(prev => ({ ...prev, [key]: value }))
+            }}
+        >
             <AppModal
                 open={open}
                 close={() => !State.loading && !State.hashkey ? closeModal() : {}}
-                size={"2xl"}
-                contentProps={{
-                    padding: "30px"
-                }}
+                size="2xl"
+                contentProps={{ padding: "30px" }}
                 isCentered={false}
             >
-                {State.hashkey ? <HashKey text="Sku record successful" blockchain={State.blockchain} hashkey={State.hashkey} close={closeModal} /> : <RecordForm close={closeModal} product={product} sku={sku} isRecordAllSKUs={isRecordAllSKUs} />}
+                {State.hashkey ?
+                    <HashKey
+                        text="Sku record successful"
+                        blockchain={State.blockchain}
+                        hashkey={State.hashkey}
+                        close={closeModal}
+                    />
+                    :
+                    <RecordForm close={closeModal}
+                        product={product}
+                        sku={sku}
+                        isRecordAllSKUs={isRecordAllSKUs}
+                    />
+                }
             </AppModal>
         </recordContext.Provider>
     )
