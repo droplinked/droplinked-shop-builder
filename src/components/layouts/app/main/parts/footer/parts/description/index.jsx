2a7fc588c006c50b268ef5208043bb034c68e714
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-10-07 11:16:10 +0330

    Remove period after 'transparently' and apply bold formatting to the entire tagline and email text

--- src/components/layouts/app/main/parts/footer/parts/description/index.jsx
+++ src/components/layouts/app/main/parts/footer/parts/description/index.jsx
@@ -1,32 +1,27 @@
-import { Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import React from "react";
-import droplinkFull from "assest/image/droplinkFull.svg";
-import AppTypography from "components/common/typography/AppTypography";
-import { Link } from "react-router-dom";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import AppIcons from "assest/icon/Appicons";
+import AppTypography from "components/common/typography/AppTypography";
+import React from "react";
 
 function Description() {
     return (
-        <VStack align="stretch" spacing={5} width={"full"}>
-            <Link to="/#banner">
-                <Box display="flex" alignItems="center" gap="16px" cursor={"pointer"}>
-                    <Box width={"40px"} height={"40px"} sx={{ svg: { path: { fill: "white" } } }}>
-                        <AppIcons.SidebarDroplinked width="40px" height="40px" />
-                    </Box>
-                    <Image src="https://upload-file-droplinked.s3.amazonaws.com/5e19eaa5a7095f55b005e12397acf5f874ec94aa3923e5e8a04f3abd8787f081.png" width="214px" height="auto" flexShrink="0" />
-                </Box>
-            </Link>
-            <AppTypography width={"full"} fontFamily={"Inter"} fontSize="14px" fontWeight="400" color={"#B1B1B1"}>
-                Commerce tools to sell and settle transparently.
+        <Flex direction="column" gap={5} sx={{ p: { fontWeight: 700 } }}>
+            <Box display="flex" alignItems="center" gap={4} cursor="pointer" sx={{ svg: { path: { fill: "white" } } }}>
+                <AppIcons.SidebarDroplinked width="40px" height="40px" />
+                <Image src="https://upload-file-droplinked.s3.amazonaws.com/5e19eaa5a7095f55b005e12397acf5f874ec94aa3923e5e8a04f3abd8787f081.png" width="214px" height="auto" flexShrink="0" />
+            </Box>
+
+            <AppTypography fontFamily="Inter" fontSize={14} color="#B1B1B1">
+                Commerce tools to sell and settle transparently
             </AppTypography>
 
             <a href="mailto:Support@droplinked.com">
-                <Text fontFamily="Avenir Next" fontSize="12px" fontWeight="500" color="lightGray">
+                <AppTypography fontFamily="Avenir Next" color="lightGray">
                     Support@droplinked.com
-                </Text>
+                </AppTypography>
             </a>
-        </VStack>
-    );
+        </Flex>
+    )
 }
 
-export default Description;
+export default Description
