a50d14713f2086a1a798c2a6df49c9a340aa80b8
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-10-10 09:27:42 +0330

    Update texts and titles on tokenizing products landing page

--- src/pages/public-pages/landings/tokenizing-products-page/TokenizingProductsPage.tsx
+++ src/pages/public-pages/landings/tokenizing-products-page/TokenizingProductsPage.tsx
@@ -7,28 +7,28 @@ function TokenizingProductsPage() {
     const data: ILandingPageTemplate = {
         aboveTheFoldSection: {
             image: "https://upload-file-droplinked.s3.amazonaws.com/2344fa8e1a4cf393e62bed5a458ed9543c31dff85d02cba30fb0994405a1d43c.png",
-            title: "Tokenize Your Physical Assets Seamlessly",
-            description: "Leverage blockchain technology to tokenize your physical products, ensuring secure ownership, transparency, and global liquidity.",
+            title: "Better Attribution for Inventory",
+            description: "Leverage onchain technology to tokenize inventory ensuring ownership and transparency while unlocking working capital for the business.",
             CTAButtonText: "Get Started"
         },
         detailsSection: {
-            title: "Turn Physical Goods into Tokens",
-            description: "Create digital representations of your physical assets on the blockchain for secure and transparent trading.",
+            title: "No-Code Tools to Tokenize Inventory",
+            description: "Create digital records of inventory and assets onchain to ensure secure and transparent distribution and trading",
             detailItems: [
                 {
                     icon: <AppIcons.LandingPageCode />,
-                    title: "Blockchain-Powered Ownership",
-                    description: "Transform physical goods into tokenized assets, secured on the blockchain for tamper-proof ownership."
+                    title: "Blockchain Backed Ownership",
+                    description: "Transform goods into tokenized assets that are secured onchain for tamper-proof ownership."
                 },
                 {
                     icon: <AppIcons.LandingPageCreditCard />,
-                    title: "Seamless Tokenization",
-                    description: "Easily tokenize luxury collectibles, physical products, and other assets with NFT technology, backed by secure custody."
+                    title: "Simplifying Tokenization",
+                    description: "Easily tokenize luxury collectibles, physical items and other assets with NFT technology that enables custody ownership for distribution."
                 },
                 {
                     icon: <AppIcons.LandingPageTemplate />,
                     title: "Global Trading & Liquidity",
-                    description: "Unlock new global markets by making your physical assets tradable as tokens, increasing liquidity and reach."
+                    description: "Unlock new global markets by making tokenized inventory tradable, increasing liquidity and reach with value-add partners and businesses."
                 }
             ]
         },
@@ -36,27 +36,27 @@ function TokenizingProductsPage() {
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/3f9b272c6436e721fed899d9dec680d05a7bb657e65bb2328e49ff5811359b4b.png",
                 title: "Secure Custody and Ownership",
-                description: "Work with trusted custodians to securely hold your physical assets while tokenized NFTs represent ownership on the blockchain"
+                description: "Work with trusted custodians to securely store assets with the tokenized product records for onchain ownership and availability."
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/2fd25a51290cae0d89fa5fb1512975cea3193c599cda080d65b09f4f0897454d.png",
                 title: "Transparent and Immutable Records",
-                description: "Tokenization ensures all relevant product information, transaction history, and ownership are stored immutably on the blockchain"
+                description: "Tokenization ensures all relevant product information, transaction history and ownership are stored immutably for accurate attribution."
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/12babd8cd671cd4587bd8cab7e78f31e725415fcc84544ded45b07f9fe3d74d2.png",
-                title: "Effortless Transferability",
-                description: "Easily trade your tokenized physical assets across platforms, with full transparency and trust guaranteed by blockchain technology"
+                title: "Simple and Secure Transfers",
+                description: "Send and trade tokenized physical and digital items across platforms with onchain transparency to ensure trust between parties."
             }
         ],
         featureGroups: [
             {
                 features: [
-                    { title: "Digital Representation of Physical Goods", description: "Tokenization allows you to create NFTs that represent physical products, enhancing traceability and securing ownership on the blockchain." },
-                    { title: "Enhanced Transparency", description: "All details, from transaction history to product metadata, are stored transparently on the blockchain, accessible to all potential buyers." },
-                    { title: "Global Market Access", description: "Tokenized assets can be traded worldwide, unlocking new market opportunities and increasing the liquidity of physical goods." },
-                    { title: "Secure Custodianship", description: "Physical assets are securely stored by trusted third parties, while the NFTs representing ownership can be traded until the owner requests the physical item." },
-                    { title: "Increased Efficiency in Ownership Transfer", description: "Instantly transfer ownership of physical assets through NFTs, reducing the time and cost associated with traditional methods of transferring physical goods." }
+                    { title: "Digital Representation of Inventory", description: "Tokenization allows you to create records representing inventory to enhancing traceability and secure ownership onchain." },
+                    { title: "Enhanced Transparency", description: "All details involved with the inventory from transaction history to product metadata are stored onchain to display to 3rd parties." },
+                    { title: "Global Market Access", description: "Tokenized assets can be traded globally to further unlock new market opportunities by adding liquidity for existing inventory." },
+                    { title: "Secure Custodianship", description: "Inventory can be securely stored by trusted third parties while ownership records can be traded until the rightful owner claims possession of the item." },
+                    { title: "Increased Efficiency in Ownership Transfer", description: "Instantly transfer ownership to reduce the time and cost associated with traditional methods of transferring inventory." }
                 ]
             }
         ]
