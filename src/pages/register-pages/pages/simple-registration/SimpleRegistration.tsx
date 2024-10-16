fe2218a2829897d118e38e558e180c2e192ac2c7
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-10-16 15:50:11 +0330

    Fix issue with redirecting newly created shop to dashboard on shop management page

--- src/pages/register-pages/pages/simple-registration/SimpleRegistration.tsx
+++ src/pages/register-pages/pages/simple-registration/SimpleRegistration.tsx
@@ -7,20 +7,22 @@ import { useNavigate } from "react-router-dom";
 function SimpleRegistration() {
     const { shop } = useProfile();
     const navigate = useNavigate();
-    const hasShopName = useMemo(() => shop?.name && !shop?.name?.startsWith("default_droplinked"), [shop?.name]);
+    const hasShopName = useMemo(() => shop?.name && !shop.name.startsWith("default_droplinked"), [shop?.name]);
     const hasCreatedCircleWallet = useMemo(() => Boolean(shop?.circleWallets?.length), [shop?.circleWallet]);
     const [modal, setModal] = useState<"URL" | "WALLET">(hasShopName ? "WALLET" : "URL");
+
     const toggleModal = () => {
-        if (hasCreatedCircleWallet) navigate("/analytics");
-        else setModal("WALLET");
-    };
+        if (hasCreatedCircleWallet) return navigate("/analytics")
+        setModal("WALLET")
+    }
+
     useEffect(() => {
         if (hasShopName && hasCreatedCircleWallet) navigate("/analytics");
-        console.log(hasShopName, hasCreatedCircleWallet)
     }, [hasCreatedCircleWallet, hasShopName]);
-    if(hasShopName && hasCreatedCircleWallet) return <></>
-    if (modal === "URL") return <SimpleRegistrationModal isOpen={true} mode="REGISTER_SHOP_NAME" toggleModal={toggleModal} />;
-    else if (modal === "WALLET") return <CreateWallet hasCreatedCircleWallet={hasCreatedCircleWallet} />;
+
+    if (hasShopName && hasCreatedCircleWallet) return <></>
+    if (modal === "URL") return <SimpleRegistrationModal isOpen={true} mode="REGISTER_SHOP_NAME" onSuccess={toggleModal} />;
+    if (modal === "WALLET") return <CreateWallet hasCreatedCircleWallet={hasCreatedCircleWallet} />;
     else return <></>;
 }
 
