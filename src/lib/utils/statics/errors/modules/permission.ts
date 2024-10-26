605929e026378ac6f6a53ea1dcb3169737d06f32
yasaman <ysmn.dev@gmail.com>
2024-10-26 10:15:39 +0330

    Refactor permission error messages for consistency and readability

--- src/lib/utils/statics/errors/modules/permission.ts
+++ src/lib/utils/statics/errors/modules/permission.ts
@@ -2,18 +2,18 @@ import { LegalUsageKey } from "lib/apis/subscription/interfaces";
 
 const permissionErrors = {
   permission_denied: "Permission Denied",
-  maxActiveLoginMethods: (maxActiveLoginMethodCount: string) =>
-    `You can only activate up to ${
-      Number(maxActiveLoginMethodCount) === 1
-        ? `${maxActiveLoginMethodCount} login  method`
-        : `${maxActiveLoginMethodCount} login  methods`
-    }`,
-  maxActivePaymentMethods: (maxActivePaymentMethodCount: string) =>
-    `You can only activate up to ${
-      Number(maxActivePaymentMethodCount) === 1
-        ? `${maxActivePaymentMethodCount} payment method`
-        : `${maxActivePaymentMethodCount} payment methods`
-    }`,
+  maxActiveLoginMethods: (maxActiveLoginMethodCount: string) => {
+    const count = Number(maxActiveLoginMethodCount);
+    return `You can only activate up to ${count} login method${
+      count !== 1 ? "s" : ""
+    }`;
+  },
+  maxActivePaymentMethods: (maxActivePaymentMethodCount: string) => {
+    const count = Number(maxActivePaymentMethodCount);
+    return `You can only activate up to ${count} payment method${
+      count !== 1 ? "s" : ""
+    }`;
+  },
   shop_subscription_data_unavailable:
     "Oops! It looks like we can not access subscription data at the moment. Give it another try soon?",
   product_creation_limit_reached: (legalUsageKey: LegalUsageKey) => {
