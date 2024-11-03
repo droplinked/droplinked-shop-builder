44fa9343cbcde3bd3079e0d28cd8a2601beeb3cf
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-11-03 21:47:08 +0330

    Added a download link for the product template in the header of the import product modal

--- src/components/redesign/modal/ModalHeaderData.tsx
+++ src/components/redesign/modal/ModalHeaderData.tsx
@@ -1,27 +1,23 @@
 import { Flex, ModalCloseButton, ModalHeader } from '@chakra-ui/react';
 import AppTypography from 'components/common/typography/AppTypography';
-import React, { ReactNode } from 'react';
+import React, { PropsWithChildren, ReactNode } from 'react';
 
-interface Props {
+interface Props extends PropsWithChildren {
     icon: ReactNode;
     title: string;
     description: string;
 }
 
-function ModalHeaderData({ icon, title, description }: Props) {
+function ModalHeaderData({ icon, title, description, children }: Props) {
     return (
         <ModalHeader>
-            <Flex justifyContent={"space-between"}>
+            <Flex justifyContent="space-between">
                 {icon}
-                <ModalCloseButton
-                    width={"fit-content"}
-                    height={"fit-content"}
-                    position={"relative"}
-                    color={"white"}
-                />
+                <ModalCloseButton width="fit-content" height="fit-content" position="relative" color="white" />
             </Flex>
-            <AppTypography mt={6} fontSize={24} fontWeight={700} color={"white"}>{title}</AppTypography>
-            <AppTypography mt={2} fontSize={16} color={"white"}>{description}</AppTypography>
+            <AppTypography mt={6} fontSize={24} fontWeight={700} color="white">{title}</AppTypography>
+            <AppTypography mt={2} fontSize={16} color="white">{description}</AppTypography>
+            {children}
         </ModalHeader>
     )
 }
--- src/pages/product/list/parts/import-product-modal/ImportProductModal.tsx
+++ src/pages/product/list/parts/import-product-modal/ImportProductModal.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@chakra-ui/react'
 import AppIcons from 'assest/icon/Appicons'
 import AppModal from 'components/redesign/modal/AppModal'
 import ModalHeaderData from 'components/redesign/modal/ModalHeaderData'
@@ -22,7 +23,19 @@ function ImportProductModal({ isOpen, closeModal }: Props) {
                 icon={<AppIcons.ConfirmPlan />}
                 title="Import Products"
                 description="Easily import products using a CSV or Excel file. Download our sample template to ensure your file is formatted correctly."
-            />
+            >
+                <Link
+                    href='https://upload-file-flatlay.s3.us-west-2.amazonaws.com/622e15c810c2e7fb08c93b7ffa185228feb223ea821a3a596dfdd64c63854597_or.xlsx'
+                    textDecoration="underline"
+                    fontSize={14}
+                    fontWeight={600}
+                    color="#179EF8"
+                    cursor="pointer"
+                    download="Droplinked-Shipping-Template.xlsx"
+                >
+                    Download Template
+                </Link>
+            </ModalHeaderData>
             <ImportProductModalBody file={uploadedFile} onFileChange={setUploadedFile} />
             <ImportProductModalFooter file={uploadedFile} closeModal={closeModal} />
         </AppModal>
