d0f7d4cc014520c1b1bc1248d3887297d71730c9
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-11-13 16:54:29 +0330

    Activate shipping method section's switch if a shipping method is selected on the invoice edit page

--- src/pages/invoice-management/create-invoice/components/form/shipping-methods/InvoiceShippingMethods.tsx
+++ src/pages/invoice-management/create-invoice/components/form/shipping-methods/InvoiceShippingMethods.tsx
@@ -1,4 +1,4 @@
-import { Flex, useDisclosure, useRadioGroup } from '@chakra-ui/react'
+import { Flex, useRadioGroup } from '@chakra-ui/react'
 import { useFormikContext } from 'formik'
 import useAppToast from 'functions/hooks/toast/useToast'
 import { addAddressToCartService, createAddressService } from 'lib/apis/invoice/invoiceServices'
@@ -8,15 +8,15 @@ import useCreateInvoice from 'pages/invoice-management/create-invoice/hooks/useC
 import useInvoiceStore from 'pages/invoice-management/create-invoice/store/invoiceStore'
 import React, { useEffect, useState } from 'react'
 import ToggleableSection from '../../ToggleableSection'
-import ShippingMethodsLoading from './ShippingMethodsLoading'
 import ShippingMethodRadio from './ShippingMethodRadio'
+import ShippingMethodsLoading from './ShippingMethodsLoading'
 
 export default function InvoiceShippingMethods() {
+    const { cart, updateCart, selectedShippingMethod, updateShippingMethod, isEditMode } = useInvoiceStore()
     const [isLoading, setLoading] = useState(false)
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isExpanded, setExpanded] = useState(!!selectedShippingMethod)
     const { values, validateForm } = useFormikContext<InvoiceFormSchema>()
     const { isInvoiceDataValid } = useCreateInvoice({ trigger: "SHIPPING_METHODS_SWITCH" })
-    const { cart, updateCart, selectedShippingMethod, updateShippingMethod, isEditMode } = useInvoiceStore()
     const { showToast } = useAppToast()
     const { getRootProps, getRadioProps } = useRadioGroup({
         name: 'selected-payment-method',
@@ -31,7 +31,7 @@ export default function InvoiceShippingMethods() {
         const validationResult = await validateForm()
         if (Object.entries(validationResult).length > 0) return
         if (!isInvoiceDataValid(values)) return
-        onOpen()
+        setExpanded(true)
     }
 
     const renderContent = () => {
@@ -56,7 +56,7 @@ export default function InvoiceShippingMethods() {
         (async () => {
             const { _id, easyPostAddressID, ...rest } = cart.address ?? {}
 
-            if ((isEditMode && !deepEqual(rest, values.address)) || (isOpen && !cart.address)) {
+            if ((isEditMode && !deepEqual(rest, values.address)) || (isExpanded && !cart.address)) {
                 try {
                     setLoading(true)
                     const { data: createdAddress } = await createAddressService(values.address)
@@ -67,26 +67,26 @@ export default function InvoiceShippingMethods() {
                 catch (error) {
                     if (error.response) showToast({ message: error.response.data.data.message, type: "error" })
                     else showToast({ message: (error as Error).message, type: "error" })
-                    onClose()
+                    setExpanded(false)
                 }
                 finally {
                     setLoading(false)
                 }
             }
         })()
-    }, [isOpen])
+    }, [isExpanded])
 
     useEffect(() => {
         const { _id, easyPostAddressID, ...rest } = cart.address ?? {}
-        if (!deepEqual(rest, values.address)) onClose()
+        if (!deepEqual(rest, values.address)) setExpanded(false)
     }, [values.address])
 
     return (
         <ToggleableSection
             title='Shipping'
             description='Shipping methods are based on the type of inventory and address on the invoice.'
-            isExpanded={isOpen}
-            onToggle={isOpen ? onClose : handleToggle}
+            isExpanded={isExpanded}
+            onToggle={isExpanded ? () => setExpanded(false) : handleToggle}
         >
             {renderContent()}
         </ToggleableSection>
