37c865c1135052c6b707a5c8d1fe98ba7aa3d559
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2024-11-25 13:45:36 +0330

    Refactor SortableCollection component
    Refactor SortableCollection component by extracting VisibilitySwitch and CollectionTitle components for improved readability and maintainability

--- src/pages/collections/components/collection-reorder-modal/sortable-collection/SortableCollection.tsx
+++ src/pages/collections/components/collection-reorder-modal/sortable-collection/SortableCollection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Flex } from '@chakra-ui/react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
@@ -7,14 +7,11 @@ import { CSS } from '@dnd-kit/utilities';
 import AppIcons from 'assest/icon/Appicons';
 
 // Components
-import AppSwitch from 'components/common/swich';
-import AppTypography from 'components/common/typography/AppTypography';
+import VisibilitySwitch from './VisibilitySwitch';
+import CollectionTitle from './CollectionTitle';
+import { Collection } from 'lib/apis/collection/interfaces';
 
-// APIs
-import { updateCollectionVisiblityService } from 'lib/apis/collection/services';
-import useAppToast from 'functions/hooks/toast/useToast';
-
-function SortableCollection({ collection, index }: { collection: any, index: number }) {
+function SortableCollection({ collection, index }: { collection: Collection, index: number }) {
     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
         id: collection._id,
         animateLayoutChanges: () => false
@@ -31,24 +28,12 @@ function SortableCollection({ collection, index }: { collection: any, index: num
         borderTop: index !== 0 && "1px solid #292929"
     };
 
-    const { showToast } = useAppToast()
-    const [isVisible, setIsVisible] = useState(collection?.published);
-
-    const handleVisibleSwitch = async () => {
-        setIsVisible((prev) => !prev);
-        try {
-            await updateCollectionVisiblityService({ collectionID: collection?._id, published: !isVisible });
-        } catch (error) {
-            showToast({ message: "You cannot change your collection status at this time. Please try again later", type: "error" })
-        }
-    };
-
     return (
         <div ref={setNodeRef} style={style} {...attributes}>
             <Flex width={"100%"} justifyContent={"space-between"} alignItems={"center"}>
                 <Flex alignItems={"center"} gap={"16px"} padding={"8px 0"}>
-                    <AppSwitch name={`visibility_${collection?._id}`} isChecked={isVisible} onChange={handleVisibleSwitch} />
-                    <AppTypography fontSize={16} fontWeight={500} color={"#c2c2c2"}>{collection?.title}</AppTypography>
+                    <VisibilitySwitch collection={collection} />
+                    <CollectionTitle title={collection?.title} />
                 </Flex>
                 <Flex {...listeners}>
                     <AppIcons.Order />
