a09ef8e11982ca8a1797ba0868469fb2b5aa9116
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2024-12-01 12:11:26 +0330

    Update RulesetAddress.tsx
    removed filter method to prevent remove first input

--- src/pages/collections/components/controls/components/rulesets/components/address/RulesetAddress.tsx
+++ src/pages/collections/components/controls/components/rulesets/components/address/RulesetAddress.tsx
@@ -1,11 +1,10 @@
-import { Flex, HStack, Input, VStack, IconButton, Box } from '@chakra-ui/react';
+import { HStack, VStack, Box } from '@chakra-ui/react';
 import AppIcons from 'assest/icon/Appicons';
 import ErrorLabel from 'components/redesign/form/errorLabel/errorLabel';
 import FieldLabel from 'components/redesign/form/fieldLabel/FieldLabel';
 import AppSkeleton from 'components/common/skeleton/AppSkeleton';
 import AppTypography from 'components/common/typography/AppTypography';
-import useAppToast from 'functions/hooks/toast/useToast';
-import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext } from 'react';
 import ruleModelContext from '../../context';
 import AppInput from 'components/redesign/form/textbox/AppInput';
 import { FaPlus } from 'react-icons/fa6';
@@ -20,7 +19,7 @@ function RulesetAddress() {
     const handleInputChange = (index: number, value: string) => {
         const newInputs = [...values.address];
         newInputs[index] = value;
-        setFieldValue("address", newInputs.filter(input => input.trim().length > 0));
+        setFieldValue("address", newInputs);
     };
 
     const handleDelete = (index: number) => {
@@ -35,9 +34,9 @@ function RulesetAddress() {
                 <AppTypography fontSize="14px" color="#7b7b7b">Enter the contract addresses to be used for validation of possession or ownership.</AppTypography>
             </VStack>
             <AppSkeleton isLoaded={loading} width={"100%"}>
-                <VStack alignItems="center" justifyContent={"center"} width={"100%"} style={{ cursor: "text", ...errors?.address && { border: "1px solid #FEB2B2" } }} borderRadius="8px">
+                <VStack alignItems="center" justifyContent={"center"} width={"100%"} style={{ cursor: "text", ...errors?.address && { border: "1px solid #FF2244" } }} borderRadius="8px">
                     {values.address.map((input, index) => (
-                        <HStack width={"100%"} key={index} borderRadius="4px" justifyContent={"center"} alignItems="center">
+                        <HStack gap={0} width={"100%"} key={index} borderRadius="4px" justifyContent={"center"} alignItems="center">
                             <AppInput
                                 name={"text"}
                                 width="100%"
