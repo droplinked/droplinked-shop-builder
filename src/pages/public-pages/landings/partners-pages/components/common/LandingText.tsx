f37d42484ee0f8e9b1ecbddc380fab217a31a8dc
yasaman <ysmn.dev@gmail.com>
2024-12-10 12:09:42 +0400

    refactor: dynamic TLD visibility and rename D3Paragraph to LandingText

--- src/pages/public-pages/landings/partners-pages/components/common/LandingText.tsx
+++ src/pages/public-pages/landings/partners-pages/components/common/LandingText.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 interface Props extends TextProps { }
 
-function D3Paragraph({ children, ...props }: Props) {
+function LandingText({ children, ...props }: Props) {
     return (
         <AppTypography fontSize={{ base: 14, md: 16 }} fontWeight={400} color="#7B7B7B" {...props}>{children}</AppTypography>
     )
 }
 
-export default D3Paragraph
+export default LandingText
