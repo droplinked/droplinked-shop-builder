98189cc237e8d06564d050000e210de72e4dbc76
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2024-12-27 14:15:10 +0330

    Connect ProductDeliveryMessage component to ProductForm context

--- src/pages/products/components/ProductDrawer/components/fields/ProductDeliveryMessage.tsx
+++ src/pages/products/components/ProductDrawer/components/fields/ProductDeliveryMessage.tsx
@@ -1,31 +1,56 @@
 import Input from 'components/redesign/input/Input'
 import Textarea from 'components/redesign/textarea/Textarea'
+import useProductForm from 'pages/products/hooks/useProductForm'
 import React, { useState } from 'react'
 import SwitchBox from '../common/SwitchBox'
 
 function ProductDeliveryMessage() {
-    const [showMore, setShowMore] = useState(false)
+    const { values: { digitalDetail }, setFieldValue } = useProductForm()
+    const { message, file_url } = digitalDetail
+    const [showDeliveryMessage, setShowDeliveryMessage] = useState(Boolean(message || file_url))
+
+    const updateField = (field: keyof typeof digitalDetail, value: string) => {
+        setFieldValue("digitalDetail", {
+            ...digitalDetail,
+            [field]: value,
+        })
+    }
+
+    const handleToggle = (isChecked: boolean) => {
+        setShowDeliveryMessage(isChecked)
+
+        if (!isChecked) {
+            updateField("message", null)
+            updateField("file_url", null)
+        }
+    }
 
     return (
         <SwitchBox
-            title='Delivery Message'
-            description='Provide customers with important delivery details at checkout, such as estimated shipping times or special instructions.'
-            isChecked={showMore}
-            onToggle={(e) => setShowMore(e.target.checked)}
+            title="Delivery Message"
+            description="Provide customers with important delivery details at checkout, such as estimated shipping times or special instructions."
+            isChecked={showDeliveryMessage}
+            onToggle={(e) => handleToggle(e.target.checked)}
         >
-            {showMore && (
+            {showDeliveryMessage && (
                 <>
                     <Textarea
-                        placeholder='e.g., Expected delivery within 5–7 business days, or include specific delivery instructions.'
+                        value={message || ""}
+                        onChange={(e) => updateField("message", e.target.value)}
+                        placeholder="e.g., Expected delivery within 5–7 business days, or include specific delivery instructions."
                         rows={8}
                     />
                     <Input
                         inputGroupProps={{ mt: 4 }}
-                        inputProps={{ placeholder: "e.g., www.sample.com", fontSize: 16 }}
+                        inputProps={{
+                            value: file_url || "",
+                            onChange: (e) => updateField("file_url", e.target.value),
+                            placeholder: "e.g., www.sample.com",
+                            fontSize: 16,
+                        }}
                     />
                 </>
             )}
-
         </SwitchBox>
     )
 }
