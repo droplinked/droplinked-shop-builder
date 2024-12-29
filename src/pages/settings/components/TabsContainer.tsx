09a238f18acabba13a9736f60f845c44d66a3c42
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2024-12-29 15:17:02 +0330

    added transition for tab component

--- src/pages/settings/components/TabsContainer.tsx
+++ src/pages/settings/components/TabsContainer.tsx
@@ -1,11 +1,11 @@
-import { Tab, TabList, Tabs } from '@chakra-ui/react';
-import AppTypography from 'components/common/typography/AppTypography';
-import * as React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Tab, TabList, Tabs } from "@chakra-ui/react";
+import AppTypography from "components/common/typography/AppTypography";
+import * as React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function TabsContainer() {
-    const navigate = useNavigate()
-    const location = useLocation()
+    const navigate = useNavigate();
+    const location = useLocation();
     const tabs = [
         {
             title: "General",
@@ -27,24 +27,47 @@ function TabsContainer() {
             title: "User Management",
             route: "user-management",
         },
-    ]
+    ];
 
     return (
-        <Tabs variant={"unstyled"} width={"100%"} borderTop={"1px solid #292929"} borderBottom={"none"}>
+        <Tabs
+            variant={"unstyled"}
+            width={"100%"}
+            borderTop={"1px solid #292929"}
+            borderBottom={"none"}
+        >
             <TabList borderBottom={"1px solid #292929"}>
-                {
-                    tabs.map((tab, index) => {
-                        const isSelected = location.pathname.endsWith(tab.route);
+                {tabs.map((tab, index) => {
+                    const isSelected = location.pathname.endsWith(tab.route);
 
-                        return (
-                            <Tab aria-selected={"false"} {...isSelected && { borderBottom: "1px solid #fff" }} _focusWithin={{ background: "transparent", borderBottom: "1px solid #fff" }} px={6} py={4} onClick={() => navigate(tab.route)} key={index}>
-                                <AppTypography color={location.pathname.endsWith(tab.route) ? "#fff" : "#7B7B7B"} fontSize={16}>{tab.title}</AppTypography>
-                            </Tab>
-                        )
-                    })
-                }
+                    return (
+                        <Tab
+                            aria-selected={"false"}
+                            _focusWithin={{
+                                background: "transparent",
+                                borderBottom: "1px solid #fff",
+                            }}
+                            sx={{
+                                transition: "border-bottom 0.3s ease-in-out",
+                                borderBottom: `1px solid ${isSelected ? "#fff" : "transparent"}`
+                            }}
+                            px={6}
+                            py={4}
+                            onClick={() => navigate(tab.route)}
+                            key={index}
+                        >
+                            <AppTypography
+                                color={isSelected ? "#fff" : "#7B7B7B"}
+                                fontWeight={isSelected ? 500 : 400}
+                                fontSize={16}
+                            >
+                                {tab.title}
+                            </AppTypography>
+                        </Tab>
+                    );
+                })}
             </TabList>
-        </Tabs >
+        </Tabs>
     );
 }
 
