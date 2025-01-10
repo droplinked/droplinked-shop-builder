f6f63a2a73e1840b982104350d626d6d24fcb62d
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-01-10 16:00:07 +0330

    Feat: implement Credits and Coupons components; add AddBalanceModal for credit management and payment processing

--- src/pages/settings/components/pages/credit-and-coupons/CreditAndCoupons.tsx
+++ src/pages/settings/components/pages/credit-and-coupons/CreditAndCoupons.tsx
@@ -1,9 +1,19 @@
-import AppTypography from 'components/common/typography/AppTypography';
+import { Box, Divider } from '@chakra-ui/react';
 import React from "react"
+import Credits from './components/credits/Credits';
+import Coupons from './components/coupons/Coupons';
 
 function CreditAndCoupons() {
     return (
-        <AppTypography>CreditAndCoupons Component</AppTypography>
+        <>
+            <Box px={{ base: 4, md: 6 }} >
+                <Credits />
+            </Box>
+            <Divider borderColor={"#292929"} />
+            <Box px={{ base: 4, md: 6 }} >
+                <Coupons />
+            </Box>
+        </>
     );
 }
 
---
+++ src/pages/settings/components/pages/credit-and-coupons/components/credits/Balance.tsx
@@ -0,0 +1,69 @@
+import { Flex, Image, Spinner, useDisclosure } from "@chakra-ui/react";
+import AppIcons from "assest/icon/Appicons";
+import AppTypography from "components/common/typography/AppTypography";
+import useAppStore from "lib/stores/app/appStore";
+import { currencyConvertion } from "lib/utils/helpers/currencyConvertion";
+import React, { useState } from "react";
+import AddBalanceModal from "./add-balance-modal/AddBalanceModal";
+import useAppToast from "functions/hooks/toast/useToast";
+
+export default function Balance() {
+    const { shop: { currency, credit, name }, fetchShop } = useAppStore();
+    const { isOpen, onClose, onOpen } = useDisclosure()
+    const { showToast } = useAppToast()
+    const [loading, setLoading] = useState(false);
+    const handleRefetchShop = async () => {
+        setLoading(true);
+        try {
+            await fetchShop({ shopName: name });
+        }
+        catch {
+            showToast({ message: "Oops! Something went wrong", type: "error" })
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <Flex
+            gap={8}
+            borderRadius={"8px"}
+            bg={"rgba(28, 28, 28, 0.64)"}
+            backdropFilter={"blur(16px)"}
+            border={"1px solid #292929"}
+            flexDirection={"column"}
+            p={4}
+        >
+            <Flex justifyContent={"space-between"} alignItems={"center"}>
+                <Image
+                    width={"32px"}
+                    src="https://upload-file-droplinked.s3.amazonaws.com/f9d54d7454b232b915a90bbd7e3c75df6f2fb2a728bcd364daecca3feb61bb47.png"
+                    alt="droplinked logo"
+                />
+                <Flex
+                    cursor={"pointer"}
+                    alignItems={"center"}
+                    gap={1}
+                    sx={{ path: { stroke: "#179EF8" } }}
+                    onClick={onOpen}
+                >
+                    <AppIcons.Refresh />
+                    <AppTypography fontWeight={500} color={"#179EF8"}>
+                        Charge
+                    </AppTypography>
+                </Flex>
+            </Flex>
+            {loading ?
+                <Spinner color="white" />
+                :
+                <AppTypography fontSize={"16px"} color={"#fff"} sx={{ span: { color: "#7B7B7B" } }}>
+                    {currency?.symbol}{" "}
+                    {currencyConvertion(credit, currency?.conversionRateToUSD, false)}{" "}
+                    <span>{currency?.abbreviation}</span>
+                </AppTypography>
+            }
+            <AddBalanceModal handleRefetchShop={handleRefetchShop} isOpen={isOpen} onClose={onClose} />
+        </Flex>
+    );
+}
