eb0e7e3f2a0cae7e0314cdc4f29b6ded1b4ccfcc
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-01-11 22:47:08 +0330

    Feat: refactor CouponForm to use separate AmountInput and QuantityInput components for improved readability and maintainability

--- src/pages/settings/components/pages/credit-and-coupons/components/coupons/modals/coupons-edit-creation/CouponForm.tsx
+++ src/pages/settings/components/pages/credit-and-coupons/components/coupons/modals/coupons-edit-creation/CouponForm.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import { CouponFormValues } from "./formConfigs";
 import { Box, Flex } from "@chakra-ui/react";
 import Input, { InputHeader } from "components/redesign/input/Input";
-import Select from "components/redesign/select/Select";
 import AppDatePicker from "components/redesign/date-picker/AppDatePicker";
+import AmountInput from "./AmountInput";
+import QuantityInput from "./QuantityInput";
 
 export default function CouponForm({ isEdit }: { isEdit?: boolean }) {
     const { values, handleChange, errors, setFieldValue } = useFormikContext<CouponFormValues>();
@@ -25,50 +26,8 @@ export default function CouponForm({ isEdit }: { isEdit?: boolean }) {
                 }}
                 {...errors.name && { error: errors.name, state: "error" }}
             />
-            <Flex flexDirection={"column"}>
-                <InputHeader label="Amount" description="Select the discount type between shop’s currency or percentage." inputProps={{ isRequired: true }} />
-                <Flex gap={4} width={"100%"}>
-                    <Input
-                        inputProps={{
-                            onChange: handleChange,
-                            name: "balance",
-                            placeholder: "0.00",
-                            value: values.balance,
-                            isDisabled: isEdit,
-                            type: "number",
-                        }}
-                        {...errors.name && { error: errors.balance, state: "error" }}
-                    />
-                    <Select
-                        items={[
-                            { label: "Percentage", value: "DISCOUNT" },
-                            { label: "USD", value: "CREDIT" }
-                        ]}
-                        labelAccessor="label"
-                        valueAccessor="value"
-                        error={errors.type}
-                        selectProps={{ name: "type", onChange: handleChange, value: values.type, isDisabled: isEdit }}
-                    />
-                </Flex>
-            </Flex>
-            <Flex justifyContent={"space-between"} alignItems={"center"}>
-                <Flex width={"100%"} flexDirection={"column"}>
-                    <InputHeader label="Quantity" description="Number of times this discount can be used." inputProps={{ isRequired: true }} />
-                </Flex>
-                <Box width={"5rem"}>
-                    <Input
-                        inputProps={{
-                            onChange: handleChange,
-                            name: "quantity",
-                            placeholder: "9999",
-                            type: "number",
-                            value: values.quantity,
-                            isDisabled: isEdit
-                        }}
-                        {...errors.name && { error: errors.quantity, state: "error" }}
-                    />
-                </Box>
-            </Flex>
+            <AmountInput isEdit={isEdit} />
+            <QuantityInput isEdit={isEdit} />
             <Flex flexDirection={"column"}>
                 <InputHeader label="Expiration Date" description="Select a specific expiration date for discount." inputProps={{ isRequired: true }} />
                 <Box {...errors.expiryDate && { border: "1px solid #F24" }} width={"min-content"} borderRadius={"8px"}>
