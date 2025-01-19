846dee131316f2d6292f4412cdbec9f660a09179
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-01-19 14:28:30 +0330

    Clean up and optimize Social Media Card component in Rewards page for better readability and code structure

--- src/pages/rewards/components/social-media/SocialMediaCard.tsx
+++ src/pages/rewards/components/social-media/SocialMediaCard.tsx
@@ -1,119 +1,106 @@
-import { Box, Flex, HStack, Text, useMediaQuery, VStack } from '@chakra-ui/react';
-import AppIcons from 'assest/icon/Appicons';
-import useAppStore from 'lib/stores/app/appStore';
-import React from 'react';
+import { Center, Circle, Flex, HStack, Text, useMediaQuery, VStack } from '@chakra-ui/react'
+import AppIcons from 'assest/icon/Appicons'
+import useAppToast from 'functions/hooks/toast/useToast'
+import { trackFollowService } from 'lib/apis/quests/services'
+import useAppStore from 'lib/stores/app/appStore'
+import useFollowStatus from 'pages/rewards/hook/useFollowStatus'
+import React from 'react'
 
-interface SocialMediaCardProps {
-  title: string;
-  description: string;
-  duration: string;
-  icon: React.ReactNode;
-  onClick?: () => void;
-  cursor?: string;
-  isFollowed?: boolean;
-  allFollowed?: boolean;
-}
-
-const SocialMediaCard: React.FC<SocialMediaCardProps> = ({ title, description, duration, icon, onClick, cursor = 'pointer', isFollowed, allFollowed }) => {
-  const { shop } = useAppStore();
+function SocialMediaCard({ promotion }: { promotion: any }) {
+  const { shop } = useAppStore()
   const [isSmallerThan1280] = useMediaQuery('(max-width: 1280px)')
+  const { followStatus, allPlatformsFollowed, updateFollowStatus } = useFollowStatus()
+  const { showToast } = useAppToast()
 
-  const renderStatus = React.useMemo(() => {
+  const { title, description, duration, platform, link, icon } = promotion
+  const isFollowed = followStatus[platform]
+
+  const handleCardClick = async () => {
+    if (!shop) return
+    window.open(link, '_blank')
+    try {
+      await trackFollowService({ platform })
+      updateFollowStatus(platform)
+    } catch (error) {
+      showToast({ message: `Failed to track follow on ${platform}. Please try again later.`, type: 'error' })
+    }
+  }
+
+  const renderIcon = () => (
+    <Center
+      flexShrink={0}
+      w="56px"
+      h="56px"
+      border="1px solid"
+      borderRadius="xl"
+      borderColor={isFollowed && !allPlatformsFollowed ? '#2BCFA11A' : '#222222'}
+      bg={isFollowed && !allPlatformsFollowed ? '#2BCFA11A' : '#141414'}
+      backdropFilter="blur(10px)"
+    >
+      {icon}
+    </Center>
+  )
+
+  const renderStatus = () => {
     if (shop) {
-      if (allFollowed) {
+      if (allPlatformsFollowed) {
         return (
           <Text color="#2BCFA1" textAlign="center" fontSize="16px" fontWeight="500" lineHeight="24px">
             Claim Reward
           </Text>
-        );
+        )
       }
-      if (isFollowed) {
-        return <AppIcons.Tick width="24px" height="24px" />;
-      }
+      if (isFollowed) return <AppIcons.Tick width="24px" height="24px" />
     }
-    return <AppIcons.ExternalArrow />;
-  }, [allFollowed, isFollowed, shop]);
+    return <AppIcons.ExternalArrow />
+  }
+
+  const renderStatusIndicator = (display) => (
+    <Center p={3} borderRadius="lg" display={display}>
+      {renderStatus()}
+    </Center>
+  )
 
   return (
     <Flex
       flex="1"
-      p={4}
-      bg={isFollowed && !allFollowed ? '#2BCFA11A' : '#010101'}
-      borderRadius="3xl"
-      border="1px solid"
-      borderColor={isFollowed && !allFollowed ? '#2BCFA11A' : '#222222'}
-      backdropFilter="blur(100px)"
-      gap={4}
-      overflow="hidden"
       flexDirection={{ base: 'column', lg: 'row' }}
       alignItems={{ base: 'flex-start', lg: 'center' }}
-      cursor={cursor}
-      onClick={onClick}
+      gap={4}
+      border="1px solid"
+      borderRadius="3xl"
+      borderColor={isFollowed && !allPlatformsFollowed ? '#2BCFA11A' : '#222222'}
+      padding={4}
+      bg={isFollowed && !allPlatformsFollowed ? '#2BCFA11A' : '#010101'}
+      backdropFilter="blur(100px)"
+      cursor={isFollowed ? 'default' : 'pointer'}
+      onClick={handleCardClick}
     >
       {/* Mobile Icon Section */}
-      {isSmallerThan1280 &&
-        <Flex w="full" justifyContent="space-between" alignItems="center" display={{ base: 'flex', lg: 'none' }}>
-          {/* Icon */}
-          <Flex
-            w="56px"
-            h="56px"
-            bg={isFollowed && !allFollowed ? '#2BCFA11A' : '#141414'}
-            borderRadius="xl"
-            border="1px solid"
-            borderColor={isFollowed && !allFollowed ? '#2BCFA11A' : '#222222'}
-            backdropFilter="blur(10px)"
-            justifyContent="center"
-            alignItems="center"
-          >
-            {icon}
-          </Flex>
-          {/* Status Indicator */}
-          <Flex p={3} borderRadius="lg" justifyContent="center" alignItems="center">
-            {renderStatus}
-          </Flex>
-        </Flex>}
+      {isSmallerThan1280 && (
+        <Flex w="full" justifyContent="space-between" alignItems="center">
+          {renderIcon()}
+          {renderStatusIndicator('flex')}
+        </Flex>
+      )}
 
       {/* Desktop Icon Section */}
-
-      {!isSmallerThan1280 &&
-        <Flex
-          w="56px"
-          h="56px"
-          bg={isFollowed && !allFollowed ? '#2BCFA11A' : '#141414'}
-          borderRadius="xl"
-          border="1px solid"
-          borderColor={isFollowed && !allFollowed ? '#2BCFA11A' : '#222222'}
-          backdropFilter="blur(10px)"
-          justifyContent="center"
-          alignItems="center"
-          display={{ base: 'none', lg: 'flex' }}
-        >
-          {icon}
-        </Flex>
-      }
+      {!isSmallerThan1280 && renderIcon()}
 
       {/* Content Section */}
-      <VStack align="flex-start" spacing={1} flex="1" w="full" mt={{ base: 4, lg: 0 }}>
-        <Text color="white" fontSize="lg" fontWeight="bold">
-          {title}
-        </Text>
+      <VStack w="full" mt={{ base: 4, lg: 0 }} align="flex-start" spacing={1}>
+        <Text color="white" fontSize="lg" fontWeight="bold">{title}</Text>
         <HStack spacing={2}>
-          <Text color="#b1b1b1" fontSize="lg">
-            {description}
-          </Text>
-          <Box w="4px" h="4px" bg="whiteAlpha.400" borderRadius="full" />
-          <Text color="#b1b1b1" fontSize="lg">
-            {duration}
-          </Text>
+          <Text color="#b1b1b1" fontSize="lg">{description}</Text>
+          <Circle size={1} bg="whiteAlpha.400" />
+          <Text color="#b1b1b1" fontSize="lg">{duration}</Text>
         </HStack>
       </VStack>
 
       {/* Desktop Status Indicator */}
-      <Flex p={3} borderRadius="lg" justifyContent="center" alignItems="center" display={{ base: 'none', lg: 'flex' }}>
-        {renderStatus}
-      </Flex>
+      {renderStatusIndicator({ base: 'none', lg: 'block' })}
     </Flex>
-  );
-};
+  )
+}
 
-export default SocialMediaCard;
+export default SocialMediaCard
