9f590a295a5358333a339f360b1759fcb09d22db
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-01-20 15:00:56 +0330

    Update customcode design and improve interactions

--- src/pages/settings/components/pages/user-management/components/referrals/custom-codes/CustomCodesGenerator.tsx
+++ src/pages/settings/components/pages/user-management/components/referrals/custom-codes/CustomCodesGenerator.tsx
@@ -1,5 +1,6 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import AppIcons from 'assest/icon/Appicons'
+import AppTypography from 'components/common/typography/AppTypography'
 import Button from 'components/redesign/button/Button'
 import Input from 'components/redesign/input/Input'
 import useAppToast from 'functions/hooks/toast/useToast'
@@ -14,7 +15,8 @@ export default function CustomCodesGenerator() {
     const { mutateAsync, isLoading } = useMutation((params: ICustomReferralCode) => updateCustomReferralCodeService(params))
     const { shop, updateState } = useAppStore()
     const { customCode } = shop.referralDetails ?? {}
-    const [value, setValue] = useState(customCode)
+    const [shopInitialCustomCode, setShopInitialCustomCode] = useState(customCode)
+    const [value, setValue] = useState("")
     const { showToast } = useAppToast()
 
     const isValidLength = value?.length >= 8
@@ -26,13 +28,15 @@ export default function CustomCodesGenerator() {
                 key: 'shop',
                 params: { ...shop, referralDetails: { ...shop.referralDetails, customCode: value } }
             });
+            setShopInitialCustomCode(customCode)
+            setValue("")
             showToast({ type: "success", message: "Custom code updated successfully" });
         } catch (error) {
             showToast({ type: "error", message: "Failed to create custom code" });
         }
     }
     const handleCopyLink = () => {
-        const link = `${BUILDER_URL}/?modal=signup&referral=${value.toLowerCase()}`;
+        const link = `${BUILDER_URL}/?modal=signup&referral=${shopInitialCustomCode.toLowerCase()}`;
         navigator.clipboard.writeText(link)
         showToast({ type: "success", message: "Custom code copied successfully" })
     }
@@ -48,7 +52,8 @@ export default function CustomCodesGenerator() {
                 inputProps={{
                     placeholder: "Type a custom code or text without any spaces (minimum 8 characters)",
                     value,
-                    onChange: handleInputChange
+                    onChange: handleInputChange,
+                    isDisabled: shopInitialCustomCode
                 }}
                 inputContainerProps={{ padding: 2, paddingLeft: 4 }}
                 rightElement={
@@ -61,14 +66,24 @@ export default function CustomCodesGenerator() {
                             paddingInline={{ base: 3, lg: 3 }}
                             padding={1}
                             onClick={handleCreateCustomCode}
-                            isDisabled={!isValidLength}
+                            isDisabled={!isValidLength || shopInitialCustomCode}
                         >
-                            {customCode ? "Edit" : "Create"}
+                            Create
                         </Button>
-                        {customCode && <AppIcons.Copy onClick={handleCopyLink} style={{ width: "24px", height: "24px", cursor: "pointer" }} />}
                     </>
                 }
             />
+            {shopInitialCustomCode &&
+                <Flex alignItems={"center"} gap={4}>
+                    <Box px={4} py={3} border={"1px solid #292929"} width={"100%"} borderRadius={"8px"}>
+                        <AppTypography fontSize={16} color={"#fff"}>
+                            {shopInitialCustomCode}
+                        </AppTypography>
+                    </Box>
+                    <AppIcons.Copy onClick={handleCopyLink} style={{ width: "24px", height: "24px", cursor: "pointer" }} />
+                    <AppIcons.RedTrash onClick={() => setShopInitialCustomCode("")} style={{ width: "24px", height: "24px", cursor: "pointer" }} />
+                </Flex>
+            }
         </Flex>
     )
 }
