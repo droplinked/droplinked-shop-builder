1734d48c0682c2e87b80c9d9524c27d67b7709e8
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-01-22 12:47:33 +0330

    Update props structure in CurrencyIcon component

--- src/components/redesign/currency-icon/CurrencyIcon.tsx
+++ src/components/redesign/currency-icon/CurrencyIcon.tsx
@@ -2,9 +2,9 @@ import { Box, BoxProps } from '@chakra-ui/react';
 import AppIcons from 'assest/icon/Appicons';
 import AppTypography from 'components/common/typography/AppTypography';
 import { useCurrencyConverter } from 'functions/hooks/useCurrencyConverter/useCurrencyConverter';
-import React from 'react'
+import React from 'react';
 
-export default function CurrencyIcon({ boxProps }: { boxProps?: BoxProps }) {
+export default function CurrencyIcon({ ...boxProps }: BoxProps) {
     const { symbol, abbreviation } = useCurrencyConverter()
 
     const renderIcon = () => {
@@ -31,7 +31,13 @@ export default function CurrencyIcon({ boxProps }: { boxProps?: BoxProps }) {
     }
 
     return (
-        <Box __css={{ path: { stroke: "#7b7b7b" }, svg: { width: "20px", height: "20px" }, ...boxProps }}>
+        <Box
+            __css={{
+                path: { stroke: "#7b7b7b" },
+                svg: { width: "20px", height: "20px" }
+            }}
+            {...boxProps}
+        >
             {renderIcon()}
         </Box>
     )
