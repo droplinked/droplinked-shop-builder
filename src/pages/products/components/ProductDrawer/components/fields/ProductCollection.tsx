dd1636e7abc5352516c78b50168150e72116472b
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-01-26 17:35:04 +0330

    Set first collection as default in product creation process

--- src/pages/products/components/ProductDrawer/components/fields/ProductCollection.tsx
+++ src/pages/products/components/ProductDrawer/components/fields/ProductCollection.tsx
@@ -1,27 +1,34 @@
 import Select from 'components/redesign/select/Select'
 import useCollections from 'functions/hooks/useCollections/useCollections'
 import useProductForm from 'pages/products/hooks/useProductForm'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProductFieldWrapper from '../common/ProductFieldWrapper'
 
 function ProductCollection() {
-    const { data } = useCollections()
-    const { values, errors, setFieldValue } = useProductForm()
+    const { data: collections } = useCollections()
+    const { values: { productCollectionID }, errors, setFieldValue } = useProductForm()
+
+    // Set default collection if none is selected
+    useEffect(() => {
+        if (!productCollectionID && collections?.data?.length > 0) {
+            setFieldValue('productCollectionID', collections.data[0]._id)
+        }
+    }, [productCollectionID, collections, setFieldValue])
 
     return (
         <ProductFieldWrapper
-            label='Collections'
-            description='Select the collection that will feature this product.'
+            label="Collections"
+            description="Select the collection that will feature this product."
             errorMessage={errors.productCollectionID}
         >
             <Select
-                items={data?.data ?? []}
-                valueAccessor='_id'
-                labelAccessor='title'
+                items={collections?.data ?? []}
+                valueAccessor="_id"
+                labelAccessor="title"
                 selectProps={{
                     placeholder: "Select a collection",
-                    value: values.productCollectionID,
-                    onChange: (e) => setFieldValue("productCollectionID", e.target.value)
+                    value: productCollectionID,
+                    onChange: (e) => setFieldValue('productCollectionID', e.target.value),
                 }}
             />
         </ProductFieldWrapper>
