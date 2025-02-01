e4ad5107ba27f9f6ba44624253130c583fbe05e4
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-02-01 15:15:03 +0330

    Refactor TabsContent component to use AppTab for improved structure and readability

--- src/pages/settings/components/common/TabsContent.tsx
+++ src/pages/settings/components/common/TabsContent.tsx
@@ -1,14 +1,14 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React from "react";
+import { useFormikContext } from "formik";
+import AppTab from "components/redesign/AppTab/AppTab";
 import General from "../pages/general/General";
 import PrivacyAndSecurity from "../pages/privacy-and-security/PrivacyAndSecurity";
 import PaymentAndWallets from "../pages/payment-and-wallets/PaymentAndWallets";
 import CreditAndCoupons from "../pages/credit-and-coupons/CreditAndCoupons";
 import UserManagement from "../pages/user-management/UserManagement";
-import { useFormikContext } from "formik";
 
 function TabsContent() {
-    const { isSubmitting } = useFormikContext()
+    const { isSubmitting } = useFormikContext();
 
     const tabs = [
         {
@@ -33,45 +33,7 @@ function TabsContent() {
         },
     ];
 
-    return (
-        <Tabs
-            variant={"unstyled"} width={"100%"} borderTop={"1px solid #292929"} borderBottom={"none"}
-        >
-            <TabList borderBottom={"1px solid #292929"}>
-                {tabs.map((tab, index) => {
-                    return (
-                        <Tab
-                            isDisabled={isSubmitting}
-                            aria-selected={"false"}
-                            _focusWithin={{
-                                background: "transparent",
-                                borderBottom: "1px solid #fff",
-                            }}
-                            _selected={{
-                                borderBottom: `1px solid #fff`,
-                                color: "#fff",
-                                fontWeight: 500
-                            }}
-                            px={6} py={4}
-                            color={"#7b7b7b"}
-                            fontWeight={400}
-                            fontSize={14}
-                            key={index}
-                        >
-                            {tab.title}
-                        </Tab>
-                    );
-                })}
-            </TabList>
-            <TabPanels>
-                {tabs.map((tab, index) => (
-                    <TabPanel padding={0} key={index}>
-                        {tab.content}
-                    </TabPanel>
-                ))}
-            </TabPanels>
-        </Tabs>
-    );
+    return <AppTab tabs={tabs} isDisabled={isSubmitting} />;
 }
 
 export default TabsContent;
