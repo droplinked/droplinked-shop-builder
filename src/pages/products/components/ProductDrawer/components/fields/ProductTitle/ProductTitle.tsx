4342cfc08c2981b0d50d607a99fbf336ee4fc5bf
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-02-08 13:14:24 +0330

    Add AI generation and improvement functionalities for product title and description

--- src/pages/products/components/ProductDrawer/components/fields/ProductTitle/ProductTitle.tsx
+++ src/pages/products/components/ProductDrawer/components/fields/ProductTitle/ProductTitle.tsx
@@ -1,28 +1,13 @@
 import Input from 'components/redesign/input/Input'
 import useProductForm from 'pages/products/hooks/useProductForm'
-import React, { useState } from 'react'
+import useProductPageStore from 'pages/products/stores/ProductPageStore'
+import React from 'react'
 import ImproveTitle from './ImproveTitle'
-import { useMutation } from 'react-query'
-import { improveTitle } from 'lib/apis/ai/services'
-import { IImproveTitle } from 'lib/apis/ai/interfaces'
-import useAppToast from 'functions/hooks/toast/useToast'
 
 function ProductTitle() {
     const { values: { product_type, title }, errors, setFieldValue } = useProductForm()
-    const [isLoaded, setIsLoaded] = useState(false);
-    const { showToast } = useAppToast()
-    const { mutateAsync, isLoading } = useMutation(
-        (params: IImproveTitle) => improveTitle(params),
-        {
-            onSuccess: (response) => {
-                setFieldValue("title", response.data)
-                setIsLoaded(true)
-            },
-            onError: (error) => {
-                showToast({ message: "Oops! Something went wrong. Please try again.", type: "error" })
-            }
-        }
-    )
+    const { aiGenerationData: { isTitleLoading } } = useProductPageStore()
+
     const label = product_type === "EVENT" ? 'Event Name' : 'Product Name'
 
     return (
@@ -40,19 +25,12 @@ function ProductTitle() {
                 padding: "8px 8px 8px 16px",
             }}
             rightElement={
-                <ImproveTitle
-                    title={title}
-                    onTitleChange={(newTitle) => setFieldValue("title", newTitle)}
-                    isLoaded={isLoaded}
-                    setIsLoaded={(isLoaded) => setIsLoaded(isLoaded)}
-                    isLoading={isLoading}
-                    mutateAsync={mutateAsync}
-                />
+                <ImproveTitle />
             }
             message={errors.title}
             maxCharacters={100}
             {...errors.title && { state: "error" }}
-            {...isLoading && { showAnimatedLoading: true }}
+            {...isTitleLoading && { showAnimatedLoading: true }}
         />
     )
 }
