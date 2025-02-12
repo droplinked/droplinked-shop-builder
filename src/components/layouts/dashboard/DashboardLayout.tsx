029e05d262f59e60f100845fddc48f6c06e092a7
Yasaman Salari <ysmnsalari@gmail.com>
2025-02-12 10:29:38 +0330

    Update DashboardLayout to adjust height and overflow behavior for plans route

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -46,7 +46,7 @@ const DashboardLayout = ({ children }: { children?: ReactNode }) => {
   }, [isMobile, isSidebarOpen]);
 
   return (
-    <VStack width="full" height="100vh" alignItems="flex-start" justifyContent="flex-start" mb={4}>
+    <VStack width="full" height="full" {...(location.endsWith('/plans') && { overflow: 'auto' })}>
       <HStack width="full" height="full" alignItems="flex-start" justifyContent="flex-start">
         {/* Sidebar: Absolute in mobile, static in desktop */}
         {isMobile ? (
@@ -62,7 +62,7 @@ const DashboardLayout = ({ children }: { children?: ReactNode }) => {
         {/* Main content */}
         <VStack flex="1" height="full" width="full">
           <DashboardLayoutHeader isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-          <Box width="100%" minH="80vh" borderColor="line" padding={shouldApplyPadding ? 6 : 0} overflow={location.endsWith('/plans') ? 'auto' : 'hidden'}>
+          <Box width="100%" minH="80vh" borderColor="line" padding={shouldApplyPadding ? 6 : 0}>
             {children || <Outlet />}
           </Box>
         </VStack>
