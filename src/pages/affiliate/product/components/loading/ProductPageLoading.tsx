e8fc6fc818a026078decc2a54467b1a21b016cf8
Yasaman Salari <ysmnsalari@gmail.com>
2025-02-13 18:10:42 +0330

    fix: update Chakra UI import in ProductPageLoading.tsx

--- src/pages/affiliate/product/components/loading/ProductPageLoading.tsx
+++ src/pages/affiliate/product/components/loading/ProductPageLoading.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid, VStack } from "@chakra-ui/layout";
+import { Box, Flex, SimpleGrid, VStack } from "@chakra-ui/react";
 import AppSkeleton from "components/common/skeleton/AppSkeleton";
 import React from "react";
 
