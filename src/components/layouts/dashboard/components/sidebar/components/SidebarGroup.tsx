14b601e69d31c2d9411076d9a074397b8f37ca65
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-02-15 13:39:19 +0330

    refactor(sidebar): improve responsive layout and spacing

--- src/components/layouts/dashboard/components/sidebar/components/SidebarGroup.tsx
+++ src/components/layouts/dashboard/components/sidebar/components/SidebarGroup.tsx
@@ -1,25 +1,34 @@
-import { Box, Divider, Flex } from '@chakra-ui/react';
+import { Box, Divider, Flex, VStack } from '@chakra-ui/react';
 import AppTypography from 'components/common/typography/AppTypography';
 import React from 'react';
 import SidebarItem from './SidebarItem';
 
-const SidebarGroup = ({ group }) => {
+const SidebarGroup = ({ group, index }) => {
   return (
-    <Box width="232px" display="flex" flexDirection="column" gap="8px">
+    <Box
+      width="232px"
+      display={"flex"}
+      flexDirection="column"
+      gap={{ base: "8px", md: "unset", lg: "8px" }}
+    >
       {/* Sidebar Group Title */}
-      <Flex height="16px" paddingLeft="12px" alignItems="center">
-        <AppTypography color="#7B7B7B" fontFamily="Inter" fontSize="10px" fontWeight="400" lineHeight="16px" whiteSpace={'nowrap'} display={{ sm: 'block', md: 'none', lg: 'block' }}>
+      <Flex display={{ sm: 'flex', md: 'none', lg: 'flex' }} paddingLeft="12px" alignItems="center">
+        <AppTypography color="#7B7B7B" fontFamily="Inter" fontSize="10px" fontWeight="400" lineHeight="16px" whiteSpace={'nowrap'}>
           {group.group}
         </AppTypography>
       </Flex>
 
-      <Flex alignItems="center">
-        <Divider borderColor={"#292929"} display={{ sm: 'none', md: 'block', lg: 'none' }}></Divider>
-      </Flex>
+      {index !== 0 &&
+        <Flex display={{ sm: 'none', md: 'flex', lg: 'none' }} mb={6} alignItems="center">
+          <Divider borderColor={"#292929"} display="block" />
+        </Flex>
+      }
 
       {/* Sidebar Items */}
       {group.items?.map((item) => (
-        <SidebarItem key={item.title} item={item} />
+        <VStack key={item.title} gap={{ base: "8px", md: "4px", lg: "8px" }}>
+          <SidebarItem item={item} />
+        </VStack>
       ))}
     </Box>
   );
