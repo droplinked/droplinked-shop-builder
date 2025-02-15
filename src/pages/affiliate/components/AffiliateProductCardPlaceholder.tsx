e5da056237de2349f90c724bc913237d6664c47c
Yasaman Salari <ysmnsalari@gmail.com>
2025-02-15 10:21:00 +0330

    Refactor AffiliateProductCard: Extract subcomponents and add image slider

--- src/pages/affiliate/components/AffiliateProductCardPlaceholder.tsx
+++ src/pages/affiliate/components/AffiliateProductCardPlaceholder.tsx
@@ -3,34 +3,34 @@ import React from 'react';
 
 export default function AffiliateProductCardPlaceholder() {
   return (
-    <Flex
-      borderRadius="lg"
-      flexDir="column"
-      justify="start"
-      align="center"
-      gap={3}
-      cursor="pointer"
-    >
-      {/* Skeleton for Product Image */}
-      <Box position="relative" borderRadius="lg" overflow="hidden" aspectRatio="1" w="72">
-        <Skeleton w="full" h="full" borderRadius="lg" />
+    <Flex borderRadius="lg" flexDir="column" justify="start" align="center" gap={3} cursor="pointer">
+      {/* Product Image Skeleton */}
+      <Box position="relative" borderRadius="lg" overflow="hidden" >
+        <Skeleton height="200px" width="full" borderRadius="lg"/>
+
+        {/* Discount Badge Skeleton */}
+        <Flex px={2} py={1} position="absolute" left={2} top={2} bg="whiteAlpha.700" borderRadius="md" shadow="md" border="1px solid #dddddd" backdropFilter="blur(10px)" align="center" gap={1}>
+          <SkeletonCircle size="3" />
+          <Skeleton width="50px" height="10px" />
+        </Flex>
       </Box>
 
-      {/* Skeleton for Store Info */}
+      {/* Product Info Skeleton */}
       <Flex w="full" flexDir="column" gap={2}>
-        <Flex w="72" align="center" gap={2}>
+        {/* Store Info Skeleton */}
+        <Flex align="center" gap={2}>
           <SkeletonCircle size="5" />
           <Flex flex={1} align="center" gap={2}>
-            <Skeleton h="10px" w="60px" />
+            <Skeleton width="60px" height="10px" />
           </Flex>
         </Flex>
 
-        {/* Skeleton for Product Details */}
+        {/* Product Details Skeleton */}
         <Flex flexDir="column" gap={1}>
-          <Skeleton h="15px" w="full" />
+          <Skeleton width="100%" height="20px" />
           <Flex w="72" align="center" gap={1}>
-            <Skeleton h="20px" w="80px" />
-            <Skeleton h="20px" w="20px" />
+            <Skeleton width="60px" height="20px" />
+            <Skeleton width="40px" height="20px" />
           </Flex>
         </Flex>
       </Flex>
