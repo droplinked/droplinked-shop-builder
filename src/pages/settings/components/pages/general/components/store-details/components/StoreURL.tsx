f7a99b21255ec68052c181a517283c299acd1ad7
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-02-16 16:30:06 +0330

    refactor: replace direct clipboard copy logic with ClipboardText component for improved reusability and consistency

--- src/pages/settings/components/pages/general/components/store-details/components/StoreURL.tsx
+++ src/pages/settings/components/pages/general/components/store-details/components/StoreURL.tsx
@@ -1,51 +1,29 @@
 import { Flex } from '@chakra-ui/react'
-import AppIcons from 'assest/icon/Appicons'
+import ClipboardText from 'components/common/clipboardText/ClipboardText'
 import AppTypography from 'components/common/typography/AppTypography'
-import useAppToast from 'functions/hooks/toast/useToast'
 import useAppStore from 'lib/stores/app/appStore'
 import { SHOP_URL } from 'lib/utils/app/variable'
 import SectionContent from 'pages/settings/components/common/SectionContent'
 import React from 'react'
 import { Link } from 'react-router-dom'
-// TODO: Import ClipboardText component
-// import ClipboardText from 'path/to/ClipboardText' 
 
 export default function StoreURL() {
-    // TODO: First, call all hooks (useAppStore, useAppToast, etc.)
-    const { shop } = useAppStore() // Call custom hook for app store
-    const { showToast } = useAppToast() // Call custom hook for toast notifications
+    const { shop } = useAppStore()
 
-    // TODO: Add a space after calling hooks
+    const { shopDomain, name } = shop
+    const link = shopDomain ? `https://${shopDomain}` : `${SHOP_URL}/${name}`
 
-    // TODO: Then define component vars and functions
-    const { shopDomain, name } = shop // Extract necessary data from the store
-    const link = shopDomain ? `https://${shopDomain}` : `${SHOP_URL}/${name}` // Generate the store URL dynamically
+    const shopLink = () => (
+        <Flex alignItems={"center"} gap={"6px"} sx={{ path: { stroke: "#179ef8" } }}>
+            <Link to={link} target='_blank'>
+                <AppTypography color={"#179EF8"} fontSize={16}>
+                    {shopDomain ? `https://${shopDomain}` : `${SHOP_URL}/[${name}]`}
+                </AppTypography>
+            </Link>
+            <ClipboardText text={link} />
+        </Flex>
+    )
 
-    // TODO: Check if ClipboardText can be used instead of `navigator.clipboard.writeText`
-    // TODO: If ClipboardText can be used, replace the `handleCopyLink` logic with ClipboardText component.
-    // ClipboardText could provide cleaner functionality for copying to clipboard with built-in support for the feedback to the user.
-
-    const handleCopyLink = () => {
-        navigator.clipboard.writeText(link) // Copy the link to clipboard
-        showToast({ type: "success", message: "Store URL copied successfully" }) // Show success toast notification
-    }
-
-    const shopLink = () => {
-        return (
-            <Flex alignItems={"center"} gap={"6px"}>
-                <Link to={link} target='_blank'>
-                    <AppTypography color={"#179EF8"} fontSize={16}>
-                        {shop?.shopDomain ? `https://${shop?.shopDomain}` : `${SHOP_URL}/[${shop.name}]`}
-                    </AppTypography>
-                </Link>
-                {/* TODO: If ClipboardText is used, replace AppIcons.Copy with ClipboardText */}
-                {/* Example usage: <ClipboardText text={link} /> */}
-                <AppIcons.Copy onClick={handleCopyLink} style={{ cursor: "pointer" }} />
-            </Flex>
-        )
-    }
-
-    // TODO: Return JSX after defining functions and variables
     return (
         <SectionContent title="Store URL" rightContent={shopLink()} />
     )
--- src/pages/settings/components/pages/payment-and-wallets/components/merchant-wallet/components/WalletInput.tsx
+++ src/pages/settings/components/pages/payment-and-wallets/components/merchant-wallet/components/WalletInput.tsx
@@ -1,28 +1,22 @@
 import { Button } from '@chakra-ui/react'
-import AppIcons from 'assest/icon/Appicons'
+import ClipboardText from 'components/common/clipboardText/ClipboardText'
 import Input from 'components/redesign/input/Input'
-import useAppToast from 'functions/hooks/toast/useToast'
 import useAppStore from 'lib/stores/app/appStore'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function WalletInput() {
     const { shop } = useAppStore()
-    const { showToast } = useAppToast()
     const { circleWallets } = shop;
     const circleWalletAddress = circleWallets?.find(cw => cw?.chain === "ETH")?.address || ""
     const navigate = useNavigate();
-    const handleCopyAddress = () => {
-        navigator.clipboard.writeText(circleWalletAddress);
-        showToast({ message: "Address copied to clipboard", type: "success" })
-    }
 
     return (
         circleWallets?.length ?
             <Input
                 inputProps={{ value: circleWalletAddress, isReadOnly: true }}
                 rightElement={
-                    <AppIcons.Copy style={{ cursor: "pointer" }} onClick={handleCopyAddress} />
+                    <ClipboardText text={circleWalletAddress} />
                 }
             />
             :
