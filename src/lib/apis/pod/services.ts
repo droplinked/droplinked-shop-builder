1ece056b0917715adfff8e8600b2376b14c85ea0
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-02-22 14:50:50 +0330

    refactor: Remove unused interfaces and services from pod API

--- src/lib/apis/pod/services.ts
+++ src/lib/apis/pod/services.ts
@@ -1,33 +1,13 @@
 import { createQueryString } from "../_utils/with.query";
 import axiosInstance from "../axiosConfig";
-import { ImockupGeneratorService, IpodAvailableVariantsService, IpodCategoryProductService, IpodCategoryService, IpodGenerateMockupService, IpodPrintPositionsService, IpodProductService, IpodVariantsService, IproviderIDService, PODCategory } from "./interfaces";
-
-export const providersService = () => {
-    return axiosInstance.get("pod/providers")
-};
-
-export const providerIDService = ({ prodviderID }: IproviderIDService) => {
-    return axiosInstance.get(`pod/provider/${prodviderID}/products`)
-};
+import { ImockupGeneratorService, IpodAvailableVariantsService, IpodCategoryProductService, IpodCategoryService, IpodProductService, PODCategory } from "./interfaces";
 
 export const podProductService = ({ pod_blank_product_id }: IpodProductService) => {
     return axiosInstance.get(`pod/product/${pod_blank_product_id}`)
 };
 
-export const podVariantsService = ({ provider, productId }: IpodVariantsService) => {
-    return axiosInstance.get(`pod/variant-options/${provider}/${productId}`)
-};
-
 export const podAvailableVariantsService = ({ provider, productId, templateID }: IpodAvailableVariantsService) => {
     return axiosInstance.get(`pod/available-variants/${provider}/${productId}${templateID ? `/${templateID}` : ''}`)
-};
-
-export const podPrintPositionsService = ({ provider, productId }: IpodPrintPositionsService) => {
-    return axiosInstance.get(`pod/print-positions/${provider}/${productId}`)
-};
-
-export const podGenerateMockupService = ({ productId, params }: IpodGenerateMockupService) => {
-    return axiosInstance.post(`pod/generate-mockup/${productId}`, params)
 };
 
 export const mockupGeneratorService = ({ params, productID }: ImockupGeneratorService) => {
@@ -45,8 +25,4 @@ export const podCategoryProductService = ({ subCategoryId }: IpodCategoryProduct
 
 export const generateThumbService = (images: Array<string>) => {
     return axiosInstance.post(`product/upload/external/images`, images)
-};
-
-export const getPodProductService = (productID: string) => {
-    return axiosInstance.get(`pod/printful/techniques/product/${productID}`)
 };
