c49b26c7233204faf0210aa589627ddf34389264
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-03-03 13:34:44 +0330

    Add new SVG React components for brand identity assets

---
+++ src/assets/brand-identity/Drop1.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { SVGProps } from "react";
+
+const Drop1 = (props: SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 30 30" {...props}>
+        <path d="M14.5828 0.417969C6.50315 0.417969 0 6.92113 0 15.0008C0 23.0805 6.50315 29.5837 14.5828 29.5837H29.1657V15.1979C29.1657 7.11821 22.6625 0.417969 14.5828 0.417969Z" fill="currentColor" />
+    </svg>
+);
+
+export default Drop1;
