5baab59100cc2a6575b9ea702e63d77a71dc1af5
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-03-09 13:55:13 +0330

    fix(onboarding): update condition to render CompletionSection at step 6

--- src/pages/onboarding/Onboarding.tsx
+++ src/pages/onboarding/Onboarding.tsx
@@ -7,7 +7,7 @@ import useOnboardingStore from './store/useOnboardingStore'
 function Onboarding() {
     const { currentStep } = useOnboardingStore()
 
-    if (currentStep === 0) return <CompletionSection />
+    if (currentStep === 6) return <CompletionSection />
 
     return (
         <OnboardingStepWrapper currentStep={currentStep}>
