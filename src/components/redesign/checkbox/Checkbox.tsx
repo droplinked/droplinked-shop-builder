ec89bb4ca9692798061eda419ba3bb3ebb5e3e79
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-12 11:54:39 +0330

    Add custom background and check icon colors for checked state in Checkbox

--- src/components/redesign/checkbox/Checkbox.tsx
+++ src/components/redesign/checkbox/Checkbox.tsx
@@ -1,21 +1,25 @@
 import { Checkbox as ChakraCheckbox, CheckboxProps } from "@chakra-ui/react"
 import React from "react"
 
-const checkboxStyles = {
-    ".chakra-checkbox__label": { fontSize: 14 },
-    ".chakra-checkbox__control": {
-        width: 20,
-        height: 20,
-        border: "1px solid #3C3C3C",
-        borderRadius: 4
-    }
-}
-
 function Checkbox({ children, ...rest }: CheckboxProps) {
     return (
         <ChakraCheckbox
             color="#FFF"
-            css={checkboxStyles}
+            css={{
+                ".chakra-checkbox__label": { fontSize: 14 },
+                ".chakra-checkbox__control": {
+                    width: 20,
+                    height: 20,
+                    border: "1px solid #3C3C3C",
+                    borderRadius: 4,
+                    backgroundColor: "transparent",
+                    "&[data-checked]": {
+                        backgroundColor: "#2BCFA1",
+                        borderColor: "#2BCFA1",
+                        "& svg": { stroke: "#141414 !important" }
+                    }
+                }
+            }}
             {...rest}
         >
             {children}
