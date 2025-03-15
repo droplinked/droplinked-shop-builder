133ef4ff472296c2d46ce79431a692db58411821
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-15 14:19:23 +0330

    Clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import AppRoutes from "routes/routes";
-
-// Styles
-import "assets/style/App.scss";
-
-// Components
-import AppToastify from "components/common/toastify/AppToastify";
-import AppGDPR from "components/common/app-gdpr/AppGDPR";
+import "assets/style/App.scss"
+import AppGDPR from "components/common/app-gdpr/AppGDPR"
+import AppToastify from "components/common/toastify/AppToastify"
+import React from "react"
+import AppRoutes from "routes/routes"
 
 function App() {
   return (
@@ -15,7 +11,7 @@ function App() {
       <AppRoutes />
       <AppGDPR />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
