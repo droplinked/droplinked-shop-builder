b0189c9d8d22cdadcd2f73d2a2205f54b8b5a8a3
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-03-18 17:04:27 +0330

    refactor(onboarding): clean up imports and remove unused navigation logic in ShopSetupForm

--- src/pages/onboarding/components/shop-setup/ShopSetupForm.tsx
+++ src/pages/onboarding/components/shop-setup/ShopSetupForm.tsx
@@ -1,33 +1,28 @@
 import { Flex, useMediaQuery } from '@chakra-ui/react'
+import useAppToast from 'hooks/toast/useToast'
+import { setupShop } from 'lib/apis/shop/shopServices'
+import useAppStore from 'lib/stores/app/appStore'
 import useOnboardingStore, { initialStoreSetup } from 'pages/onboarding/stores/useOnboardingStore'
 import { OnboardingStepProps } from 'pages/onboarding/types/onboarding'
 import React from 'react'
+import { useMutation } from 'react-query'
+import { validateStoreData } from '../../utils/shopSetupFormValidation'
 import ControlButtons from '../common/ControlButtons'
+import OnboardingStepHeader from '../common/OnboardingStepHeader'
+import ShopPreview from '../shop-preview/ShopPreview'
 import AiAssistant from './AiAssistant/desktop/AiAssistant'
+import AiAssistantButton from './AiAssistant/mobile/AiAssistantButton'
 import CoverImage from './CoverImage'
 import DescriptionField from './DescriptionField'
 import LogoUploader from './LogoUploader'
 import NameField from './NameField'
 import UrlChooser from './UrlChooser'
-import { validateStoreData } from '../../utils/shopSetupFormValidation'
-import OnboardingStepHeader from '../common/OnboardingStepHeader'
-import AiAssistantButton from './AiAssistant/mobile/AiAssistantButton'
-import ShopPreview from '../shop-preview/ShopPreview'
-import useAppToast from 'hooks/toast/useToast'
-import { useMutation } from 'react-query'
-import { setupShop } from 'lib/apis/shop/shopServices'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import useAppStore from 'lib/stores/app/appStore'
 
 function ShopSetupForm({ onNext }: OnboardingStepProps) {
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams()
     const { reset, updateState } = useAppStore()
     const { updateOnboardingState, storeSetup, setError } = useOnboardingStore()
     const { showToast } = useAppToast()
     const [isSmallerThan1024] = useMediaQuery("(max-width: 1024px)")
-
-    const origin = searchParams?.get('origin')
 
     const { mutateAsync: setupShopMutation, isLoading } = useMutation({
         mutationFn: () => setupShop(storeSetup),
@@ -48,8 +43,6 @@ function ShopSetupForm({ onNext }: OnboardingStepProps) {
     }
 
     const handleBack = () => {
-        if (origin) return navigate(`/${origin}`)
-
         reset()
         updateOnboardingState("storeSetup", initialStoreSetup)
         updateOnboardingState("currentStep", "SIGN_IN")
@@ -66,7 +59,7 @@ function ShopSetupForm({ onNext }: OnboardingStepProps) {
             <UrlChooser />
             <NameField />
             <DescriptionField />
-            <ControlButtons onBack={handleBack} onSubmit={handleSubmit} isLoading={isLoading} backText={origin ? "Back" : "Exit"} />
+            <ControlButtons onBack={handleBack} onSubmit={handleSubmit} isLoading={isLoading} />
             {!isSmallerThan1024 && <AiAssistant />}
             {isSmallerThan1024 && <ShopPreview />}
         </>
