1fc5c86992287b058a7f4a59e4940846b2f2cdc2
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-18 17:03:46 +0330

    refactor(onboarding): streamline onboarding logic by removing unused dependencies and enhancing state management

--- src/pages/onboarding/hooks/useLogin.ts
+++ src/pages/onboarding/hooks/useLogin.ts
@@ -2,38 +2,46 @@ import useAppToast from "hooks/toast/useToast"
 import { useCustomNavigate } from "hooks/useCustomeNavigate/useCustomNavigate"
 import useAppStore from "lib/stores/app/appStore"
 import { useCallback } from "react"
-import { useNavigate } from "react-router-dom"
-import { navigateUserBasedOnStatus } from "utils/helpers"
+import useOnboardingStore from "../stores/useOnboardingStore"
 
 export function useLogin() {
-    const navigate = useNavigate()
     const { login: authenticateUser, loading } = useAppStore()
     const { shopNavigate } = useCustomNavigate()
     const { showToast } = useAppToast()
+    const { updateOnboardingState } = useOnboardingStore()
 
     const finalizeLogin = useCallback(async (data: any) => {
         try {
             const { user } = data
             const status = user.status
 
-            if (status === "DELETED") {
+            if (status === "DELETED")
                 return showToast({ message: "This account has been deleted", type: "error" })
-            }
 
-            if (user.type !== "SHOPBUILDER") {
+            if (user.type !== "SHOPBUILDER")
                 return showToast({
                     message: "This account is unable to log in. Please check your credentials.",
                     type: "error"
                 })
-            }
 
-            const { href, dashboard } = navigateUserBasedOnStatus(status, data)
-            dashboard ? shopNavigate(href) : navigate(href)
+            switch (status) {
+                case "NEW":
+                    return updateOnboardingState("currentStep", "EMAIL_CONFIRMATION")
+
+                case "VERIFIED":
+                case "PROFILE_COMPLETED":
+                    return updateOnboardingState("currentStep", "STORE_DETAILS")
+
+                case "SHOP_INFO_COMPLETED":
+                case "IMS_TYPE_COMPLETED":
+                case "ACTIVE":
+                    return shopNavigate("dashboard")
+            }
         }
         catch (error) {
             showToast({ message: error?.message || "An error occurred", type: "error" })
         }
-    }, [showToast, shopNavigate, navigate])
+    }, [showToast, updateOnboardingState, shopNavigate])
 
     const onLoginSubmit = useCallback(async (data: any) => {
         try {
