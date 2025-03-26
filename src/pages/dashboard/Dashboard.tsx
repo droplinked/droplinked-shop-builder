380ad60e37b94011f5d9b7259e5869f870333a64
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-26 15:47:56 +0330

    Adjust gaps in Dashboard component for improved spacing

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -30,7 +30,7 @@ function Dashboard() {
     }
 
     return (
-        <Flex direction="column" gap={{ base: 6, md: 9, lg: 12 }}>
+        <Flex direction="column" gap={{ base: 6, lg: 9, xl: 12 }}>
             <GreetingBanner />
             {renderContent()}
         </Flex>
