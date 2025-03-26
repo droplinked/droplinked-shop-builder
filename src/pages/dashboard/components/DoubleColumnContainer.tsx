874e3f5e486876b9781ba9e6b2cd8c75fb400c19
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-26 14:57:07 +0330

    Update Dashboard page - SalesAndAffiliates section: Replace old AppIcons with new icons, swap hardcoded colors for theme colors, and adjust responsiveness to match Figma design

--- src/pages/dashboard/components/DoubleColumnContainer.tsx
+++ src/pages/dashboard/components/DoubleColumnContainer.tsx
@@ -1,18 +1,16 @@
 import { Flex, FlexProps } from "@chakra-ui/react"
 import React, { Children } from "react"
 
-interface Props extends FlexProps { }
-
 // Used to render and handle the responsiveness of a two-column layout in the Dashboard page.
-function DoubleColumnContainer({ children, ...rest }: Props) {
+function DoubleColumnContainer({ children, ...rest }: FlexProps) {
     return (
         <Flex
-            direction={{ base: "column", md: "row" }}
-            gap={{ base: 4, xl: 6 }}
+            direction={{ base: "column", lg: "row" }}
+            gap={{ base: 4, md: 6, lg: 4, "2xl": 6 }}
             {...rest}
         >
             {Children.map(children, (child) => (
-                <Flex flex={{ base: "1", md: "0.5" }}>
+                <Flex flex={{ base: 1, lg: 0.5 }}>
                     {child}
                 </Flex>
             ))}
