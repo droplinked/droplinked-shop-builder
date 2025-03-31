545385cca40f8d7af3d6aa6bc1430789f838cf62
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-03-31 20:26:57 +0330

    Clean up ProductEngagementDashboard component in the Analytics page

--- src/pages/analytics/components/ProductEngagementDashboard/MostImportedProductsList.tsx
+++ src/pages/analytics/components/ProductEngagementDashboard/MostImportedProductsList.tsx
@@ -6,7 +6,7 @@ function MostImportedProductsList() {
         <EmptyState
             image='https://upload-file-droplinked.s3.amazonaws.com/1fe7643467d9377029d79c30e6c055bdea622cde3e746af5315b9b89a3c979b4.png'
             title='Grow with droplinked!'
-            description='Click on the tasks and done them to increase grow your shop and increase your sell.'
+            description='Click on the tasks, complete them to grow your shop, and increase your sales.'
         />
     )
 }
