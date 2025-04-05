8ce99e285475922bb995076a70356170b485244c
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-04-05 15:04:54 +0330

    Add Pause and Play icons as React components

---
+++ src/assets/icons/System/Pause/PauseSm.tsx
@@ -0,0 +1,8 @@
+// Generated from SVG to React Component
+import React, { SVGProps } from "react";
+
+export const PauseSm = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M5.3335 3.33333V12.6667M10.6668 3.33333V12.6667" stroke={color} strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
---
+++ src/assets/icons/System/Play2/Play2Lg.tsx
@@ -0,0 +1,8 @@
+// Generated from SVG to React Component
+import React, { SVGProps } from "react";
+
+export const Play2Lg = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M16.0417 9.21096C17.6632 10.123 18.4739 10.5791 18.744 11.1783C18.9794 11.7008 18.9794 12.2992 18.744 12.8217C18.4739 13.4209 17.6632 13.877 16.0417 14.789L9.76884 18.3175C8.2012 19.1993 7.41738 19.6402 6.77534 19.5677C6.21522 19.5043 5.70782 19.2076 5.37802 18.7504C5 18.2264 5 17.3271 5 15.5285V8.47152C5 6.67289 5 5.77358 5.37802 5.24958C5.70782 4.79243 6.21522 4.49568 6.77534 4.43236C7.41738 4.35978 8.2012 4.80068 9.76884 5.68247L16.0417 9.21096Z" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
