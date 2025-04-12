7679bda20e31127ee77c434861506b845c017897
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-04-12 11:37:01 +0330

    Add Jest testing framework and setup for regex utilities

---
+++ config/jest/fileTransform.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+// This is a custom Jest transformer turning file imports into filenames.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process(src, filename) {
+    const assetFilename = JSON.stringify(path.basename(filename));
+
+    if (filename.match(/\.svg$/)) {
+      // Convert SVG filename to Pascal case without camelcase dependency
+      const pascalCaseFilename = path
+        .parse(filename)
+        .name.split(/[-_\s./]+/)
+        .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+        .join("");
+
+      const componentName = `Svg${pascalCaseFilename}`;
+      return {
+        code: `const React = require('react');
+        module.exports = {
+          __esModule: true,
+          default: ${assetFilename},
+          ReactComponent: React.forwardRef(function ${componentName}(props, ref) {
+            return {
+              $$typeof: Symbol.for('react.element'),
+              type: 'svg',
+              ref: ref,
+              key: null,
+              props: Object.assign({}, props, {
+                children: ${assetFilename}
+              })
+            };
+          }),
+        };`,
+      };
+    }
+
+    return {
+      code: `module.exports = ${assetFilename};`,
+    };
+  },
+};
