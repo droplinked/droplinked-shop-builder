83f6dd5b851bbf29d7be859b66aed18228e7e3d8
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-04-13 12:53:10 +0330

    Add new SVG icons for Bug, Hammer, Pluster, and Wrench in various sizes

---
+++ src/assets/icons/Sign/Bug/BugMd.tsx
@@ -0,0 +1,23 @@
+// Generated from SVG to React Component
+import React, { SVGProps } from "react";
+
+export const BugMd = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <g clip-path="url(#clip0_12312_240)">
+            <path d="M7.5 7.50008V6.66675C7.5 6.00371 7.76339 5.36782 8.23223 4.89898C8.70107 4.43014 9.33696 4.16675 10 4.16675C10.663 4.16675 11.2989 4.43014 11.7678 4.89898C12.2366 5.36782 12.5 6.00371 12.5 6.66675V7.50008" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M6.66683 7.5H13.3335C13.8298 8.24382 14.1176 9.10714 14.1668 10V12.5C14.1668 13.6051 13.7278 14.6649 12.9464 15.4463C12.165 16.2277 11.1052 16.6667 10.0002 16.6667C8.89509 16.6667 7.83529 16.2277 7.05388 15.4463C6.27248 14.6649 5.8335 13.6051 5.8335 12.5V10C5.88272 9.10714 6.1705 8.24382 6.66683 7.5Z" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M2.5 10.8333H5.83333" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M14.1665 10.8333H17.4998" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M10 16.6667V11.6667" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M3.3335 15.8334L6.12516 14.1667" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M16.6667 15.8334L13.875 14.1667" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M3.3335 5.83325L6.4585 7.83325" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M16.6665 5.83325L13.5415 7.83325" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        </g>
+        <defs>
+            <clipPath id="clip0_12312_240">
+                <rect width="20" height="20" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+);
---
+++ src/assets/icons/Sign/Pluster/PlusterLg.tsx
@@ -0,0 +1,11 @@
+// Generated from SVG to React Component
+import React, { SVGProps } from "react";
+
+export const PlusterLg = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M15.9111 2.43269L2.43361 15.9101C1.85758 16.4862 1.85758 17.4201 2.43361 17.9961L6.00803 21.5705C6.58406 22.1466 7.51797 22.1466 8.094 21.5705L21.5715 8.09308C22.1475 7.51705 22.1475 6.58314 21.5715 6.00711L17.997 2.43269C17.421 1.85667 16.4871 1.85666 15.9111 2.43269Z" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M6.00803 2.42947L2.43361 6.00389C1.85758 6.57992 1.85758 7.51383 2.43361 8.08986L15.9111 21.5673C16.4871 22.1433 17.421 22.1433 17.997 21.5673L21.5715 17.9929C22.1475 17.4169 22.1475 16.4829 21.5715 15.9069L8.094 2.42947C7.51797 1.85345 6.58406 1.85344 6.00803 2.42947Z" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M14.5 12L12 14.5" stroke={color} strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M12 9.5L9.5 12" stroke={color} strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
