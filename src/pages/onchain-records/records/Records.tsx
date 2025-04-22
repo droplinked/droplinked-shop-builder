eded9d1b8262d3f0ec8d715b5cc98b4df4b55120
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-04-22 15:45:14 +0330

    Enhance Records component: include search and filter states in useOnchainRecords hook and conditionally render Filters based on NFT presence, fetching status, and active filters.

--- src/pages/onchain-records/records/Records.tsx
+++ src/pages/onchain-records/records/Records.tsx
@@ -7,13 +7,14 @@ import { useOnchainRecords } from '../context/OnchainRecordsContext';
 import RecordsSkeleton from '../components/RecordsSkeleton';
 
 export default function Records() {
-    const { isFetching, data, isError } = useOnchainRecords();
+    const { isFetching, data, isError, searchValue, walletFilter, recordFilter } = useOnchainRecords();
     const { droplinkedNFTs, walletNFTs } = data?.data ?? {};
     const hasNFT = (droplinkedNFTs?.length !== 0 || walletNFTs?.length !== 0);
+    const hasFilters = (searchValue || walletFilter?.length || recordFilter?.length);
 
     return (
         <Flex flexDirection={"column"} gap={{ base: 4, md: 6 }} width={"100%"}>
-            <Filters />
+            {(hasNFT || isFetching || hasFilters) && <Filters />}
             {(isFetching) && <RecordsSkeleton />}
             {(hasNFT && !isFetching && !isError) && <RecordsList droplinkedNFTs={droplinkedNFTs} walletNFTs={walletNFTs} />}
             {(!hasNFT && !isFetching) && <EmptyView />}
