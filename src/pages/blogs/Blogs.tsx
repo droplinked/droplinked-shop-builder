f89e2a5840e35cfda3d16e03a0aecffa262b4148
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-04-22 12:51:14 +0330

    Pass props with correct names to PageGrid.Header in Blogs Page

--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -1,5 +1,4 @@
 import { PlusMd } from 'assets/icons/Sign/Plus/PlusMd'
-import ButtonGrid from 'components/redesign/button-grid/ButtonGrid'
 import PageGrid from 'components/redesign/page-grid/PageGrid'
 import useDebounce from 'hooks/debounce/useDebounce'
 import React, { useState } from 'react'
@@ -14,16 +13,13 @@ function Blogs() {
             <PageGrid.Header
                 title="Blog Posts"
                 description="Here you can create, edit and manage posts related to your blog."
-                rightContent={
-                    <ButtonGrid buttons={[
-                        {
-                            caption: "New Post",
-                            leftIcon: <PlusMd color="#000" />,
-                            onClick: () => console.log("New Collection"),
-                        }
-                    ]}
-                    />
-                }
+                actionButtons={[
+                    {
+                        title: "New Post",
+                        leftIcon: <PlusMd color="#000" />,
+                        onClick: () => console.log("New Collection"),
+                    }
+                ]}
             />
             <PageGrid.Actions
                 search={{
