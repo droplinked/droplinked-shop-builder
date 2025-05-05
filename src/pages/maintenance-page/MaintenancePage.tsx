050dbeff620af894a0dd5afa668dfbafdd4b72ec
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-05-05 09:57:37 +0330

    Enhance MaintenancePage by adding objectFit property to AppImage for better image display

--- src/pages/maintenance-page/MaintenancePage.tsx
+++ src/pages/maintenance-page/MaintenancePage.tsx
@@ -25,7 +25,14 @@ function MaintenancePage() {
             gap={9}
             my={!isDashboardRoute ? "4rem" : undefined}
         >
-            <AppImage width="500px" height="500px" src="https://upload-file-droplinked.s3.amazonaws.com/73e41aa957001b34d9101d8dce854372d9660273f767466466f11bf5c1c7cbf9.png" alt="Maintenance Illustration" />
+            <AppImage
+                width="500px"
+                height="500px"
+                src="https://upload-file-droplinked.s3.amazonaws.com/73e41aa957001b34d9101d8dce854372d9660273f767466466f11bf5c1c7cbf9.png"
+                alt="Maintenance Illustration"
+                objectFit="contain"
+            />
+
             <Flex maxWidth="600px" direction="column" gap={4} textAlign="center">
                 <AppTypography fontSize={20} fontWeight={700} color="#fff">
                     We’re Temporarily Unavailable
@@ -34,6 +41,7 @@ function MaintenancePage() {
                     Our system is currently undergoing maintenance to improve your experience. We’re working hard to bring everything back online as quickly as possible.
                 </AppTypography>
             </Flex>
+
             <Button
                 variant="secondary"
                 border="none"
