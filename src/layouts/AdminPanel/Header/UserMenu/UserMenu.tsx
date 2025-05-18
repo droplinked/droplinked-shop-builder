3a78f9471941d9975ecee4dcf0a97c9bfad4a97e
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-05-19 00:53:12 +0330

    Convert Figma designs of user menu components into reusable React components

--- src/layouts/AdminPanel/Header/UserMenu/UserMenu.tsx
+++ src/layouts/AdminPanel/Header/UserMenu/UserMenu.tsx
@@ -1,16 +1,14 @@
-import { Menu, MenuButton, MenuItem, MenuList, useDisclosure } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuList, useDisclosure } from '@chakra-ui/react'
 import { UserMd } from 'assets/icons/System/User/UserMd'
 import IconWrapper from 'components/redesign/icon-wrapper/IconWrapper'
 import RuledGrid from 'components/redesign/ruled-grid/RuledGrid'
 import React from 'react'
+import MenuItemActions from './MenuItemActions'
+import MenuItemAppVersion from './MenuItemAppVersion'
+import MenuItemShopInfo from './MenuItemShopInfo'
 
 export const UserMenu = () => {
     const disclosure = useDisclosure()
-
-    const menuItems = [
-        { label: 'Profile', action: () => console.log('Profile clicked') },
-        { label: 'Logout', action: () => console.log('Logout clicked') },
-    ]
 
     return (
         <Menu variant="unstyled" {...disclosure}>
@@ -23,14 +21,14 @@ export const UserMenu = () => {
                 border="none"
                 borderRadius={16}
                 padding={0}
-                backgroundColor="neutral.gray.900"
                 overflow="hidden"
+                bgColor="neutral.gray.900"
                 sx={{ ".menuItem": { padding: 4 } }}
             >
                 <RuledGrid columns={1} borderColor="neutral.gray.750" nested>
-                    {menuItems.map((item) => (
-                        <MenuItem key={item.label} onClick={item.action}>{item.label}</MenuItem>
-                    ))}
+                    <MenuItemShopInfo />
+                    <MenuItemActions isMenuOpen={disclosure.isOpen} />
+                    <MenuItemAppVersion />
                 </RuledGrid>
             </MenuList>
         </Menu>
