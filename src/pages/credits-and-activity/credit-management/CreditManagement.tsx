b8bd487e6650b80985bd669dfe82956f21626783
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-05-20 11:42:45 +0330

    refactor(credits-and-activity): remove unused FlexContainer and MobileContainer components, update CreditManagement layout with RuledGrid

--- src/pages/credits-and-activity/credit-management/CreditManagement.tsx
+++ src/pages/credits-and-activity/credit-management/CreditManagement.tsx
@@ -1,35 +1,30 @@
-import { Flex } from '@chakra-ui/react'
-import FlexContainer from 'pages/credits-and-activity/components/flex-container/FlexContainer'
+import { Flex, useMediaQuery } from '@chakra-ui/react'
 import TransactionsTable from 'pages/credits-and-activity/components/transaction-table/TransactionsTable'
 import React from 'react'
 import OverallTransactionsDisplay from '../components/OverallTransactionsDisplay'
 import AccountBalance from './account-balance/AccountBalance'
 import useCreditsData from 'hooks/credits-and-activity/useCreditsData'
+import RuledGrid from 'components/redesign/ruled-grid/RuledGrid'
 
 export default function CreditManagement() {
+    const [isSmallerThan768] = useMediaQuery("(max-width: 768px)")
     const { transactionsQuery } = useCreditsData()
-    const { data } = transactionsQuery;
-    const isEmpty = data?.pages?.[0]?.data?.data?.totalDocuments === 0;
+    const { data } = transactionsQuery
+    const isEmpty = data?.pages?.[0]?.data?.data?.totalDocuments === 0
 
     return (
-        <Flex flexDirection="column" gap={6}>
-            <FlexContainer
-                items={[
-                    {
-                        content: <AccountBalance />,
-                        isFullWidth: true
-                    },
-                    {
-                        content: <OverallTransactionsDisplay type="inbound" />,
-                        isFullWidth: false
-                    },
-                    {
-                        content: <OverallTransactionsDisplay type="outbound" />,
-                        isFullWidth: false
-                    },
-                ]}
-            />
+        <>
+            <RuledGrid columns={1} borderRadius="8px" overflow="hidden" mb={6}>
+                <AccountBalance />
+
+                <RuledGrid columns={isSmallerThan768 ? 1 : 2} nested overflow="hidden">
+                    <OverallTransactionsDisplay type="inbound" />
+                    <OverallTransactionsDisplay type="outbound" />
+                </RuledGrid>
+
+            </RuledGrid>
+
             {!isEmpty && <TransactionsTable />}
-        </Flex>
+        </>
     )
 }
