85b13e6dd0b424273e1628b71a606922e6aa1cb0
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-05-21 09:49:52 +0330

    Refactor DesktopSidebar and TabletSidebar components to enhance layout consistency and organization. Added sticky positioning and improved JSX structure by wrapping elements in Box components for better padding and overflow management.

--- src/layouts/ProducerLayout/Sidebar/DesktopSidebar.tsx
+++ src/layouts/ProducerLayout/Sidebar/DesktopSidebar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import DroplinkedBrand from 'pages/onboarding/components/common/DroplinkedBrand'
 import React from 'react'
 import NavLinks from './NavLinks'
@@ -7,15 +7,27 @@ function DesktopSidebar() {
     return (
         <Flex
             as="aside"
+            position="sticky"
+            top={0}
+            height="100vh"
             direction="column"
-            padding="28px 16px"
             gap={6}
         >
-            <DroplinkedBrand
-                dropProps={{ width: "32px", height: "32px" }}
-                typographyProps={{ width: "127px", height: "24px" }}
-            />
-            <NavLinks marginBlock={9} />
+            <Box paddingTop={7} paddingInline={4}>
+                <DroplinkedBrand
+                    dropProps={{ width: "32px", height: "32px" }}
+                    typographyProps={{ width: "127px", height: "24px" }}
+                />
+            </Box>
+
+            <Box
+                flex="1"
+                paddingBottom={6}
+                paddingInline={4}
+                overflowY="auto"
+            >
+                <NavLinks paddingBlock={9} />
+            </Box>
         </Flex>
     )
 }
--- src/layouts/ProducerLayout/Sidebar/TabletSidebar/TabletSidebar.tsx
+++ src/layouts/ProducerLayout/Sidebar/TabletSidebar/TabletSidebar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import Drop3 from 'assets/brand-identity/Drop3'
 import { producerSidebarLinks } from 'data/producerSidebarLinks'
 import React from 'react'
@@ -8,25 +8,26 @@ function TabletSidebar() {
     return (
         <Flex
             as="aside"
+            position="sticky"
+            top={0}
+            height="100vh"
             direction="column"
-            alignItems="center"
             gap={6}
-            paddingTop={7}
-            paddingBottom={6}
-            paddingInline={4}
         >
-            <Drop3 color='#2BCFA1' width='40px' height='40px' />
+            <Box paddingTop={7} paddingInline={4}>
+                <Drop3 color='#2BCFA1' width='40px' height='40px' />
+            </Box>
 
-            <Flex
-                width="100%"
-                direction="column"
-                gap={6}
-                paddingBlock={9}
+            <Box
+                flex="1"
+                paddingBottom={6}
+                paddingInline={4}
+                overflowY="auto"
             >
                 {producerSidebarLinks.map((sidebarGroup) => (
                     <TabletSidebarGroup key={sidebarGroup.group} sidebarGroup={sidebarGroup} />
                 ))}
-            </Flex>
+            </Box>
         </Flex>
     )
 }
