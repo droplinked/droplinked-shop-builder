52cacec8d5ae0b2f65abb58168739ed229329c94
Yasaman Salari <ysmnsalari@gmail.com>
2025-05-24 11:02:48 +0400

    refactor(layouts): remove legacy layout components

--- src/components/layouts/dashboard/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export { SIDEBAR_CONSTANTS } from "./sidebar.constants";
-export { PROFILE_CONSTANTS } from "./profile.constants";
-export { SUBSCRIPTION_STATUS_CONSTANTS } from "./subscription.constants";
-export { GROWTH_HACK_CONSTANTS } from './growthHack.constants'
