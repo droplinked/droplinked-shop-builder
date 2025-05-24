0d2c78a280d054c6814b89100fd43afda8aaa95c
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-05-24 13:19:38 +0330

    refactor(i18n): remove unused home and settings translations for Arabic and English

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,11 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import arCommon from '../data/translations/ar/common.json'
-import arHome from '../data/translations/ar/home.json'
-import arSettings from '../data/translations/ar/settings.json'
 import enCommon from '../data/translations/en/common.json'
-import enHome from '../data/translations/en/home.json'
-import enSettings from '../data/translations/en/settings.json'
 
 i18n
     .use(initReactI18next)
@@ -20,13 +16,9 @@ i18n
         resources: {
             en: {
                 common: enCommon,
-                home: enHome,
-                settings: enSettings,
             },
             ar: {
                 common: arCommon,
-                home: arHome,
-                settings: arSettings,
             }
         }
     })
