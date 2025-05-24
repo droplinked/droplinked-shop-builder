54b98666c03cc6e931624f145d782532fdcc7aa9
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-05-24 15:31:41 +0330

    refactor(NotFoundPage): restructure astronaut image handling and integrate localization for 404 text

--- src/pages/404/NotFoundPage.tsx
+++ src/pages/404/NotFoundPage.tsx
@@ -3,17 +3,18 @@ import astronautLookingLeft from 'assets/image/astronaut-looking-left.png'
 import astronautLookingRight from 'assets/image/astronaut-looking-right.png'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import useAstronautImageSwitcher from './useAstronautImageSwitcher'
+import useAstronautImageSwitcher from './hooks/useAstronautImageSwitcher'
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
+import enLocal from './translations/en.json'
+import arLocal from './translations/ar.json'
+import { Trans } from 'react-i18next'
 
 export default function NotFoundPage() {
-  const images = [astronautLookingRight, astronautLookingLeft]
-  const { currentImageIndex } = useAstronautImageSwitcher()
-
   return (
     <Flex justifyContent="center" paddingTop="100px">
       <Flex>
         <Flex flex={1} direction="column" gap={{ base: '16px', md: '24px' }} paddingTop={9}>
-          <AstronautImage images={images} currentImageIndex={currentImageIndex} />
+          <AstronautImage />
           <NotFoundText />
         </Flex>
       </Flex>
@@ -21,7 +22,10 @@ export default function NotFoundPage() {
   )
 }
 
-function AstronautImage({ images, currentImageIndex }: { images: string[], currentImageIndex: number }) {
+function AstronautImage() {
+  const images = [astronautLookingRight, astronautLookingLeft]
+  const { currentImageIndex } = useAstronautImageSwitcher()
+
   return (
     <Flex order={{ base: 4, md: 1 }} justify="center">
       <Image
@@ -36,23 +40,28 @@ function AstronautImage({ images, currentImageIndex }: { images: string[], curre
 }
 
 function NotFoundText() {
+  const { t } = useLocaleResources('404Page', {
+    en: enLocal,
+    ar: arLocal
+  })
+
   return (
     <>
       <Text color="text.white" order={{ base: 1, md: 2 }} fontSize={{ base: '24px', md: '32px' }} fontWeight={700}>
-        Oooops
+        {t('title')}
       </Text>
       <Text color="text.white" order={{ base: 2, md: 3 }} fontSize={{ base: '16px', md: '24px' }}>
-        Looks like the store you’re looking for has gone on a little adventure.
+        {t('description')}
       </Text>
       <Text color="text.white" order={{ base: 3, md: 4 }} fontSize={{ base: '14px', md: '16px' }}>
-        You can{' '}
-        <Link to="/" style={{ color: "#33a9ec" }}>
-          go back to the homepage
-        </Link>
-        , or unleash your inner entrepreneur and{' '}
-        <Link to={`/onboarding?entry=signup`} style={{ color: "#33a9ec" }}>
-          create your own store
-        </Link>
+        <Trans
+          i18nKey="action"
+          t={t}
+          components={[
+            <Link to="/" style={{ color: '#33a9ec' }} />,
+            <Link to="/onboarding?entry=signup" style={{ color: '#33a9ec' }} />
+          ]}
+        />
       </Text>
     </>
   )
