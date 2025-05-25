10403a70dfb340af755f9ede142bf46a19557efd
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-05-25 11:08:44 +0330

    refactor(CurrentPlan): simplify component structure and improve readability

--- src/pages/subscription-plans/_components/redesign-current-plan/CurrentPlan.tsx
+++ src/pages/subscription-plans/_components/redesign-current-plan/CurrentPlan.tsx
@@ -6,24 +6,27 @@ import { getSubscriptionPlanIcon } from 'utils/helpers';
 import PlanBadge from './_components/PlanBadge';
 import PlanDescription from './_components/PlanDescription';
 import StatisticModal from './_components/statistics/StatisticModal';
-export interface ICurrentSubData {
-    icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>,
-    title: string
-}
+
 function NewCurrentPlan() {
     const { isFetching, data } = useShopSubscriptionData();
+    const { type } = data.data.subscriptionId;
+    const currentSubData = getSubscriptionPlanIcon(type);
+
     if (isFetching) {
         return <AppSkeleton borderRadius={"8px"} isLoaded={!isFetching} width={"100%"} height={"6rem"} />
     }
-    const { type } = data.data.subscriptionId;
-    const currentSubData: ICurrentSubData = getSubscriptionPlanIcon(type);
+
     return (
-        <VStack backgroundColor={"neutral.gray.1000"} borderRadius={"8px"} padding={"36px"} justifyItems={"start"} alignItems={"start"}>
+        <VStack
+            backgroundColor={"neutral.gray.1000"}
+            borderRadius={"8px"}
+            padding={"36px"}
+            justifyItems={"start"}
+            alignItems={"start"}
+        >
             <HStack flexWrap={"wrap"} justifyContent={"space-between"} width={"100%"}>
                 <PlanBadge currentSubData={currentSubData} data={data} />
-                <HStack gap={"1rem"} flexWrap={"wrap"}>
-                    <StatisticModal data={data} />
-                </HStack>
+                <StatisticModal data={data} />
             </HStack>
             <PlanDescription data={data} currentSubData={currentSubData} />
         </VStack>
