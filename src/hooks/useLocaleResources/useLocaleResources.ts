8d709be7815513550002a4629a46e01c09d9823f
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-01 00:49:38 +0330

    Add localization support for maintenance page in Arabic and English

--- src/hooks/useLocaleResources/useLocaleResources.ts
+++ src/hooks/useLocaleResources/useLocaleResources.ts
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'
 type Namespace = '404Page' | 'homePage' | 'shop' | 'common' | 'shopManagement' |
     'onchainRecords' | 'changelogPage' | 'purchaseHistory' | 'creditsAndActivity' | 'analyticsPage' | 'onboarding' | 'dashboardPage' |
     'layout/sidebar' | 'layout/levelUp' | 'layout/userMenu' | 'subscription' | 'settings' |
-    'collections' | 'blogs' | 'gamification' | 'orderSamplePOD'
+    'collections' | 'blogs' | 'gamification' | 'orderSamplePOD' | 'maintenancePage'
 
 /**
  * A custom hook to easily load translation resources into i18next and provide the translation function
