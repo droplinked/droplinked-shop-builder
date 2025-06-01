b345840620be84612914e51407a30354cbff3e6b
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-01 14:30:59 +0330

    Refactor Effects component to utilize localization for RTL support

--- src/pages/public-pages/homePage/parts/effects/Effects.tsx
+++ src/pages/public-pages/homePage/parts/effects/Effects.tsx
@@ -1,14 +1,31 @@
 import { Box } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
+import React from 'react'
 import classes from "./style.module.scss"
 
 function Effects() {
-    const [isPauesd, setPaused] = useState(false)
+    const { isRTL } = useLocaleResources("homePage")
 
     return (
         <Box position={"absolute"} inset={0}>
-            <Box className={`${classes.shape} ${classes.shape1} ${isPauesd ? classes.animationPaused : ''}`} fontSize={{ base: "400px", lg: "700px" }} top={"0"} right={"200px"}>s</Box>
-            <Box className={`${classes.shape} ${classes.shape2} ${isPauesd ? classes.animationPaused : ''}`} fontSize={{ base: "400px", lg: "1400px" }} top="200px" left={{ base: "0", lg: "-210px" }}>s</Box>
+            <Box
+                className={`${classes.shape} ${classes.shape1}`}
+                fontSize={{ base: "400px", lg: "700px" }}
+                top="0"
+                left={isRTL ? "unset" : "200px"}
+                right={isRTL ? "200px" : "unset"}
+            >
+                s
+            </Box>
+            <Box
+                className={`${classes.shape} ${classes.shape2}`}
+                fontSize={{ base: "400px", lg: "1400px" }}
+                top="200px"
+                left={{ base: "0", lg: isRTL ? "unset" : "-210px" }}
+                right={{ base: "0", lg: isRTL ? "-210px" : "unset" }}
+            >
+                s
+            </Box>
         </Box>
     )
 }
