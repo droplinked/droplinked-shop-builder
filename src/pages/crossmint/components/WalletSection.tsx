0c527e7f86b59338c2237621ae92514b60191575
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-02 16:14:25 +0330

    feat(wallet-section): add copy functionality for wallet address with toast notification

--- src/pages/crossmint/components/WalletSection.tsx
+++ src/pages/crossmint/components/WalletSection.tsx
@@ -1,9 +1,29 @@
+import { CopyMd } from 'assets/icons/Action/Copy/CopyMd'
 import AppInput from 'components/redesign/input/AppInput'
+import useAppToast from 'hooks/toast/useToast'
 import SectionContainer from 'pages/settings/components/common/SectionContainer'
 import SectionContent from 'pages/settings/components/common/SectionContent'
 import React from 'react'
 
+const WALLET_ADDRESS = "0xe29E7479c23Db494aAa0D36C93844B2d79f50c2245"
+
 function WalletSection() {
+    const { showToast } = useAppToast()
+
+    const handleCopyAddress = () => {
+        navigator.clipboard.writeText(WALLET_ADDRESS)
+        showToast({
+            type: "success",
+            message: "The wallet address has been copied to your clipboard."
+        })
+    }
+
+    const CopyButton = () => (
+        <button onClick={handleCopyAddress}>
+            <CopyMd color='#FFF' />
+        </button>
+    )
+
     return (
         <SectionContainer title="Crossmint Wallet">
             <SectionContent
@@ -11,9 +31,10 @@ function WalletSection() {
                 description="The crossmint wallet offers a secure and seamless experience."
                 rightContent={
                     <AppInput
+                        rightElement={<CopyButton />}
                         inputProps={{
                             placeholder: "Wallet Address",
-                            value: "0xe29E7479c23Db494aAa0D36C93844B2d79f50c2245",
+                            value: WALLET_ADDRESS,
                             name: "walletAddress",
                             isDisabled: true
                         }}
