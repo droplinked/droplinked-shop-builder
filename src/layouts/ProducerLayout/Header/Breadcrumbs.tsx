74a019e486f68e46f7d60268b36237d0e2eb1ab0
Yasaman Salari <ysmnsalari@gmail.com>
2025-06-03 18:30:25 +0400

    refactor(breadcrumbs): rename interface and improve breadcrumb generation logic
    - Renamed BreadcrumbItem to CustomBreadcrumbItem for clarity.
    - Updated getBreadcrumbs function to return CustomBreadcrumbItem[].
    - Enhanced breadcrumb rendering logic to improve styling based on current page state.

--- src/layouts/ProducerLayout/Header/Breadcrumbs.tsx
+++ src/layouts/ProducerLayout/Header/Breadcrumbs.tsx
@@ -13,14 +13,14 @@ interface SidebarItem  {
     list: Array<{ listTitle: string; linkTo: string }>;
     onClick?: () => void;
     external?: boolean;
-  };
+};
 
 interface SidebarGroup {
     group: string;
     items: SidebarItem[];
 }
 
-interface BreadcrumbItem {
+interface CustomBreadcrumbItem {
     title: string
     linkTo?: string
 }
@@ -34,8 +34,8 @@ export const Breadcrumbs = () => {
         : <ChevronrightSm color='#b1b1b1' />
 
     // Function to generate breadcrumbs based on the current path
-    const getBreadcrumbs = (path: string): BreadcrumbItem[] => {
-        const breadcrumbs: BreadcrumbItem[] = [{ title: 'Home', linkTo: '/analytics' }]
+    const getBreadcrumbs = (path: string): CustomBreadcrumbItem[] => {
+        const breadcrumbs: CustomBreadcrumbItem[] = [{ title: 'Home', linkTo: '/analytics' }]
 
         producerSidebarLinks.forEach((group: SidebarGroup) => {
             group.items.forEach((item) => {
@@ -65,18 +65,18 @@ export const Breadcrumbs = () => {
         <Breadcrumb separator={separator}>
             {breadcrumbs
                 .filter(crumb => crumb.linkTo)
-                .map((crumb, index) => {
-                    const isCurrentPage = pathname === crumb.linkTo
+                .map((crumb, index, items) => {
+                    const isWhiteText = items.length === 1 || pathname === crumb.linkTo
 
                     return (
-                        <BreadcrumbItem key={index} isCurrentPage={isCurrentPage}>
+                        <BreadcrumbItem key={index} isCurrentPage={pathname === crumb.linkTo}>
                             <BreadcrumbLink
                                 as={NavLink}
-                                {...(!isCurrentPage && { to: crumb.linkTo })}
+                                to={pathname !== crumb.linkTo ? crumb.linkTo : undefined}
                                 fontSize={{ base: 14, md: 16, xl: 20 }}
-                                fontWeight={isCurrentPage ? { base: 500, md: 700 } : 400}
-                                color={isCurrentPage ? "text.white" : 'text.subtext.placeholder.light'}
-                                cursor={isCurrentPage ? 'default' : 'pointer'}
+                                fontWeight={isWhiteText ? { base: 500, md: 700 } : 400}
+                                color={isWhiteText ? "text.white" : 'text.subtext.placeholder.light'}
+                                cursor={isWhiteText ? 'default' : 'pointer'}
                             >
                                 {crumb.title}
                             </BreadcrumbLink>
