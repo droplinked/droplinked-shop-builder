0ab5a14bba2e6c94d85c8563335a9ef308081aca
Yasaman Salari <ysmnsalari@gmail.com>
2025-06-07 15:42:25 +0400

    Enhance styling and structure in AppTooltip and TabletSidebarItem components
    - Updated AppTooltip to use a darker border color and lighter background for improved visibility.
    - Refactored TabletSidebarItem to include new SVG arrow components and enhanced layout with better styling for popover content.

--- src/components/common/tooltip/AppTooltip.tsx
+++ src/components/common/tooltip/AppTooltip.tsx
@@ -5,11 +5,11 @@ function AppTooltip(props: TooltipProps) {
     return (
         <Tooltip
             border="1px solid"
-            borderColor={"neutral.gray.700"}
+            borderColor={"neutral.gray.800"}
             borderRadius="6px"
             padding="4px 16px"
-            backgroundColor="neutral.gray.800"
-            color="#C2C2C2"
+            backgroundColor="neutral.gray.1000"
+            color="white"
             placement='auto-start'
             {...props}
         />
