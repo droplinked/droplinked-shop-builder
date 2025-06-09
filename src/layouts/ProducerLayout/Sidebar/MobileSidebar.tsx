a89a2081a7d206b027a4bed8932a6dd8d78dac8a
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-09 17:32:09 +0330

    Refactor MobileSidebar to utilize Chakra UI's Drawer component for improved sidebar functionality and remove manual overflow handling

--- src/layouts/ProducerLayout/Sidebar/MobileSidebar.tsx
+++ src/layouts/ProducerLayout/Sidebar/MobileSidebar.tsx
@@ -1,55 +1,29 @@
-import { Box } from '@chakra-ui/react'
+import { Drawer, DrawerBody, DrawerContent, DrawerOverlay } from '@chakra-ui/react'
 import { useProducerLayout } from 'context/ProducerLayoutContext'
-import React, { useEffect } from 'react'
+import React from 'react'
 import NavLinks from './NavLinks'
 
 function MobileSidebar() {
   const { isSidebarOpen, toggleSidebar } = useProducerLayout()
 
-  useEffect(() => {
-    if (isSidebarOpen) {
-      document.body.style.overflow = 'hidden'
-    } else {
-      document.body.style.overflow = 'unset'
-    }
-
-    return () => {
-      document.body.style.overflow = 'unset'
-    }
-  }, [isSidebarOpen])
-
   return (
-    <>
-      <Box
-        width="100%"
-        position="fixed"
-        top="119px"
-        left="0"
-        bottom="0"
-        zIndex={999}
-        opacity={isSidebarOpen ? 1 : 0}
-        visibility={isSidebarOpen ? 'visible' : 'hidden'}
-        bg="rgba(0, 0, 0, 0.4)"
-        transition="all 0.6s cubic-bezier(0.65, 0, 0.35, 1)"
-        onClick={toggleSidebar}
-      />
-      <Box
-        width={isSidebarOpen ? "80%" : "0%"}
-        position="fixed"
-        top="119px"
-        left="0"
-        bottom="0"
-        zIndex={1000}
-        transform={`translateX(${isSidebarOpen ? '0' : '-100%'})`}
-        paddingBlock={8}
-        paddingInline={4}
+    <Drawer
+      isOpen={isSidebarOpen}
+      placement="left"
+      onClose={toggleSidebar}
+    >
+      <DrawerOverlay bg="rgba(0, 0, 0, 0.4)" />
+      <DrawerContent
+        mt="119px" // Start from below the header
         bg="neutral.background"
-        overflow={isSidebarOpen ? "auto" : "hidden"}
-        transition="all 0.6s cubic-bezier(0.65, 0, 0.35, 1)"
+        maxH="calc(100vh - 119px)"
+        padding={0}
       >
-        <NavLinks />
-      </Box>
-    </>
+        <DrawerBody padding={0}>
+          <NavLinks padding="32px 16px" />
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
   )
 }
 
