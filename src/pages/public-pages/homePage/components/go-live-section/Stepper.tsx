28ea77372b81c33d8415255f10e42aa78fc96acd
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-15 20:30:08 +0330

    Add StepTexts component to display step information in Stepper; integrate current step tracking

--- src/pages/public-pages/homePage/components/go-live-section/Stepper.tsx
+++ src/pages/public-pages/homePage/components/go-live-section/Stepper.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 import { useStepController } from '../../hooks/useStepController'
 import ProgressBar from './ProgressBar'
 import AnimationFrame from './AnimationFrame'
+import StepTexts from './StepTexts'
 
 export default function Stepper() {
-    const { containerRef, fixedPercentage, LottieView } = useStepController()
+    const { containerRef, step, fixedPercentage, LottieView } = useStepController()
 
     return (
         <Flex
@@ -14,9 +15,11 @@ export default function Stepper() {
             flexDirection="column"
             alignItems="center"
             justifyContent="center"
+            px={{ base: 4, md: 8, lg: 12 }}
         >
             <AnimationFrame LottieView={LottieView} />
             <ProgressBar percentage={fixedPercentage} />
+            <StepTexts currentStep={step} />
         </Flex>
     )
 }
