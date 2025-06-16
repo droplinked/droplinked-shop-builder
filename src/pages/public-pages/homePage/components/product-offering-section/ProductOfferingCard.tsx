1b50df044a2005d8036e3ca4c52853d465760bf4
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-16 15:35:35 +0330

    Add ProductOfferingCard component and integrate it into ProductOfferingSection; implement grid layout for product offerings with icons, titles, descriptions, and animations.

---
+++ src/pages/public-pages/homePage/components/product-offering-section/ProductOfferingCard.tsx
@@ -0,0 +1,44 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import IconWrapper from "components/redesign/icon-wrapper/IconWrapper";
+import React from "react";
+
+interface Props {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+    animation: React.ReactNode;
+}
+
+export default function ProductOfferingCard({ icon, title, description, animation }: Props) {
+    return (
+        <Flex
+            flexDirection="column"
+            border="1px solid"
+            borderColor="neutral.gray.900"
+            borderRadius={16}
+        >
+            <Flex flexDirection="column" gap={4} p={6}>
+                <IconWrapper
+                    border="1px solid"
+                    borderColor="neutral.gray.900"
+                    background="neutral.background"
+                    icon={icon}
+                />
+                <Box>
+                    <Text fontSize={20} fontWeight={500} color="text.white" mb={1}>
+                        {title}
+                    </Text>
+                    <Text
+                        fontSize={16}
+                        fontWeight={400}
+                        color="text.subtext.placeholder.dark"
+                        mb={1}
+                    >
+                        {description}
+                    </Text>
+                </Box>
+            </Flex>
+            {animation}
+        </Flex>
+    );
+}
--- src/pages/public-pages/homePage/components/product-offering-section/ProductOfferingSection.tsx
+++ src/pages/public-pages/homePage/components/product-offering-section/ProductOfferingSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MaxWidthWrapper from '../common/MaxWidthWrapper'
 import SectionContainer from '../common/SectionContainer/SectionContainer'
+import { Grid } from '@chakra-ui/react'
+import ProductOfferingCard from './ProductOfferingCard'
 
 export default function ProductOfferingSection() {
     const headingSubtitle = `droplinked supports a wide variety of inventory, from digital goods, physical items,
@@ -15,6 +17,33 @@ export default function ProductOfferingSection() {
                 headingSubtitle={headingSubtitle}
                 typographyText='Products'
             />
+
+            <Grid>
+                <ProductOfferingCard
+                    icon={<div>Services</div>}
+                    title="Services"
+                    description="Offer skills like consulting, design work, and more with onchain automation"
+                    animation={<div>Animation 1</div>}
+                />
+                <ProductOfferingCard
+                    icon={<div>Icon 2</div>}
+                    title="Physical Products"
+                    description="Manage inventory and sales with secure blockchain-backed transactions"
+                    animation={<div>Animation 2</div>}
+                />
+                <ProductOfferingCard
+                    icon={<div>Icon 2</div>}
+                    title="Digital Products"
+                    description="Offer eBooks, online courses, software, and more with easy distribution"
+                    animation={<div>Animation 2</div>}
+                />
+                <ProductOfferingCard
+                    icon={<div>POD and NFT’s - Mint to MerchTM</div>}
+                    title="POD and NFT’s - Mint to MerchTM"
+                    description="Create and sell unique items using digital art, collectibles, and other blockchain-based items"
+                    animation={<div>Animation 2</div>}
+                />
+            </Grid>
         </MaxWidthWrapper>
     )
 }
