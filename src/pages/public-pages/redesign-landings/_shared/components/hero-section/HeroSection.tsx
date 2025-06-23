08cbdcf4a2ee294d48dd3823ddcfe47690170bc5
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-23 16:00:33 +0330

    Enhance HeroSection with animated BluredStar background and gradient overlay; remove unused backgroundImage prop from CrossmintHero.

--- src/pages/public-pages/redesign-landings/_shared/components/hero-section/HeroSection.tsx
+++ src/pages/public-pages/redesign-landings/_shared/components/hero-section/HeroSection.tsx
@@ -2,11 +2,12 @@ import { Box, Flex, Grid, Text } from '@chakra-ui/react'
 import React from 'react'
 import HeroAnimation from './HeroAnimation'
 import { LottieOptions } from 'lottie-react'
+import { motion } from 'framer-motion'
+import { ReactComponent as BluredStar } from '../../svgs/BluredStar.svg'
 
 interface HeroSectionProps {
     title?: string
     subtitle?: string
-    backgroundImage?: string
     subTitleElements?: React.ReactNode
     heroDesktop?: Object
     heroTablet?: Object
@@ -17,7 +18,6 @@ interface HeroSectionProps {
 export default function HeroSection({
     title,
     subtitle,
-    backgroundImage,
     subTitleElements,
     heroDesktop,
     heroTablet,
@@ -30,41 +30,90 @@ export default function HeroSection({
             templateRows="repeat(1, 1fr)"
             height="max-content"
             mx={{ base: 4, lg: 6 }}
-            backgroundImage={`url(${backgroundImage})`}
             backgroundSize="cover"
             backgroundRepeat="no-repeat"
             borderRadius="24px"
+            position="relative"
+            overflow="hidden"
         >
-            <Box>
-                <Flex flexDirection="column" alignItems="center" gap={{ base: 2, md: 4 }}>
-                    <Text
-                        color="neutral.white"
-                        fontWeight={500}
-                        lineHeight={{ base: "48px", md: "64px", xl: "72px", "2xl": "80px" }}
-                        fontSize={{ base: "32px", md: "48px", xl: "56px", "2xl": "64px" }}
-                        mt={{ base: "48px", "md": "80px" }}
-                        whiteSpace="pre-line"
-                        textAlign="center"
-                    >
-                        {title}
-                    </Text>
-                    <Text
-                        color="text.subtext.placeholder.light"
-                        maxWidth={{ base: "90%", md: "80%", xl: "45%" }}
-                        textAlign="center"
-                        fontSize={{ base: "14px", lg: "16px" }}
-                    >
-                        {subtitle}
-                    </Text>
-                </Flex>
-                {subTitleElements}
+            <Box
+                position="absolute"
+                inset="0"
+                zIndex={0}
+                bg="linear-gradient(0deg, rgba(43, 207, 161, 0.04) 0%, rgba(43, 207, 161, 0.04) 100%), linear-gradient(0deg, rgba(10, 10, 10, 0.00) 0%, #0A0A0A 100%)"
+            >
+                <Box
+                    position="absolute"
+                    inset="0"
+                    bgImage="url(https://upload-file-droplinked.s3.amazonaws.com/3b12a2fc765465de8080fadb321a30195c0df35dcffea9f75956465f70d35826.png)"
+                    bgPosition="center"
+                    bgSize="cover"
+                    bgRepeat="no-repeat"
+                    bgColor="lightgray"
+                />
+                <Box
+                    position="absolute"
+                    inset="0"
+                    bg="var(--Main-Primary, #2BCFA1)"
+                    mixBlendMode="color"
+                />
+                <motion.div
+                    animate={{ rotate: 360 }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 40,
+                        ease: 'linear',
+                    }}
+                    style={{
+                        position: 'absolute',
+                        top: '45%',
+                        left: '-50%',
+                        width: '200%',
+                        height: '200%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 999,
+                    }}
+                >
+                    <BluredStar width="100%" height="100%" />
+                </motion.div>
+                <Box
+                    position="absolute"
+                    inset="0"
+                    backdropFilter="blur(50px)"
+                />
             </Box>
-            <HeroAnimation
-                heroDesktop={heroDesktop}
-                heroTablet={heroTablet}
-                heroMobile={heroMobile}
-                lottieOptions={lottieOptions}
-            />
+            <Box zIndex={1} gridArea="1 / 1 / 2 / 2">
+                <Box>
+                    <Flex flexDirection="column" alignItems="center" gap={{ base: 2, md: 4 }}>
+                        <Text
+                            color="neutral.white"
+                            fontWeight={500}
+                            lineHeight={{ base: "48px", md: "64px", xl: "72px", "2xl": "80px" }}
+                            fontSize={{ base: "32px", md: "48px", xl: "56px", "2xl": "64px" }}
+                            mt={{ base: "48px", "md": "80px" }}
+                            whiteSpace="pre-line"
+                            textAlign="center"
+                        >
+                            {title}
+                        </Text>
+                        <Text
+                            color="text.subtext.placeholder.light"
+                            maxWidth={{ base: "90%", md: "80%", xl: "45%" }}
+                            textAlign="center"
+                            fontSize={{ base: "14px", lg: "16px" }}
+                        >
+                            {subtitle}
+                        </Text>
+                    </Flex>
+                    {subTitleElements}
+                </Box>
+                <HeroAnimation
+                    heroDesktop={heroDesktop}
+                    heroTablet={heroTablet}
+                    heroMobile={heroMobile}
+                    lottieOptions={lottieOptions}
+                />
+            </Box>
         </Grid>
     )
 }
--- src/pages/public-pages/redesign-landings/crossmint/components/crossmint-hero/CrossmintHero.tsx
+++ src/pages/public-pages/redesign-landings/crossmint/components/crossmint-hero/CrossmintHero.tsx
@@ -8,7 +8,6 @@ export default function CrossmintHero() {
         <HeroSection
             title={`Powering \n Agentic Commerce`}
             subtitle={`Crossmint members unlock 3 months of the Pro Plan absolutely free! \n Redeem the exclusive offer today.`}
-            backgroundImage="https://upload-file-droplinked.s3.amazonaws.com/bb43d560151ac7d20966da30d1d9affdd9b462943a49cbf40e4f893e35c94b07.png"
             heroDesktop={Animation}
             heroMobile={Animation}
             heroTablet={Animation}
