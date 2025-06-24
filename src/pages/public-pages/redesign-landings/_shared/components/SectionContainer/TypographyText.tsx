126777096bde9b6abb2802dd3590d160e5bc5a79
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-24 14:13:12 +0330

    Enhance TypographyText component: simplify background styling by using bgGradient and bgClip props, and update text stroke properties for improved visual clarity.

--- src/pages/public-pages/redesign-landings/_shared/components/SectionContainer/TypographyText.tsx
+++ src/pages/public-pages/redesign-landings/_shared/components/SectionContainer/TypographyText.tsx
@@ -14,18 +14,13 @@ function TypographyText({ text }: { text?: string }) {
             fontWeight={900}
             lineHeight={{ md: '88px', xl: '136px', '2xl': '144px' }}
             letterSpacing={{ md: "-1.76px", xl: "-2.88px", '2xl': "-3.2px" }}
+            bgGradient="linear(to-b, rgba(20, 20, 20, 0.50) 0%, rgba(20, 20, 20, 0.00) 100%)"
+            bgClip="text"
             sx={{
-                background: 'linear-gradient(180deg, rgba(20, 20, 20, 0.50) 0%, rgba(20, 20, 20, 0.00) 100%)',
-                backgroundClip: 'text',
                 WebkitBackgroundClip: 'text',
                 WebkitTextFillColor: 'transparent',
-                position: 'relative',
-                maskImage: 'linear-gradient(to bottom, black 80%, transparent 100%)',
-                WebkitMaskImage: 'linear-gradient(to bottom, black 80%, transparent 100%)',
-                textShadow: `
-                    0px 2px 0px rgba(20, 20, 20, 0.3),
-                    0px 4px 0px rgba(20, 20, 20, 0.25)
-                `,
+                textStroke: '1px rgba(20, 20, 20, 0.50)',
+                WebkitTextStroke: '1px rgba(20, 20, 20, 0.50)'
             }}
         >
             {text}
