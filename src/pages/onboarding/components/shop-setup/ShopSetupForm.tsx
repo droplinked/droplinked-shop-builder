970de404fd0e06c12f01e8d1f1518ca0748693d5
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-24 13:17:43 +0330

    Refactor ControlButtons and ShopSetupForm components: remove unused backText prop, streamline back navigation logic, and enhance code formatting for clarity.

--- src/pages/onboarding/components/shop-setup/ShopSetupForm.tsx
+++ src/pages/onboarding/components/shop-setup/ShopSetupForm.tsx
@@ -1,49 +1,43 @@
-import { Flex, useMediaQuery } from '@chakra-ui/react';
-import { useShopSetupSubmit } from 'pages/onboarding/hooks/useShopSetupSubmit';
-import React from 'react';
-import useAppStore from 'stores/app/appStore';
-import ControlButtons from '../common/ControlButtons';
-import OnboardingStepHeader from '../common/OnboardingStepHeader';
-import ShopPreview from '../shop-preview/ShopPreview';
-import AICard from './ai/AICard';
-import AutoPopulateSampleProductsToggle from './inputs/AutoPopulateSampleProductsToggle';
-import CoverImage from './uploads/CoverImage';
-import DescriptionField from './inputs/DescriptionField';
-import ExistingShopUrlProcessor from './inputs/ExistingShopUrlProcessor';
-import LogoUploader from './uploads/LogoUploader';
-import NameField from './inputs/NameField';
-import UrlChooser from './inputs/UrlChooser';
-import useOnboardingStore from 'pages/onboarding/stores/useOnboardingStore';
-
+import { useMediaQuery } from '@chakra-ui/react'
+import { useShopSetupSubmit } from 'pages/onboarding/hooks/useShopSetupSubmit'
+import useOnboardingStore from 'pages/onboarding/stores/useOnboardingStore'
+import React from 'react'
+import ControlButtons from '../common/ControlButtons'
+import OnboardingStepHeader from '../common/OnboardingStepHeader'
+import ShopPreview from '../shop-preview/ShopPreview'
+import AICard from './ai/AICard'
+import AutoPopulateSampleProductsToggle from './inputs/AutoPopulateSampleProductsToggle'
+import DescriptionField from './inputs/DescriptionField'
+import ExistingShopUrlProcessor from './inputs/ExistingShopUrlProcessor'
+import NameField from './inputs/NameField'
+import UrlChooser from './inputs/UrlChooser'
+import CoverImage from './uploads/CoverImage'
+import LogoUploader from './uploads/LogoUploader'
 
 function ShopSetupForm() {
-  const [isSmallerThan1024] = useMediaQuery('(max-width: 1024px)');
-  const { reset } = useAppStore();
-  const { handleSubmit, isLoading, resetOnboarding } = useShopSetupSubmit();
-  const { shopSetupUI } = useOnboardingStore()
+  const [isSmallerThan1024] = useMediaQuery('(max-width: 1024px)')
+  const { handleSubmit, isLoading } = useShopSetupSubmit()
+  const { shopSetupUI: { hasExistingShop }, updateOnboardingState } = useOnboardingStore()
 
-  const handleBack = () => {
-    reset();
-    resetOnboarding();
-  };
+  const handleBack = () => updateOnboardingState("currentStep", "EXISTING_WEBSITE")
 
   return (
     <>
-      <Flex flexDirection={{ base: 'column', md: 'row' }} justifyContent="space-between" gap={4}>
-        <OnboardingStepHeader heading="Account Details" description="Complete the information below to optimize your storefront." />
-      </Flex>
-      {shopSetupUI.hasExistingShop ? <ExistingShopUrlProcessor /> :  <AICard />} 
+      <OnboardingStepHeader
+        heading="Account Details"
+        description="Complete the information below to optimize your storefront."
+      />
+      {hasExistingShop ? <ExistingShopUrlProcessor /> : <AICard />}
       <LogoUploader />
       <CoverImage />
       <UrlChooser />
       <NameField />
       <DescriptionField />
       <AutoPopulateSampleProductsToggle />
-      <ControlButtons onBack={handleBack} onSubmit={handleSubmit} isLoading={isLoading} backText="Exit" />
+      <ControlButtons onBack={handleBack} onSubmit={handleSubmit} isLoading={isLoading} />
       {isSmallerThan1024 && <ShopPreview />}
     </>
-  );
+  )
 }
 
-export default ShopSetupForm;
- 
+export default ShopSetupForm
