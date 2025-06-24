d5d0aee2bff70b524a2d571a26d771c74a06a912
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-24 11:48:29 +0330

    Refactor Onboarding component to remove unused PLAN_SELECTION_DISPLAY step

--- src/pages/onboarding/Onboarding.tsx
+++ src/pages/onboarding/Onboarding.tsx
@@ -31,7 +31,6 @@ function Onboarding() {
 
     if (entry === 'signin') updateOnboardingState('currentStep', 'SIGN_IN')
     else if (entry === 'signup') updateOnboardingState('currentStep', 'SIGN_UP')
-
   }, [updateOnboardingState])
 
   useEffect(() => {
@@ -51,7 +50,6 @@ function Onboarding() {
     PAYMENT_DETAILS: { leftContent: <PaymentSetup />, rightContent: <PaymentFeatures />, isAuthStep: false },
     PLAN_SELECTION: { leftContent: <SubscriptionPlans />, rightContent: <SubscriptionPlansDisplay />, isAuthStep: false },
     YOU_ARE_ALL_SET: { leftContent: <CompletionSection />, rightContent: null, isAuthStep: false },
-    PLAN_SELECTION_DISPLAY: { leftContent: <SubscriptionPlansDisplay />, rightContent: null, isAuthStep: false }
   }
 
   const { leftContent, rightContent, isAuthStep } = stepContentMap[currentStep]
