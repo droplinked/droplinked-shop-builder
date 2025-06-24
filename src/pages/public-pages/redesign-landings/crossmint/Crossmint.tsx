5d0912ade9532cb6ed2a422a7eb49bd55cfafcb6
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-24 11:34:29 +0330

    Refactor Crossmint and HomePage components to replace PartnersSection with MarqueeSection; update SectionContainer props for consistency and remove unused components.

--- src/pages/public-pages/redesign-landings/crossmint/Crossmint.tsx
+++ src/pages/public-pages/redesign-landings/crossmint/Crossmint.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import CrossmintHero from './components/crossmint-hero/CrossmintHero';
-import PartnersSection from './components/PartnersSection';
-import SetOfPerks from './components/SetOfPerks';
-import ModularStack from './components/ModularStack';
-import JoinTheCommuity from './components/JoinTheCommuity';
-import ClaimNow from './components/ClaimNow';
-import MaxWidthWrapper from '../_shared/components/MaxWidthWrapper';
+import React from 'react';
 import { LazyLoad } from '../_shared/components/LazyLoad';
+import MaxWidthWrapper from '../_shared/components/MaxWidthWrapper';
+import MarqueeSection from '../_shared/components/marquee-wrapper/MarqueeSection';
+import ClaimNow from './components/ClaimNow';
+import JoinTheCommuity from './components/JoinTheCommuity';
+import ModularStack from './components/ModularStack';
+import SetOfPerks from './components/SetOfPerks';
+import CrossmintHero from './components/crossmint-hero/CrossmintHero';
 
 export default function Crossmint() {
     const sections = [
         { id: 'hero', component: <CrossmintHero /> },
-        { id: 'partners', component: <PartnersSection /> },
+        { id: 'partners', component: <MarqueeSection /> },
         { id: 'set-of-perks', component: <SetOfPerks /> },
         { id: 'modular-stack', component: <ModularStack /> },
         { id: 'join-the-community', component: <JoinTheCommuity /> },
--- src/pages/public-pages/redesign-landings/crossmint/components/ClaimNow.tsx
+++ src/pages/public-pages/redesign-landings/crossmint/components/ClaimNow.tsx
@@ -1,18 +1,16 @@
 import AppButton from 'components/redesign/button/AppButton'
-import React from 'react'
-import SectionContainer from '../../_shared/components/SectionContainer/SectionContainer'
-import Lottie from 'lottie-react'
-import FinalCta from '../lottie/FinalCta.json'
-import { Link } from 'react-router-dom'
 import { AUTH_ROUTES } from 'constants/authRoutes'
+import Lottie from 'lottie-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SectionContainer from '../../_shared/components/SectionContainer/SectionContainer'
+import FinalCta from '../lottie/FinalCta.json'
 
 export default function ClaimNow() {
     return (
         <SectionContainer
-            flexProps={{
-                paddingTop: { base: "80px", lg: "128px" },
-                paddingBottom: 0
-            }}
+            paddingTop={{ base: "80px", lg: "128px" }}
+            paddingBottom={0}
             icon='bolt'
             sectionTitle='CLAIM NOW'
             headingTitle='Free 3 Month Pro Plan'
@@ -26,6 +24,6 @@ export default function ClaimNow() {
             }
         >
             <Lottie animationData={FinalCta} style={{ paddingTop: '16px' }} />
-        </SectionContainer>
+        </SectionContainer >
     )
 }
