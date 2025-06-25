fc657a82a07aabd85ce8abc53a7a12d88cbb4156
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-25 12:48:33 +0330

    Add new SVG components for navigation and system icons: ExternalArrowLeftAligned (Lg, Md, Sm), Equal (Lg, Md, Sm), Cursor (Lg, Md, Sm), and GameController (Lg, Md, Sm).

---
+++ src/assets/icons/Navigation/ExternalArrowLeftAligned/ExternalArrowLeftAlignedLg.tsx
@@ -0,0 +1,8 @@
+// Generated from SVG to React Component
+import React, { SVGProps } from "react";
+
+export const ExternalArrowLeftAlignedLg = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M17 17L7 7M7 7H16M7 7V16" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
