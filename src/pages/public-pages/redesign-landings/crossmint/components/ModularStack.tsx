7cd3386ce42dd6186b1a3d6dbc2b2e399382cf77
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-06-25 14:11:26 +0330

    Update image sources in ModularStack component for improved visuals and consistency

--- src/pages/public-pages/redesign-landings/crossmint/components/ModularStack.tsx
+++ src/pages/public-pages/redesign-landings/crossmint/components/ModularStack.tsx
@@ -13,13 +13,13 @@ export default function ModularStack() {
         title: "Access +10k Products Instantly",
         description: "Choose from a vast catalog to customize and sell unique merchandise on demand.",
         gridColumn: { base: "1fr", md: "span 1", lg: "span 1" },
-        children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/a011794e8a2da29dfb241e853e2382bebfbed7e0505f0a21a86ead66f01e1bff.png' />
+        children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/49f43950da54190be0f48855598789155a36a5a2fea8cf5d98f4b3c6aedf18cc.png' />
     }, {
         icon: <Layer1Lg color="#fff" />,
         title: "Web3 Technology Support",
         description: "Seamlessly integrate NFTs alongside blockchain features to create novel experiences with assets you own or want to offer.",
         gridColumn: { base: "1fr", md: "span 1", lg: "span 1" },
-        children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/692b7055b74f370007199b6f1cac42e7a8522e7780841f7d0855a043cd21a8bc.png' />
+        children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
     }, {
         icon: <PriceplanLg color="#fff" />,
         title: "3 Month Pro Plan",
