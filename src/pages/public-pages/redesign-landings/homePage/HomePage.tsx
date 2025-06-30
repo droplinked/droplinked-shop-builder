65b6998ddb33ef591a55715a36f3017fd052d6a3
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-06-30 15:08:50 +0330

    Add Google login handling in HomePage component

--- src/pages/public-pages/redesign-landings/homePage/HomePage.tsx
+++ src/pages/public-pages/redesign-landings/homePage/HomePage.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useLogin } from 'pages/onboarding/hooks/useLogin'
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import JoinTheCommuity from '../_shared/components/JoinTheCommuity'
 import MaxWidthWrapper from '../_shared/components/MaxWidthWrapper'
 import MarqueeSection from '../_shared/components/marquee-wrapper/MarqueeSection'
@@ -9,6 +11,28 @@ import GoLiveSection from './components/go-live-section/GoLiveSection'
 import KeyFeatures from './components/key-features/KeyFeatures'
 
 export default function HomePage() {
+  const [searchParams] = useSearchParams()
+  const { authenticateUser, finalizeLogin, loading } = useLogin()
+
+  useEffect(() => {
+    async function handleGoogleLogin() {
+      const access_token = searchParams.get("access_token")
+      const refresh_token = searchParams.get("refresh_token")
+
+      if (access_token && refresh_token && !loading) {
+        const result = await authenticateUser({
+          type: "get",
+          access_token,
+          refresh_token,
+          params: { access_token }
+        })
+        if (result) await finalizeLogin(result)
+      }
+    }
+
+    handleGoogleLogin()
+  }, [searchParams, loading, authenticateUser, finalizeLogin])
+
   return (
     <>
       <HomePageHero />
