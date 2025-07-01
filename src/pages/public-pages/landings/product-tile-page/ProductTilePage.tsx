7e37e4cbc584a3e6a7c7dd75304c1c5477f8b6f3
Yasaman Salari <ysmnsalari@gmail.com>
2025-07-01 10:13:38 +0300

    Implement localization support in ProductTilePage component
    - Integrated useLocaleResources hook to enable multilingual content in the ProductTilePage.
    - Updated static text to utilize translation keys for titles and descriptions, enhancing localization management.
    - Improved user experience by allowing dynamic text rendering based on selected language.

--- src/pages/public-pages/landings/product-tile-page/ProductTilePage.tsx
+++ src/pages/public-pages/landings/product-tile-page/ProductTilePage.tsx
@@ -1,60 +1,73 @@
 import AppIcons from 'assets/icon/Appicons'
 import React from 'react'
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources';
 import LandingPageTemplate from '../_components/landing-page-template/LandingPageTemplate'
 import { ILandingPageTemplate } from '../types/interfaces'
+import localEn from 'locales/public-pages/landings/product-tile-page/en.json';
+import localAr from 'locales/public-pages/landings/product-tile-page/ar.json';
 
 function ProductTilePage() {
+    const { t } = useLocaleResources('public-pages/landings/product-tile-page', { en: localEn , ar: localAr });
+
     const data: ILandingPageTemplate = {
         aboveTheFoldSection: {
             image: "https://upload-file-droplinked.s3.amazonaws.com/42f9efc9a24934b427005ef43e1e75c38a92e5e752bda75a930b873d572d8283_or.png",
-            title: "Embed and Sell Anywhere",
-            description: "Easily create product tiles to share across any platform to sell directly, no complex integrations or coding required.",
-            CTAButtonText: "Get Started"
+            title: t('aboveTheFoldSection.title'),
+            description: t('aboveTheFoldSection.description')
         },
         detailsSection: {
-            title: "Full Functionality Everywhere",
-            description: "Generate and embed customizable product tiles across any site, enabling seamless purchases anywhere online",
+            title: t('detailsSection.title'),
+            description: t('detailsSection.description'),
             detailItems: [
                 {
                     icon: <AppIcons.LandingPageCode />,
-                    title: "Effortless Embeddable",
-                    description: "Copy and paste the component into your website's source code to create a seamless experience for your customers."
+                    title: t('detailsSection.detailItems.effortlessEmbeddable.title'),
+                    description: t('detailsSection.detailItems.effortlessEmbeddable.description')
                 },
                 {
                     icon: <AppIcons.LandingPageCreditCard />,
-                    title: "Seamless Checkout",
-                    description: "Customers stay on the same page during the checkout experience to keep things cohesive and consistent."
+                    title: t('detailsSection.detailItems.seamlessCheckout.title'),
+                    description: t('detailsSection.detailItems.seamlessCheckout.description')
                 },
                 {
                     icon: <AppIcons.LandingPageTemplate />,
-                    title: "Customizable Designs",
-                    description: "Tailor the product tile design to match your brand's aesthetic, look and feel."
+                    title: t('detailsSection.detailItems.customizableDesigns.title'),
+                    description: t('detailsSection.detailItems.customizableDesigns.description')
                 }
             ]
         },
         dualSideFlexData: [
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/0f1e603c9754eab3dd8d5a5b5f8a9c8d0a6f2de5c9ee70184691ca3012e24bc8_or.png",
-                title: "Instantly Sell Everywhere",
-                description: "Embed product tiles onto blogs, websites or any online platform that turns any space into a storefront."
+                title: t('dualSideFlexData.instantlySellEverywhere.title'),
+                description: t('dualSideFlexData.instantlySellEverywhere.description')
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/f6293c4f44ae1b4af7be12033ac41c54263a56bb23010a533530208a23e457a9_or.png",
-                title: "Flexible Functionality",
-                description: "Use product tiles as a simple purchase button and simplified payment gateway with support for crypto and fiat payments."
+                title: t('dualSideFlexData.flexibleFunctionality.title'),
+                description: t('dualSideFlexData.flexibleFunctionality.description')
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/450df138917ce5ed98a90438ab38c3a87129d91d341c7cfff3011a19bf2c7303_or.png",
-                title: "Smooth Integration",
-                description: "Integrate droplinkeds’ tools into any project or platform effortlessly to drive sales without disrupting any existing customer journey."
+                title: t('dualSideFlexData.smoothIntegration.title'),
+                description: t('dualSideFlexData.smoothIntegration.description')
             }
         ],
         featureGroups: [
             {
                 features: [
-                    { title: "Component Creation & Embeddables", description: "Easily generate and embed product tiles into various platforms to make selling directly from any environment incredibly simple." },
-                    { title: "Seamless Purchase Flows", description: "Keep customers on the same page while they browse and checkout." },
-                    { title: "Customizable, Flexible Components", description: "Customize product tiles and payment methods to support both crypto and fiat payments." }
+                    { 
+                        title: t('featureGroups.features.componentCreation.title'), 
+                        description: t('featureGroups.features.componentCreation.description') 
+                    },
+                    { 
+                        title: t('featureGroups.features.seamlessPurchaseFlows.title'), 
+                        description: t('featureGroups.features.seamlessPurchaseFlows.description') 
+                    },
+                    { 
+                        title: t('featureGroups.features.customizableComponents.title'), 
+                        description: t('featureGroups.features.customizableComponents.description') 
+                    }
                 ]
             }
         ]
