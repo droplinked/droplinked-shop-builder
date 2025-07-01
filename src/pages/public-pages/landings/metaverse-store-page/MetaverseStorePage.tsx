4cf8339fef843c7d9b86f8d43809a82b14efd1ef
Yasaman Salari <ysmnsalari@gmail.com>
2025-07-01 10:13:16 +0300

    Implement localization support in MetaverseStorePage component
    - Integrated useLocaleResources hook to enable multilingual content in the MetaverseStorePage.
    - Updated static text to utilize translation keys for titles, descriptions, and feature items, enhancing localization management.
    - Improved user experience by allowing dynamic text rendering based on selected language.

--- src/pages/public-pages/landings/metaverse-store-page/MetaverseStorePage.tsx
+++ src/pages/public-pages/landings/metaverse-store-page/MetaverseStorePage.tsx
@@ -1,60 +1,64 @@
 import AppIcons from 'assets/icon/Appicons'
 import React from 'react'
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
 import LandingPageTemplate from '../_components/landing-page-template/LandingPageTemplate'
 import { ILandingPageTemplate } from '../types/interfaces'
+import localEn from 'locales/public-pages/landings/metaverse-store-page/en.json'
+import localAr from 'locales/public-pages/landings/metaverse-store-page/ar.json'
 
 function MetaverseStorePage() {
+    const { t } = useLocaleResources('public-pages/landings/metaverse-store-page', { en: localEn , ar: localAr })
+
     const data: ILandingPageTemplate = {
         aboveTheFoldSection: {
             image: "https://upload-file-droplinked.s3.amazonaws.com/592c32c9822f20852dad6260680fd091c79ff7cba1a372d043f5410fe0bc0423_or.png",
-            title: "The future of commerce is immersive",
-            description: "Enable existing and future customers to explore and shop in virtual stores and environments. Create and manage inventory alongside interactive AR/VR 3D shopping experiences that are metaverse- ready.",
-            CTAButtonText: "Get Started"
+            title: t('aboveTheFold.title'),
+            description: t('aboveTheFold.description')
         },
         detailsSection: {
-            title: "Interactive Commerce Anywhere",
-            description: "Transform existing shops and inventory into a 3D experience where customers can explore and interact like never before",
+            title: t('details.title'),
+            description: t('details.description'),
             detailItems: [
                 {
                     icon: <AppIcons.LandingPageCode />,
-                    title: "Immersive 3D Experiences",
-                    description: "Bring inventory into metaverses and beyond with fully interactive, 3D virtual spaces that customers can explore."
+                    title: t('details.items.immersive3DExperiences.title'),
+                    description: t('details.items.immersive3DExperiences.description')
                 },
                 {
                     icon: <AppIcons.LandingPageCreditCard />,
-                    title: "Customizable Marketplace",
-                    description: "Separately offer customers a unique 3D store tailored to deliver an interactive and engaging shopping experience."
+                    title: t('details.items.customizableMarketplace.title'),
+                    description: t('details.items.customizableMarketplace.description')
                 },
                 {
                     icon: <AppIcons.LandingPageTemplate />,
-                    title: "Seamlessly Interactive",
-                    description: "Allow customers to browse, select, and purchase products directly within your virtual store."
+                    title: t('details.items.seamlesslyInteractive.title'),
+                    description: t('details.items.seamlesslyInteractive.description')
                 }
             ]
         },
         dualSideFlexData: [
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/bb1c5cad5efc86f2ccca3a5952e89911478888a9caeaf90d4138c05691964e7a_or.png",
-                title: "Virtual Showrooms",
-                description: "Create fully immersive shopping environments where customers can explore, browse and engage."
+                title: t('dualSideFlex.virtualShowrooms.title'),
+                description: t('dualSideFlex.virtualShowrooms.description')
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/c9308a13011d8a774f2affdf53f83b4c825f5bd2d791535bc8477920917a7468_or.png",
-                title: "Boost Engagement",
-                description: "Enhance the customer experience with interactive 3D showrooms that offer a more dynamic way to interact with items."
+                title: t('dualSideFlex.boostEngagement.title'),
+                description: t('dualSideFlex.boostEngagement.description')
             },
             {
                 image: "https://upload-file-droplinked.s3.amazonaws.com/33bf3bf105b88697cebea4357965b84dead76023706666196785801e82172771_or.png",
-                title: "Next-Gen Retail",
-                description: "Embrace the future of commerce by offering an interactive experience that stands out."
+                title: t('dualSideFlex.nextGenRetail.title'),
+                description: t('dualSideFlex.nextGenRetail.description')
             }
         ],
         featureGroups: [
             {
                 features: [
-                    { title: "Virtual Showroom Creation", description: "Design and launch an immersive shop complete with virtual shelves, products and interactive elements." },
-                    { title: "Immersive Experiences", description: "Let customers interact with products in an entirely new way by browsing a showroom filled with dynamic inventory." },
-                    { title: "Brand Personalization", description: "Customize the virtual showroom to reflect your brand's identity while creating an unforgettable experience that embodies your brand." }
+                    { title: t('features.virtualShowroomCreation.title'), description: t('features.virtualShowroomCreation.description') },
+                    { title: t('features.immersiveExperiences.title'), description: t('features.immersiveExperiences.description') },
+                    { title: t('features.brandPersonalization.title'), description: t('features.brandPersonalization.description') }
                 ]
             }
         ]
