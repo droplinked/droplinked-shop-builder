5c62fbd17dbba2447888e266d49d75e81ce7678a
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-07-07 18:31:40 +0330

    Update OnchainAffiliateModularStack to utilize CardImage component for images, enhancing visual consistency and maintainability. Replaced <img> tags with CardImage across all card entries.

--- src/pages/public-pages/redesign-landings/onchain-affiliate/OnchainAffiliateModularStack.tsx
+++ src/pages/public-pages/redesign-landings/onchain-affiliate/OnchainAffiliateModularStack.tsx
@@ -6,7 +6,7 @@ import { ConfigureLg } from 'assets/icons/System/Configure/ConfigureLg'
 import { SoonLg } from 'assets/icons/System/Soon/SoonLg'
 import React from 'react'
 import SectionContainer from '../_shared/components/SectionContainer/SectionContainer'
-import { Cards } from '../_shared/components/card'
+import { CardImage, Cards } from '../_shared/components/card'
 import { CardData } from '../_shared/components/card/Cards'
 import ModularStackTypography from '../_shared/svgs/ModularStackTypography'
 
@@ -17,42 +17,42 @@ function OnchainAffiliateModularStack() {
             title: "Collaborate & Grow",
             description: "Partner with co-sellers to boost product visibility and increase sales to earn more",
             gridColumn: { base: '1', md: '1', lg: '1 / 3' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/49f43950da54190be0f48855598789155a36a5a2fea8cf5d98f4b3c6aedf18cc.png' />
+            children: <CardImage alt='Collaborate & Grow' src='https://upload-file-droplinked.s3.amazonaws.com/8b578704d2f9a317f0a0068cc5e96d74adad4272cd396fdceaaa52b3cb3c9e81.png' />
         },
         {
             icon: <ReceivemoneyLg color="#fff" />,
             title: "Promote & Earn",
             description: "Select products, submit approval requests and track the status of requests and sales on the go",
             gridColumn: { base: '1', md: '2', lg: '3' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
+            children: <CardImage alt='Promote & Earn' src='https://upload-file-droplinked.s3.amazonaws.com/3940656e3e595c6d59e4b11fbac744a36060ea16ae6f05201ee6c221594c53a7.png' />
         },
         {
             icon: <CartLg color="#fff" />,
             title: "Collaborate to Sell",
             description: "When approved, products are instantly accessible so you can start promoting and earning better commissions transparently",
             gridColumn: { base: '1', md: '1', lg: '1' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
+            children: <CardImage alt='Collaborate to Sell' src='https://upload-file-droplinked.s3.amazonaws.com/6bcb307853e9282ae38a0501eeff45f326e602a6dbd13059d738c3a68c7d1ea5.png' />
         },
         {
             icon: <DesignLg color="#fff" />,
             title: "Create Storefronts",
             description: "Set up a branded store, upload products, activate affiliates and set commission rates in minutes",
             gridColumn: { base: '1', md: '2', lg: '2 / 4' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
+            children: <CardImage alt='Create Storefronts' src='https://upload-file-droplinked.s3.amazonaws.com/c79cb9a3c1472a42a9ea34e4bc3b6b8060455a04106383eedaf9af4cdfbe36dd.png' />
         },
         {
             icon: <ConfigureLg color="#fff" />,
             title: "Manage Requests",
             description: "Review and approve co-seller requests inside the affiliate panel to whitelist trusted co-sellers",
             gridColumn: { base: '1', md: '1', lg: '1 / 3' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
+            children: <CardImage alt='Manage Requests' src='https://upload-file-droplinked.s3.amazonaws.com/d60dbd4e81187a6c534b5b7cc63336e9e57c123688216b64ea6ffb2aa5a6ac0b.png' />
         },
         {
             icon: <SoonLg color="#fff" />,
             title: "Getting Started",
             description: "Create a storefront and explore the affiliate panel to find a variety of brand products to instantly add to a shop or product tile",
             gridColumn: { base: '1', md: '2', lg: '3' },
-            children: <img width="100%" height="100%" alt='products-icons' src='https://upload-file-droplinked.s3.amazonaws.com/3dde6acd17201deac931f01af2fc6959161021afcd23697727b7042ee79489f2.png' />
+            children: <CardImage alt='Getting Started' src='https://upload-file-droplinked.s3.amazonaws.com/4b886aabec7e48e608e3087c07194d4d508f7a24bb12f3909a0d3aa0feee214a.png' />
         }
     ]
 
