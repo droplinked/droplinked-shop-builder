ae4bbd3afbe51b0b3a54987ffebbe1c979f21e9f
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-07-13 22:21:43 +0330

    Refactor dashboard links by removing the dedicated data file and integrating links directly into the useLocalizedDashboardLinks hook for improved localization and maintainability

--- src/pages/dashboard/hooks/useLocalizedDashboardLinks.ts
+++ src/pages/dashboard/hooks/useLocalizedDashboardLinks.ts
@@ -1,11 +1,48 @@
 import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
-import { BLOG_LINKS, HELP_CENTER_LINKS } from 'pages/dashboard/data/dashboardLinks'
 
 export interface DashboardPageLink {
     title: string
     summary?: string
     url: string
 }
+
+const BLOG_LINKS = [
+    {
+        id: 'transformNFT',
+        url: 'https://droplinked.com/blogs/how-to-transform-and-sell-nft-artwork-and-ip-into-premium-merchandise-ac599f59'
+    },
+    {
+        id: 'gatingProduct',
+        url: 'https://droplinked.com/blogs/how-to-sell-your-nfts-through-gating-your-product-b7d6e94e'
+    },
+    {
+        id: 'secondarySales',
+        url: 'https://droplinked.com/blogs/how-to-earn-from-secondary-sales-by-putting-your-product-on-the-chain-eea86743'
+    },
+    {
+        id: 'monetizeInventory',
+        url: 'https://droplinked.com/blogs/how-to-monetize-your-physical-inventory-with-personalized-storefront-0082dd88'
+    }
+]
+
+const HELP_CENTER_LINKS = [
+    {
+        id: 'gettingStarted',
+        url: 'https://droplinked.gitbook.io/droplinked-store-front-help-center/getting-started/getting-set-up-to-start-selling'
+    },
+    {
+        id: 'managingProducts',
+        url: 'https://droplinked.gitbook.io/droplinked-store-front-help-center/how-to-guides/managing-products'
+    },
+    {
+        id: 'managingCollections',
+        url: 'https://droplinked.gitbook.io/droplinked-store-front-help-center/how-to-guides/managing-collections'
+    },
+    {
+        id: 'affiliateNetwork',
+        url: 'https://droplinked.gitbook.io/droplinked-store-front-help-center/how-to-guides/decentralized-affiliate-network'
+    }
+]
 
 /**
  * Provide localized blog & help-center links.
