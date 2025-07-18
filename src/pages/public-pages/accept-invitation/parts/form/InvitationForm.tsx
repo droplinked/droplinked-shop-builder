5963ce22705ffa992dbd9bc3580cbc29d5087350
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-07-18 22:31:57 +0330

    Refactor localization implementation in AcceptInvitation component for improved clarity and consistency in Arabic and English

--- src/pages/public-pages/accept-invitation/parts/form/InvitationForm.tsx
+++ src/pages/public-pages/accept-invitation/parts/form/InvitationForm.tsx
@@ -2,16 +2,14 @@ import { Flex } from '@chakra-ui/react'
 import BasicButton from 'components/common/BasicButton/BasicButton'
 import { Form, Formik } from 'formik'
 import useAppToast from 'hooks/toast/useToast'
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { acceptInvitationService } from 'services/user/services'
 import useAppStore from 'stores/app/appStore'
 import { passwordRegex } from 'utils/helpers'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import * as Yup from "yup"
 import InvitationInput from '../input/Input'
-import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
-import arLocale from 'locales/public-pages/accept-invitation/ar.json'
-import enLocale from 'locales/public-pages/accept-invitation/en.json'
 
 interface Props {
     invitationId: string;
@@ -23,10 +21,7 @@ function InvitationForm({ invitationId, email }: Props) {
     const [isLoading, setLoading] = useState(false)
     const { login } = useAppStore()
     const { showToast } = useAppToast()
-    const { t } = useLocaleResources("acceptInvitation", {
-        ar: arLocale,
-        en: enLocale
-    })
+    const { t } = useLocaleResources("acceptInvitation")
 
     const onSubmit = async (data: any) => {
         const { password } = data
@@ -44,9 +39,9 @@ function InvitationForm({ invitationId, email }: Props) {
     }
 
     const formSchema = Yup.object().shape({
-        email: Yup.string().email(t("validations.invalidEmail")).required(t("validations.fieldRequired")),
-        password: Yup.string().matches(passwordRegex, t("validations.passwordRequirements")).required(t("validations.fieldRequired")),
-        confirmPassword: Yup.string().oneOf([Yup.ref("password"), null], t("validations.passwordsDoNotMatch")).required(t("validations.fieldRequired")),
+        email: Yup.string().email(t("InvitationForm.validations.invalidEmail")).required(t("InvitationForm.validations.fieldRequired")),
+        password: Yup.string().matches(passwordRegex, t("InvitationForm.validations.passwordRequirements")).required(t("InvitationForm.validations.fieldRequired")),
+        confirmPassword: Yup.string().oneOf([Yup.ref("password"), null], t("InvitationForm.validations.passwordsDoNotMatch")).required(t("InvitationForm.validations.fieldRequired")),
     })
 
     return (
@@ -56,41 +51,40 @@ function InvitationForm({ invitationId, email }: Props) {
             validationSchema={formSchema}
             onSubmit={onSubmit}
         >
-            {({ errors, values, setFieldValue }) => <Form style={{ width: "100%" }}>
-                <Flex direction={"column"} gap={9}>
-                    <Flex direction={"column"} gap={4}>                        <InvitationInput
-                        label={t("emailLabel")}
+            {({ errors, values, setFieldValue }) =>
+                <Form style={{ width: "100%", display: "flex", flexDirection: "column", gap: 9 }}>
+                    <Flex direction="column" gap={4}>                        <InvitationInput
+                        label={t("InvitationForm.emailLabel")}
                         type='email'
                         value={values.email}
-                        placeholder={t("emailLabel")}
+                        placeholder={t("InvitationForm.emailLabel")}
                         isReadOnly
                         error={errors.email && errors.email.toString()}
                         onChange={(e) => setFieldValue("email", e.target.value)}
                     />
 
                         <InvitationInput
-                            label={t("passwordLabel")}
+                            label={t("InvitationForm.passwordLabel")}
                             type='password'
                             value={values.password}
-                            placeholder={t("passwordLabel")}
+                            placeholder={t("InvitationForm.passwordLabel")}
                             isRequired
                             error={errors.password && errors.password.toString()}
                             onChange={(e) => setFieldValue("password", e.target.value)}
                         />
 
                         <InvitationInput
-                            label={t("repeatPasswordLabel")}
+                            label={t("InvitationForm.repeatPasswordLabel")}
                             type='password'
                             value={values.confirmPassword}
-                            placeholder={t("repeatPasswordLabel")}
+                            placeholder={t("InvitationForm.repeatPasswordLabel")}
                             isRequired
                             error={errors.confirmPassword && errors.confirmPassword.toString()}
                             onChange={(e) => setFieldValue("confirmPassword", e.target.value)}
                         />
                     </Flex>
-                    <BasicButton type='submit' isDisabled={isLoading} isLoading={isLoading}>{t("signUpButton")}</BasicButton>
-                </Flex>
-            </Form>
+                    <BasicButton type='submit' isDisabled={isLoading} isLoading={isLoading}>{t("InvitationForm.signUpButton")}</BasicButton>
+                </Form>
             }
         </Formik>
     )
--- src/pages/public-pages/accept-invitation/parts/input/Input.tsx
+++ src/pages/public-pages/accept-invitation/parts/input/Input.tsx
@@ -1,33 +1,32 @@
-import { Box, Flex, InputProps } from "@chakra-ui/react";
-import AppInput from "components/common/form/textbox/AppInput";
-import AppTypography from "components/common/typography/AppTypography";
-import ShowPassword from "components/modals/signup-modal/ShowPassword";
-import React, { useState } from 'react';
-import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources';
-import arLocale from 'locales/public-pages/accept-invitation/ar.json';
-import enLocale from 'locales/public-pages/accept-invitation/en.json';
+import { Box, Flex, InputProps } from "@chakra-ui/react"
+import AppInput from "components/common/form/textbox/AppInput"
+import AppTypography from "components/common/typography/AppTypography"
+import ShowPassword from "components/modals/signup-modal/ShowPassword"
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
+import React, { useState } from 'react'
 
 interface Props extends InputProps {
-    label: string;
-    type: string;
-    value: any;
-    isRequired?: boolean;
+    label: string
+    type: string
+    value: any
+    isRequired?: boolean
     error?: string
 }
 
 function InvitationInput({ label, type, value, isRequired, error, ...props }: Props) {
     const [inputType, setInputType] = useState(type)
+    const { t } = useLocaleResources("acceptInvitation")
+
     const toggleInputType = () => setInputType(inputType === "password" ? "text" : "password")
-    const { t } = useLocaleResources("acceptInvitation", {
-        ar: arLocale,
-        en: enLocale
-    })
 
     return (
-        <Flex direction="column" gap={2} userSelect={"none"}>
-            <AppTypography fontSize={14} fontWeight={600} color={"#fff"}>{label} {isRequired && <Box as="span" color={"#2BCFA1"}>({t("required")})</Box>}</AppTypography>
-            <Flex direction={"column"} gap={2}>
-                <Box position={"relative"}>
+        <Flex direction="column" gap={2} userSelect="none">
+            <AppTypography fontSize={14} fontWeight={600} color="#fff">
+                {label}
+                {isRequired && <Box as="span" color="label.primary">({t("common:required")})</Box>}
+            </AppTypography>
+            <Flex direction="column" gap={2}>
+                <Box position="relative">
                     <AppInput
                         name=""
                         type={inputType}
