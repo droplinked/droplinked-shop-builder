05a372c62a4d99a5ca12493022c227dc2aecc784
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-07-18 21:40:43 +0330

    Refactor localization implementation in DIMST components for improved clarity and consistency

--- src/pages/public-pages/landings/DIMST/DIMST.tsx
+++ src/pages/public-pages/landings/DIMST/DIMST.tsx
@@ -1,12 +1,17 @@
+import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
+import arLocale from 'locales/public-pages/landings/DIMST/ar.json'
+import enLocale from 'locales/public-pages/landings/DIMST/en.json'
 import React from 'react'
-import MarqueeSection from '../_shared/components/marquee-wrapper/MarqueeSection'
 import JoinCommunity from '../_shared/components/JoinCommunity'
-import SignUpCta from '../_shared/components/SignUpCta'
-import DIMSTHero from './DIMSTHero'
-import MaxWidthWrapper from '../_shared/components/MaxWidthWrapper'
 import { LazyLoad } from '../_shared/components/LazyLoad'
+import MaxWidthWrapper from '../_shared/components/MaxWidthWrapper'
+import SignUpCta from '../_shared/components/SignUpCta'
+import MarqueeSection from '../_shared/components/marquee-wrapper/MarqueeSection'
+import DIMSTHero from './DIMSTHero'
 
 export default function DIMST() {
+    useLocaleResources('public-pages/landings/DIMST', { en: enLocale, ar: arLocale })
+
     const sections = [
         { id: 'partner-list', component: <MarqueeSection /> },
         { id: 'join-community', component: <JoinCommunity /> },
--- src/pages/public-pages/landings/DIMST/DIMSTHero.tsx
+++ src/pages/public-pages/landings/DIMST/DIMSTHero.tsx
@@ -3,12 +3,10 @@ import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
 import React from 'react'
 import HeroSection from '../_shared/components/hero-section/HeroSection'
 import CalculationSections from './CalculationSections'
-import localEn from 'locales/public-pages/landings/DIMST/en.json'
-import localAr from 'locales/public-pages/landings/DIMST/ar.json'
 
 export default function DIMSTHero() {
     const isMobile = useBreakpointValue({ base: true, md: false })
-    const { t } = useLocaleResources('public-pages/landings/DIMST', { en: localEn, ar: localAr })
+    const { t } = useLocaleResources('public-pages/landings/DIMST')
 
     return (
         <>
