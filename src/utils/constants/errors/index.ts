9ee0a3e1b0aa6fe4ca9dc39cb5e2c3da2628439c
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-07-23 11:39:33 +0330

    Refactor error handling by removing unused error modules and simplifying the error constants structure

--- src/utils/constants/errors/index.ts
+++ src/utils/constants/errors/index.ts
@@ -1,16 +1,10 @@
-import collectionErrors from "./modules/collection";
 import permissionErrors from "./modules/permission";
 import productErrors from "./modules/product";
-import signinErrors from "./modules/signin";
-import signupErrors from "./modules/signup";
 import storeErrors from "./modules/store";
 
 const AppErrors = {
-    signup: signupErrors,
-    signin: signinErrors,
     store: storeErrors,
     product: productErrors,
-    collection: collectionErrors,
     permission: permissionErrors
 }
 
--- src/utils/constants/errors/modules/store.ts
+++ src/utils/constants/errors/modules/store.ts
@@ -5,14 +5,6 @@ export interface ISizeLimit {
 
 const storeErrors = {
     sizeLimit: ({ fieldName, size }: ISizeLimit) => `${fieldName} must be less than ${size}. Please choose a smaller file size and try again.`,
-    headerBannerSizeLimit: "Header banner must be less than 5MB. Please choose a smaller file size and try again.",
-    storePublished: "Your store has been published successfully!",
-    storeAddressAdded: "Store address has been added successfully!",
-    storeAddressUpdated: "Store address has been updated!",
-    socialLinksUpdated: "Social Links have been updated",
-    paymentOptionsUpdated: "Payment options have been updated", 
-    hasBeenUpdated: (target: string) => `${target} has been updated`, 
-    uploadSuccess: (field: string) => `${field} has been uploaded successfully!`
 };
 
 export default storeErrors;
