e3d91358702dd540ed5c95dd3c6c55e2c6fba0b2
Yasaman Salari <ysmnsalari@gmail.com>
2025-07-29 11:03:15 +0300

    Update ChevronrightSm icon color from black to white for improved visibility.

--- src/assets/icons/Navigation/ChevronRight/ChevronrightSm.tsx
+++ src/assets/icons/Navigation/ChevronRight/ChevronrightSm.tsx
@@ -1,7 +1,7 @@
 // Generated from SVG to React Component
 import React, { SVGProps } from "react";
 
-export const ChevronrightSm = ({ color = "black", ...props }: SVGProps<SVGSVGElement>) => (
+export const ChevronrightSm = ({ color = "white", ...props }: SVGProps<SVGSVGElement>) => (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path d="M6 12L10 8L6 4" stroke={color} strokeLinecap="round" strokeLinejoin="round" />
     </svg>
