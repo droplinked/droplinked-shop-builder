103f2dcf671a1d05925a05859754cd032f95bd42
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-08-10 12:10:08 +0330

    Remove custom shipping form and file upload components from the product drawer, along with associated localization entries in Arabic and English

--- src/pages/products/components/ProductDrawer/components/fields/ProductShippingType/ProductShippingType.tsx
+++ src/pages/products/components/ProductDrawer/components/fields/ProductShippingType/ProductShippingType.tsx
@@ -1,41 +1,17 @@
-import AppIcons from 'assets/icon/Appicons'
-import BlueButton from 'components/redesign/button/BlueButton'
 import FormFieldWrapper from 'components/redesign/form-field-wrapper/FormFieldWrapper'
 import useLocaleResources from 'hooks/useLocaleResources/useLocaleResources'
-import { useShippingTypes } from 'pages/products/hooks/useShippingTypes'
-import React, { useState } from 'react'
-import LoadingPlaceholder from '../../common/LoadingPlaceholder'
-import CustomShippingForm from './CustomShippingForm'
-import ShippingTypeSelector from './ShippingTypeSelector'
+import React from 'react'
 
 function ProductShippingType() {
     const { t } = useLocaleResources('products')
-    const [isFormVisible, setFormVisibility] = useState(false)
-    const { hasCustomShippingPermission, shippingTypes, shippingTypesQuery } = useShippingTypes()
-
-    const rightContent = (
-                    <BlueButton
-                sx={{ path: { stroke: "#179EF8" } }}
-                onClick={() => hasCustomShippingPermission && setFormVisibility(true)}
-            >
-                <AppIcons.BlackPlus />
-                {t('ProductShippingType.customShipping')}
-            </BlueButton>
-    )
 
     return (
         <FormFieldWrapper
             label={t('ProductShippingType.label')}
             description={t('ProductShippingType.description')}
             isRequired
-            {...hasCustomShippingPermission && { rightContent }}
         >
-            {shippingTypesQuery.isFetching
-                ? <LoadingPlaceholder numberOfSkeletons={3} />
-                : <ShippingTypeSelector shippingTypes={shippingTypes} />
-            }
 
-            {isFormVisible && <CustomShippingForm onDiscard={() => setFormVisibility(false)} />}
         </FormFieldWrapper>
     )
 }
