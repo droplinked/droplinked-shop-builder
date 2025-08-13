6c9444084aed76e05e7312e743c3091b53c9e604
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-08-13 14:10:51 +0330

    Refactor shipping management service endpoints to use a constant for the base URL, improving maintainability and readability of API calls.

--- src/services/shipping-management/services.ts
+++ src/services/shipping-management/services.ts
@@ -1,21 +1,23 @@
 import axiosInstance from "lib/axiosConfig"
 import { ShippingProfile } from "pages/shipping-management/types/shipping"
 
+const shippingManagementEndpoint = '/shippings/v2'
+
 export const getShippingProfiles = async () => {
-    const response = await axiosInstance.get<{ data: ShippingProfile[] }>('/shippings/v2').then(res => res.data)
+    const response = await axiosInstance.get<{ data: ShippingProfile[] }>(shippingManagementEndpoint).then(res => res.data)
     return response.data
 }
 
 export const deleteShippingProfile = async (shippingProfileId: string) =>
-    axiosInstance.delete(`/shippings/v2/${shippingProfileId}`)
+    axiosInstance.delete(`${shippingManagementEndpoint}/${shippingProfileId}`)
 
 export const createShippingProfile = async (shippingProfile: ShippingProfile) =>
-    axiosInstance.post('/shippings/v2/', shippingProfile)
+    axiosInstance.post(shippingManagementEndpoint, shippingProfile)
 
 export const updateShippingProfile = async (shippingProfileId: string, shippingProfile: ShippingProfile) =>
-    axiosInstance.patch(`/shippings/v2/${shippingProfileId}`, shippingProfile)
+    axiosInstance.put(`${shippingManagementEndpoint}/${shippingProfileId}`, shippingProfile)
 
 export const getShippingProviders = async () => {
-    const response = await axiosInstance.get<{ data: string[] }>('/shippings/v2/providers').then(res => res.data)
+    const response = await axiosInstance.get<{ data: string[] }>(`${shippingManagementEndpoint}/providers`).then(res => res.data)
     return response.data
 }
