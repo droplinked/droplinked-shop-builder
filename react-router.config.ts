57f6742b59716b5967e9fbb3acfcc3f350b78762
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-08-25 22:43:10 +0330

    feat: Update prerender routes in react-router configuration to include additional paths for improved navigation

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,36 @@ export default {
     appDirectory: "src",
     ssr: false,
     async prerender(args) {
-        return ["/404", "/onboarding", "/"]
+        return [
+            "/404",
+            "/onboarding",
+            "/",
+            "/physical-inventory",
+            "/enquiry",
+            "/terms",
+            "/about",
+            "/contact-us",
+            "/privacy",
+            "/digital-goods",
+            "/products-on-demand",
+            "/tokenpay",
+            "/payment-links",
+            "/product-tiles",
+            "/tokenizing-products",
+            "/onchain-subscriptions",
+            "/custom-tokens",
+            "/metaverse-store",
+            "/book-demo",
+            "/d3",
+            "/unstoppable-domains",
+            "/polygon",
+            "/crossmint",
+            "/base",
+            "/onchain-affiliate",
+            "/roi",
+            "/dpp",
+            "/plans",
+            "/rewards"
+        ]
     },
 } satisfies Config;
