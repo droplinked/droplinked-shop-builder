49523bb62366e1b6570570b011420f055406d284
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-08-26 09:09:28 +0330

    feat: Remove unused prerender routes from react-router configuration

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,37 +3,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
     appDirectory: "src",
     ssr: true,
-    async prerender(args) {
-        return [
-            "/404",
-            "/onboarding",
-            "/",
-            "/physical-inventory",
-            "/enquiry",
-            "/terms",
-            "/about",
-            "/contact-us",
-            "/privacy",
-            "/digital-goods",
-            "/products-on-demand",
-            "/tokenpay",
-            "/payment-links",
-            "/product-tiles",
-            "/tokenizing-products",
-            "/onchain-subscriptions",
-            "/custom-tokens",
-            "/metaverse-store",
-            "/book-demo",
-            "/d3",
-            "/unstoppable-domains",
-            "/polygon",
-            "/crossmint",
-            "/base",
-            "/onchain-affiliate",
-            "/roi",
-            "/dpp",
-            "/plans",
-            "/rewards"
-        ]
-    },
 } satisfies Config;
