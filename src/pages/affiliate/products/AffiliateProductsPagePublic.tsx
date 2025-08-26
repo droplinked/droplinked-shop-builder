b99bbe4865e4cbaafa49d6024ab905764d7ebdd4
Mohammad Reza Kamali <81362178+last-mamad@users.noreply.github.com>
2025-08-26 16:06:33 +0330

    refactor: Simplify props handling in AffiliateProductsPage and update clientLoader in AffiliateProductsPagePublic

--- src/pages/affiliate/products/AffiliateProductsPagePublic.tsx
+++ src/pages/affiliate/products/AffiliateProductsPagePublic.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import AffiliateProductsPage from "./AffiliateProductsPage";
 
+export async function clientLoader() {
+    return {
+        isPublic: true,
+    };
+}
+
 export default function AffiliateProductsPagePublic() {
-    return <AffiliateProductsPage isPublic={true} />;
+    return <AffiliateProductsPage />;
 }
