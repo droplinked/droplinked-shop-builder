68c8ecb2c0ff9d9cdf7d38f9fd092c3061be4f9c
Mahdi Poursepahi <mahdipoursepahi1376@gmail.com>
2025-08-30 23:09:35 +0330

    Utilize string literals for the status property in InvoiceQueryParams interface

--- src/services/invoice/interfaces.ts
+++ src/services/invoice/interfaces.ts
@@ -1,31 +1,8 @@
-import { Address } from "pages/invoice-management/create-invoice/helpers/helpers"
-
-export type InvoiceStatus = "ACTIVE" | "CHECKED_OUT" | "PENDING"
-
-export interface Invoice {
-    _id: string;
-    status: string;
-    type: string;
-    shopID: string;
-    paymentType: string;
-    availableShipmentRates: any[];
-    selectedShipmentRates: any[];
-    selectedShipmentRateIDs: any[];
-    shipmentInformation: any[];
-    isInvoice: boolean;
-    items: any[];
-    groupedItems: any[];
-    passedRules: any[];
-    createdAt: string;
-    email?: string;
-    checkoutAddressID?: Address;
-}
-
 export interface InvoiceQueryParams {
     page: number;
     limit: number;
     search?: string;
-    status?: InvoiceStatus
+    status?: "ACTIVE" | "CHECKED_OUT" | "PENDING"
 }
 
 export interface DeleteInvoiceProduct {
